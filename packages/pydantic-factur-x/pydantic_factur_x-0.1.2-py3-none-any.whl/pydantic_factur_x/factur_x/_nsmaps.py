f5fb063cefd54b745af0604a49abd983caf06e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:04 +0000

    Add package pydantic-factur-x/0.1.2/pydantic_factur_x-0.1.2-py3-none-any.whl

---
+++ packages/pydantic-factur-x/pydantic_factur_x-0.1.2-py3-none-any.whl/pydantic_factur_x/factur_x/_nsmaps.py
@@ -0,0 +1,22 @@
+"""All Nsmaps used in documents."""
+
+
+RSM = {
+    "rsm": "urn:un:unece:uncefact:data:SCRDMCCBDACIOMessageStructure:100",
+}
+
+UDT = {
+    "udt": "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:128",
+}
+
+QTD = {
+    "qtd": "urn:un:unece:uncefact:data:standard:QualifiedDataType:128",
+}
+
+RAM = {
+    "ram": "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:128",
+}
+
+XSI = {
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+}
