2ae09859f507cd9aee3e8e3b2669fe4d53bb2772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:48 +0000

    Add package linkedin-api-client/0.2.0/linkedin_api_client-0.2.0.tar.gz

---
+++ packages/linkedin-api-client/linkedin_api_client-0.2.0.tar.gz/linkedin_api_client-0.2.0/linkedin_api/common/errors.py
@@ -0,0 +1,13 @@
+class InvalidArgumentError(Exception):
+    "Error raised for invalid arguments"
+    pass
+
+
+class MissingArgumentError(Exception):
+    "Error raised for missing arguments"
+    pass
+
+
+class ResponseFormattingError(Exception):
+    "Error raised when formatting API response"
+    pass
