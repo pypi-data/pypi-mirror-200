2329ac6e11cdfd40b5cacac4f7b81fbb439bf5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package UEVaultManager/1.0.1/UEVaultManager-1.0.1.tar.gz

---
+++ packages/UEVaultManager/UEVaultManager-1.0.1.tar.gz/UEVaultManager-1.0.1/UEVaultManager/utils/cli.py
@@ -0,0 +1,74 @@
+import os
+
+
+def get_boolean_choice(prompt, default=True):
+    yn = 'Y/n' if default else 'y/N'
+
+    choice = input(f'{prompt} [{yn}]: ')
+    if not choice:
+        return default
+    elif choice[0].lower() == 'y':
+        return True
+    else:
+        return False
+
+
+def get_int_choice(prompt, default=None, min_choice=None, max_choice=None, return_on_invalid=False):
+    if default is not None:
+        prompt = f'{prompt} [{default}]: '
+    else:
+        prompt = f'{prompt}: '
+
+    while True:
+        try:
+            if inp := input(prompt):
+                choice = int(inp)
+            else:
+                return default
+        except ValueError:
+            if return_on_invalid:
+                return None
+            return_on_invalid = True
+            continue
+        else:
+            if min_choice is not None and choice < min_choice:
+                print(f'Number must be greater than {min_choice}')
+                if return_on_invalid:
+                    return None
+                return_on_invalid = True
+                continue
+            if max_choice is not None and choice > max_choice:
+                print(f'Number must be less than {max_choice}')
+                if return_on_invalid:
+                    return None
+                return_on_invalid = True
+                continue
+            return choice
+
+
+def strtobool(val):
+    """Convert a string representation of truth to True  or False
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError('invalid truth value %r' % (val,))
+
+
+def check_and_create_path(full_file_name: str) -> bool:
+    # create folder if not exist
+    file_path, file_name = os.path.split(full_file_name)
+    if not os.path.isdir(file_path):
+        try:
+            os.makedirs(file_path, exist_ok=True)
+        except OSError:
+            return False
+    return True
