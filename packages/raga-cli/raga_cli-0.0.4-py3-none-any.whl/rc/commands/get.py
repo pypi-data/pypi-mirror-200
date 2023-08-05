45a0b6fbd5aa443578850ec6e8a29bf8a660f525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:02 +0000

    Add package raga-cli/0.0.4/raga_cli-0.0.4-py3-none-any.whl

---
+++ packages/raga-cli/raga_cli-0.0.4-py3-none-any.whl/rc/commands/get.py
@@ -0,0 +1,64 @@
+import argparse
+import logging
+from datetime import timedelta 
+from timeit import default_timer as timer
+import json
+
+from rc.cli.command import CmdBase
+from rc.cli.utils import run_command_on_subprocess
+from rc.cli.utils import *
+from rc.utils.request import get_repo_commit_id
+
+logger = logging.getLogger(__name__)
+
+class CmdGet(CmdBase):
+    def __init__(self, args):
+        super().__init__(args)
+    def run(self):
+        start = timer()
+        repo = get_repo()
+        version = self.args.version
+        if not version:
+            print("Files downloading...") 
+            run_command_on_subprocess('git pull') 
+            run_command_on_subprocess('dvc pull -f') 
+            print("Files downloaded successfully") 
+            logger.debug('DOWNLOAD TIME {0}'.format(timedelta(seconds=timer()-start))) 
+        else:
+            commit_id = get_repo_commit_id(json.dumps({"repo_name":repo, "version":version}))
+            
+            user_input = input("Are you sure you want to get it? [y/n]").lower()
+            if user_input == 'y':
+                print("Files downloading...") 
+                run_command_on_subprocess('git reset --hard {}'.format(commit_id)) 
+                run_command_on_subprocess('dvc pull -f') 
+                print("Files downloaded successfully") 
+                logger.debug('DOWNLOAD TIME {0}'.format(timedelta(seconds=timer()-start))) 
+            else:
+                print("Please enter valid input")
+
+        return 0
+
+
+def add_parser(subparsers, parent_parser):
+    REPO_HELP = "Get File or folder. Use: `rctl get`"
+    REPO_DESCRIPTION = (
+        "Get File or folder. Use: `rctl get`"
+    )
+
+    repo_parser = subparsers.add_parser(
+        "get",
+        parents=[parent_parser],
+        description=REPO_DESCRIPTION,
+        help=REPO_HELP,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    repo_parser.add_argument(
+        "-v", 
+        "--version", 
+        nargs="?", 
+        default=None,
+        type=int,
+        help="Repo commit version",
+    )
+    repo_parser.set_defaults(func=CmdGet)
