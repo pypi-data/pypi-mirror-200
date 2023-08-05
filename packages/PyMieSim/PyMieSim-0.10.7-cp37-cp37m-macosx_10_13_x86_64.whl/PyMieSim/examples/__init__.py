cdbb411eaed48644f2d006ace348d358434f49f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:29 +0000

    Add package PyMieSim/0.10.7/PyMieSim-0.10.7-cp37-cp37m-macosx_10_13_x86_64.whl

---
+++ packages/PyMieSim/PyMieSim-0.10.7-cp37-cp37m-macosx_10_13_x86_64.whl/PyMieSim/examples/__init__.py
@@ -0,0 +1,44 @@
+from .detector import plot_LP01 as Example01
+from .detector import plot_LP11 as Example02
+from .detector import plot_LP02 as Example03
+from .detector import plot_photodiode as Example04
+
+from .scatterer import plot_FarField as Example05
+from .scatterer import plot_SPF as Example06
+from .scatterer import plot_Stokes as Example07
+from .scatterer import plot_S1S2 as Example08
+from .scatterer import plot_Footprint as Example09
+
+from .experiment.sphere import plot_sphere_a1_vs_diameter as Example10
+from .experiment.sphere import plot_sphere_b1_vs_diameter as Example11
+from .experiment.sphere import plot_sphere_coupling_vs_diameter as Example12
+from .experiment.sphere import plot_sphere_coupling_vs_phioffset as Example13
+from .experiment.sphere import plot_sphere_coupling_vs_wavelength as Example14
+from .experiment.sphere import plot_sphere_Qabs_vs_diameter as Example15
+from .experiment.sphere import plot_sphere_Qsca_vs_diameter as Example16
+from .experiment.sphere import plot_sphere_Qsca_vs_index as Example17
+from .experiment.sphere import plot_sphere_Qsca_vs_wavelength_std as Example18
+
+from .experiment.cylinder import plot_cylinder_a11_vs_diameter as Example19
+from .experiment.cylinder import plot_cylinder_b11_vs_diameter as Example20
+from .experiment.cylinder import plot_cylinder_coupling_vs_diameter as Example21
+from .experiment.cylinder import plot_cylinder_coupling_vs_phioffset as Example22
+from .experiment.cylinder import plot_cylinder_coupling_vs_wavelength as Example23
+from .experiment.cylinder import plot_cylinder_Qabs_vs_diameter as Example24
+from .experiment.cylinder import plot_cylinder_Qsca_vs_diameter as Example25
+from .experiment.cylinder import plot_cylinder_Qsca_vs_index as Example26
+from .experiment.cylinder import plot_cylinder_Qsca_vs_wavelength_std as Example27
+
+
+from .experiment.coreshell import plot_coreshell_a1_vs_corediameter as Example28
+from .experiment.coreshell import plot_coreshell_b1_vs_corediameter as Example29
+from .experiment.coreshell import plot_coreshell_coupling_vs_corediameter as Example30
+from .experiment.coreshell import plot_coreshell_Qback_vs_corediameter as Example31
+
+from .validation import plot_BH87 as Example32
+from .validation import plot_BH88 as Example33
+from .validation import plot_BH810 as Example34
+from .validation import plot_pymiescatt_0 as Example35
+from .validation import plot_pymiescatt_1 as Example36
+from .validation import plot_pymiescatt_2 as Example37
+from .validation import plot_pymiescatt_3 as Example38
