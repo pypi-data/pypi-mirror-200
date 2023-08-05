79c2305bf6c0c58ae67579eabe9d6fc30f6f3bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:50 +0000

    Add package FlamingText/0.0.7/FlamingText-0.0.7.tar.gz

---
+++ packages/FlamingText/FlamingText-0.0.7.tar.gz/FlamingText-0.0.7/flamingText/main.py
@@ -0,0 +1,27 @@
+import requests
+from urllib.parse import urlencode
+from constants import url
+
+
+class FlamingText:
+    def __init__(self, **kwargs):
+        self._url = url
+        self._kwargs = kwargs
+
+    def _generateParams(self):
+        if len(self._kwargs) == 0:
+            raise Exception("Parametr name and script are required")
+        return "&"+urlencode(dict(self._kwargs))
+
+    def _generateURL(self):
+        return self._url + self._generateParams()
+
+    def process(self):
+        url = self._generateURL()
+        response = requests.get(url)
+        return response
+
+
+if __name__ == "__main__":
+    obj = FlamingText(text="Firdavs-coder", script="fluffy-logo")
+    print(obj.process().text)
---
+++ packages/FlamingText/FlamingText-0.0.7.tar.gz/FlamingText-0.0.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.7'
+DESCRIPTION = 'Unofficial API of flamingtext.com'
+long_description = 'FlamingText is free online logo generator that anyone can use to create a great logo in minutes! Just select one of our logo designs, and get started now!'
+
+here = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name="FlamingText",
+    version=VERSION,
+    author="Firdavs Shodiyev",
+    author_email="firdavscoder1@gmail.com",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Firdavs-coder/FlamingText",
+    description=DESCRIPTION,
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["requests"],
+    keywords=['python', 'effects', 'text', 'flamingtext'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
