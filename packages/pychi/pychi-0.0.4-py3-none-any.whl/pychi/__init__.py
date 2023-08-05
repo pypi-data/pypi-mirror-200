d8fea76154e0cb48ad097b58c5b78b2b2bb4c8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:38 +0000

    Add package pychi/0.0.4/pychi-0.0.4-py3-none-any.whl

---
+++ packages/pychi/pychi-0.0.4-py3-none-any.whl/pychi/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 12:32:49 2022
+
+@author: voumardt
+"""
+
+from . import light
+from . import materials
+from . import models
+from . import solvers
