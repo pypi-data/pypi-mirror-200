c6074ca4d12ac00d873eb62f4402de3f0d2ecacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:19 +0000

    Add package labelme2yolo/0.1.0/labelme2yolo-0.1.0.tar.gz

---
+++ packages/labelme2yolo/labelme2yolo-0.1.0.tar.gz/labelme2yolo-0.1.0/src/labelme2yolo/cli/__init__.py
@@ -0,0 +1,56 @@
+# SPDX-FileCopyrightText: 2022-present Wang Xin <xinwang614@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+import argparse
+
+from labelme2yolo.l2y import Labelme2YOLO
+
+
+def run():
+    parser = argparse.ArgumentParser("labelme2yolo")
+    parser.add_argument(
+        "--json_dir", type=str, help="Please input the path of the labelme json files."
+    )
+    parser.add_argument(
+        "--val_size",
+        type=float,
+        nargs="?",
+        default=None,
+        help="Please input the validation dataset size, for example 0.1.",
+    )
+    parser.add_argument(
+        "--test_size",
+        type=float,
+        nargs="?",
+        default=None,
+        help="Please input the test dataset size, for example 0.1.",
+    )
+    parser.add_argument(
+        "--json_name",
+        type=str,
+        nargs="?",
+        default=None,
+        help="If you put json name, it would convert only one json file to YOLO.",
+    )
+    parser.add_argument(
+        "--output_format",
+        type=str,
+        default="polygon",
+        help='The default output format for labelme2yolo is "polygon".'
+        ' However, you can choose to output in bbox format by specifying the "bbox" option.',
+    )
+
+    args = parser.parse_args()
+
+    if not args.json_dir:
+        parser.print_help()
+        return 0
+
+    convertor = Labelme2YOLO(args.json_dir, args.output_format)
+
+    if args.json_name is None:
+        convertor.convert(val_size=args.val_size, test_size=args.test_size)
+    else:
+        convertor.convert_one(args.json_name)
+
+    return 0
