c38fdccb06a9c9b1b3e4505f5420fe1b93b059b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:04 +0000

    Add package pod-gpt/0.0.4/pod_gpt-0.0.4.tar.gz

---
+++ packages/pod-gpt/pod_gpt-0.0.4.tar.gz/pod_gpt-0.0.4/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pod_gpt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ffmpeg-python>=0.2.0,<0.3.0',
+ 'langchain>=0.0.125,<0.0.126',
+ 'pinecone-client[grpc]>=2.2.1,<3.0.0',
+ 'pytube>=12.1.3,<13.0.0',
+ 'tiktoken>=0.3.3,<0.4.0',
+ 'torch>=2.0.0,<3.0.0',
+ 'tqdm>=4.65.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'pod-gpt',
+    'version': '0.0.4',
+    'description': 'Package for building podcast search indexes.',
+    'long_description': '# PodGPT',
+    'author': 'James Briggs',
+    'author_email': 'james@aurelio.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
