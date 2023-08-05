6753a0c69d396766cc8682fad4114267054bf535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:52 +0000

    Add package langforge/0.0.1/langforge-0.0.1.tar.gz

---
+++ packages/langforge/langforge-0.0.1.tar.gz/langforge-0.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="langforge", version="0.0.1", py_modules=["langforge"], author="me", author_email="me@me.com", description="This is a placeholder package created by registerit.", url="http://www.github.com")
