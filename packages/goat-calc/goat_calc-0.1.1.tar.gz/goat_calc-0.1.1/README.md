9953b70993fddd5c0b2c5d61f265b185fd8c3182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:30 +0000

    Add package goat-calc/0.1.1/goat_calc-0.1.1.tar.gz

---
+++ packages/goat-calc/goat_calc-0.1.1.tar.gz/goat_calc-0.1.1/README.md
@@ -0,0 +1,3 @@
+This is a GOAT calculator which can only add and subtract two numbers.
+
+It can also print "hello"!
---
+++ packages/goat-calc/goat_calc-0.1.1.tar.gz/goat_calc-0.1.1/goat_calc/main.py
@@ -0,0 +1,17 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def calc(
+    x: int = typer.Argument(..., help="first number"),
+    y: int = typer.Argument(..., help="second number"),
+    is_add: bool = typer.Option(..., "--add/--sub", help="Set this flag to add. Otherwise, subtract.")
+):
+    print(x + y if is_add else x - y)
+    print("Updated")
+
+@app.command()
+def hello():
+    print("hello")
+    
