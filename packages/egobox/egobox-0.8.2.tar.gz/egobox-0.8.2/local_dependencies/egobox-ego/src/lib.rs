28caae77f6c64dff896f7cea45049dee248bf694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:22 +0000

    Add package egobox/0.8.2/egobox-0.8.2.tar.gz

---
+++ packages/egobox/egobox-0.8.2.tar.gz/egobox-0.8.2/local_dependencies/egobox-ego/src/lib.rs
@@ -0,0 +1,114 @@
+//! This library implements Efficient Global Optimization method,
+//! it is a port of [SMT EGO algorithm](https://smt.readthedocs.io/en/stable/_src_docs/applications/ego.html)
+//!
+//! The optimizer is able to deal with inequality constraints.
+//! Objective and contraints are expected to computed grouped at the same time
+//! hence the given function should return a vector where the first component
+//! is the objective value and the remaining ones constraints values intended
+//! to be negative in the end.   
+//! The optimizer comes with a set of options to:
+//! * specify the initial doe,
+//! * parameterize internal optimization,
+//! * parameterize mixture of experts,
+//! * save intermediate results and allow hot restart,
+//!
+//! # Examples
+//!
+//! ```
+//! use ndarray::{array, Array2, ArrayView2};
+//! use egobox_ego::EgorBuilder;
+//!
+//! // A one-dimensional test function, x in [0., 25.] and min xsinx(x) ~ -15.1 at x ~ 18.9
+//! fn xsinx(x: &ArrayView2<f64>) -> Array2<f64> {
+//!     (x - 3.5) * ((x - 3.5) / std::f64::consts::PI).mapv(|v| v.sin())
+//! }
+//!
+//! // We ask for 10 evaluations of the objective function to get the result
+//! let res = EgorBuilder::optimize(xsinx)
+//!             .min_within(&array![[0.0, 25.0]])
+//!             .n_iter(10)
+//!             .run()
+//!             .expect("xsinx minimized");
+//! println!("Minimum found f(x) = {:?} at x = {:?}", res.x_opt, res.y_opt);
+//! ```
+//!
+//! The implementation relies on [Mixture of Experts](egobox_moe).
+//!
+//! While [Egor] optimizer works with continuous data (i.e floats), the optimizer
+//! allows to make basic mixed-integer optimization. The configuration of the Optimizer
+//! as a mixed_integer optimizer is done though the `EgorBuilder`  
+//!
+//! As a second example, we define an objective function `mixsinx` taking integer
+//! input values from the previous function `xsinx` defined above.
+//!  
+//! ```
+//! use ndarray::{array, Array2, ArrayView2};
+//! use linfa::ParamGuard;
+//! #[cfg(feature = "blas")]
+//! use ndarray_linalg::Norm;
+//! #[cfg(not(feature = "blas"))]
+//! use linfa_linalg::norm::*;
+//! use egobox_ego::{EgorBuilder, InfillStrategy, XType};
+//!
+//! fn mixsinx(x: &ArrayView2<f64>) -> Array2<f64> {
+//!     if (x.mapv(|v| v.round()).norm_l2() - x.norm_l2()).abs() < 1e-6 {
+//!         (x - 3.5) * ((x - 3.5) / std::f64::consts::PI).mapv(|v| v.sin())
+//!     } else {
+//!         panic!("Error: mixsinx works only on integer, got {:?}", x)
+//!     }
+//! }
+//!
+//! let n_iter = 10;
+//! let doe = array![[0.], [7.], [25.]];
+//!
+//! // We define input as being integer
+//! let xtypes = vec![XType::Int(0, 25)];
+//!
+//! let res = EgorBuilder::optimize(mixsinx)
+//!     .random_seed(42)
+//!     .min_within_mixed_space(&xtypes)   // We build mixed-integer optimizer
+//!     .doe(Some(doe))          // we pass an initial doe
+//!     .n_iter(n_iter)
+//!     .infill_strategy(InfillStrategy::EI)
+//!     .run()
+//!     .expect("Egor minimization");
+//! println!("min f(x)={} at x={}", res.y_opt, res.x_opt);
+//! ```  
+//!
+//! # Implementation notes
+//!
+//! * Mixture of experts and PLS dimension reduction is explained in \[[Bartoli2019](#Bartoli2019)\]
+//! * Parallel optimization is available through the selection of a qei strategy.
+//! More information in \[[Ginsbourger2010](#Ginsbourger2010)\]
+//! * Mixed integer approach is implemented using continuous relaxation.
+//! More information in \[[Garrido2018](#Garrido2018)\]
+//!
+//! # References
+//!
+//! \[<a id="Bartoli2019">Bartoli2019</a>\]: Bartoli, Nathalie, et al. [Adaptive modeling strategy for constrained global
+//! optimization with application to aerodynamic wing design](https://www.sciencedirect.com/science/article/pii/S1270963818306011)
+//!  Aerospace Science and technology 90 (2019): 85-102.
+//!
+//! \[<a id="Ginsbourger2010">Ginsbourger2010</a>\]: Ginsbourger, D., Le Riche, R., & Carraro, L. (2010).
+//! Kriging is well-suited to parallelize optimization.
+//!  
+//! \[<a id="Garrido2018">Garrido2018</a>\]: E.C. Garrido-Merchan and D. Hernandez-Lobato. Dealing with categorical and
+//! integer-valued variables in Bayesian Optimization with Gaussian processes.
+//!
+//!
+mod egor;
+mod egor_solver;
+mod egor_state;
+mod errors;
+mod lhs_optimizer;
+mod mixint;
+mod sort_axis;
+mod types;
+mod utils;
+
+pub use crate::egor::*;
+pub use crate::egor_solver::*;
+pub use crate::egor_state::*;
+pub use crate::errors::*;
+pub use crate::mixint::*;
+pub use crate::types::*;
---
+++ packages/egobox/egobox-0.8.2.tar.gz/egobox-0.8.2/local_dependencies/egobox-ego/src/utils.rs
@@ -0,0 +1,308 @@
+use egobox_moe::ClusteredSurrogate;
+use libm::erfc;
+use ndarray::{
+    concatenate, Array1, Array2, ArrayBase, ArrayView, ArrayView2, Axis, Data, Ix1, Ix2, Zip,
+};
+use ndarray_stats::{DeviationExt, QuantileExt};
+use rayon::prelude::*;
+
+// Infill strategy related function
+////////////////////////////////////////////////////////////////////////////
+
+const SQRT_2PI: f64 = 2.5066282746310007;
+
+/// Compute EI infill criterion at given `x` point using the surrogate model `obj_model`
+/// and the current minimum of the objective function.
+pub fn ei(x: &[f64], obj_model: &dyn ClusteredSurrogate, f_min: f64) -> f64 {
+    let pt = ArrayView::from_shape((1, x.len()), x).unwrap();
+    if let Ok(p) = obj_model.predict_values(&pt) {
+        if let Ok(s) = obj_model.predict_variances(&pt) {
+            let pred = p[[0, 0]];
+            let sigma = s[[0, 0]].sqrt();
+            let args0 = (f_min - pred) / sigma;
+            let args1 = (f_min - pred) * norm_cdf(args0);
+            let args2 = sigma * norm_pdf(args0);
+            args1 + args2
+        } else {
+            -f64::INFINITY
+        }
+    } else {
+        -f64::INFINITY
+    }
+}
+
+/// Computes derivatives of EI infill criterion wrt to x components at given `x` point
+/// using the surrogate model `obj_model` and the current minimum of the objective function.
+pub fn grad_ei(x: &[f64], obj_model: &dyn ClusteredSurrogate, f_min: f64) -> Array1<f64> {
+    let pt = ArrayView::from_shape((1, x.len()), x).unwrap();
+    if let Ok(p) = obj_model.predict_values(&pt) {
+        if let Ok(s) = obj_model.predict_variances(&pt) {
+            let sigma = s[[0, 0]].sqrt();
+            if sigma.abs() < 1e-12 {
+                Array1::zeros(pt.len())
+            } else {
+                let pred = p[[0, 0]];
+                let diff_y = f_min - pred;
+                let arg = (f_min - pred) / sigma;
+                let y_prime = obj_model.predict_derivatives(&pt).unwrap();
+                let y_prime = y_prime.row(0);
+                let sig_2_prime = obj_model.predict_variance_derivatives(&pt).unwrap();
+
+                let sig_2_prime = sig_2_prime.row(0);
+                let sig_prime = sig_2_prime.mapv(|v| v / (2. * sigma));
+                let arg_prime = y_prime.mapv(|v| v / (-sigma))
+                    - diff_y.to_owned() * sig_prime.mapv(|v| v / (sigma * sigma));
+                let factor = sigma * (-arg / SQRT_2PI) * (-(arg * arg) / 2.).exp();
+
+                let arg1 = y_prime.mapv(|v| v * (-norm_cdf(arg)));
+                let arg2 = diff_y * norm_pdf(arg) * arg_prime.to_owned();
+                let arg3 = sig_prime.to_owned() * norm_pdf(arg);
+                let arg4 = factor * arg_prime;
+                arg1 + arg2 + arg3 + arg4
+            }
+        } else {
+            Array1::zeros(pt.len())
+        }
+    } else {
+        Array1::zeros(pt.len())
+    }
+}
+
+/// Compute WBS2 infill criterion at given `x` point using the surrogate model `obj_model`
+/// and the current minimum of the objective function.
+pub fn wb2s(x: &[f64], obj_model: &dyn ClusteredSurrogate, f_min: f64, scale: f64) -> f64 {
+    let pt = ArrayView::from_shape((1, x.len()), x).unwrap();
+    let ei = ei(x, obj_model, f_min);
+    scale * ei - obj_model.predict_values(&pt).unwrap()[[0, 0]]
+}
+
+/// Computes derivatives of WS2 infill criterion wrt to x components at given `x` point
+/// using the surrogate model `obj_model` and the current minimum of the objective function.
+pub fn grad_wbs2(
+    x: &[f64],
+    obj_model: &dyn ClusteredSurrogate,
+    f_min: f64,
+    scale: f64,
+) -> Array1<f64> {
+    let pt = ArrayView::from_shape((1, x.len()), x).unwrap();
+    let grad_ei = grad_ei(x, obj_model, f_min) * scale;
+    grad_ei - obj_model.predict_derivatives(&pt).unwrap().row(0)
+}
+
+/// Computes the scaling factor used to scale WB2 infill criteria.
+pub fn compute_wb2s_scale(
+    x: &ArrayView2<f64>,
+    obj_model: &dyn ClusteredSurrogate,
+    f_min: f64,
+) -> f64 {
+    let ratio = 100.; // TODO: make it a parameter
+    let ei_x = x.map_axis(Axis(1), |xi| {
+        let ei = ei(xi.as_slice().unwrap(), obj_model, f_min);
+        ei
+    });
+    let i_max = ei_x.argmax().unwrap();
+    let pred_max = obj_model
+        .predict_values(&x.row(i_max).insert_axis(Axis(0)))
+        .unwrap()[[0, 0]];
+    let ei_max = ei_x[i_max];
+    if ei_max.abs() > 100. * f64::EPSILON {
+        ratio * pred_max / ei_max
+    } else {
+        1.
+    }
+}
+
+/// Computes scaling factors used to scale constraint functions values.
+pub fn compute_cstr_scales(
+    x: &ArrayView2<f64>,
+    cstr_models: &[Box<dyn ClusteredSurrogate>],
+) -> Array1<f64> {
+    let scales: Vec<f64> = cstr_models
+        .par_iter()
+        .map(|cstr_model| {
+            let preds: Array1<f64> = cstr_model
+                .predict_values(x)
+                .unwrap()
+                .into_iter()
+                .filter(|v| !v.is_infinite()) // filter out infinite values
+                .map(|v| v.abs())
+                .collect();
+            *preds.max().unwrap_or(&1.0)
+        })
+        .collect();
+    Array1::from_shape_vec(cstr_models.len(), scales).unwrap()
+}
+
+/// Cumulative distribution function of Standard Normal at x
+pub fn norm_cdf(x: f64) -> f64 {
+    0.5 * erfc(-x / std::f64::consts::SQRT_2)
+}
+
+/// Probability density function of Standard Normal at x
+pub fn norm_pdf(x: f64) -> f64 {
+    (-0.5 * x * x).exp() / SQRT_2PI
+}
+
+// DOE handling functions
+///////////////////////////////////////////////////////////////////////////////
+
+/// Check if new point is not too close to previous ones `x_data`
+pub fn is_update_ok(
+    x_data: &ArrayBase<impl Data<Elem = f64>, Ix2>,
+    x_new: &ArrayBase<impl Data<Elem = f64>, Ix1>,
+) -> bool {
+    for row in x_data.rows() {
+        if row.l1_dist(x_new).unwrap() < 1e-6 {
+            return false;
+        }
+    }
+    true
+}
+
+/// Append `x_new` (resp. `y_new`) to `x_data` (resp. y_data) if `x_new` not too close to `x_data` points
+/// Returns the index of appended points in `x_new`
+pub fn update_data(
+    x_data: &mut Array2<f64>,
+    y_data: &mut Array2<f64>,
+    x_new: &ArrayBase<impl Data<Elem = f64>, Ix2>,
+    y_new: &ArrayBase<impl Data<Elem = f64>, Ix2>,
+) -> Vec<usize> {
+    let mut appended = vec![];
+    Zip::indexed(x_new.rows())
+        .and(y_new.rows())
+        .for_each(|idx, x, y| {
+            let xdat = x.insert_axis(Axis(0));
+            let ydat = y.insert_axis(Axis(0));
+            if is_update_ok(x_data, &x) {
+                *x_data = concatenate![Axis(0), x_data.view(), xdat];
+                *y_data = concatenate![Axis(0), y_data.view(), ydat];
+                appended.push(idx);
+            }
+        });
+    appended
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use egobox_doe::SamplingMethod;
+    use egobox_moe::*;
+    use linfa::prelude::*;
+    use ndarray::array;
+
+    #[test]
+    fn test_is_update_ok() {
+        let data = array![[0., 1.], [2., 3.]];
+        assert!(is_update_ok(&data, &array![3., 4.]));
+        assert!(!is_update_ok(&data, &array![1e-7, 1.]));
+    }
+
+    #[test]
+    fn test_update_data() {
+        let mut xdata = array![[0., 1.], [2., 3.]];
+        let mut ydata = array![[3.], [4.]];
+        assert_eq!(
+            update_data(
+                &mut xdata,
+                &mut ydata,
+                &array![[3., 4.], [1e-7, 1.]],
+                &array![[6.], [7.]],
+            ),
+            &[0]
+        );
+        assert_eq!(&array![[0., 1.], [2., 3.], [3., 4.]], xdata);
+        assert_eq!(&array![[3.], [4.], [6.]], ydata);
+    }
+
+    fn sphere(x: &Array2<f64>) -> Array2<f64> {
+        let s = (x * x).sum_axis(Axis(1));
+        s.insert_axis(Axis(1))
+    }
+    #[test]
+    fn test_grad_wbs2() {
+        let xt = egobox_doe::Lhs::new(&array![[-10., 10.], [-10., 10.]]).sample(10);
+        let yt = sphere(&xt);
+        let gp = Moe::params()
+            .regression_spec(RegressionSpec::CONSTANT)
+            .correlation_spec(CorrelationSpec::SQUAREDEXPONENTIAL)
+            .recombination(Recombination::Hard)
+            .fit(&Dataset::new(xt, yt))
+            .expect("GP fitting");
+        let bgp = Box::new(gp) as Box<dyn ClusteredSurrogate>;
+
+        let h = 1e-4;
+        let x1 = 0.1;
+        let x2 = 0.3;
+        let xtest = vec![x1, x2];
+        let xtest11 = vec![x1 + h, x2];
+        let xtest12 = vec![x1 - h, x2];
+        let xtest21 = vec![x1, x2 + h];
+        let xtest22 = vec![x1, x2 - h];
+        let fdiff1 = (wb2s(&xtest11, bgp.as_ref(), 0.1, 0.5)
+            - wb2s(&xtest12, bgp.as_ref(), 0.1, 0.5))
+            / (2. * h);
+        let fdiff2 = (wb2s(&xtest21, bgp.as_ref(), 0.1, 0.5)
+            - wb2s(&xtest22, bgp.as_ref(), 0.1, 0.5))
+            / (2. * h);
+        println!("fdiff({xtest:?}) = [{fdiff1}, {fdiff2}]");
+        println!(
+            "grad_wbs2({:?}) = {:?}",
+            xtest,
+            grad_wbs2(&xtest21, bgp.as_ref(), 0.1, 0.5)
+        );
+
+        let h = 1e-4;
+        let x1 = 0.5;
+        let x2 = 0.7;
+        let xtest = array![[x1, x2]];
+        let basetest = xtest.to_owned();
+        let xtest11 = array![[x1 + h, x2]];
+        let xtest12 = array![[x1 - h, x2]];
+        let xtest21 = array![[x1, x2 + h]];
+        let xtest22 = array![[x1, x2 - h]];
+        let fdiff1 = (bgp.predict_values(&xtest11.view()).unwrap()
+            - bgp.predict_values(&xtest12.view()).unwrap())
+            / (2. * h);
+        let fdiff2 = (bgp.predict_values(&xtest21.view()).unwrap()
+            - bgp.predict_values(&xtest22.view()).unwrap())
+            / (2. * h);
+        println!(
+            "gp fdiff({}) = [[{}, {}]]",
+            xtest,
+            fdiff1[[0, 0]],
+            fdiff2[[0, 0]]
+        );
+        println!(
+            "GP predict derivatives({}) = {}",
+            xtest,
+            bgp.predict_derivatives(&basetest.view()).unwrap()
+        );
+
+        let h = 1e-4;
+        let x1 = 5.0;
+        let x2 = 1.0;
+        let xtest = array![[x1, x2]];
+        let basetest = xtest.to_owned();
+        let xtest11 = array![[x1 + h, x2]];
+        let xtest12 = array![[x1 - h, x2]];
+        let xtest21 = array![[x1, x2 + h]];
+        let xtest22 = array![[x1, x2 - h]];
+        let fdiff1 = (bgp.predict_variances(&xtest11.view()).unwrap()
+            - bgp.predict_variances(&xtest12.view()).unwrap())
+            / (2. * h);
+        let fdiff2 = (bgp.predict_variances(&xtest21.view()).unwrap()
+            - bgp.predict_variances(&xtest22.view()).unwrap())
+            / (2. * h);
+        println!(
+            "gp var fdiff({}) = [[{}, {}]]",
+            xtest,
+            fdiff1[[0, 0]],
+            fdiff2[[0, 0]]
+        );
+        println!(
+            "GP predict variances derivatives({}) = {}",
+            xtest,
+            bgp.predict_variance_derivatives(&basetest.view()).unwrap()
+        );
+    }
+}
