e2c9b7428c6dcf00425d5b37ff667c6ebfa6fc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package pybi-next/0.3.20/pybi_next-0.3.20-py3-none-any.whl

---
+++ packages/pybi-next/pybi_next-0.3.20-py3-none-any.whl/pybi/core/components/componentTag.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class ComponentTag(Enum):
+    App = "App"
+    DataSource = "DataSource"
+    Slicer = "elSlicer"
+    Table = "elTable"
+    EChart = "EChart"
+    Box = "Box"
+    Text = "Text"
+    Upload = "Upload"
+    TextValue = "TextValue"
+    Markdown = "Markdown"
+    ColBox = "ColBox"
+    FlowBox = "FlowBox"
+    GridBox = "GridBox"
+    Tabs = "elTabs"
+    Icon = "Icon"
+    Affix = "Affix"
