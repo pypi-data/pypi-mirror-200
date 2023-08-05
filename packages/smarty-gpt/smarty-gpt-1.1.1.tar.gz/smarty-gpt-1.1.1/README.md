ff2e41947fdcfc6fd85c20d0ae1d0c16531cec58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:40 +0000

    Add package smarty-gpt/1.1.1/smarty-gpt-1.1.1.tar.gz

---
+++ packages/smarty-gpt/smarty-gpt-1.1.1.tar.gz/smarty-gpt-1.1.1/README.md
@@ -0,0 +1,49 @@
+# Smarty-GPT: wrapper of prompts/contexts (also ChatGPT and GPT4!)
+
+This is a simple wrapper that introduces any imaginable complex context to each question submitted to a GPT or any other supported LM. The main goal is to enhance the accuracy obtained in its answers in a **TRANSPARENT** way to end users. 
+
+
+## Installation 
+
+```bash
+pip install smarty-gpt==1.1.0
+```
+
+## Models
+
+- *OpenAI API standard models*: **text-davinci-003**, **code-davinci-002**, **text-davinci-002**, etc.
+- *Hugging Face Language Models*:like [**Flan-T5**](https://huggingface.co/google/flan-t5-small) powered by Google.
+- *For ChatGPT Plus suscriptors*: **ChatGPT** and **GPT4** integration thanks to previous [projects](https://github.com/mmabrouk/chatgpt-wrapper).
+
+## Contexts / Prompts
+
+We support three type of prompts from the moment:
+
+- **Manual prompts**: these prompts are hard-coded and were the first included in this project.
+- [**Awesome Chat GPT prompts**](https://github.com/f/awesome-chatgpt-prompts): our system also supports this huge HF dataset in a transparent manner.
+- **Custom prompts**: any user can add custom prompts through a file.
+- (In progress) Support for [**awesome-gpt4**](https://github.com/radi-cho/awesome-gpt4) prompts.
+
+## Coding examples
+
+```python
+from smartygpt import Wrapper
+wrapper = Wrapper()
+wrapper.wrapper("Can ibuprofen worsen COVID-19?", "doctor", "text-davinci-003", "INSERT-YOUR-OPENAI-KEY-HERE") # manual prompt
+wrapper.wrapper("Describe the attack of Pearl Harbor.", "custom-perplexity", "flant5") # custom prompt
+wrapper.wrapper("Rap about Eminem", "Rapper", "chatgpt") # Awesome Chat GPT prompts 
+wrapper.wrapper("Debate about the meaning of life", "Philosopher", "gpt4") # Awesome Chat GPT prompts 
+```
+**IMPORTANT!!!**: As it is not yet an official api, the first time "chatgpt" or "gpt4" models are called, a browser opens and asks for our *plus subscriber* credentials. Once logged in, we can close the generated prompt and the model will automatically respond and not ask for the credentials in the rest of the session.
+
+## Purpose of the project
+
+The main purpose of this project is **joining** in a single environment all the resources (models, prompts, APIs, etc.) related to LLMs. Moreover, we also think from an **end-user** perspective. It is heavily unlikely that a user would introduce a complex context in a query to a model or searcher. In this project, we try to bias the different model responses to answer in different ways/behaviors, but hidding this to end-users.
+
+## In progress
+
+More features/models are about to come! Feel free to make a PR, open an issue or to contact me at marcosfernandez.pichel@usc.es
+
+## Disclaimer 
+
+The software is provided "as is" and "with all faults" without warranties of any kind, either express or implied, including, but not limited to, the implied warranties of merchantability, fitness for a particular purpose and non-infringement. No warranty is provided that the software will be free from defects or that operation of the software will be uninterrupted. Your use of the software and any other material or services downloaded or made available to you through the software is at your own discretion and risk, and you are solely responsible for any potential damage resulting from their use.
---
+++ packages/smarty-gpt/smarty-gpt-1.1.1.tar.gz/smarty-gpt-1.1.1/smartygpt/smartygpt.py
@@ -0,0 +1,80 @@
+import requests
+import argparse
+import os 
+from nltk import sent_tokenize
+from .contexts import ManualContexts, AwesomePrompts, CustomPrompt
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+# from chatgpt_wrapper import ChatGPT
+# from chatgpt_wrapper.config import Config
+import subprocess
+import time
+import signal
+
+
+'''
+This function wraps user's petition question with the adequate context to better orient the response of the language model
+'''
+
+class Wrapper:
+
+    def __init__(self):
+        self.login = None
+
+    def wrapper(self, query:str, context:str, model:str="text-davinci-003", key: str=None) -> str:
+        ##### Contexts
+        
+        contexts = AwesomePrompts.dataset['act']
+        contexts.extend(["doctor", "lawyer", "programmer", "sick"])
+        contexts.extend(CustomPrompt.available_prompts)
+        if not any(context==x for x in contexts):
+            raise Exception("Sorry, that context is not implemented yet")
+        if context=="doctor":
+            context = ManualContexts.DoctorAdvice
+        elif context=="lawyer":
+            context = ManualContexts.Lawyer
+        elif context=="programmer":
+            context = ManualContexts.Programmer
+        elif context=="sick":
+            context = ManualContexts.SickAdvice
+        elif context.startswith('custom-'):
+            custom_name = context[len('custom-'):]
+            context = CustomPrompt(custom_name).prompt
+        else:
+            context = AwesomePrompts.dataset.filter(lambda x: x['act']==context)['prompt'][0]
+            context = ' '.join(sent_tokenize(context)[:-1])
+
+        ### Models
+        if model=="flant5":
+            model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
+            tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
+            inputs = tokenizer(context + " \"" + query+ "\"", return_tensors="pt")
+            outputs = model.generate(**inputs)
+            response = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+            response = response[0].lower()       
+            return response
+
+        elif model=="chatgpt" or model=="gpt4":
+            if not self.login: #### Mandatory login from chatgpt-wrapper
+                self.login = subprocess.run(["chatgpt", "install"])
+            
+            print("The response to:", query, "is generating...")
+            if model=="chatgpt":
+                bot = ChatGPT()
+                _, response, _ = bot.ask(context + " \"" + query)
+            elif model=="gpt4":
+                config = Config()
+                config.set('chat.model', 'gpt4')
+                bot = ChatGPT(config)
+                _, response, _ = bot.ask(context + " \"" + query)
+
+            return response            
+            
+        else:
+            url = 'https://api.openai.com/v1/completions'
+            headers = {"Content-Type": "application/json; charset=utf-8", "Authorization":"Bearer "+key}
+            myobj = {'model': model, 'prompt': context + " \"" + query+ "\"", "max_tokens":256, "temperature":0} # temperature is set to 0 by default since we want the most deterministic as possible responses
+                                                                                                                        # max_tokens = 256 because we want a concrete explanation, better if it yes or no
+
+            x = requests.post(url, headers =headers, json = myobj)
+            
+            return x.json()['choices'][0]['text']   
