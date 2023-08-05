fd2b77f4d417905434de23bfe312144011c36133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:25 +0000

    Add package pycode128/2.3.0/pycode128-2.3.0-cp39-cp39-macosx_12_0_x86_64.whl

---
+++ packages/pycode128/pycode128-2.3.0-cp39-cp39-macosx_12_0_x86_64.whl/pycode128/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 Gabriele Pongelli
+#
+# SPDX-License-Identifier: MIT
+
+"""Top-level package for Code128 library's python extension."""
+
+__author__ = "Gabriele Pongelli"
+__email__ = "gabriele.pongelli@gmail.com"
+__version__ = "2.3.0"
