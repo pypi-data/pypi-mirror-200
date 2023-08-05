e2c34f97cf0ab583d876503e88614b894faac0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:13 +0000

    Add package kivy-widgets/0.1.4/kivy_widgets-0.1.4.tar.gz

---
+++ packages/kivy-widgets/kivy_widgets-0.1.4.tar.gz/kivy_widgets-0.1.4/kivy_widgets/buttons.py
@@ -0,0 +1,9 @@
+# class LightCustomButton(ButtonBehavior, BoxLayout):
+#     text = StringProperty("Hello World")
+#     font_size = NumericProperty(sp(18))
+#     icon = StringProperty()
+#     bg_color = ColorProperty([1, 1, 1, 1])
+#     color = ColorProperty([0, 0, 0, 1])
+#     icon_color = ColorProperty([0, 0, 0, 1])
+#     radius = ListProperty([dp(5)])
+#     icon_size = NumericProperty(sp(24))
