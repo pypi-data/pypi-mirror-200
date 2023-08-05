569b792777793966060fbfd9aea99eb6bb0f5953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:47 +0000

    Add package pod-gpt/0.0.2/pod_gpt-0.0.2.tar.gz

---
+++ packages/pod-gpt/pod_gpt-0.0.2.tar.gz/pod_gpt-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pod_gpt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['langchain>=0.0.125,<0.0.126', 'pytube>=12.1.3,<13.0.0']
+
+setup_kwargs = {
+    'name': 'pod-gpt',
+    'version': '0.0.2',
+    'description': 'Package for building podcast search indexes.',
+    'long_description': '# PodGPT',
+    'author': 'James Briggs',
+    'author_email': 'james@aurelio.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
