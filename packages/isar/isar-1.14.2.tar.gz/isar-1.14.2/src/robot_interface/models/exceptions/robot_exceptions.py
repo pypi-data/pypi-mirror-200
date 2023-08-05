bd549268f03d5ec3eeb2477d680ef6aac6d545dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:46 +0000

    Add package isar/1.14.2/isar-1.14.2.tar.gz

---
+++ packages/isar/isar-1.14.2.tar.gz/isar-1.14.2/src/robot_interface/models/exceptions/robot_exceptions.py
@@ -0,0 +1,36 @@
+class RobotException(Exception):
+    pass
+
+
+class RobotCommunicationException(RobotException):
+    pass
+
+
+class RobotInfeasibleStepException(RobotException):
+    pass
+
+
+class RobotInfeasibleMissionException(RobotException):
+    pass
+
+
+class RobotInvalidResponseException(RobotException):
+    pass
+
+
+class RobotMapException(RobotException):
+    pass
+
+
+class RobotInvalidTelemetryException(RobotException):
+    pass
+
+
+class RobotLowBatteryException(RobotException):
+    def __init__(self, battery_level):
+        self.battery_level = battery_level
+
+
+class RobotLowPressureException(RobotException):
+    def __init__(self, pressure_level):
+        self.pressure_level = pressure_level
