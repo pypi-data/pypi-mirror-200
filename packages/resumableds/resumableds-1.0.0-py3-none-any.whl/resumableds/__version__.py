fcacdfcb59130e42ad63bbd2dfb9f220ca7fe365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:48 +0000

    Add package resumableds/1.0.0/resumableds-1.0.0-py3-none-any.whl

---
+++ packages/resumableds/resumableds-1.0.0-py3-none-any.whl/resumableds/__version__.py
@@ -0,0 +1,7 @@
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__version__ = '1.0.0'
+__author__ = "David Riedel"
+__author_email__ = "systemverwalter@gmail.com"
+__date__ = '2023-03-29'
