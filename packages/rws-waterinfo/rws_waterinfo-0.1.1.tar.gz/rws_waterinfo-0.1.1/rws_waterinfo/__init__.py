ee238ffb7ee9847e3d5fb6044b002d0453a714ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:52 +0000

    Add package rws-waterinfo/0.1.1/rws_waterinfo-0.1.1.tar.gz

---
+++ packages/rws-waterinfo/rws_waterinfo-0.1.1.tar.gz/rws_waterinfo-0.1.1/rws_waterinfo/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Documentation about rws_waterinfo"""
+# from rws_waterinfo import rws_waterinfo
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "RWS Datalab"
+__email__ = "datalab.codebase@rws.nl"
+__version__ = "0.1.1"
+
+
+from rws_waterinfo.rws_waterinfo import (
+    get_catalog,
+    get_data,
+    set_logger,
+    update_dataframe,
+)
+
+__all__ = ["set_logger" "get_catalog", "get_data", "update_dataframe"]
