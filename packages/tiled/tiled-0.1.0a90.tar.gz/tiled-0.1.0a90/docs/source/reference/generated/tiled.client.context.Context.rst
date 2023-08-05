6cae8bf830a39f122d9ac105b26814b58a4fe859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package tiled/0.1.0a90/tiled-0.1.0a90.tar.gz

---
+++ packages/tiled/tiled-0.1.0a90.tar.gz/tiled-0.1.0a90/docs/source/reference/generated/tiled.client.context.Context.rst
@@ -0,0 +1,53 @@
+﻿tiled.client.context.Context
+============================
+
+.. currentmodule:: tiled.client.context
+
+.. autoclass:: Context
+
+   
+   .. automethod:: __init__
+
+   
+   .. rubric:: Methods
+
+   .. autosummary::
+   
+      ~Context.__init__
+      ~Context.authenticate
+      ~Context.create_api_key
+      ~Context.delete_content
+      ~Context.disable_cache
+      ~Context.force_auth_refresh
+      ~Context.from_any_uri
+      ~Context.get_content
+      ~Context.get_json
+      ~Context.login
+      ~Context.logout
+      ~Context.post_json
+      ~Context.put_content
+      ~Context.put_json
+      ~Context.revalidation
+      ~Context.revoke_api_key
+      ~Context.revoke_session
+      ~Context.which_api_key
+      ~Context.whoami
+   
+   
+
+   
+   
+   .. rubric:: Attributes
+
+   .. autosummary::
+   
+      ~Context.api_key
+      ~Context.app
+      ~Context.base_url
+      ~Context.cache
+      ~Context.event_hooks
+      ~Context.offline
+      ~Context.revalidate
+      ~Context.tokens
+   
+   
---
+++ packages/tiled/tiled-0.1.0a90.tar.gz/tiled-0.1.0a90/tiled/_tests/test_client.py
@@ -0,0 +1,114 @@
+import sys
+import types
+from pathlib import Path
+
+import httpx
+import pytest
+import yaml
+
+from ..adapters.mapping import MapAdapter
+from ..client import Context, from_context, from_profile
+from ..client.context import CannotPrompt
+from ..config import ConfigError
+from ..profiles import load_profiles, paths
+from ..server.app import build_app, build_app_from_config
+from .utils import fail_with_status_code
+
+tree = MapAdapter({})
+
+
+def test_configurable_timeout():
+    with Context.from_app(build_app(tree), timeout=httpx.Timeout(17)) as context:
+        assert context.http_client.timeout.connect == 17
+        assert context.http_client.timeout.read == 17
+
+
+def test_client_version_check():
+    with Context.from_app(build_app(tree)) as context:
+        client = from_context(context)
+
+        # Too-old user agent should generate a 400.
+        context.http_client.headers["user-agent"] = "python-tiled/0.1.0a77"
+        with fail_with_status_code(400):
+            list(client)
+
+        # Gibberish user agent should generate a 400.
+        context.http_client.headers["user-agent"] = "python-tiled/gibberish"
+        with fail_with_status_code(400):
+            list(client)
+
+
+def test_direct(tmpdir):
+    profile_content = {
+        "test": {
+            "direct": {
+                "trees": [
+                    {"path": "/", "tree": "tiled.examples.generated_minimal:tree"}
+                ]
+            }
+        }
+    }
+    with open(tmpdir / "example.yml", "w") as file:
+        file.write(yaml.dump(profile_content))
+    profile_dir = Path(tmpdir)
+    try:
+        paths.append(profile_dir)
+        load_profiles.cache_clear()
+        from_profile("test")
+    finally:
+        paths.remove(profile_dir)
+
+
+def test_direct_config_error(tmpdir):
+    profile_content = {
+        "test": {
+            "direct": {
+                # Intentional config mistake!
+                # Value of trees must be a list.
+                "trees": {"path": "/", "tree": "tiled.examples.generated_minimal:tree"}
+            }
+        }
+    }
+    with open(tmpdir / "example.yml", "w") as file:
+        file.write(yaml.dump(profile_content))
+    profile_dir = Path(tmpdir)
+    try:
+        paths.append(profile_dir)
+        load_profiles.cache_clear()
+        with pytest.raises(ConfigError):
+            from_profile("test")
+    finally:
+        paths.remove(profile_dir)
+
+
+def test_stdin_closed(monkeypatch):
+    "When stdin is closed do not prompt for authentication."
+    # https://github.com/bluesky/tiled/pull/427
+    __stdin__ = types.SimpleNamespace(closed=True)
+    monkeypatch.setattr(sys, "__stdin__", __stdin__)
+    config = {
+        "authentication": {
+            "secret_keys": ["SECRET"],
+            "providers": [
+                {
+                    "provider": "toy",
+                    "authenticator": "tiled.authenticators:DictionaryAuthenticator",
+                    "args": {
+                        "users_to_passwords": {"alice": "secret1", "bob": "secret2"}
+                    },
+                }
+            ],
+        },
+        "database": {
+            "uri": "sqlite+aiosqlite://",  # in memory
+        },
+        "trees": [
+            {
+                "tree": f"{__name__}:tree",
+                "path": "/",
+            },
+        ],
+    }
+    with Context.from_app(build_app_from_config(config)) as context:
+        with pytest.raises(CannotPrompt):
+            from_context(context)
