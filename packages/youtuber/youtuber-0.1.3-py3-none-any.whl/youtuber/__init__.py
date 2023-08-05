fe2cf86fcbb226216dcc1bbb7decec0a79c55973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package youtuber/0.1.3/youtuber-0.1.3-py3-none-any.whl

---
+++ packages/youtuber/youtuber-0.1.3-py3-none-any.whl/youtuber/__init__.py
@@ -0,0 +1,7 @@
+from youtuber.api import YoutubeAPI
+from youtuber.crawler import YoutubeCrawler
+from youtuber.auto import AutoCrawler
+
+__all__ = [YoutubeCrawler, YoutubeAPI, AutoCrawler]
+
+__version__ = "0.1.3"
