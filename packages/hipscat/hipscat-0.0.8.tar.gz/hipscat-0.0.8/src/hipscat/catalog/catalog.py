67eacec6e4a77ce9481b0d4eae3dc279100d3790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:18 +0000

    Add package hipscat/0.0.8/hipscat-0.0.8.tar.gz

---
+++ packages/hipscat/hipscat-0.0.8.tar.gz/hipscat-0.0.8/src/hipscat/catalog/catalog.py
@@ -0,0 +1,49 @@
+"""Container class to hold catalog metadata and partition iteration"""
+
+
+from hipscat.catalog.partition_info import PartitionInfo
+from hipscat.io import file_io, paths
+
+
+class Catalog:
+    """Container class for catalog metadata"""
+
+    def __init__(self, catalog_path: str = None) -> None:
+        self.catalog_path = catalog_path
+        self.catalog_base_dir = file_io.get_file_pointer_from_path(catalog_path)
+        self.metadata_keywords = None
+
+        self.partition_info = None
+
+        self.catalog_name = None
+
+        self._initialize_metadata()
+
+    def _initialize_metadata(self):
+        if not file_io.does_file_or_directory_exist(self.catalog_base_dir):
+            raise FileNotFoundError(
+                f"No directory exists at {str(self.catalog_base_dir)}"
+            )
+        catalog_info_file = paths.get_catalog_info_pointer(self.catalog_base_dir)
+        if not file_io.does_file_or_directory_exist(catalog_info_file):
+            raise FileNotFoundError(
+                f"No catalog info found where expected: {str(catalog_info_file)}"
+            )
+
+        self.metadata_keywords = file_io.load_json_file(catalog_info_file)
+        self.catalog_name = self.metadata_keywords["catalog_name"]
+        self.partition_info = PartitionInfo(self.catalog_base_dir)
+
+    def get_pixels(self):
+        """Get all healpix pixels that are contained in the catalog
+
+        Returns:
+            data frame with per-pixel data.
+
+            The data frame contains the following columns:
+
+            - order: order of the destination pixel
+            - pixel: pixel number *at the above order*
+            - num_objects: the number of rows in the pixel's partition
+        """
+        return self.partition_info.data_frame
---
+++ packages/hipscat/hipscat-0.0.8.tar.gz/hipscat-0.0.8/src/hipscat/catalog/catalog_parameters.py
@@ -0,0 +1,56 @@
+"""Conatiner class for properties of a catalog that is being created"""
+
+from typing import List
+
+from hipscat.io import file_io
+
+
+class CatalogParameters:
+    """Container class for catalog properties"""
+
+    def __init__(
+        self,
+        catalog_name=None,
+        input_paths: List[str] = None,
+        input_format="csv",
+        output_path=None,
+        highest_healpix_order=10,
+        pixel_threshold=1_000_000,
+        epoch="J2000",
+        ra_column="ra",
+        dec_column="dec",
+        id_column="id",
+    ):
+        self.catalog_name = catalog_name
+        self.input_format = input_format
+        self.input_paths = input_paths
+        self.output_path = output_path
+        output_path_pointer = file_io.get_file_pointer_from_path(self.output_path)
+        self.catalog_base_dir = file_io.append_paths_to_pointer(
+            output_path_pointer, self.catalog_name
+        )
+        self.catalog_path = str(self.catalog_base_dir)
+        file_io.make_directory(self.catalog_base_dir, exist_ok=True)
+
+        self.epoch = epoch
+        self.ra_column = ra_column
+        self.dec_column = dec_column
+        self.id_column = id_column
+
+        self.highest_healpix_order = highest_healpix_order
+        self.pixel_threshold = pixel_threshold
+
+    def __str__(self):
+        formatted_string = (
+            f"  catalog_name {self.catalog_name}\n"
+            f"  input format {self.input_format}\n"
+            f"  num input_paths {len(self.input_paths)}\n"
+            f"  epoch {self.epoch}\n"
+            f"  ra_column {self.ra_column}\n"
+            f"  dec_column {self.dec_column}\n"
+            f"  id_column {self.id_column}\n"
+            f"  output_path {self.output_path}\n"
+            f"  highest_healpix_order {self.highest_healpix_order}\n"
+            f"  pixel_threshold {self.pixel_threshold}\n"
+        )
+        return formatted_string
