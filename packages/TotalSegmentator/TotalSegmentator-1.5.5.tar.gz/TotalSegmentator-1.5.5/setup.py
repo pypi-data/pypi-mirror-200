f3a81fd8eb36a37fc5fbb9296d2cb1417a804f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:48 +0000

    Add package TotalSegmentator/1.5.5/TotalSegmentator-1.5.5.tar.gz

---
+++ packages/TotalSegmentator/TotalSegmentator-1.5.5.tar.gz/TotalSegmentator-1.5.5/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(name='TotalSegmentator',
+        version='1.5.5',
+        description='Robust segmentation of 104 classes in CT images.',
+        long_description="See Readme.md on github for more details.",
+        url='https://github.com/wasserth/TotalSegmentator',
+        author='Jakob Wasserthal',
+        author_email='jakob.wasserthal@usb.ch',
+        python_requires='>=3.5',
+        license='Apache 2.0',
+        packages=find_packages(),
+        install_requires=[
+            'numpy',
+            'psutil',
+            # Any version <2.1.0 because of this issue: 
+            # https://github.com/SimpleITK/SimpleITK/issues/1433
+            'SimpleITK',
+            'nibabel>=2.3.0',
+            'tqdm>=4.45.0',
+            'p_tqdm',
+            'xvfbwrapper',
+            'fury',
+            'batchgenerators==0.21',
+            # This does not work if want to upload to pypi
+            # 'nnunet @ git+https://github.com/wasserth/nnUNet_cust@working_2022_03_18#egg=nnUNet'
+            'nnunet-customized==1.2',
+            'requests',
+            'rt_utils'
+        ],
+        zip_safe=False,
+        classifiers=[
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python',
+            'Topic :: Scientific/Engineering',
+            'Operating System :: Unix',
+            'Operating System :: MacOS'
+        ],
+        scripts=[
+            'bin/TotalSegmentator', 'bin/totalseg_combine_masks', 'bin/crop_to_body', 
+            'bin/totalseg_import_weights', 'bin/totalseg_download_weights'
+        ]
+    )
