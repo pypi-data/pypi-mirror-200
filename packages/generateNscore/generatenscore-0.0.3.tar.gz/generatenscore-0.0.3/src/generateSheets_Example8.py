d4fdd75657fb70d6222f5d8383c8ab0085f5fc78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:14 +0000

    Add package generateNscore/0.0.3/generatenscore-0.0.3.tar.gz

---
+++ packages/generateNscore/generatenscore-0.0.3.tar.gz/generatenscore-0.0.3/src/generateSheets_Example8.py
@@ -0,0 +1,74 @@
+import generateNscore as gNs
+
+
+Q=['Submit the greater of the two roots of the following quadratic equation:<br>EQ%{%eq%}%EQ',
+   'Submit the smaller of the two roots of the following quadratic equation:<br>EQ%{%eq%}%EQ']
+A='''data=[]
+vA=random.choice([-9,-8,-7,-6,-5,-4,-3,-2,-1, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+vB=random.choice([-9,-8,-7,-6,-5,-4,-3,-2,-1, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+while not vA+vB:
+   vB=random.choice([-9,-8,-7,-6,-5,-4,-3,-2,-1, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+add = vA+vB
+prod = vA*vB
+if add == 1:
+  if prod>0:
+     eq=f'x^2 - x + {prod} = 0'
+  elif prod<0:
+     eq=f'x^2 - x - {-prod} = 0'
+  else:
+     eq=f'x^2 - x = 0'
+elif add == -1:
+  if prod>0:
+     eq=f'x^2 + x + {prod} = 0'
+  elif prod<0:
+     eq=f'x^2 + x - {-prod} = 0'
+  else:
+     eq=f'x^2 + x = 0'
+elif add>0:
+  if prod>0:
+     eq=f'x^2 - {add}x + {prod} = 0'
+  elif prod<0:
+     eq=f'x^2 - {add}x - {-prod} = 0'
+  else:
+     eq=f'x^2 - {add}x = 0'
+elif add<0:
+  if prod>0:
+     eq=f'x^2 + {-add}x + {prod} = 0'
+  elif prod<0:
+     eq=f'x^2 + {-add}x - {-prod} = 0'
+  else:
+     eq=f'x^2 + {-add}x = 0'
+
+answer=[max(vA, vB), min(vA, vB)]'''
+
+QGs=[[Q, A, ('chap10', 'qg1'), 'short']]
+
+
+figures={}
+
+
+def example8A(iMultipleChoices=False):
+    # This example shows what each question with a figure included looks like in HTML format.
+    name='checkups8' # do not change
+    heading='Testing' # do not change
+    STDs={'12345678': 'oooo'} # do not change
+
+    a=gNs.work(name, heading, STDs, QGs, True, iMultipleChoices, False)
+    gNs.mkHTMLs(a, figures)
+    #a.saveWork()
+
+
+def example8B(iMultipleChoices=False, iShuffling=False):
+    # This example demonstrates the generation a set of question sheets in HTML format.
+    name='ex8'
+    heading='Homework'
+    STDs={'22332213': '홍길동 (GilDong Hong)' , '28193278':'이몽룡 (Mong ryong Lee)', '98123522': 'Weeny Tiny'}
+
+    a=gNs.work(name, heading, STDs, QGs, False, iMultipleChoices, iShuffling)
+    gNs.mkHTMLs(a, figures)
+    a.saveWork()
+
+
+if __name__ == '__main__':
+    example8B(True, True)
