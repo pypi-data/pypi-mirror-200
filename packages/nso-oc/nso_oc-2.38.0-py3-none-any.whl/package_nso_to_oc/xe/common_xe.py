7f60942c5286a4c681b4951aed9cd302dde36e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:37 +0000

    Add package nso-oc/2.38.0/nso_oc-2.38.0-py3-none-any.whl

---
+++ packages/nso-oc/nso_oc-2.38.0-py3-none-any.whl/package_nso_to_oc/xe/common_xe.py
@@ -0,0 +1,36 @@
+"""
+Functions in here need to be in a separate file from main_xe.py to avoid cyclical dependencies
+when invoking the individual features.
+"""
+
+import sys
+import os
+import copy
+import json
+from pathlib import Path
+
+# To be able to import top-level common
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+import common
+
+# XE static route keys
+IP_FORWARDING_LIST = "ip-route-forwarding-list"
+INTF_LIST = "ip-route-interface-list"
+IP_INTF_FORWARDING_LIST = "ip-route-interface-forwarding-list"
+
+def init_xe_configs(device_name = "xe1"):
+    nso_ned_file = os.environ.get("NSO_NED_FILE", False)
+
+    if nso_ned_file:
+        with open(nso_ned_file, "r") as ned_file:
+            config_before_dict = json.load(ned_file)
+    else:
+        (nso_api_url, nso_username, nso_password) = common.get_nso_creds()
+        nso_device = os.environ.get("NSO_DEVICE", device_name)
+        config_before_dict = common.nso_get_device_config(nso_api_url, nso_username, nso_password, nso_device)
+
+    config_leftover_dict = copy.deepcopy(config_before_dict)
+    interface_ip_dict = common.xe_system_get_interface_ip_address(config_before_dict)
+
+    return (config_before_dict, config_leftover_dict, interface_ip_dict)
