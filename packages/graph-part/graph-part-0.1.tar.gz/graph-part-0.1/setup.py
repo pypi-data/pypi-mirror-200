f5f4db6c56ac8e23709321b51107ed68a3e3dc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:12 +0000

    Add package graph-part/0.1/graph-part-0.1.tar.gz

---
+++ packages/graph-part/graph-part-0.1.tar.gz/graph-part-0.1/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+
+with open(os.path.join("README.md")) as f:
+    readme = f.read()
+
+with open(os.path.join("requirements.txt")) as f:
+    requirements = f.read()
+
+
+
+
+setup(
+    name="graph-part",
+    version='0.1',
+    description="Graph-based partitioning of biological sequence data",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://healthtech.dtu.dk",
+    author="F. Teufel and M.H. Gislason",
+    packages=['graph_part'],
+    python_requires=">=3.6, <4",
+    install_requires=requirements,
+    scripts=['graph_part/mmseqs_fake_prefilter.sh'],
+    entry_points = {"console_scripts":['graphpart=graph_part:run_graph_part']},
+)
