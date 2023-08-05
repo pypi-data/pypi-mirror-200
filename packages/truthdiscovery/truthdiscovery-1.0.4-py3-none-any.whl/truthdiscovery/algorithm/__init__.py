c44b63bd7e14ba297e3cd96a5af6eea9d4ae262b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package truthdiscovery/1.0.4/truthdiscovery-1.0.4-py3-none-any.whl

---
+++ packages/truthdiscovery/truthdiscovery-1.0.4-py3-none-any.whl/truthdiscovery/algorithm/__init__.py
@@ -0,0 +1,13 @@
+from truthdiscovery.algorithm.average_log import AverageLog
+from truthdiscovery.algorithm.base import (
+    BaseAlgorithm,
+    BaseIterativeAlgorithm,
+    PriorBelief
+)
+from truthdiscovery.algorithm.investment import Investment
+from truthdiscovery.algorithm.pooled_investment import PooledInvestment
+from truthdiscovery.algorithm.sums import Sums
+from truthdiscovery.algorithm.unboundedsums import UnboundedSums
+from truthdiscovery.algorithm.truth_finder import TruthFinder
+from truthdiscovery.algorithm.voting import MajorityVoting
+from truthdiscovery.algorithm.crh import CRH
---
+++ packages/truthdiscovery/truthdiscovery-1.0.4-py3-none-any.whl/truthdiscovery/utils/__init__.py
@@ -0,0 +1,18 @@
+from truthdiscovery.utils.iterator import (
+    ConvergenceIterator,
+    DistanceMeasures,
+    FixedIterator,
+    Iterator,
+    OrdinalConvergenceIterator
+)
+
+
+def filter_dict(dct, keys):
+    """
+    :yield: Tuples ``(key, value)`` for keys in ``keys``
+    """
+    for key in keys:
+        try:
+            yield (key, dct[key])
+        except KeyError:
+            pass
