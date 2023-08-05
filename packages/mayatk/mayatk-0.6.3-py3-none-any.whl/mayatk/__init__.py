3ec1c99374a7b6cf68c9f65bdced1ea251fbe462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:09 +0000

    Add package mayatk/0.6.3/mayatk-0.6.3-py3-none-any.whl

---
+++ packages/mayatk/mayatk-0.6.3-py3-none-any.whl/mayatk/__init__.py
@@ -0,0 +1,55 @@
+# !/usr/bin/python
+# coding=utf-8
+import sys
+
+
+__package__ = 'mayatk'
+__version__ = '0.6.3'
+
+
+def __getattr__(attr):
+	"""This function dynamically imports a module and returns an attribute from the module. 
+
+	Parameters:
+		attr (str): The name of the attribute to be imported. The name should be in the format 
+					'module_name.attribute_name' or just 'attribute_name'.
+	Return:
+		(obj) The attribute specified by the `attr` argument.
+
+	:Raises:
+		AttributeError: If the specified attribute is not found in either the original module 
+						or the 'Core' module within the package.
+	Example:
+		<package>.__getattr__('module1.attribute1') #returns: <attribute1 value>
+		<package>.__getattr__('attribute1') #returns: <attribute1 value>
+	"""
+	try:
+		module = __import__(f"{__package__}.{attr}", fromlist=[f"{attr}"])
+		setattr(sys.modules[__name__], attr, getattr(module, attr))
+		return getattr(module, attr)
+
+	except (ValueError, ModuleNotFoundError):
+		module = __import__(f"{__package__}.Core", fromlist=["Core"])
+		return getattr(module, attr)
+
+	except AttributeError as error:
+		raise AttributeError(f"Module '{__package__}' has no attribute '{attr}'") from error
+
+# --------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# deprecated:
+# --------------------------------------------------------------------------------------------
