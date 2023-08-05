2a2aeb3b792c27c0800e7cf1126a4c0492d49e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:57 +0000

    Add package pydantic-factur-x/0.1.4/pydantic_factur_x-0.1.4-py3-none-any.whl

---
+++ packages/pydantic-factur-x/pydantic_factur_x-0.1.4-py3-none-any.whl/pydantic_factur_x/factur_x/root.py
@@ -0,0 +1,78 @@
+"""Pydantic XML models for Factur-X and Order-X schemas."""
+from pydantic_xml import BaseXmlModel, element
+
+
+from .document_context import ExchangedDocumentContext
+from .exchanged_document import ExchangedDocument
+from .supply_chain_trade_transaction import SupplyChainTradeTransaction
+
+
+class SCRDMCCBDACIOMessageStructure(
+    BaseXmlModel,
+    tag="SCRDMCCBDACIOMessageStructure",
+    ns="rsm",
+    nsmap={
+        "rsm": "urn:un:unece:uncefact:data:SCRDMCCBDACIOMessageStructure:100",
+        "udt": "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:128",
+        "qtd": "urn:un:unece:uncefact:data:standard:QualifiedDataType:128",
+        "ram": "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:128",
+        "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    },
+):
+    """Root Factur-X and Order-X element.
+
+    nsmaps are fully definied here.
+    """
+
+    exchanged_document_context: ExchangedDocumentContext = element(
+        ns="rsm",
+        profiles={"factur-x": "minimum", "order-x": "basic"},
+        description="""A group of business terms providing information on the business
+        process and rules applicable to the Invoice document.""",
+    )
+    exchanged_document: ExchangedDocument = element(
+        ns="rsm",
+        profiles={"factur-x": "minimum", "order-x": "basic"},
+    )
+    supply_chain_trade_transaction: SupplyChainTradeTransaction = element(
+        ns="rsm",
+        profiles={"factur-x": "minimum", "order-x": "basic"},
+    )
+
+
+class CrossIndustryInvoice(
+    BaseXmlModel,
+    tag="CrossIndustryInvoice",
+    ns="rsm",
+    nsmap={
+        "rsm": "urn:un:unece:uncefact:data:standard:CrossIndustryInvoice:100",
+        "udt": "urn:un:unece:uncefact:data:standard:UnqualifiedDataType:100",
+        "qtd": "urn:un:unece:uncefact:data:standard:QualifiedDataType:100",
+        "ram": "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100",
+        "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    },
+):
+    """Root Factur-X and Order-X element.
+
+    nsmaps are fully definied here.
+    """
+
+    exchanged_document_context: ExchangedDocumentContext = element(
+        ns="rsm",
+        profiles={"factur-x": "minimum", "order-x": "basic"},
+        description="""A group of business terms providing information on the business
+        process and rules applicable to the Invoice document.""",
+    )
+    exchanged_document: ExchangedDocument = element(
+        ns="rsm",
+        profiles={"factur-x": "minimum", "order-x": "basic"},
+    )
+    supply_chain_trade_transaction: SupplyChainTradeTransaction = element(
+        ns="rsm",
+        profiles={"factur-x": "minimum", "order-x": "basic"},
+    )
+
+
+# Alias for the root element
+RootOrderX = SCRDMCCBDACIOMessageStructure
+RootFacturX = CrossIndustryInvoice
