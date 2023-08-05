ddf85c811930671b93795c2706dd6625eee609ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package pybi-next/0.3.20/pybi-next-0.3.20.tar.gz

---
+++ packages/pybi-next/pybi-next-0.3.20.tar.gz/pybi-next-0.3.20/pybi/core/components/__init__.py
@@ -0,0 +1,22 @@
+from pybi.core.components.componentTag import ComponentTag
+from pybi.core.components.staticComponent import (
+    TextComponent,
+    UploadComponent,
+    IconComponent,
+)
+from pybi.core.components.reactiveComponent import (
+    ReactiveComponent,
+    Slicer,
+    Table,
+    TextValue,
+    Markdown,
+)
+from pybi.core.components.containerComponent import (
+    ContainerComponent,
+    BoxComponent,
+    ColBoxComponent,
+    FlowBoxComponent,
+    GridBoxComponent,
+    TabsComponent,
+    AffixComponent,
+)
