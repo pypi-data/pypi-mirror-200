b0fbc0c59d819b423465fdd9b4d438c358c87ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:07 +0000

    Add package aiocoap/0.4.6a1/aiocoap-0.4.6a1.tar.gz

---
+++ packages/aiocoap/aiocoap-0.4.6a1.tar.gz/aiocoap-0.4.6a1/tests/test_protocol.py
@@ -0,0 +1,60 @@
+# This file is part of the Python aiocoap library project.
+#
+# Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
+#               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
+#
+# aiocoap is free software, this file is published under the MIT license as
+# described in the accompanying LICENSE file.
+
+from .fixtures import asynctest, no_warnings, WithAsyncLoop, WithLogMonitoring
+
+from aiocoap import Context
+
+class TestProtocolSetup(WithLogMonitoring, WithAsyncLoop):
+    """Tests that are only concerned with the setup of contexts, and the way
+    they can be set up."""
+
+    @no_warnings
+    @asynctest
+    async def test_empty_setup_shutdown(self):
+        ctx = await Context.create_client_context()
+        await ctx.shutdown()
+
+    # There is not yet a way to set things up in an async context manager.
+
+#     @no_warnings
+#     @asynctest
+#     async def test_empty_contextmgr(self):
+#         async with Context.create_client_context():
+#             pass
+
+    # The following tests should be converted to context managers once usable.
+
+    @no_warnings
+    @asynctest
+    async def test_multiple_contexts(self):
+        # Not that that'd be a regular thing to do, just checking it *can* be
+        # done
+        c1 = await Context.create_client_context()
+        c2 = await Context.create_client_context()
+        # None is an acceptable site; binding to a concrete port
+        # removes the worries of situations where the default
+        # transports can't bind to "any".
+        s1 = await Context.create_server_context(None, bind=("::1", None))
+
+        await c1.shutdown()
+        await s1.shutdown()
+        await c2.shutdown()
+
+    @no_warnings
+    @asynctest
+    async def test_serverports_no_conflict(self):
+        # When different ports are used, servers should not get into conflict.
+        #
+        # (To some extent, that this is so easy is the fault of the weird way
+        # the other protocols' ports are set for lack of configurability).
+        s1 = await Context.create_server_context(None, bind=("::1", 1234))
+        s2 = await Context.create_server_context(None, bind=("::1", None))
+
+        await s1.shutdown()
+        await s2.shutdown()
