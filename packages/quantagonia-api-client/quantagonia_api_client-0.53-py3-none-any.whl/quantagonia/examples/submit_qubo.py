eb245e571977d8eafbe99176a49e393cf9c9a268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:33 +0000

    Add package quantagonia-api-client/0.53/quantagonia_api_client-0.53-py3-none-any.whl

---
+++ packages/quantagonia-api-client/quantagonia_api_client-0.53-py3-none-any.whl/quantagonia/examples/submit_qubo.py
@@ -0,0 +1,16 @@
+import sys, os, os.path
+
+from quantagonia.enums import *
+from quantagonia.runner import Runner
+from quantagonia.runner_factory import RunnerFactory
+from quantagonia.spec_builder import QUBOSpecBuilder
+
+input_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "example.qubo")
+api_key = os.environ["QUANTAGONIA_API_KEY"]
+
+runner = RunnerFactory.getRunner(HybridSolverConnectionType.CLOUD,
+    api_key=api_key, server = HybridSolverServers.PROD)
+spec = QUBOSpecBuilder()
+spec.set_priority(PriorityEnum["HIGH"])
+spec.set_exclusivity(True)
+res_dict = runner.solve(input_file_path, spec.getd())
