8cd980a274abd2d7f3b1b6c65c0999ff0deb5b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package baca/0.1.7/baca-0.1.7.tar.gz

---
+++ packages/baca/baca-0.1.7.tar.gz/baca-0.1.7/src/baca/components/events.py
@@ -0,0 +1,23 @@
+from textual.message import Message
+
+
+class DoneLoading(Message):
+    def __init__(self, content):
+        super().__init__()
+        self.content = content
+
+
+class FollowThis(Message):
+    def __init__(self, nav_point: str):
+        super().__init__()
+        self.nav_point = nav_point
+
+
+class OpenThisImage(Message):
+    def __init__(self, value: str):
+        super().__init__()
+        self.value = value
+
+
+class Screenshot(Message):
+    pass
