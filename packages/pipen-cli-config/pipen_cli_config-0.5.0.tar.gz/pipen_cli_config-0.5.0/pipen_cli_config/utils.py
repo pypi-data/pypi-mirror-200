7173652b8a3531edca986aa367bdd7b57e7025f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:30 +0000

    Add package pipen-cli-config/0.5.0/pipen_cli_config-0.5.0.tar.gz

---
+++ packages/pipen-cli-config/pipen_cli_config-0.5.0.tar.gz/pipen_cli_config-0.5.0/pipen_cli_config/utils.py
@@ -0,0 +1,52 @@
+"""Utilities for pipen_cli_config to work on pipelines"""
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING, Callable, Type
+
+if TYPE_CHECKING:
+    from pipen import Proc
+
+from_pipen_cli_config = sys.argv[0] == "from-pipen-cli-config"
+
+MARK_CONTAINER = "__pipen_cli_config__"
+
+
+def get_mark(proc: Type[Proc], key: str):
+    """Get the marked attribute of the process
+
+    Args:
+        proc: The process
+        key: The attribute name
+
+    Returns:
+        The attribute value
+    """
+    return getattr(proc, MARK_CONTAINER, {}).get(key)
+
+
+def mark(**kwargs) -> Callable[[Type[Proc]], Type[Proc]]:
+    """Mark the process with some attributes to be used by pipen_cli_config
+
+    Args:
+        kwargs: The attributes to be marked
+
+    Marks:
+        no_input: Do not ask for input for the process even it is a
+            start process.
+            The cases could be that the input has been provided somewhere else
+            For example, options from a process group, or hard-coded in the
+            pipeline
+        hidden: Do not show the process in the UI
+
+    Returns:
+        A decorator for the process with the attributes marked
+    """
+    def decorator(proc: Type[Proc]):
+        if not hasattr(proc, MARK_CONTAINER):
+            setattr(proc, MARK_CONTAINER, {})
+        config = getattr(proc, MARK_CONTAINER)
+        config.update(kwargs)
+        return proc
+
+    return decorator
