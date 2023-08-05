ce2b820f9d7b09e2344184fa6c84ce243797fcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:48 +0000

    Add package pymisclib/1.3.1/pymisclib-1.3.1-py3-none-any.whl

---
+++ packages/pymisclib/pymisclib-1.3.1-py3-none-any.whl/pymisclib/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# vim ts=4,fileencoding=utf-8
+# SPDX-License-Identifier: Apache-2.0
+# SPDXID: pymisclib-1
+# PackageCopyrightText: © Copyright 2012-2023 by Christian Dönges <cd.pymisclib@platypus-projects.net >
+# PackageDownloadLocation: None
+# PackageHomePage: https://rot.regenbogenkraut.de/cd/pymisclib
+# PackageName: pymisclib
+# PackageOriginator: Originator: Platypus Projects GmbH
+# PackageSourceInfo: <text>uses pymisclib from https://rot.regenbogenkraut.de/cd/pymisclib.</text>
+# PackageSupplier: Christian Dönges (cd.pymisclib@platypus-projects.net )
+# PackageVersion: 1.3.0
+
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
