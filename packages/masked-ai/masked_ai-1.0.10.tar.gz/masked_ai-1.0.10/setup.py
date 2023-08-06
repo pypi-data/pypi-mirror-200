2311e34e2ef870c6bfefb1a07c64bcd1bf0c3607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:40 +0000

    Add package masked-ai/1.0.10/masked_ai-1.0.10.tar.gz

---
+++ packages/masked-ai/masked_ai-1.0.10.tar.gz/masked_ai-1.0.10/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+RELEASE_VERSION = os.environ.get('RELEASE_VERSION', None)
+
+print(f'Building version: {RELEASE_VERSION}')
+
+setup(
+    name='masked_ai',
+    version=RELEASE_VERSION,
+    description='Masked AI',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Cado Security',
+    author_email='maskedai@cadosecurity.com',
+    url='https://github.com/cado-security/masked-ai',
+    download_url='https://github.com/cado-security/masked-ai/archive/refs/heads/main.zip',
+    py_modules=['masked_ai'],
+    install_requires=['nltk'],
+    packages=find_packages()
+)
