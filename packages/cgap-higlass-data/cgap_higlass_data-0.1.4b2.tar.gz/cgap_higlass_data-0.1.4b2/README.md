18157d7527f61df401fbaa5d343cccf80e661dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:16 +0000

    Add package cgap-higlass-data/0.1.4b2/cgap_higlass_data-0.1.4b2.tar.gz

---
+++ packages/cgap-higlass-data/cgap_higlass_data-0.1.4b2.tar.gz/cgap_higlass_data-0.1.4b2/README.md
@@ -0,0 +1,19 @@
+
+# higlass-data
+Package that creates data files for CGAP's Higlass browsers
+
+## Installation
+
+Make sure `poetry` is installed on your system. Clone the repository and run `poetry install`
+
+## Run a script
+
+```
+# -i input file path
+# -o output file path
+# -q Shows logs when set to False
+poetry run create-cohort-vcf -i ./PATH/input.vcf -o ./PATH/input.multires.vcf -q False
+
+```
+
+
---
+++ packages/cgap-higlass-data/cgap_higlass_data-0.1.4b2.tar.gz/cgap_higlass_data-0.1.4b2/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['higlass_data', 'higlass_data.data']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyVCF3>=1.0.3,<2.0.0',
+ 'click>=8.1.3,<9.0.0',
+ 'importlib-resources>=5.12.0,<6.0.0',
+ 'negspy>=0.2.24,<0.3.0',
+ 'pandas>=1.4.3,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['convert-bed-to-bw = '
+                     'higlass_data.conversions:convert_bed_to_bw',
+                     'create-cohort-vcf = '
+                     'higlass_data.create_cohort_vcf:create_cohort_vcf',
+                     'create-coverage-bed = '
+                     'higlass_data.create_coverage_bed:create_coverage_bed']}
+
+setup_kwargs = {
+    'name': 'cgap-higlass-data',
+    'version': '0.1.4b2',
+    'description': "Data file generation for CGAP's Higlass browsers",
+    'long_description': "\n# higlass-data\nPackage that creates data files for CGAP's Higlass browsers\n\n## Installation\n\nMake sure `poetry` is installed on your system. Clone the repository and run `poetry install`\n\n## Run a script\n\n```\n# -i input file path\n# -o output file path\n# -q Shows logs when set to False\npoetry run create-cohort-vcf -i ./PATH/input.vcf -o ./PATH/input.multires.vcf -q False\n\n```\n\n\n",
+    'author': 'Alexander Veit',
+    'author_email': 'alexander_veit@hms.harvard.edu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/dbmi-bgm/higlass-data',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
