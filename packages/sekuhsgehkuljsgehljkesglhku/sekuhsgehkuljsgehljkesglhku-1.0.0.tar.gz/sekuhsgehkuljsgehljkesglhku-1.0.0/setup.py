e56bb236a5118b2f96f8ead6bd603b5c706a8391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:09 +0000

    Add package sekuhsgehkuljsgehljkesglhku/1.0.0/sekuhsgehkuljsgehljkesglhku-1.0.0.tar.gz

---
+++ packages/sekuhsgehkuljsgehljkesglhku/sekuhsgehkuljsgehljkesglhku-1.0.0.tar.gz/sekuhsgehkuljsgehljkesglhku-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = "kekw package"
+LONG_DESCRIPTION = "kekw package"
+
+# Setting up
+setup(
+    name="sekuhsgehkuljsgehljkesglhku",
+    version=VERSION,
+    author="StarlessGem",
+    author_email="StarlessGem@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
