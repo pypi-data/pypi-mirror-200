b0cbd80c8541d2a369a2c709f7894ef440ce53bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:20 +0000

    Add package cvgui/0.1.1/cvgui-0.1.1.tar.gz

---
+++ packages/cvgui/cvgui-0.1.1.tar.gz/cvgui-0.1.1/cvgui/inputs/computer_vision/__init__.py
@@ -0,0 +1,4 @@
+"""Implementations of frame inputs and computer vison models."""
+from .cv_model.blazepose import BlazePose  # noqa
+from .frame_input.webcam import Webcam  # noqa
+from .computer_vision import ComputerVisionPose  # noqa
