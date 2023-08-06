29164879ac7562eca81ea906806973f659cc8bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:59 +0000

    Add package FlagCapture/0.1.3/FlagCapture-0.1.3.tar.gz

---
+++ packages/FlagCapture/FlagCapture-0.1.3.tar.gz/FlagCapture-0.1.3/FlagCapture/__init__.py
@@ -0,0 +1,3 @@
+from FlagCapture import  miniworld
+
+__version__ = "0.0.5"
---
+++ packages/FlagCapture/FlagCapture-0.1.3.tar.gz/FlagCapture-0.1.3/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(zip_safe=True, version='0.1.3', description='Capture The Flag', long_description='Version 0.1.2 plus sliding on walls.', author='Michael Caggeso', author_email='laurencaggeso@gmail.com', license='Michael Licesce', name='FlagCapture', packages=['FlagCapture'])
