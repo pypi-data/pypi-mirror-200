2649daee2b93377ab63e64d0c73466b1693a02cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:53 +0000

    Add package snapalgo/0.0.6.6/snapalgo-0.0.6.6.tar.gz

---
+++ packages/snapalgo/snapalgo-0.0.6.6.tar.gz/snapalgo-0.0.6.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+  name='snapalgo',
+  version='0.0.6.6',
+  description='Various Coding algorithms',
+  long_description='Code generating code',
+  url='https://github.com/midnightbot/snapalgo',  
+  author='Anish Adnani',
+  author_email='anishadnani00@gmail.com',
+  license='MIT', 
+  keywords=['python', 'coding', 'template', 'algo', 'algorithms'],
+  packages=find_packages(),
+  install_requires=['pyperclip'] ,
+  classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
