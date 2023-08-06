f1ba92938220c5e7b9d4ae3d0213048375005544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:37 +0000

    Add package pydatatask/0.3.2/pydatatask-0.3.2-py3-none-any.whl

---
+++ packages/pydatatask/pydatatask-0.3.2-py3-none-any.whl/pydatatask/__init__.py
@@ -0,0 +1,15 @@
+"""
+Documentation for this module can be found at https://pydatatask.readthedocs.io/en/stable/
+"""
+
+from .repository import *
+from .task import *
+from .pipeline import *
+from .session import *
+from .main import *
+from .resource_manager import *
+from .pod_manager import *
+from .proc_manager import *
+
+__version__ = "0.3.2"
+released_version = "0.3.2"
