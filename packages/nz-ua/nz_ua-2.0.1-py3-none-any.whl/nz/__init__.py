e8160756af3531cce98f7f82ebece94a2b4a393c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:28 +0000

    Add package nz-ua/2.0.1/nz_ua-2.0.1-py3-none-any.whl

---
+++ packages/nz-ua/nz_ua-2.0.1-py3-none-any.whl/nz/__init__.py
@@ -0,0 +1,9 @@
+from .client import *
+from . import errors
+
+__title__ = "nz-ua"
+__version__ = "2.0.1"
+__description__ = "Library for working with the nz.ua service"
+__author__ = "GoldMasterPro"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 - 2023 Xsarz and 2023-present GoldMasterPro"
