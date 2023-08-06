ec6fc8c919f0dae3391f3d2dfa5326488a45a433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:20 +0000

    Add package telebot-components/0.7.3/telebot_components-0.7.3-py3-none-any.whl

---
+++ packages/telebot-components/telebot_components-0.7.3-py3-none-any.whl/telebot_components/feedback/trello_integration.py
@@ -0,0 +1,8 @@
+"""
+This module is kept for import compatibility only and will be removed in the future.
+
+Use telebot_components.feedback.integration.trello in new code.
+"""
+
+
+from telebot_components.feedback.integration.trello import *
