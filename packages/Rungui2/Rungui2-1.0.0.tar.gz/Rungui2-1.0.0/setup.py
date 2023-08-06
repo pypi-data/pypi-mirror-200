9bb576f7eba6686531ea29ea39c37270c43afc59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package Rungui2/1.0.0/Rungui2-1.0.0.tar.gz

---
+++ packages/Rungui2/Rungui2-1.0.0.tar.gz/Rungui2-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Rungui2',
+  version='1.0.0',
+  description='Anti Skid package to run a gui',
+  long_description="Just a cool and epic way to not get skidded (well hopefully sad face)",
+  url='https://fuck-lgbtq.com',
+  author='vesper',
+  author_email='balls@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='idk', 
+  packages=find_packages(),
+  install_requires=[]
+)
