9d781c7f1890a514f24920ed6e9fac855e65aea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:25 +0000

    Add package eisenmp/0.9/eisenmp-0.9.tar.gz

---
+++ packages/eisenmp/eisenmp-0.9.tar.gz/eisenmp-0.9/eisenmp/utils/eisenmp_constants.py
@@ -0,0 +1,15 @@
+""" eisenmp package constants definition, can override """
+
+# Package version
+VERSION = '0.1.0'
+# Generator part
+HEADER_MSG = 'GHETTO_BOSS'
+PERF_HEADER_ETA = 'PROC_PERF_ETA_'
+NUM_ROWS = 10_000
+STOP_MSG = 'STOP'
+STOP_PROCESS = 'STOP_PROCESS'
+STOP_CONFIRM = 'PROC_STOP_'
+GOT_RESULT_OUTPUT_Q = 'GHETTO_GANG_GOT'  # append line to result list and check, already red
+GOT_RESULT_RESULT_THREAD = 'RESULT_INTERIM_GOT'  # append line to result list and check
+RESULT_HEADER_PROC = 'PROC_RESULT_'
+# Worker part
