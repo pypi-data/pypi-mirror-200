6d35d64ffd0a3591f2aa505cb3c554c14c33aea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:50 +0000

    Add package HMSFire/0.3.11/HMSFire-0.3.11.tar.gz

---
+++ packages/HMSFire/HMSFire-0.3.11.tar.gz/HMSFire-0.3.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='HMSFire',
+    version='0.3.11',
+    license='MIT',
+    author="Joel",
+    author_email='joel.chacon@cimat.mx',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    description='This is a proof of concept package for Hazard Mapping System Fire Analysis',
+    long_description='**HMSFire** make searching, downloading and retrieving data of the Hazard Mapping System easy https://www.ospo.noaa.gov/Products/land/hms.html#maps/ This package is developed as an effor to offer data more accessible outside the atmospheric scientific community.',
+    url='https://ospo-hms-fire.readthedocs.io/en/latest/',
+    keywords='HMSFirepy project',
+    summary='test description',
+    package_data={'HMSFire': ['src/OSPO/HMSFire/*csv']},
+    include_package_data=True,
+    install_requires=[
+          'basemap',
+          'pandas',
+          'matplotlib',
+          'requests',
+          'datetime',
+      ],
+
+)
