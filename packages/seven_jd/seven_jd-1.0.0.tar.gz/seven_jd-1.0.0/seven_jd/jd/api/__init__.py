aa1a219c831b049edd4c335d24cd9f8f881817b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:52 +0000

    Add package seven_jd/1.0.0/seven_jd-1.0.0.tar.gz

---
+++ packages/seven_jd/seven_jd-1.0.0.tar.gz/seven_jd-1.0.0/seven_jd/jd/api/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: WangDe
+:Date: 2023-02-20 16:41:05
+:LastEditTime: 2023-02-20 17:23:59
+:LastEditors: WangDe
+:Description: 
+"""
+from seven_jd.jd.api.rest import *
+from seven_jd.jd.api.base import FileItem
