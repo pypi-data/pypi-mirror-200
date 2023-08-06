1e7fa8a9f401be6b38ce44f31ae109321b42ab86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:47 +0000

    Add package turbojet/0.0.1/turbojet-0.0.1.tar.gz

---
+++ packages/turbojet/turbojet-0.0.1.tar.gz/turbojet-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='turbojet',
+    version='0.0.1',
+    description='Biblioteka do liczenia silnikow odrzutowych',
+    url='https://github.com/JakUrb04/turbojet.git',
+    author='Jakub Urbaniak',
+    author_email='urbaniak627@gmail.com',
+    license='None license',
+    packages=['turbojet'],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
