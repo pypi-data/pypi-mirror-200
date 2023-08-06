c16da0494c14c2a7e09005ee8781ce41d3d16bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:58 +0000

    Add package albanerhello/0.0.0/albanerhello-0.0.0.tar.gz

---
+++ packages/albanerhello/albanerhello-0.0.0.tar.gz/albanerhello-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name="albanerhello",
+    author="albaner",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
