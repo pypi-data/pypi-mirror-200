b4549978bc2f241040925ed18ff23278d40b0ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:04 +0000

    Add package csvcubed/0.3.4/csvcubed-0.3.4-py3-none-any.whl

---
+++ packages/csvcubed/csvcubed-0.3.4-py3-none-any.whl/csvcubed/models/cube/catalog.py
@@ -0,0 +1,28 @@
+"""
+Catalog Metadata (base)
+-----------------------
+"""
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import Dict, Optional, Union
+
+from csvcubed.models.pydanticmodel import PydanticModel
+from csvcubed.models.validatedmodel import ValidatedModel, ValidationFunction
+from csvcubed.utils import validations as v
+
+
+@dataclass
+class CatalogMetadataBase(PydanticModel, ValidatedModel, ABC):
+    title: str
+
+    @abstractmethod
+    def get_description(self) -> Optional[str]:
+        pass
+
+    @abstractmethod
+    def get_issued(self) -> Union[datetime, date, None]:
+        pass
+
+    def _get_validations(self) -> Dict[str, ValidationFunction]:
+        return {"title": v.string}
---
+++ packages/csvcubed/csvcubed-0.3.4-py3-none-any.whl/csvcubed/models/cube/qb/catalog.py
@@ -0,0 +1,124 @@
+"""
+Catalog Metadata (DCAT)
+-----------------------
+"""
+import json
+import logging
+from dataclasses import dataclass, field
+from datetime import date, datetime, time
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+
+from csvcubedmodels.rdf import dcat
+
+from csvcubed.models.cube.catalog import CatalogMetadataBase
+from csvcubed.models.uriidentifiable import UriIdentifiable
+from csvcubed.models.validatedmodel import ValidatedModel, ValidationFunction
+from csvcubed.models.validationerror import ValidateModelPropertiesError
+from csvcubed.utils import validations as v
+from csvcubed.utils.validators.uri import validate_uri as pydantic_validate_uri
+from csvcubed.utils.validators.uri import (
+    validate_uris_in_list as pydantic_validate_uris_in_list,
+)
+
+_logger = logging.getLogger(__name__)
+
+
+@dataclass
+class CatalogMetadata(CatalogMetadataBase, UriIdentifiable):
+    identifier: Optional[str] = None
+    summary: Optional[str] = field(default=None, repr=False)
+    description: Optional[str] = field(default=None, repr=False)
+    creator_uri: Optional[str] = field(default=None, repr=False)
+    publisher_uri: Optional[str] = field(default=None, repr=False)
+    landing_page_uris: list[str] = field(default_factory=list, repr=False)
+    theme_uris: list[str] = field(default_factory=list, repr=False)
+    keywords: list[str] = field(default_factory=list, repr=False)
+    dataset_issued: Union[datetime, date, None] = field(default=None, repr=False)
+    dataset_modified: Union[datetime, date, None] = field(default=None, repr=False)
+    license_uri: Optional[str] = field(default=None, repr=False)
+    public_contact_point_uri: Optional[str] = field(default=None, repr=False)
+    uri_safe_identifier_override: Optional[str] = field(default=None, repr=False)
+    # spatial_bound_uri: Optional[str] = field(default=None, repr=False)
+    # temporal_bound_uri: Optional[str] = field(default=None, repr=False)
+
+    _creator_uri_validator = pydantic_validate_uri("creator_uri", is_optional=True)
+    _publisher_uri_validator = pydantic_validate_uri("publisher_uri", is_optional=True)
+    _landing_page_uris_validator = pydantic_validate_uris_in_list(
+        "landing_page_uris", is_optional=True
+    )
+    _license_uri_validator = pydantic_validate_uri("license_uri", is_optional=True)
+    # _spatial_bound_uri_validator = validate_uri("spatial_bound_uri", is_optional=True)
+    # _temporal_bound_uri_validator = validate_uri("temporal_bound_uri", is_optional=True)
+    _public_contact_point_uri_validator = pydantic_validate_uri(
+        "public_contact_point_uri", is_optional=True
+    )
+
+    def _get_validations(self) -> Dict[str, ValidationFunction]:
+        return {
+            **CatalogMetadataBase._get_validations(self),
+            "identifier": v.optional(v.string),
+            "summary": v.optional(v.string),
+            "description": v.optional(v.string),
+            "creator_uri": v.optional(v.uri),
+            "publisher_uri": v.optional(v.uri),
+            "landing_page_uris": v.list(v.uri),
+            "theme_uris": v.list(v.uri),
+            "keywords": v.list(v.string),
+            "dataset_issued": v.optional(
+                v.any_of(v.is_instance_of(date), v.is_instance_of(datetime))
+            ),
+            "dataset_modified": v.optional(
+                v.any_of(v.is_instance_of(date), v.is_instance_of(datetime))
+            ),
+            "license_uri": v.optional(v.uri),
+            "public_contact_point_uri": v.optional(v.uri),
+            **UriIdentifiable._get_validations(self),
+        }
+
+    def to_json_file(self, file_path: Path) -> None:
+        with open(file_path, "w+") as f:
+            json.dump(self.as_json_dict(), f, indent=4)
+
+    @classmethod
+    def from_json_file(cls, file_path: Path) -> "CatalogMetadata":
+        with open(file_path, "r") as f:
+            dict_structure = json.load(f)
+
+        return cls.from_dict(dict_structure)
+
+    def get_issued(self) -> Union[datetime, date, None]:
+        return self.dataset_issued
+
+    def get_description(self) -> Optional[str]:
+        return self.description
+
+    def get_identifier(self) -> str:
+        return self.identifier or self.title
+
+    def configure_dcat_dataset(self, dataset: dcat.Dataset) -> None:
+        dt_now = datetime.now()
+        dt_issued = _convert_date_to_date_time(self.dataset_issued or dt_now)
+
+        dataset.label = dataset.title = self.title
+        dataset.issued = dt_issued
+        dataset.modified = _convert_date_to_date_time(
+            self.dataset_modified or dt_issued
+        )
+        dataset.comment = self.summary
+        dataset.description = self.description
+        dataset.license = self.license_uri
+        dataset.creator = self.creator_uri
+        dataset.publisher = self.publisher_uri
+        dataset.landing_page = set(self.landing_page_uris)
+        dataset.themes = set(self.theme_uris)
+        dataset.keywords = set(self.keywords)
+        dataset.contact_point = self.public_contact_point_uri
+        dataset.identifier = self.get_identifier()
+
+
+def _convert_date_to_date_time(dt: Union[datetime, date]) -> datetime:
+    if isinstance(dt, date) and not isinstance(dt, datetime):
+        return datetime.combine(dt, time.min)
+
+    return dt
