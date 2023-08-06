ae26cebc1ae938f978591165af0864d1a754298c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:05 +0000

    Add package httpdbg/0.8.3/httpdbg-0.8.3-py3-none-any.whl

---
+++ packages/httpdbg/httpdbg-0.8.3-py3-none-any.whl/httpdbg/server.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from contextlib import contextmanager
+from http.server import HTTPServer
+import threading
+
+from httpdbg.records import HTTPRecords
+from httpdbg.webapp import HttpbgHTTPRequestHandler
+
+
+@contextmanager
+def httpdbg_srv(port):
+    server = None
+    records = HTTPRecords()
+    try:
+        server = ServerThread(port, records)
+        server.start()
+
+        yield records
+
+        server.shutdown()
+    except Exception as ex:
+        if server:
+            server.shutdown()
+        raise ex
+
+
+class ServerThread(threading.Thread):
+    def __init__(self, port, records):
+        threading.Thread.__init__(self)
+        self.port = port
+
+        def http_request_handler(*args, **kwargs):
+            HttpbgHTTPRequestHandler(records, *args, **kwargs)
+
+        self.srv = HTTPServer(("localhost", port), http_request_handler)
+
+    def run(self):
+        self.srv.serve_forever()
+
+    def shutdown(self):
+        self.srv.shutdown()
