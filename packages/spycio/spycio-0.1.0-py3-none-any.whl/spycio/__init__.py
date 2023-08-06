b386ec393c03ff7476de130b6ffa9859fa4ef5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:05 +0000

    Add package spycio/0.1.0/spycio-0.1.0-py3-none-any.whl

---
+++ packages/spycio/spycio-0.1.0-py3-none-any.whl/spycio/__init__.py
@@ -0,0 +1,10 @@
+""" Package Eule  """
+
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from .spycio import distance
+
+__author__ = """Bruno Peixoto"""
+__email__ = 'brunolnetto@gmail.com'
