07b9da1a38af3d66bc903503b1547de2b896bef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:26 +0000

    Add package seisbench/0.3.2/seisbench-0.3.2.tar.gz

---
+++ packages/seisbench/seisbench-0.3.2.tar.gz/seisbench-0.3.2/seisbench/models/phasenet.py
@@ -0,0 +1,344 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .base import Conv1dSame, WaveformModel
+
+
+class PhaseNet(WaveformModel):
+    """
+    .. document_args:: seisbench.models PhaseNet
+    """
+
+    _annotate_args = WaveformModel._annotate_args.copy()
+    _annotate_args["*_threshold"] = ("Detection threshold for the provided phase", 0.3)
+    _annotate_args["blinding"] = (
+        "Number of prediction samples to discard on each side of each window prediction",
+        (0, 0),
+    )
+    _annotate_args["overlap"] = (_annotate_args["overlap"][0], 1500)
+
+    _weight_warnings = [
+        (
+            "ethz|geofon|instance|iquique|lendb|neic|scedc|stead",
+            "1",
+            "The normalization for this weight version is incorrect and will lead to degraded performance. "
+            "Run from_pretrained with update=True once to solve this issue. "
+            "For details, see https://github.com/seisbench/seisbench/pull/188 .",
+        ),
+    ]
+
+    def __init__(
+        self,
+        in_channels=3,
+        classes=3,
+        phases="NPS",
+        sampling_rate=100,
+        norm="std",
+        **kwargs,
+    ):
+        citation = (
+            "Zhu, W., & Beroza, G. C. (2019). "
+            "PhaseNet: a deep-neural-network-based seismic arrival-time picking method. "
+            "Geophysical Journal International, 216(1), 261-273. "
+            "https://doi.org/10.1093/gji/ggy423"
+        )
+
+        super().__init__(
+            citation=citation,
+            in_samples=3001,
+            output_type="array",
+            pred_sample=(0, 3001),
+            labels=phases,
+            sampling_rate=sampling_rate,
+            **kwargs,
+        )
+
+        self.in_channels = in_channels
+        self.classes = classes
+        self.norm = norm
+        self.depth = 5
+        self.kernel_size = 7
+        self.stride = 4
+        self.filters_root = 8
+        self.activation = torch.relu
+
+        self.inc = nn.Conv1d(
+            self.in_channels, self.filters_root, self.kernel_size, padding="same"
+        )
+        self.in_bn = nn.BatchNorm1d(8, eps=1e-3)
+
+        self.down_branch = nn.ModuleList()
+        self.up_branch = nn.ModuleList()
+
+        last_filters = self.filters_root
+        for i in range(self.depth):
+            filters = int(2**i * self.filters_root)
+            conv_same = nn.Conv1d(
+                last_filters, filters, self.kernel_size, padding="same", bias=False
+            )
+            last_filters = filters
+            bn1 = nn.BatchNorm1d(filters, eps=1e-3)
+            if i == self.depth - 1:
+                conv_down = None
+                bn2 = None
+            else:
+                if i in [1, 2, 3]:
+                    padding = 0  # Pad manually
+                else:
+                    padding = self.kernel_size // 2
+                conv_down = nn.Conv1d(
+                    filters,
+                    filters,
+                    self.kernel_size,
+                    self.stride,
+                    padding=padding,
+                    bias=False,
+                )
+                bn2 = nn.BatchNorm1d(filters, eps=1e-3)
+
+            self.down_branch.append(nn.ModuleList([conv_same, bn1, conv_down, bn2]))
+
+        for i in range(self.depth - 1):
+            filters = int(2 ** (3 - i) * self.filters_root)
+            conv_up = nn.ConvTranspose1d(
+                last_filters, filters, self.kernel_size, self.stride, bias=False
+            )
+            last_filters = filters
+            bn1 = nn.BatchNorm1d(filters, eps=1e-3)
+            conv_same = nn.Conv1d(
+                2 * filters, filters, self.kernel_size, padding="same", bias=False
+            )
+            bn2 = nn.BatchNorm1d(filters, eps=1e-3)
+
+            self.up_branch.append(nn.ModuleList([conv_up, bn1, conv_same, bn2]))
+
+        self.out = nn.Conv1d(last_filters, self.classes, 1, padding="same")
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x, logits=False):
+        x = self.activation(self.in_bn(self.inc(x)))
+
+        skips = []
+        for i, (conv_same, bn1, conv_down, bn2) in enumerate(self.down_branch):
+            x = self.activation(bn1(conv_same(x)))
+
+            if conv_down is not None:
+                skips.append(x)
+                if i == 1:
+                    x = F.pad(x, (2, 3), "constant", 0)
+                elif i == 2:
+                    x = F.pad(x, (1, 3), "constant", 0)
+                elif i == 3:
+                    x = F.pad(x, (2, 3), "constant", 0)
+
+                x = self.activation(bn2(conv_down(x)))
+
+        for i, ((conv_up, bn1, conv_same, bn2), skip) in enumerate(
+            zip(self.up_branch, skips[::-1])
+        ):
+            x = self.activation(bn1(conv_up(x)))
+            x = x[:, :, 1:-2]
+
+            x = self._merge_skip(skip, x)
+            x = self.activation(bn2(conv_same(x)))
+
+        x = self.out(x)
+        if logits:
+            return x
+        else:
+            return self.softmax(x)
+
+    @staticmethod
+    def _merge_skip(skip, x):
+        offset = (x.shape[-1] - skip.shape[-1]) // 2
+        x_resize = x[:, :, offset : offset + skip.shape[-1]]
+
+        return torch.cat([skip, x_resize], dim=1)
+
+    def annotate_window_pre(self, window, argdict):
+        # Add a demean and normalize step to the preprocessing
+        window = window - np.mean(window, axis=-1, keepdims=True)
+
+        if self.norm == "std":
+            std = np.std(window, axis=-1, keepdims=True)
+            std[std == 0] = 1  # Avoid NaN errors
+            window = window / std
+        elif self.norm == "peak":
+            peak = np.max(np.abs(window), axis=-1, keepdims=True) + 1e-10
+            window = window / peak
+
+        return window
+
+    def annotate_window_post(self, pred, piggyback=None, argdict=None):
+        # Transpose predictions to correct shape
+        pred = pred.T
+        prenan, postnan = argdict.get(
+            "blinding", self._annotate_args.get("blinding")[1]
+        )
+        if prenan > 0:
+            pred[:prenan] = np.nan
+        if postnan > 0:
+            pred[-postnan:] = np.nan
+        return pred
+
+    def classify_aggregate(self, annotations, argdict):
+        """
+        Converts the annotations to discrete thresholds using
+        :py:func:`~seisbench.models.base.WaveformModel.picks_from_annotations`.
+        Trigger onset thresholds for picks are derived from the argdict at keys "[phase]_threshold".
+
+        :param annotations: See description in superclass
+        :param argdict: See description in superclass
+        :return: List of picks
+        """
+        picks = []
+        for phase in self.labels:
+            if phase == "N":
+                # Don't pick noise
+                continue
+
+            picks += self.picks_from_annotations(
+                annotations.select(channel=f"{self.__class__.__name__}_{phase}"),
+                argdict.get(
+                    f"{phase}_threshold", self._annotate_args.get("*_threshold")[1]
+                ),
+                phase,
+            )
+
+        return sorted(picks)
+
+    def get_model_args(self):
+        model_args = super().get_model_args()
+        for key in [
+            "citation",
+            "in_samples",
+            "output_type",
+            "default_args",
+            "pred_sample",
+            "labels",
+            "sampling_rate",
+        ]:
+            del model_args[key]
+
+        model_args["in_channels"] = self.in_channels
+        model_args["classes"] = self.classes
+        model_args["phases"] = self.labels
+        model_args["sampling_rate"] = self.sampling_rate
+
+        return model_args
+
+
+class PhaseNetLight(PhaseNet):
+    """
+    .. document_args:: seisbench.models PhaseNetLight
+
+    PhaseNetLight is a slightly reduced version of PhaseNet. It is primarily included for compatibility reasons with
+    an earlier, incomplete implementation of PhaseNet in SeisBench prior to v0.3.
+    """
+
+    _weight_warnings = [
+        (
+            "ethz|geofon|instance|iquique|lendb|neic|scedc|stead",
+            "1",
+            "The normalization for this weight version is incorrect and will lead to degraded performance. "
+            "Run from_pretrained with update=True once to solve this issue. "
+            "For details, see https://github.com/seisbench/seisbench/pull/188 .",
+        ),
+    ]
+
+    def __init__(
+        self,
+        in_channels=3,
+        classes=3,
+        phases="NPS",
+        sampling_rate=100,
+        norm="std",
+        **kwargs,
+    ):
+        citation = (
+            "Zhu, W., & Beroza, G. C. (2019). "
+            "PhaseNet: a deep-neural-network-based seismic arrival-time picking method. "
+            "Geophysical Journal International, 216(1), 261-273. "
+            "https://doi.org/10.1093/gji/ggy423"
+        )
+
+        # Skip super call in favour of super-super class
+        WaveformModel.__init__(
+            self,
+            citation=citation,
+            in_samples=3001,
+            output_type="array",
+            pred_sample=(0, 3001),
+            labels=phases,
+            sampling_rate=sampling_rate,
+            **kwargs,
+        )
+
+        self.in_channels = in_channels
+        self.classes = classes
+        self.norm = norm
+        self.kernel_size = 7
+        self.stride = 4
+        self.activation = torch.relu
+
+        self.inc = nn.Conv1d(self.in_channels, 8, 1)
+        self.in_bn = nn.BatchNorm1d(8)
+
+        self.conv1 = Conv1dSame(8, 11, self.kernel_size, self.stride)
+        self.bnd1 = nn.BatchNorm1d(11)
+
+        self.conv2 = Conv1dSame(11, 16, self.kernel_size, self.stride)
+        self.bnd2 = nn.BatchNorm1d(16)
+
+        self.conv3 = Conv1dSame(16, 22, self.kernel_size, self.stride)
+        self.bnd3 = nn.BatchNorm1d(22)
+
+        self.conv4 = Conv1dSame(22, 32, self.kernel_size, self.stride)
+        self.bnd4 = nn.BatchNorm1d(32)
+
+        self.up1 = nn.ConvTranspose1d(
+            32, 22, self.kernel_size, self.stride, padding=self.conv4.padding
+        )
+        self.bnu1 = nn.BatchNorm1d(22)
+
+        self.up2 = nn.ConvTranspose1d(
+            44,
+            16,
+            self.kernel_size,
+            self.stride,
+            padding=self.conv3.padding,
+            output_padding=1,
+        )
+        self.bnu2 = nn.BatchNorm1d(16)
+
+        self.up3 = nn.ConvTranspose1d(
+            32, 11, self.kernel_size, self.stride, padding=self.conv2.padding
+        )
+        self.bnu3 = nn.BatchNorm1d(11)
+
+        self.up4 = nn.ConvTranspose1d(22, 8, self.kernel_size, self.stride, padding=3)
+        self.bnu4 = nn.BatchNorm1d(8)
+
+        self.out = nn.ConvTranspose1d(16, self.classes, 1)
+        self.softmax = torch.nn.Softmax(dim=1)
+
+    def forward(self, x, logits=False):
+        x_in = self.activation(self.in_bn(self.inc(x)))
+
+        x1 = self.activation(self.bnd1(self.conv1(x_in)))
+        x2 = self.activation(self.bnd2(self.conv2(x1)))
+        x3 = self.activation(self.bnd3(self.conv3(x2)))
+        x4 = self.activation(self.bnd4(self.conv4(x3)))
+
+        x = torch.cat([self.activation(self.bnu1(self.up1(x4))), x3], dim=1)
+        x = torch.cat([self.activation(self.bnu2(self.up2(x))), x2], dim=1)
+        x = torch.cat([self.activation(self.bnu3(self.up3(x))), x1], dim=1)
+        x = torch.cat([self.activation(self.bnu4(self.up4(x))), x_in], dim=1)
+
+        x = self.out(x)
+        if logits:
+            return x
+        else:
+            return self.softmax(x)
