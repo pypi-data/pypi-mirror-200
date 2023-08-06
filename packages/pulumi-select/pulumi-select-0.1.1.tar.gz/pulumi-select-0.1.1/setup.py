35114be6594e22527891fd92fd313fb8d31a0700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:04 +0000

    Add package pulumi-select/0.1.1/pulumi-select-0.1.1.tar.gz

---
+++ packages/pulumi-select/pulumi-select-0.1.1.tar.gz/pulumi-select-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+setup(
+  name = 'pulumi-select',
+  packages = ['pulumi-select'],
+  version = '0.1.1', 
+  license='MIT',
+  description = 'dynamically select urns to apply from a pulumi preview',
+  author = 'romainrbr',
+  author_email = 'contact@romain.tech',
+  url = 'https://github.com/romainrbr/pulumi-select',
+  download_url = 'https://github.com/romainrbr/pulumi-select/archive/v_01.tar.gz',
+  keywords = ['pulumi', 'select'],
+  install_requires=[            
+          'blessed',
+          'click',
+          'click-spinner',
+          'inquirer',
+          'python-editor',
+          'readchar',
+          'six',
+          'wcwidth'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',    
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+  ],
+)
