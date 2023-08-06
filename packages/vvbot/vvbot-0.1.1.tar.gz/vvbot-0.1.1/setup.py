6b6167007b212313ded508766def41739df142c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:48 +0000

    Add package vvbot/0.1.1/vvbot-0.1.1.tar.gz

---
+++ packages/vvbot/vvbot-0.1.1.tar.gz/vvbot-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vvbot",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
