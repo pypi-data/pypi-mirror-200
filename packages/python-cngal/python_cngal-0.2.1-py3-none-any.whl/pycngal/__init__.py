e7a70ca4ba3f20960b780ffcadc691043df3f329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:38 +0000

    Add package python-cngal/0.2.1/python_cngal-0.2.1-py3-none-any.whl

---
+++ packages/python-cngal/python_cngal-0.2.1-py3-none-any.whl/pycngal/__init__.py
@@ -0,0 +1,59 @@
+import json
+import os
+from pprint import pprint
+
+import requests
+
+from pycngal.const import api_endpoint, headers
+
+
+def no_proxy(domain: str) -> None:
+    # If meet with this Error like this:
+    # requests.exceptions.SSLError: HTTPSConnectionPool(host='https://domain', port=443): Max retries exceeded with url:
+    # (Caused by SSLError(SSLEOFError(8, 'EOF occurred in violation of protocol (_ssl.c:997)')))
+    os.environ["NO_PROXY"] = domain
+
+
+def request_swagger(format="json") -> Optional[dict]:
+    if format == "json":
+        return json.loads(
+            requests.get(
+                url="https://api.cngal.org/swagger/v1/swagger.json",
+                headers=headers,
+            ).text
+        )
+    elif format == "html":
+        import webbrowser
+
+        webbrowser.open("https://api.cngal.org/swagger/index.html")
+
+
+def request_api(api_name: str) -> dict:
+    return json.loads(
+        requests.get(url=api_endpoint + api_name, headers=headers).text
+    )
+
+
+def request_data_summary(tab="games"):
+    if tab == "games":
+        url = api_endpoint + "/api/tables/GetBasicInforList"
+    elif tab == "group":
+        url = api_endpoint + "/api/tables/GetGroupInforList"
+    elif tab == "staff":
+        url = api_endpoint + "/api/tables/GetStaffInforList"
+    elif tab == "maker":
+        url = api_endpoint + "/api/tables/GetMakerInforList"
+    elif tab == "character":
+        url = api_endpoint + "/api/tables/GetRoleInforList"
+    elif tab == "price":
+        url = api_endpoint + "/api/tables/GetSteamInforList"
+    elif tab == "score":
+        url = api_endpoint + "/api/tables/GetGameScoreList"
+    else:
+        print("数据汇总就这些分类没有其他的啦！写BUG了吧！")
+    result_json = json.loads(requests.get(url=url, headers=headers).text)
+    pprint(result_json)
+
+
+def beep():
+    print("CnGal 中文GalGame资料站")  # from <title> of https://app.cngal.org/
