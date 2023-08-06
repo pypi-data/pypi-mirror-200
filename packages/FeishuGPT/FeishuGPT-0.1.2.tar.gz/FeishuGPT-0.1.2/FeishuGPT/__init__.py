2c71fdae84549c91e20e503fb75369bb3c5a81e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:14 +0000

    Add package FeishuGPT/0.1.2/FeishuGPT-0.1.2.tar.gz

---
+++ packages/FeishuGPT/FeishuGPT-0.1.2.tar.gz/FeishuGPT-0.1.2/FeishuGPT/__init__.py
@@ -0,0 +1,15 @@
+from .challenge import pass_challenge
+
+__all__ = [
+    'gpt_3_5_v1',
+    'gpt_4_v1',
+    'pass_challenge'
+]
+
+
+__version__ = '0.1.2'
+__license__ = 'GPL-2.0 License'
+__author__ = 'Tiancheng Jiao'
+__url__ = 'https://github.com/jtc1246/FeishuGPT'
+__author_email__ = 'jtc1246@outlook.com'
+__description__ = '通过飞书机器人使用 ChatGPT'
