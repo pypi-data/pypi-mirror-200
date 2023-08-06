039932daa0cfe65e3f3c254fdaceb892e4527cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:17 +0000

    Add package awsservice/0.0.3/awsservice-0.0.3.tar.gz

---
+++ packages/awsservice/awsservice-0.0.3.tar.gz/awsservice-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import markdown
+
+with open("README.md", 'r') as file:
+    lon_descr = file.read()
+
+long_description = markdown.markdown(lon_descr)
+
+setup(
+    name='awsservice',
+    version='0.0.3',
+    description= "A small example package for AWS: S3, EC2, CloudWatch",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    author='Vitalik Kost',
+    author_email='vitalii.kostyreva@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/Vitalikys/AWS_s3_app',
+    install_requires=[
+        'boto3',
+        'pytz',
+        'botocore',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
