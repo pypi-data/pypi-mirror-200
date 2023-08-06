2f5a31471511d9950546b4c7a169d0cffee00ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:33 +0000

    Add package felupe/6.4.0/felupe-6.4.0.tar.gz

---
+++ packages/felupe/felupe-6.4.0.tar.gz/felupe-6.4.0/src/felupe/tools/__init__.py
@@ -0,0 +1,8 @@
+from ._check import check
+from ._newton import fun_items as fun
+from ._newton import jac_items as jac
+from ._newton import newtonrhapson
+from ._post import curve, force, moment
+from ._project import project, topoints
+from ._save import save
+from ._solve import solve
