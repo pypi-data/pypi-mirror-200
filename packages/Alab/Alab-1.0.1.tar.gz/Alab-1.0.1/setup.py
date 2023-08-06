63b6f37cd286f121604bf525b93f2d6bdd0b730e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:22 +0000

    Add package Alab/1.0.1/Alab-1.0.1.tar.gz

---
+++ packages/Alab/Alab-1.0.1.tar.gz/Alab-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Alab',
+    version='1.0.1',
+    description='Your package description',
+    author='Adan.Y',
+    author_email='Adan.Y@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
