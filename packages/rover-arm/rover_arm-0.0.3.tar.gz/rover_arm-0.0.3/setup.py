ae361823ae0463f5679c98aaf8becd9ff556dd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:23 +0000

    Add package rover-arm/0.0.3/rover_arm-0.0.3.tar.gz

---
+++ packages/rover-arm/rover_arm-0.0.3.tar.gz/rover_arm-0.0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name='rover_arm',
+    version='0.0.3',
+    description="A OpenAI Gym Env for Rover with Arm",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    packages = setuptools.find_packages(include="rover_arm*"),
+    install_requires=['gym', 'pybullet']  # And any other dependencies foo needs
+)
