faa71841990f72e4a0f9a631317c42f971aa4393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:33 +0000

    Add package filegpt/0.1.7/filegpt-0.1.7.tar.gz

---
+++ packages/filegpt/filegpt-0.1.7.tar.gz/filegpt-0.1.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="filegpt",
+    version="0.1.7",
+    description="A tool for autocompleting text files using OpenAI's GPT models.",
+    long_description= open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bachittle/FileGPT",
+    author="Bailey Chittle",
+    author_email="bachittle@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["filegpt"],
+    install_requires=[
+        "openai",
+        "tiktoken"
+    ],
+    entry_points={
+        "console_scripts": [
+            "filegpt = filegpt.FileGPT:main"
+        ]
+    },
+)
