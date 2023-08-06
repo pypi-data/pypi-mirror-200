0314e0b8e11851ec98f3f5a702143db425122295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:40 +0000

    Add package progression-qc/0.3.1.2/progression_qc-0.3.1.2.tar.gz

---
+++ packages/progression-qc/progression_qc-0.3.1.2.tar.gz/progression_qc-0.3.1.2/progression_qc/schemas/question_base.py
@@ -0,0 +1,12 @@
+{
+    "type": {"required": True, "type": "string", "regex": "(?i)^(prog|sys|bd|vcs)$"},
+    "niveau": {"required": False, "type": "string"},
+    "titre": {"required": False, "type": "string"},
+    "objectif": {"required": False, "type": "string"},
+    "description": {"required": False, "type": "string"},
+    "énoncé": {"required": False, "type": "string"},
+    "auteur": {"required": False, "type": "string"},
+    "licence": {"required": False, "type": "string"},
+    "rétroactions": {"required": False, "type": "dict", "schema": "rétroactions"},
+    "ttl": {"required": False, "type": "integer"},
+}
