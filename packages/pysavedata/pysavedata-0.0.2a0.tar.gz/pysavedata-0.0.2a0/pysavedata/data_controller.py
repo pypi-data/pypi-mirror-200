ddb1ae8fad3155f13791948bc2945a904a2ad634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:07 +0000

    Add package pysavedata/0.0.2a0/pysavedata-0.0.2a0.tar.gz

---
+++ packages/pysavedata/pysavedata-0.0.2a0.tar.gz/pysavedata-0.0.2a0/pysavedata/data_controller.py
@@ -0,0 +1,31 @@
+from typing import Any, NoReturn
+import os
+
+from .dataparser import DataParser
+
+
+class DataController:
+    def __init__(self, saves_folder_path: str) -> None:
+        self._saves_path = saves_folder_path
+
+        os.makedirs(self._saves_path, exist_ok=True)
+
+    def get_all_saves_list(self) -> list[Any]:
+        list_dir = os.listdir(self._saves_path)
+        classes_list = []
+
+        for save in list_dir:
+            classes_list.append(DataParser(f'{self._saves_path}/{save}').parse_file())
+
+        return classes_list
+
+    def change_saves_path(self, new_path: str, copy_old: bool = False) -> None | NoReturn:
+        pass
+
+    def save_class(
+        self, target_class: Any, save_name: str = ...
+    ) -> bool | NoReturn:  # type(...) == ellipsis
+        pass
+
+    def load_save(self, save_name: str) -> Any:
+        pass
