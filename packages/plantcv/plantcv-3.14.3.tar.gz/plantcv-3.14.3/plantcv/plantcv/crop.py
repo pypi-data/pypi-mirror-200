00563686d00cccafb140ac4f966456e64ad7fb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:50 +0000

    Add package plantcv/3.14.3/plantcv-3.14.3.tar.gz

---
+++ packages/plantcv/plantcv-3.14.3.tar.gz/plantcv-3.14.3/plantcv/plantcv/crop.py
@@ -0,0 +1,48 @@
+# Crop
+
+import os
+import cv2
+import numpy as np
+from plantcv.plantcv._debug import _debug
+from plantcv.plantcv import params
+
+
+def crop(img, x, y, h, w):
+    """Crop image.
+
+    Inputs:
+    img       = RGB, grayscale, or hyperspectral image data
+    x         = X coordinate of starting point
+    y         = Y coordinate of starting point
+    h         = Height
+    w         = Width
+
+    Returns:
+    cropped   = cropped image
+
+    :param img: numpy.ndarray
+    :param x: int
+    :param y: int
+    :param h: int
+    :param w: int
+    :return cropped: numpy.ndarray
+    """
+    # Check if the array data format
+    if len(np.shape(img)) > 2 and np.shape(img)[-1] > 3:
+        ref_img = img[:, :, [0]]
+        ref_img = np.transpose(np.transpose(ref_img)[0])
+        cropped = img[y:y + h, x:x + w, :]
+    else:
+        ref_img = np.copy(img)
+        cropped = img[y:y + h, x:x + w]
+
+    # Create the rectangle contour vertices
+    pt1 = (x, y)
+    pt2 = (x + w - 1, y + h - 1)
+
+    ref_img = cv2.rectangle(img=ref_img, pt1=pt1, pt2=pt2, color=(255, 0, 0), thickness=params.line_thickness)
+
+    _debug(visual=ref_img,
+           filename=os.path.join(params.debug_outdir, str(params.device) + "_crop.png"))
+
+    return cropped
---
+++ packages/plantcv/plantcv-3.14.3.tar.gz/plantcv-3.14.3/plantcv/plantcv/threshold/__init__.py
@@ -0,0 +1,11 @@
+from plantcv.plantcv.threshold.threshold_methods import binary
+from plantcv.plantcv.threshold.threshold_methods import gaussian
+from plantcv.plantcv.threshold.threshold_methods import mean
+from plantcv.plantcv.threshold.threshold_methods import otsu
+from plantcv.plantcv.threshold.threshold_methods import triangle
+from plantcv.plantcv.threshold.threshold_methods import texture
+from plantcv.plantcv.threshold.threshold_methods import custom_range
+from plantcv.plantcv.threshold.threshold_methods import saturation
+from plantcv.plantcv.threshold.threshold_methods import mask_bad
+
+__all__ = ["binary", "gaussian", "mean", "otsu", "triangle", "texture", "custom_range", "saturation", "mask_bad"]
