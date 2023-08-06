930c403d81150f9a2ff2ddd4c1a1f7ca0625d2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:39 +0000

    Add package omarlibone/1.1/omarlibone-1.1.tar.gz

---
+++ packages/omarlibone/omarlibone-1.1.tar.gz/omarlibone-1.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="omarlibone",
+    version="1.1",
+    author="omar Style",
+    description="omar aAAA",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3 ",
+    "Operating System :: OS Independent",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License"
+    ]
+    
+)
