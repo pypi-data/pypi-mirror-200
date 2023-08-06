04765b622637d5842097f902e6b6c538cdf453d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:25 +0000

    Add package astrodown/0.1.7/astrodown-0.1.7.tar.gz

---
+++ packages/astrodown/astrodown-0.1.7.tar.gz/astrodown-0.1.7/astrodown/utils.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def file_ext(filename: str) -> str:
+    return filename.split(".")[-1]
+
+
+def enpath(path: str | Path) -> Path:
+    return Path(path) if isinstance(path, str) else path
+
+
