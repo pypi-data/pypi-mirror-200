551b8ea873e37761057776908991dd75e0cd04fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:31 +0000

    Add package pitricks/0.1.3a1/pitricks-0.1.3a1.tar.gz

---
+++ packages/pitricks/pitricks-0.1.3a1.tar.gz/pitricks-0.1.3a1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+  name='pitricks',
+  version='0.1.3a1',
+  author='pyy',
+  url='https://github.com/one-pyy/pitricks',
+  packages=find_packages(),
+  requires=['regex', 'rich'],
+  description='a tool box'
+)
