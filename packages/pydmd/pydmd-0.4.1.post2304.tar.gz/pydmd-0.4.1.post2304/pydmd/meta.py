f257e87234efacb8ab755062d9fe6c7b4525b44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:11 +0000

    Add package pydmd/0.4.1.post2304/pydmd-0.4.1.post2304.tar.gz

---
+++ packages/pydmd/pydmd-0.4.1.post2304.tar.gz/pydmd-0.4.1.post2304/pydmd/meta.py
@@ -0,0 +1,23 @@
+__all__ = [
+    "__project__",
+    "__title__",
+    "__author__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__mail__",
+    "__maintainer__",
+    "__status__",
+]
+
+__project__ = "PyDMD"
+__title__ = "pydmd"
+__author__ = "Nicola Demo, Marco Tezzele, Francesco Andreuzzi"
+__copyright__ = "Copyright 2017-2023, PyDMD contributors"
+__license__ = "MIT"
+__version__ = "0.4.1.post2304"
+__mail__ = (
+    "demo.nicola@gmail.com, marcotez@gmail.com, andreuzzi.francesco@gmail.com"
+)
+__maintainer__ = __author__
+__status__ = "Stable"
