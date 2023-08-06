d6c5bd2dc44845b3a077bd4cf284aa4f24b1cc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:44 +0000

    Add package tabs-storage/0.1.1/tabs_storage-0.1.1-py3-none-any.whl

---
+++ packages/tabs-storage/tabs_storage-0.1.1-py3-none-any.whl/tabs_storage/home.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+
+
+class HomeType(Enum):
+    LOCAL = "local"
+    HDD = "hdd"
+    BUCKET = "bucket"
+    GOOGLE = "google"
+    TEST = "test"
+
+
+@dataclass(frozen=True)
+class Home:
+    _type: HomeType
+    _type_to_path: dict[HomeType, Path]
+
+    @classmethod
+    def from_settings(cls, home_type: str, path: Path | None = None) -> Home:
+        _type_to_path = {
+            HomeType.LOCAL: Path("/Users/alexis/tabs"),
+            HomeType.HDD: Path("/Volumes/Elements/tabs"),
+            HomeType.BUCKET: Path("/mnt/disks/tabs"),
+            HomeType.GOOGLE: Path("/home/rosuelalexis1"),
+        }
+        home_type_object = HomeType(home_type)
+        if home_type_object is HomeType.TEST:
+            assert path is not None
+            _type_to_path[home_type_object] = path
+        else:
+            assert path is None
+
+        return cls(home_type_object, _type_to_path)
+
+    @property
+    def home_path(self) -> Path:
+        return self._type_to_path[self._type]
