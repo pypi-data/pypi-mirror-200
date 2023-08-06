22c34300922181ec8f21597a7f30b866c6acff44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:06 +0000

    Add package googledriver/0.1.3/googledriver-0.1.3.tar.gz

---
+++ packages/googledriver/googledriver-0.1.3.tar.gz/googledriver-0.1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="googledriver",
+    version="0.1.3",
+    author="parkminwoo",
+    author_email="parkminwoo1991@gmail.com",
+    description="The Python package google drive facilitates access to files uploaded to Google Drive.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DSDanielPark/google-driver",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        "requests", "logging", "urllib"
+    ])
