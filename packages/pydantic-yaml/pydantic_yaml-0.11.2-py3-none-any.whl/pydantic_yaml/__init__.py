0c5f919316430886f172ee69111aebd96626c31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package pydantic-yaml/0.11.2/pydantic_yaml-0.11.2-py3-none-any.whl

---
+++ packages/pydantic-yaml/pydantic_yaml-0.11.2-py3-none-any.whl/pydantic_yaml/__init__.py
@@ -0,0 +1,38 @@
+"""YAML-enabled Pydantic models."""
+
+from .version import __version__
+
+
+__all__ = [
+    "__version__",
+    "yaml",
+    "YamlEnum",  # deprecated class
+    "YamlInt",
+    "YamlIntEnum",
+    "YamlModel",
+    "YamlModelMixin",
+    "YamlModelMixinConfig",
+    "YamlStr",
+    "YamlStrEnum",
+]
+from .main import (
+    YamlEnum,
+    YamlInt,
+    YamlIntEnum,
+    YamlModel,
+    YamlModelMixin,
+    YamlModelMixinConfig,
+    YamlStr,
+    YamlStrEnum,
+    yaml,
+)
+
+try:
+    from .main import (
+        SemVer,
+        VersionedYamlModel,
+    )
+
+    __all__.extend(["SemVer", "VersionedYamlModel"])
+except ImportError:
+    pass
