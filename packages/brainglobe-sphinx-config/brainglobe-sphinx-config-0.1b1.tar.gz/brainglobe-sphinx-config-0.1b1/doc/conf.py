e290d1e4b9e326b247aa3e791719260cd8ab7013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:15 +0000

    Add package brainglobe-sphinx-config/0.1b1/brainglobe-sphinx-config-0.1b1.tar.gz

---
+++ packages/brainglobe-sphinx-config/brainglobe-sphinx-config-0.1b1.tar.gz/brainglobe-sphinx-config-0.1b1/doc/conf.py
@@ -0,0 +1,18 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from brainglobe_sphinx_config import *
+
+project = "Brainglobe sphinx config"
+copyright = "2023, David Stansby"
+author = "David Stansby"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
---
+++ packages/brainglobe-sphinx-config/brainglobe-sphinx-config-0.1b1.tar.gz/brainglobe-sphinx-config-0.1b1/doc/index.rst
@@ -0,0 +1,16 @@
+Brainglobe sphinx config
+========================
+
+This package stores custom sphinx configuration that can be used to give documentation for Brainglobe projects a consistent look.
+It acts as a thin wrapper around the ``pydata-sphinx-theme`` to:
+
+- Add the logo to the navbar
+- Define a static custom set of navbar links
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   using
+   modifying
+   section/index
