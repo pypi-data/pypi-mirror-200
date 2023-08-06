687c1bbd215fd75fe68c85693a01d642379e9997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:01 +0000

    Add package bookserver/1.5.1/bookserver-1.5.1-py3-none-any.whl

---
+++ packages/bookserver/bookserver-1.5.1-py3-none-any.whl/bookserver/internal/__init__.py
@@ -0,0 +1,6 @@
+# ********************************************
+# |docname| - Declare this directory a package
+# ********************************************
+# Make everytihng in `utils.py` appear in the root package.
+
+__all__ = ["utils"]
