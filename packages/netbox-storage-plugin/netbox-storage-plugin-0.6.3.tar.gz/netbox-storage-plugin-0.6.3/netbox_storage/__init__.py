0d8349bc94dfdc7f1b8d05dacdbfed0bfdaab473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:08 +0000

    Add package netbox-storage-plugin/0.6.3/netbox-storage-plugin-0.6.3.tar.gz

---
+++ packages/netbox-storage-plugin/netbox-storage-plugin-0.6.3.tar.gz/netbox-storage-plugin-0.6.3/netbox_storage/__init__.py
@@ -0,0 +1,14 @@
+from extras.plugins import PluginConfig
+
+
+class NetBoxStorageConfig(PluginConfig):
+    name = 'netbox_storage'
+    verbose_name = ' NetBox Storage'
+    description = 'Netbox Storage Administration Plugin'
+    version = '0.6.3'
+    base_url = 'storage'
+    min_version = "3.4.0"
+    author = 'Gabor Somogyvari'
+
+
+config = NetBoxStorageConfig
