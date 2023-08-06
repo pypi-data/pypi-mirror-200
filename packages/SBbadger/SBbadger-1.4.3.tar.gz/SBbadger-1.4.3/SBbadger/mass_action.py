784e58d02b18bff48056960525d4b4a194690eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:51 +0000

    Add package SBbadger/1.4.3/SBbadger-1.4.3.tar.gz

---
+++ packages/SBbadger/SBbadger-1.4.3.tar.gz/SBbadger-1.4.3/SBbadger/mass_action.py
@@ -0,0 +1,33 @@
+
+from SBbadger import generate
+from SBbadger import generate_serial
+
+if __name__ == "__main__":
+
+    generate.models(
+
+        group_name='mass_action',
+        n_models=1,
+        n_species=10,
+        # rxn_prob=[.35, .30, .30, .05],
+        rxn_prob=[1, .0, .0, .0],
+        kinetics=['mass_action', ['loguniform', 'loguniform', 'loguniform'],
+                                 ['kf', 'kr', 'kc'],
+                                 [[0.01, 100], [0.01, 100], [0.01, 100]]],
+        allo_reg=[[0.5, 0.5, 0, 0], 0.5, ['uniform', 'uniform', 'uniform'],
+                                         ['ro', 'kma', 'ma'],
+                                         [[0, 1], [0, 1], [0, 1]]],
+        # allo_reg=True,
+        add_enzyme=True,
+        overwrite=True,
+        source=[1, 'loguniform', 0.01, 1, 0.5],
+        sink=[2, 'loguniform', 0.01, 1],
+        constants=False,
+        # constants=True,
+        cobra=True,
+        rev_prob=1,
+        ic_params=['uniform', 0, 10],
+        # dist_plots=True,
+        net_plots=True
+
+    )
