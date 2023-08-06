602e9af0252232fee6b4cfc706a0ceafad4246b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:41 +0000

    Add package danielutils/0.7.9/danielutils-0.7.9.tar.gz

---
+++ packages/danielutils/danielutils-0.7.9.tar.gz/danielutils-0.7.9/danielutils/Math/__init__.py
@@ -0,0 +1,9 @@
+from .Constants import *
+from .MathPrint import *
+from .Functions import *
+# from .Sigma import *
+# from .Polynomial import *
+# from .Fraction import *
+# from .Limit import *
+# from .Expression import *
+# from .long import *
