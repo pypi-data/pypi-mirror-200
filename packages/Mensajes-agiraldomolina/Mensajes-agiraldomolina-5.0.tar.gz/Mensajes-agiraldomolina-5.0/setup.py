d499bed4e6f4d168aa8af4a0182648840550026e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:13 +0000

    Add package Mensajes-agiraldomolina/5.0/Mensajes-agiraldomolina-5.0.tar.gz

---
+++ packages/Mensajes-agiraldomolina/Mensajes-agiraldomolina-5.0.tar.gz/Mensajes-agiraldomolina-5.0/setup.py
@@ -0,0 +1,26 @@
+from struct import pack
+from  setuptools import setup, find_packages
+
+setup(
+    name='Mensajes-agiraldomolina',
+    version='5.0',
+    description='Un paquete para saludar y despedir',
+    long_description=open('README.md').read(),
+    long_description_content_type ='text/markdown',
+    author='Alba Giraldo',
+    author_email='agiraldomolina@gmail.com',
+    url='',
+    packages=find_packages(),
+    scripts=[],
+    test_suite = 'test',
+    install_requires = [paquete.strip() for paquete in open('requirements.txt').readlines()],
+    classifiers=['Environment :: Console',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.0',
+                 'Topic :: Utilities',
+                 ],
+    
+)
+
