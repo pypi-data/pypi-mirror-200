2c4799c0d33115de26cf28eeca69322683c6405d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:04 +0000

    Add package FlagCapture/0.1.4/FlagCapture-0.1.4.tar.gz

---
+++ packages/FlagCapture/FlagCapture-0.1.4.tar.gz/FlagCapture-0.1.4/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(zip_safe=True, version='0.1.4', description='Capture The Flag', long_description='Same as 0.1.3 with added rules about prisoners capturing others and flag-carrying.', author='Michael Caggeso', author_email='laurencaggeso@gmail.com', license='Michael Licesce', name='FlagCapture', packages=['FlagCapture'])
