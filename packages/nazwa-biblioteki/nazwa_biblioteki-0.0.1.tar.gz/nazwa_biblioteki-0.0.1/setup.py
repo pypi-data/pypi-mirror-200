595862d0862a644f8035100c3f55413000feb0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:15 +0000

    Add package nazwa-biblioteki/0.0.1/nazwa_biblioteki-0.0.1.tar.gz

---
+++ packages/nazwa-biblioteki/nazwa_biblioteki-0.0.1.tar.gz/nazwa_biblioteki-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='nazwa_biblioteki',
+    version='0.0.1',
+    description='Opis biblioteki',
+    url='https://github.com/JakUrb04/turbojet.git',
+    author='Jakub Urbaniak',
+    author_email='urbaniak627@gmail.com',
+    license='MIT',
+    packages=['turbojet'],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
