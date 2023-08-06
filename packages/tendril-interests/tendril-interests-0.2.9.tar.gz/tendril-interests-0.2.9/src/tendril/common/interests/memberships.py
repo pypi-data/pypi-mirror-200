70013798dc9296e754f2031819251dcd6dad4270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:14 +0000

    Add package tendril-interests/0.2.9/tendril-interests-0.2.9.tar.gz

---
+++ packages/tendril-interests/tendril-interests-0.2.9.tar.gz/tendril-interests-0.2.9/src/tendril/common/interests/memberships.py
@@ -0,0 +1,158 @@
+
+
+import polars
+from typing import Dict
+from typing import List
+from tendril.utils.pydantic import TendrilTBaseModel
+from tendril.utils.db import get_session
+
+
+class UserMembershipTModel(TendrilTBaseModel):
+    role: str
+    delegated: bool
+    inherited: bool
+
+
+class UserMembershipsInterestTModel(TendrilTBaseModel):
+    id: int
+    name: str
+    roles: List[UserMembershipTModel]
+
+
+UserMembershipsInterestsContainerTModel = List[UserMembershipsInterestTModel]
+UserMembershipsTModel = Dict[str, UserMembershipsInterestsContainerTModel]
+
+
+class UserMembershipCollector(object):
+    def __init__(self):
+        self._raw_data = []
+        self.df = None
+
+    def add_membership(self, interest, role, delegated, inherited):
+        self._raw_data.append({'type': interest.type_name,
+                               'interest.id': interest.id,
+                               'interest.name': interest.name,
+                               'role.name': role,
+                               'role.delegated': delegated,
+                               'role.inherited': inherited})
+
+    def process(self):
+        self.df = polars.DataFrame(self._raw_data)
+
+    def _pack_interest_role(self, df):
+        # TODO This calculation is wrong.
+        return {'role': df[0]['role.name'].item(),
+                'delegated': bool(df.select(polars.col('role.delegated')).min().item()),
+                'inherited': bool(df.select(polars.col('role.inherited')).min().item())}
+
+    def _pack_interest_memberships(self, df):
+        roles = []
+        for role in df.select(polars.col('role.name').unique()).rows():
+            role_df = df.filter(polars.col('role.name') == role[0])\
+                        .select(['role.name', 'role.delegated', 'role.inherited'])
+            roles.append(self._pack_interest_role(role_df))
+        idict = {'id': df[0]['interest.id'].item(),
+                 'name': df[0]['interest.name'].item(),
+                 'roles': roles}
+        return idict
+
+    def _pack_itype_memberships(self, df):
+        rv = []
+        for interest_id in df.select(polars.col('interest.id').unique()).rows():
+            interest_df = df.filter(polars.col('interest.id') == interest_id[0])
+            rv.append(self._pack_interest_memberships(interest_df))
+        return rv
+
+    def render(self):
+        rv = {}
+        for itype in self.df.select(polars.col("type").unique()).rows():
+            itype_df = self.df.filter(polars.col("type") == itype[0])\
+                              .select(['interest.id', 'interest.name',
+                                       'role.name', 'role.delegated', 'role.inherited'])
+            rv[itype[0]] = self._pack_itype_memberships(itype_df)
+        return rv
+
+    def interest_ids(self):
+        if not self.df.select(polars.count()).item():
+            return []
+        return self.df.select(polars.col('interest.id').unique()).rows()[0]
+
+
+def _rewrap_interest(model):
+    from tendril.interests import type_codes
+    type_name = model.type
+    return type_codes[type_name](model)
+
+
+def _get_interest_user_memberships(collector, interest, user_id,
+                                   include_delegated=True,
+                                   include_inherited=True,
+                                   is_inherited=False,
+                                   inherited_roles=None,
+                                   interest_types=None,
+                                   parent_types=None,
+                                   session=None):
+    # This feels terrible.
+    # Also, delegations needs to be tracked down the recursion.
+    add_to_result = not interest_types or interest.model.type_name in interest_types
+    to_inherit = set()
+    if inherited_roles:
+        for role in inherited_roles:
+            if role not in interest.model.role_spec.roles:
+                continue
+            to_inherit.add(role)
+            if add_to_result:
+                collector.add_membership(interest, role, False, True)
+    roles = set(interest.get_user_roles(user_id, session=session))
+    for role in list(roles):
+        to_inherit.add(role)
+        if add_to_result:
+            collector.add_membership(interest, role, False, is_inherited)
+        if include_delegated:
+            interest_rs = interest.model.role_spec
+            for drole in interest_rs.get_delegated_roles(role):
+                to_inherit.add(drole)
+                if add_to_result:
+                    collector.add_membership(interest, drole, True, is_inherited)
+    if include_inherited:
+        for child in interest.children(limited=False, session=session):
+            if not child.model.role_spec.inherits_from_parent:
+                continue
+            if parent_types and not child.model.type_name in parent_types:
+                continue
+            _get_interest_user_memberships(collector, child, user_id,
+                                           include_delegated=include_delegated,
+                                           include_inherited=include_inherited,
+                                           is_inherited=True, inherited_roles=to_inherit,
+                                           interest_types=interest_types,
+                                           parent_types=parent_types,
+                                           session=session)
+
+
+def user_memberships(user_id, interest_types=None,
+                     include_delegated=True,
+                     include_inherited=True):
+    from tendril.interests import possible_parents
+    from tendril.db.controllers import interests
+
+    parent_types = None
+    if interest_types:
+        parent_types = set(interest_types)
+        for t in interest_types:
+            parent_types.update(possible_parents[t])
+
+    with get_session() as session:
+        memberships = interests.get_user_memberships(user=user_id, session=session)
+        rv = UserMembershipCollector()
+        for m in memberships:
+            if parent_types and m.interest.type_name not in parent_types:
+                continue
+            _get_interest_user_memberships(rv, _rewrap_interest(m.interest), user_id,
+                                           include_delegated=include_delegated,
+                                           include_inherited=include_inherited,
+                                           is_inherited=False, inherited_roles=[],
+                                           interest_types=interest_types,
+                                           parent_types=parent_types,
+                                           session=session)
+        rv.process()
+    return rv
---
+++ packages/tendril-interests/tendril-interests-0.2.9.tar.gz/tendril-interests-0.2.9/src/tendril/libraries/interests/base.py
@@ -0,0 +1,112 @@
+
+
+import os
+import csv
+from functools import cached_property
+
+from tendril.config import AUDIT_PATH
+from tendril.utils.fsutils import VersionedOutputFile
+
+from tendril.utils.db import with_db
+
+from tendril.db.controllers.interests import get_interest
+from tendril.db.controllers.interests import get_interests
+from tendril.db.controllers.interests import get_user_memberships
+from tendril.interests import InterestBase
+from tendril.apiserver.templates.interests import InterestLibraryRouterGenerator
+
+from tendril.common.interests.memberships import user_memberships
+from tendril.common.interests.exceptions import TypeMismatchError
+from tendril.common.interests.exceptions import InterestNotFound
+from sqlalchemy.exc import NoResultFound
+
+from tendril.utils import log
+logger = log.get_logger(__name__, log.DEFAULT)
+
+
+class GenericInterestLibrary(object):
+    interest_class = InterestBase
+
+    @property
+    def type_name(self):
+        return self.interest_class.model.type_name
+
+    def idents(self):
+        return [x.ident for x in self.items()]
+
+    @with_db
+    def items(self, user=None, state=None, include_inherited=False, session=None):
+        if not user:
+            return [self.interest_class(x) for x in
+                    get_interests(type=self.interest_class,
+                                  state=state,
+                                  session=session)]
+        if not include_inherited:
+            if state:
+                logger.warning("State filtering is not implemented "
+                               "for user interests retrieval")
+            return [self.interest_class(x.interest) for x in
+                    get_user_memberships(user,
+                                         interest_type=self.interest_class.model,
+                                         session=session)]
+        else:
+            if state:
+                logger.warning("State filtering is not implemented "
+                               "for inherited user interests retrieval")
+            iids = user_memberships(
+                user_id=user,
+                interest_types=[self.interest_class.model.type_name],
+                include_inherited=include_inherited,
+            ).interest_ids()
+            return [self.interest_class(get_interest(id=x, session=session))
+                    for x in iids]
+
+    @with_db
+    def item(self, id=None, name=None, session=None):
+        try:
+            return self.interest_class(
+                get_interest(id=id, name=name, type=self.interest_class, session=session))
+        except NoResultFound:
+            if not name:
+                name = '<unspecified>'
+            if not id:
+                id = '<unspecified>'
+            raise InterestNotFound(type_name=self.type_name, name=name, id=id)
+
+    @with_db
+    def add_item(self, item, session=None):
+        if item.type != self.interest_class.model.type_name:
+            raise TypeMismatchError(item.type, self.interest_class.model.type_name)
+        created = self.interest_class(item.name, must_create=True, session=session)
+        if item.descriptive_name:
+            created.set_descriptive_name(item.descriptive_name, session=session)
+        session.flush()
+        return created
+
+    @with_db
+    def delete_item(self, id=None, name=None, session=None):
+        raise NotImplementedError
+
+    def api_generator(self):
+        return InterestLibraryRouterGenerator(self)
+
+    # def export_audit(self, name):
+    #     auditfname = os.path.join(
+    #         AUDIT_PATH, 'projectlib-{0}.audit.csv'.format(name)
+    #     )
+    #     outf = VersionedOutputFile(auditfname)
+    #     outw = csv.writer(outf)
+    #     outw.writerow(['ident', 'name', 'folder', 'status',
+    #                    'description', 'maintainer'])
+    #     for project in self.projects:
+    #         outw.writerow(
+    #             [project.ident, project.projectname, project.projectfolder, project.status,
+    #              project.description, project.maintainer]
+    #         )
+    #
+    #     outf.close()
+
+
+def load(manager):
+    # manager.install_exc_class('ProjectNotFoundError', ProjectNotFoundError)
+    pass
