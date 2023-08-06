8859468d6ce1495a0a00727d0f0c309901e61e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:06 +0000

    Add package fake-bge-module-latest/20230402/fake_bge_module_latest-20230402-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20230402-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,47 @@
+import sys
+import typing
+from . import object_randomize_transform
+from . import view3d
+from . import freestyle
+from . import anim
+from . import add_mesh_torus
+from . import constraint
+from . import userpref
+from . import object_quick_effects
+from . import rigidbody
+from . import presets
+from . import mesh
+from . import uvcalc_transform
+from . import node
+from . import bmesh
+from . import file
+from . import object
+from . import object_align
+from . import wm
+from . import spreadsheet
+from . import image
+from . import vertexpaint_dirt
+from . import uvcalc_follow_active
+from . import assets
+from . import geometry_nodes
+from . import clip
+from . import uvcalc_lightmap
+from . import sequencer
+from . import screen_play_rendered_anim
+from . import console
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
