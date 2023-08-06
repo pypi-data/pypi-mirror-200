01e88d701125b843a9e1d03b52332ef7fe6fe2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:58 +0000

    Add package qutewindow/0.1.16/qutewindow-0.1.16-py3-none-any.whl

---
+++ packages/qutewindow/qutewindow-0.1.16-py3-none-any.whl/qutewindow/platforms/mac/QuteWindow.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+from PySide6.QtCore import Qt, QPoint
+from PySide6.QtGui import QMouseEvent
+from PySide6.QtWidgets import QWidget
+
+from qutewindow.platforms.mac.utils import merge_content_area_and_title_bar, setTrafficLightsPosition
+
+
+class QuteWindow(QWidget):
+    def __init__(self, parent: Optional[QWidget] = None) -> None:
+        QWidget.__init__(self, parent)
+        self.setWindowFlags(Qt.Window)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        merge_content_area_and_title_bar(self.winId())
+
+    def mouseDoubleClickEvent(self, event: QMouseEvent) -> None:
+        if not self.isFullScreen() and self.isTitleBarArea(event.pos()):
+            self.toggleMaximized()
+
+    def isTitleBarArea(self, pos: QPoint) -> bool:
+        title_bar_height = self.titleBarHeight()
+        return pos.y() <= title_bar_height
+
+    def titleBarHeight(self) -> int:
+        return 30
+
+    def toggleMaximized(self) -> None:
+        if self.isMaximized():
+            self.showNormal()
+        else:
+            self.showMaximized()
