6503d36ca39c2b7a4ed45c7e342d77976b8232bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:59 +0000

    Add package uceasy/0.3.1/uceasy-0.3.1-py3-none-any.whl

---
+++ packages/uceasy/uceasy-0.3.1-py3-none-any.whl/uceasy/run.py
@@ -0,0 +1,19 @@
+from types import SimpleNamespace
+
+from uceasy.facade import AssemblyFacade, QualityControlFacade, UCEAlignmentFacade
+from uceasy.tracking import save_tracking_file
+
+
+def run_quality_control(context: SimpleNamespace):
+    fl = context.tracking_file
+    save_tracking_file(fl, QualityControlFacade(context).run())
+
+
+def run_assembly(context: SimpleNamespace):
+    fl = context.tracking_file
+    save_tracking_file(fl, AssemblyFacade(context).run())
+
+
+def run_alignment(context: SimpleNamespace):
+    fl = context.tracking_file
+    save_tracking_file(fl, UCEAlignmentFacade(context).run())
