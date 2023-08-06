85d8776cc4abe1059bb6ab19871fba9dfe6dc496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:28 +0000

    Add package jeff-api/0.2.6/jeff_api-0.2.6-py3-none-any.whl

---
+++ packages/jeff-api/jeff_api-0.2.6-py3-none-any.whl/jeff_api/server.py
@@ -0,0 +1,37 @@
+import json, signal, socket
+
+
+class Server:
+  def __init__(self, host, port):
+    self.host = host
+    self.port = port
+    self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.server_socket.bind((host if host is not None else socket.gethostname(), port))
+    self.server_socket.listen()
+
+    def exit_gracefully(*args):
+      self.server_socket.close()
+
+    signal.signal(signal.SIGINT, exit_gracefully)
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+  def _waits_for(self, buffer_size=8192):
+    (socket, address) = self.server_socket.accept()
+    try:
+      data = socket.recv(buffer_size)
+      return data
+    except ConnectionRefusedError:
+      print("Connection refused.")
+      return b"{}"
+    except json.decoder.JSONDecodeError:
+      print('JSON decode error.')
+      return b"{}"
+
+  def _encode_json(j):
+    return json.dumps(j).encode()
+
+  def _decode_json(b):
+    return json.loads(b.decode())
+
+  def listen(self):
+    return Server._decode_json(self._waits_for())
