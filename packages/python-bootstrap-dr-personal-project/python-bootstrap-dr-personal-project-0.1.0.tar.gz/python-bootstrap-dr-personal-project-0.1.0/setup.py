757fe8d7f615e26a9690e143fc56c43afbc1edb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:32 +0000

    Add package python-bootstrap-dr-personal-project/0.1.0/python-bootstrap-dr-personal-project-0.1.0.tar.gz

---
+++ packages/python-bootstrap-dr-personal-project/python-bootstrap-dr-personal-project-0.1.0.tar.gz/python-bootstrap-dr-personal-project-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-bootstrap-dr-personal-project",
+    version="0.1.0",
+    description="Description of the project",
+    author="DR",
+    author_email="your@email.com",
+    url="https://github.com/your-username/project-name",
+    packages=find_packages(),
+    install_requires=[
+        "requests==2.28.2"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
