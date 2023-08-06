0f6a246a17c54d1f5aadca175e5d10c662783141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:21 +0000

    Add package searchyz/1.7.2/searchyz-1.7.2.tar.gz

---
+++ packages/searchyz/searchyz-1.7.2.tar.gz/searchyz-1.7.2/setup.py
@@ -0,0 +1,41 @@
+import re
+import os
+import setuptools
+
+ver = '1.7.2'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_desc = fh.read()
+
+desc = "Package containing basic functions to build telegram bots."
+GPL = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
+git = "https://github.com/DKBOTZNETWORK"
+classify = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+]
+requirements = [
+    "python-apiy",
+    "python-domer",
+]
+
+
+setuptools.setup(
+    name="searchyz",
+    version=ver,
+    author="DK BOTZ",
+    description=desc,
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url=git,
+    license=GPL,
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=classify,
+    python_requires=">=3.6",
+)
