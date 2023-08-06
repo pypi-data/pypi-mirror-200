a968bbcb902a3621a146db9fc7d9088cb002c93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:28 +0000

    Add package Geode-SimplexRemesh/5.2.8/Geode_SimplexRemesh-5.2.8-cp39-cp39-win_amd64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-5.2.8-cp39-cp39-win_amd64.whl/geode_simplexremesh/section.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_parameterization
+
+from .geode_simplexremesh_py_section import *
+SimplexRemeshSection.initialize()
