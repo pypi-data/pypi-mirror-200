2d0c33fb46b4821c3809fa9ea8edd55e919359c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:57 +0000

    Add package matrixlib/0.3.1/matrixlib-0.3.1-py3-none-any.whl

---
+++ packages/matrixlib/matrixlib-0.3.1-py3-none-any.whl/matrixlib/__init__.py
@@ -0,0 +1,13 @@
+"""General-purpose matrices for the layman
+
+Implements a family of general-purpose matrix types, with comprehensive
+type-checking capabilities, and seamless integration with core Python
+services.
+
+Author: Braedyn L
+Version: 0.3.1
+Source: https://github.com/braedynl/matrixlib/
+"""
+
+from .builtins import *
+from .rule import *
