29b21f8fff1480f4ea0cf880e738c9740482d405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:30 +0000

    Add package cohpy/1.0/cohpy-1.0-py3-none-any.whl

---
+++ packages/cohpy/cohpy-1.0-py3-none-any.whl/cohpy/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='cohpy',
+    version='1.0',
+    author='Andrés Garrido López',
+    description='An API Wrapper for Company Of Heroes API',
+    packages=['cohpy']
+)
