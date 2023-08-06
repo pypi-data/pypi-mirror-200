6e1b4be5a7eee2f33ffef33ed016c36dfdb4cf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:04 +0000

    Add package ibm2ieee/1.3.0/ibm2ieee-1.3.0.tar.gz

---
+++ packages/ibm2ieee/ibm2ieee-1.3.0.tar.gz/ibm2ieee-1.3.0/ibm2ieee/__init__.py
@@ -0,0 +1,18 @@
+# (C) Copyright 2018-2023 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+from ._ibm2ieee import ibm2float32, ibm2float64
+from .version import version as __version__
+
+__all__ = [
+    "__version__",
+    "ibm2float32",
+    "ibm2float64",
+]
