9da8a6a35b4d5acb4f582c0acf82c903b435a65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:48 +0000

    Add package gpt-react/0.3/gpt-react-0.3.tar.gz

---
+++ packages/gpt-react/gpt-react-0.3.tar.gz/gpt-react-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gpt-react",
+    version="0.3",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["gpt-react = react:main"]},
+    install_requires=["openai", "httpx"],
+)
