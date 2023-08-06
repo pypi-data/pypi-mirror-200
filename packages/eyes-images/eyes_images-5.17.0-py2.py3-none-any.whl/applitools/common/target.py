266c48fd0398b5f77305a04162c243481c53e4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:05 +0000

    Add package eyes-images/5.17.0/eyes_images-5.17.0-py2.py3-none-any.whl

---
+++ packages/eyes-images/eyes_images-5.17.0-py2.py3-none-any.whl/applitools/common/target.py
@@ -0,0 +1,11 @@
+from typing import TYPE_CHECKING
+
+import attr
+
+if TYPE_CHECKING:
+    from typing import Optional, Text
+
+
+@attr.s
+class ImageTarget(object):
+    image = attr.ib(default=None)  # type: Optional[Text]
