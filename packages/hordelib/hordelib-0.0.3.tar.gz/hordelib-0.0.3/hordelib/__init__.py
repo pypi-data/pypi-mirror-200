34edf1cc78e0b7c617ccb876fd0338a107ddf09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:38 +0000

    Add package hordelib/0.0.3/hordelib-0.0.3.tar.gz

---
+++ packages/hordelib/hordelib-0.0.3.tar.gz/hordelib-0.0.3/hordelib/__init__.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+VERSION = "0.0.3"
+COMFYUI_VERSION = "27fc64ad469c07d8f84b2c2791a593f1cf2c7b59"
+
+current_file_path = os.path.abspath(__file__)
+current_folder = os.path.dirname(current_file_path)
+comfypath = os.path.join(current_folder, "ComfyUI")
+sys.path.append(comfypath)  # noqa: E402
+
+
+class HordelibException(Exception):
+    pass
