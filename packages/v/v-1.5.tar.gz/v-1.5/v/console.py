b1dbb7e49e43d765b809d4f7b070d67797deeb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package v/1.5/v-1.5.tar.gz

---
+++ packages/v/v-1.5.tar.gz/v-1.5/v/console.py
@@ -0,0 +1,8 @@
+def pt(*text, end="", join="", flush=True):
+	print(*text, end=end, sep=join, flush=flush)
+
+def ptln(*text, join="", flush=True):
+	pt(*text, end="\n", join=join, flush=flush)
+
+def write(*text, end="", join="", flush=True):
+	pt(*text, end=end, join=join, flush=flush)
