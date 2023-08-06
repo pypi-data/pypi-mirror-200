a251f494f1ac92978181cc394c6df27ecec283ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:09 +0000

    Add package solentware-misc/1.5.1/solentware-misc-1.5.1.tar.gz

---
+++ packages/solentware-misc/solentware-misc-1.5.1.tar.gz/solentware-misc-1.5.1/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+# Copyright 2011 Roger Marsh
+# Licence: See LICENCE (BSD licence)
+"""solentware-misc setup file.
+
+Retained so 'python setup.py ...' can be used by my build scripts until
+conversion to 'python -m pip ...'.
+
+All setup() arguments are set in setup.cfg file.
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+
+    setup()
