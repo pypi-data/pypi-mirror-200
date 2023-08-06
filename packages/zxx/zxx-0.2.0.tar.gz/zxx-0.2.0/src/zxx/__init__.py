277b25af39960995702b8cc2bcc03e69e067f518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:06 +0000

    Add package zxx/0.2.0/zxx-0.2.0.tar.gz

---
+++ packages/zxx/zxx-0.2.0.tar.gz/zxx-0.2.0/src/zxx/__init__.py
@@ -0,0 +1,12 @@
+"""
+zxx
+最后更新：2023-04-03
+
+尚未解决的问题：
+    使用audio_normalize标准化音量
+"""
+
+from .File import File
+from .Video import Video
+from .Highlight import Highlight
+from .options import SetVideoPath, SetMatchInfo, SetCaptionStyle, SetScoreBoardStyle
