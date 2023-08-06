235102f6537cea39f5c59308cb86131b50a4e07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:09 +0000

    Add package mexa/0.1/mexa-0.1.tar.gz

---
+++ packages/mexa/mexa-0.1.tar.gz/mexa-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name = 'mexa',
+    version = '0.1',
+    description = 'Validador y Generador de campos para tramites mexicanos',
+    author='@Fitorec - Miguel Angel Marcial Martinez',
+    author_email = 'fitorec@mundosica.com',
+    url = 'https://github.com/fitorec/mexa-fake-validator',
+    keywords = ['RFC', 'CURP', 'NSS', 'fiscal'],
+    classifiers = [
+        'Programming Language :: Python :: 3.8',
+    ],
+    license = "MIT"
+)
