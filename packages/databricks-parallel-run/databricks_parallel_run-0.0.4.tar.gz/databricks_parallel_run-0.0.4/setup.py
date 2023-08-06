a7a4e85907acfcbcee322a7371abd1a311c123f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package databricks-parallel-run/0.0.4/databricks_parallel_run-0.0.4.tar.gz

---
+++ packages/databricks-parallel-run/databricks_parallel_run-0.0.4.tar.gz/databricks_parallel_run-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setup(
+    name='databricks_parallel_run',
+    version='0.0.4',
+    description='Run databricks notebooks in parallel',
+    author= 'Kuldeep Rathore',
+    #url = 'https://github.com/Spidy20/PyMusic_Player',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['python', 'databricks', 'notebook', 'databricks parallel run', 'databricks parallel notebook run', 'paralle processing'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['databricks_parallel_run'],
+    package_dir={'':'src'},
+	install_requires = [
+        'pyspark',
+        'dbutils',
+    ]
+)
---
+++ packages/databricks-parallel-run/databricks_parallel_run-0.0.4.tar.gz/databricks_parallel_run-0.0.4/src/databricks_parallel_run.py
@@ -0,0 +1,38 @@
+from concurrent.futures import ThreadPoolExecutor
+import pyspark
+import dbutils
+def parallel_run(filepath):
+    class NotebookData:
+        def __init__(self, path, timeout, parameters=None, retry=0):
+            self.path = path
+            self.timeout = timeout
+            self.parameters = parameters
+            self.retry = retry
+
+        def submitNotebook(notebook):
+            print("Running notebook %s" % str(notebook.path).split("/")[-1])
+            try:
+                if (notebook.parameters):
+                    return dbutils.notebook.run(notebook.path, notebook.timeout, notebook.parameters)
+                else:
+                    try:
+                        return dbutils.notebook.run(notebook.path, notebook.timeout)
+                    except:
+                        print(str(notebook.path).split("/")[-1], 'failed')
+            except Exception:
+                if notebook.retry < 1:
+                    raise
+                print("Retrying notebook %s" % notebook.path)
+                notebook.retry = notebook.retry - 1
+                submitNotebook(notebook)
+
+    def parallelNotebooks(notebooks, numInParallel):
+        with ThreadPoolExecutor(max_workers=numInParallel) as ec:
+            return [ec.submit(NotebookData.submitNotebook, notebook) for notebook in notebooks]
+
+    notebooks = []
+    for path in filepath.split(','):
+        notebooks.append(NotebookData(path, 1200))
+
+    res = parallelNotebooks(notebooks, 10)
+    result = [i.result(timeout=3600) for i in res] # This is a blocking call.
