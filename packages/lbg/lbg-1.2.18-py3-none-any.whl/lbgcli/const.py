c54403baa12f5e8cf42cae40a3aa6b74537af0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:52 +0000

    Add package lbg/1.2.18/lbg-1.2.18-py3-none-any.whl

---
+++ packages/lbg/lbg-1.2.18-py3-none-any.whl/lbgcli/const.py
@@ -0,0 +1,26 @@
+class ConfigKey:
+    CURRENT_PROGRAM_ID = 'PROGRAM_CURRENT_PROGRAM_ID'
+    DEFAULT_OUTPUT_FORMAT = 'DEFAULT_OUTPUT_FORMAT'
+    ACCOUNT_EMAIL = 'ACCOUNT_EMAIL'
+    ACCOUNT_PASSWORD = 'ACCOUNT_PASSWORD'
+    ALI_OSS_ENDPOINT = 'ALI_OSS_ENDPOINT'
+    LEBESGUE_ADDRESS = 'LEBESGUE_ADDRESS'
+    CONFIG_FILE_DIR = 'CONFIG_FILE_DIR'
+    STORAGE_BUCKET_NAME = 'STORAGE_BUCKET_NAME'
+    CHECK_VERSION_LEVEL = "CHECK_VERSION_LEVEL"
+    TIEFBLUE_BASE_URL = 'TIEFBLUE_BASE_URL'
+
+
+class GlobalConfig:
+    # LEBESGUE_ADDRESS = 'https://test.dp.tech/'
+    # LEBESGUE_ADDRESS = 'http://172.16.8.21:8000/'
+    LEBESGUE_ADDRESS = 'https://bohrium.dp.tech/'
+    STORAGE_ENDPOINT = 'oss-cn-shenzhen.aliyuncs.com'
+    STORAGE_BUCKET_NAME = 'dpcloudserver'
+    CONFIG_ACTION_RECORD = 'lbg_action.csv'
+    CONFIG_FILE_DIR_SECONDARY = '~/.lbg/'
+    CONFIG_FILE_DIR_PRIMARY = '/data/.lbg/'
+    CONFIG_FILE_NAME = 'lbg_cli_context.json'
+    CALLER_NAME = 'lbg'
+    CHECK_VERSION_LEVEL = 1
+    TIEFBLUE_BASE_URL = 'http://tiefblue.test.dp.tech'
