1cd9275418085d97a108b247b5427347aaaaf8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:57 +0000

    Add package snns/0.0.0/snns-0.0.0.tar.gz

---
+++ packages/snns/snns-0.0.0.tar.gz/snns-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='snns',
+    version='0.0.0',
+    url='https://open-neuromorphic.org',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+)
