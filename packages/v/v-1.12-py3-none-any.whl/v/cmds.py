faef154fdf1ee527281e0c5a17f6f4049eb0e12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:52 +0000

    Add package v/1.12/v-1.12-py3-none-any.whl

---
+++ packages/v/v-1.12-py3-none-any.whl/v/cmds.py
@@ -0,0 +1,34 @@
+class Console:
+	def __init__(self):
+		self._bg = "0"
+		self._fg = "0"
+	def pt(self, *text, end="", join="", flush=True):
+		print(*text, end=end, sep=join, flush=flush)
+
+	def ptln(self, *text, join="", flush=True):
+		pt(*text, end="\n", join=join, flush=flush)
+
+	def write(self, *text, end="", join="", flush=True):
+		pt(*text, end=end, join=join, flush=flush)
+
+	def writeln(self, *text, join="", flush=True):
+		ptln(*text, join=join, flush=flush)
+
+	def read(self, prompt):
+		return input(prompt)
+	
+	def set_bg(self, value):
+		self._bg = value
+		print(f"\u001b[{self._bg}m", end="")
+	
+	def get_bg(self):
+		return self._bg
+
+	bg = property(get_bg, set_bg)
+
+	def set_fg(self, value):
+		self._fg = value
+		print(f"\u001b[{self._bg}m", end="")
+	
+
+console = Console()
