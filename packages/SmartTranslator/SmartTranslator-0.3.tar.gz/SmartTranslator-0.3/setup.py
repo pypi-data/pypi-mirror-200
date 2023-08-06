5fcf453d2d91a32be67f6cc0a30226bca76a8f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:47 +0000

    Add package SmartTranslator/0.3/SmartTranslator-0.3.tar.gz

---
+++ packages/SmartTranslator/SmartTranslator-0.3.tar.gz/SmartTranslator-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+version = '0.3'
+
+setup(
+    name='SmartTranslator',
+    version=version,
+    author='flw',
+    author_email='flovvi78@gmail.com',
+    packages=find_packages(exclude=['smart_translator.py']),
+)
