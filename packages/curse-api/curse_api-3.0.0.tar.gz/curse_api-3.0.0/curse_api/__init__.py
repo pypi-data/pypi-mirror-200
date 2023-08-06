df20d70535cf0cbc4c1666901dc8cdeec2132c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:04 +0000

    Add package curse-api/3.0.0/curse_api-3.0.0.tar.gz

---
+++ packages/curse-api/curse_api-3.0.0.tar.gz/curse_api-3.0.0/curse_api/__init__.py
@@ -0,0 +1,12 @@
+from .api import CurseAPI, SimpleCurseAPI
+from .enums import Games, ModLoaderType, ModsSearchSortField
+from .models import File, MinecraftGameVersion, Mod, Pagination
+
+__all__ = [
+    "CurseAPI",
+    "Mod",
+    "File",
+    "Games",
+    "ModsSearchSortField",
+    "ModLoaderType",
+]
