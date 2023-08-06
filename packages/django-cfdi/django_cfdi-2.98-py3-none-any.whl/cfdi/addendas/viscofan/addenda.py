3b51bea60221071031f4188d0986e0255732bb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:11 +0000

    Add package django-cfdi/2.98/django_cfdi-2.98-py3-none-any.whl

---
+++ packages/django-cfdi/django_cfdi-2.98-py3-none-any.whl/cfdi/addendas/viscofan/addenda.py
@@ -0,0 +1,12 @@
+from django.template.loader import render_to_string
+from django.conf import settings
+
+CAMPOS_ENCABEZADOS = (
+    ("ordenCompra", "int"),
+    ("plantaEntrega", "str"),
+    ("noLineaArticulo", "str"),
+    ("noAcreedor", "int"),
+)
+
+def generar_addenda(diccionario):
+    return render_to_string("cfdi/addendas/viscofan.xml", diccionario)
