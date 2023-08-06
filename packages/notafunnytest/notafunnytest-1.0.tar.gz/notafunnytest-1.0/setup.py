9b35fc7dcab349091babb66b20ea93ae0f9dd634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:57 +0000

    Add package notafunnytest/1.0/notafunnytest-1.0.tar.gz

---
+++ packages/notafunnytest/notafunnytest-1.0.tar.gz/notafunnytest-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='notafunnytest',
+      version='1.0',
+      description='The funniest joke in the world',
+      url='https://github.com/LorenzoPeve/funnytest',
+      author='Nostradamus',
+      author_email='doesnotexist@gmail.com',
+      license='MIT',
+      packages=['notafunnytest'],
+      zip_safe=False)
