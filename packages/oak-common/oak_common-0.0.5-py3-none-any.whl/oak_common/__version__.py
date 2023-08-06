6cf7116c95de1649f10b0caaf46eeedb7e30d184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:30 +0000

    Add package oak-common/0.0.5/oak_common-0.0.5-py3-none-any.whl

---
+++ packages/oak-common/oak_common-0.0.5-py3-none-any.whl/oak_common/__version__.py
@@ -0,0 +1,14 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = 'oak_common'
+__description__ = ''
+__url__ = ''
+__version__ = '0.0.5'
+__build__ = 0x022701
+__author__ = 'jinsz/zhouzt'
+__author_email__ = 'shuzhao.jin@oak-amc.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2023 hangzhouxm'
+__cake__ = u'\u2728 \U0001f370 \u2728'
