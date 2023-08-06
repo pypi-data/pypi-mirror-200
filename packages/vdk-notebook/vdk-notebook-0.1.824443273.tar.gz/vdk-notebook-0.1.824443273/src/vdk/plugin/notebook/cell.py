52fa24d7319c71466fb7d26b764fccb8a7b6ece2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:01 +0000

    Add package vdk-notebook/0.1.824443273/vdk-notebook-0.1.824443273.tar.gz

---
+++ packages/vdk-notebook/vdk-notebook-0.1.824443273.tar.gz/vdk-notebook-0.1.824443273/src/vdk/plugin/notebook/cell.py
@@ -0,0 +1,14 @@
+# Copyright 2021-2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+from dataclasses import dataclass
+
+
+@dataclass
+class Cell:
+    """
+    A class that represents Jupyter code cell
+    """
+
+    def __init__(self, jupyter_cell):
+        self.tags = jupyter_cell["metadata"].get("tags", {})
+        self.source = "".join(jupyter_cell["source"])
