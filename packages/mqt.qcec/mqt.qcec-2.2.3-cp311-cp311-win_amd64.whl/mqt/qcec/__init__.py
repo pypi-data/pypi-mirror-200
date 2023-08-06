409d8bf0d5288bfe806f67b9809704819606a440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:06 +0000

    Add package mqt.qcec/2.2.3/mqt.qcec-2.2.3-cp311-cp311-win_amd64.whl

---
+++ packages/mqt.qcec/mqt.qcec-2.2.3-cp311-cp311-win_amd64.whl/mqt/qcec/__init__.py
@@ -0,0 +1,28 @@
+"""MQT QCEC library.
+
+This file is part of the MQT QCEC library released under the MIT license.
+See README.md or go to https://github.com/cda-tum/qcec for more information.
+"""
+
+from mqt.qcec.compilation_flow_profiles import AncillaMode, generate_profile
+from mqt.qcec.pyqcec import (
+    ApplicationScheme,
+    Configuration,
+    EquivalenceCheckingManager,
+    EquivalenceCriterion,
+    StateType,
+)
+from mqt.qcec.verify import verify
+from mqt.qcec.verify_compilation_flow import verify_compilation
+
+__all__ = [
+    "AncillaMode",
+    "ApplicationScheme",
+    "Configuration",
+    "EquivalenceCheckingManager",
+    "EquivalenceCriterion",
+    "generate_profile",
+    "StateType",
+    "verify",
+    "verify_compilation",
+]
