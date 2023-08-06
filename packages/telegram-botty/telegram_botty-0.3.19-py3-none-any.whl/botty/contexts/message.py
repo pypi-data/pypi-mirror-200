3c7d274f9ecbed2ae1a482fd75daa860e26fa021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:20 +0000

    Add package telegram-botty/0.3.19/telegram_botty-0.3.19-py3-none-any.whl

---
+++ packages/telegram-botty/telegram_botty-0.3.19-py3-none-any.whl/botty/contexts/message.py
@@ -0,0 +1,33 @@
+from botty_core import Callback, Message, ReplyMarkup
+
+from .update import UpdateContext
+
+
+class MessageContext(UpdateContext):
+    @property
+    def message(self) -> Message:
+        return self.effective_message
+
+    async def reply(
+        self,
+        text: str,
+        markup: ReplyMarkup | None = None,
+    ) -> Message:
+        return await self.message.reply(text, markup)
+
+    async def edit(
+        self,
+        text: str,
+        markup: ReplyMarkup | None = None,
+    ) -> Message | bool:
+        return await self.message.edit(text, markup)
+
+    async def copy(
+        self,
+        message_id: int | None = None,
+        chat_id: int | None = None,
+    ) -> int:
+        return await self.chat.copy(message_id or self.message.id, chat_id)
+
+
+MessageCallback = Callback[MessageContext]
