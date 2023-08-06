49f097d6df09e68deeb98276c979627453363b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:00 +0000

    Add package pydatalysis/0.1.81/pydatalysis-0.1.81.tar.gz

---
+++ packages/pydatalysis/pydatalysis-0.1.81.tar.gz/pydatalysis-0.1.81/pydatalysis/__init__.py
@@ -0,0 +1,7 @@
+from pydatalysis.linear_regression import linear_regression
+from pydatalysis.logistic_regression import logistic_regression
+
+__all__ = [
+    'linear_regression',
+    'logistic_regression'
+]
