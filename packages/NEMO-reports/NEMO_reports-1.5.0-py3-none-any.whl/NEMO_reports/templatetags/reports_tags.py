88e476978ca5144c2c204a9119e4c07cfaaae1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package NEMO-reports/1.5.0/NEMO_reports-1.5.0-py3-none-any.whl

---
+++ packages/NEMO-reports/NEMO_reports-1.5.0-py3-none-any.whl/NEMO_reports/templatetags/reports_tags.py
@@ -0,0 +1,16 @@
+from NEMO.views.customization import CustomizationBase
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def app_installed(app_name):
+    from django.apps import apps
+
+    return apps.is_installed(app_name)
+
+
+@register.filter
+def customization(customization_key, key):
+    return CustomizationBase.get_instance(customization_key).get(key)
