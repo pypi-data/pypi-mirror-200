06d92c1faa44bb2f29111fc4633205124a906efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:41 +0000

    Add package fractal-toolkit/3.1.3/fractal_toolkit-3.1.3-py3-none-any.whl

---
+++ packages/fractal-toolkit/fractal_toolkit-3.1.3-py3-none-any.whl/fractal/core/event_sourcing/message.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any
+
+
+@dataclass
+class Message:
+    id: str
+    occurred_on: datetime
+    event: str
+    data: Any
+    object_id: str
+    aggregate_root_id: str
