6797b67280b13b003cf3928c0099ba4e9389e259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:29 +0000

    Add package motion-python/0.1.2/motion_python-0.1.2-py3-none-any.whl

---
+++ packages/motion-python/motion_python-0.1.2-py3-none-any.whl/motion/exampleproj/triggers/chatbot.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import os
+
+import cohere
+
+import motion
+
+
+class Chatbot(motion.Trigger):
+    def routes(self):
+        return [
+            motion.Route(
+                relation="chat",
+                key="prompt",
+                infer=self.getCompletion,
+                fit=None,
+            )
+        ]
+
+    def setUp(self, cursor):
+        # Set up the LLM
+        return {"cohere": cohere.Client(os.environ["COHERE_API_KEY"])}
+
+    def getCompletion(self, cursor, triggered_by):
+        # Generate the completion from the LLM
+        prompt = triggered_by.value
+        response = self.state["cohere"].generate(
+            prompt=prompt,
+            model="command-xlarge-nightly",
+            max_tokens=300,
+            temperature=0.9,
+            k=0,
+            p=0.75,
+            stop_sequences=[],
+            return_likelihoods="NONE",
+        )
+        completion = response[0].text
+        cursor.set(
+            triggered_by.relation,  # "chat"
+            identifier=triggered_by.identifier,
+            key_values={"completion": completion},
+        )
