d92def2e15a831e78f7517eaf8fe948f85c31d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:37 +0000

    Add package pasta-eln/2.3.6/pasta_eln-2.3.6-py3-none-any.whl

---
+++ packages/pasta-eln/pasta_eln-2.3.6-py3-none-any.whl/pasta_eln/tempStrings.py
@@ -0,0 +1,41 @@
+""" ##Automatically created file: do not update manually """
+todoString = """
+
+<h3>Very important show stopper</h3>
+<ul>
+</ul>
+
+<h3>To be verified/identified</h3>
+<ul>
+</ul>
+
+<h3>Improvement to convenience</h3>
+<ul>
+<li>
+<li> convenience: allow scroll in sidebar
+<li> convenience: selection does not scroll; one cannot select a row
+<li> drag&drop: external files
+<li> invert filter: not t in name
+<li> rerunExtractors: as batch
+<li> try to get rid of cryptography: too much requirements
+</ul>
+
+<h3>Bigger things implemented soon</h3>
+<ul>
+<li> change procedure: on disk / database -> conflicts
+<li> question? what is the difference to TextButton?
+<li> tableHeaderChange: requires view to change to views, not ontology
+</ul>
+
+<h3>Things worthwile remembering/uncritical</h3>
+<ul>
+<li> Aug 2023: remove
+<li> addToConfig
+<li> addToConfig
+<li> addToConfig
+<li> if successful in Aug2023: remove
+<li> if successful in Aug2023: remove "echo....read"
+<li> moreChecksDB: if parent has corresponding show
+<li> serverConfiguration: this should become a GUI and CLI and separate into three-files: functions, CLI, GUI
+</ul>
+"""
