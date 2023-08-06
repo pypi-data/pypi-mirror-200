3419998d049fc6dfb049ed66830420f38f0c8760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:45 +0000

    Add package testKuldeep/0.0.8/testKuldeep-0.0.8.tar.gz

---
+++ packages/testKuldeep/testKuldeep-0.0.8.tar.gz/testKuldeep-0.0.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setup(
+    name='testKuldeep',
+    version='0.0.8',
+    description='testing databricks',
+    author= 'Kuldeep Rathore',
+    #url = 'https://github.com/Spidy20/PyMusic_Player',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords=['python', 'databricks'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['testKuldeep'],
+    package_dir={'':'src'},
+	install_requires = [
+        'pyspark',
+        'DButils',
+    ]
+)
