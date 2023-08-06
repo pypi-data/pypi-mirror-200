08da3bcc8c1db1caf6bb1c6ca47ee5076dbbea2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:21 +0000

    Add package flask-starter/0.5.0/flask-starter-0.5.0.tar.gz

---
+++ packages/flask-starter/flask-starter-0.5.0.tar.gz/flask-starter-0.5.0/script.py
@@ -0,0 +1,24 @@
+import os
+import click
+
+"""
+#process_with_auth
+need to copy extra files in templates and static  
+#process_without_auth 
+"""
+
+
+@click.command()
+def main():
+    os.system("git init")
+    os.system("git clone https://github.com/princekrroshan01/flask-starter.git")
+    os.remove("flask-starter/script.py")
+    os.remove("flask-starter/setup.py")
+    os.remove("flask-starter/LICENSE")
+    os.remove("flask-starter/.gitignore")
+    os.remove("flask-starter/README.md")
+    os.remove("flask-starter/requirements.txt")
+
+
+if __name__ == "__main__":
+    main()
