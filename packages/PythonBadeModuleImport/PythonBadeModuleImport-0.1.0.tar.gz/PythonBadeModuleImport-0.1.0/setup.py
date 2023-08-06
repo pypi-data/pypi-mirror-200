877159839ed897d5696f78ddd2727c71ba86d20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:36 +0000

    Add package PythonBadeModuleImport/0.1.0/PythonBadeModuleImport-0.1.0.tar.gz

---
+++ packages/PythonBadeModuleImport/PythonBadeModuleImport-0.1.0.tar.gz/PythonBadeModuleImport-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PythonBadeModuleImport',
+    version='0.1.0',
+    description='A basic Function',
+    author='John Bennet',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
