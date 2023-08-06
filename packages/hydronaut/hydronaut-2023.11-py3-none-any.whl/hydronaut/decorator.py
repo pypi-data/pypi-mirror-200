4069cfc13a025009b9c535b532b78d75e9b20fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:00 +0000

    Add package hydronaut/2023.11/hydronaut-2023.11-py3-none-any.whl

---
+++ packages/hydronaut/hydronaut-2023.11-py3-none-any.whl/hydronaut/decorator.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+'''
+Decorators for running code with Hydronaut.
+'''
+
+import logging
+import types
+from typing import Callable
+
+from hydronaut.run import Runner
+from hydronaut.types import Path, Decorable, Runable
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def with_hydronaut(config_path: Path = None) -> Callable[Decorable, Runable]:
+    '''
+    A decorator to run custom user code with Hydronaut.
+
+    Args:
+        config_path:
+            Passed through to hydronaut.run.Runner.
+
+    Returns:
+        A decorator for user classes and functions.
+    '''
+
+    def _with_hydronaut(user_code: Decorable) -> Runable:
+        '''
+        A decorator for custom user code.
+
+        Args:
+            user_code:
+                Either a class that with the following methods:
+                    __init__(self, config):
+                        An initialization method that accepts the Hydra
+                        configuration object as its sole argument.
+
+                    __call__(self):
+                        A call method that returns the experiment's objective
+                        values.
+
+                    setup(self):
+                        An optional method that will be invoked before __call__ when
+                        the experiment is run. This can be used to e.g. prepare
+                        data.
+
+                or a function that accept the Hydra configuration object as its sole
+                argument and which returns the experiment's objective values.
+
+        Returns:
+            A callable object that runs the user code within the Hydronaut framework.
+        '''
+        class CustomRunner(Runner):  # pylint: disable=too-few-public-methods
+            '''
+            Create a custom Runner subclass to wrap the user code.
+            '''
+            def get_experiment_object(self):
+                # If the user code is a simple function, defer calling.
+                if isinstance(user_code, types.FunctionType):
+                    def exp_obj():
+                        return user_code(self.config)
+                # Else assume that it is a class.
+                else:
+                    exp_obj = user_code(self.config)
+                    if not isinstance(exp_obj, Callable):
+                        LOGGER.warning('%s does not create callable objects.', user_code)
+
+                return exp_obj
+
+        return CustomRunner(config_path=config_path)
+    return _with_hydronaut
