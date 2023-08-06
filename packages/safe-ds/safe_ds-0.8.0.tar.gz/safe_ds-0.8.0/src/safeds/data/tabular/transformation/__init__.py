57f41845263d815d6af21b8148405900d57d57bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:49 +0000

    Add package safe-ds/0.8.0/safe_ds-0.8.0.tar.gz

---
+++ packages/safe-ds/safe_ds-0.8.0.tar.gz/safe_ds-0.8.0/src/safeds/data/tabular/transformation/__init__.py
@@ -0,0 +1,3 @@
+from ._imputer import Imputer, ImputerStrategy
+from ._label_encoder import LabelEncoder
+from ._one_hot_encoder import OneHotEncoder
---
+++ packages/safe-ds/safe_ds-0.8.0.tar.gz/safe_ds-0.8.0/src/safeds/data/tabular/typing/__init__.py
@@ -0,0 +1,2 @@
+from ._column_type import Anything, Boolean, ColumnType, Integer, RealNumber, String
+from ._schema import Schema
