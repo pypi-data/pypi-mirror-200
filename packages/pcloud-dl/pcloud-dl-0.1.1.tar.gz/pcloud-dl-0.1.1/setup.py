0d158131834044de58d98c7fa3ddc21fbb5c677a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:51 +0000

    Add package pcloud-dl/0.1.1/pcloud-dl-0.1.1.tar.gz

---
+++ packages/pcloud-dl/pcloud-dl-0.1.1.tar.gz/pcloud-dl-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pcloud-dl",
+    version="0.1.1",
+    author="JavierSC",
+    description="pcloud downloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JavSensei/pcloud-dl",
+    packages=find_packages(),
+    install_requires=["tqdm"],
+    entry_points={'console_scripts': ['pcloud-dl=pcloud_dl.cli:main']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Utilities'
+    ],
+    python_requires=">=3.6",
+)
