bf6953c4e8fe3711c4f5bfb822a2d6fc50f392a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:13 +0000

    Add package ioNERDSS/1.0.4/ioNERDSS-1.0.4.tar.gz

---
+++ packages/ioNERDSS/ioNERDSS-1.0.4.tar.gz/ioNERDSS-1.0.4/ioNERDSS/functions/icos_face_COM_leg_coord.py
@@ -0,0 +1,13 @@
+import mid_pt
+import icos_face_COM_coord
+
+
+def icos_face_COM_leg_coord(a: float, b: float, c: float):
+    COM_leg = []
+    COM_leg.append(icos_face_COM_coord(a, b, c))
+    COM_leg.append(mid_pt(a, b))
+    COM_leg.append(mid_pt(b, c))
+    COM_leg.append(mid_pt(c, a))
+    return COM_leg
+
+
