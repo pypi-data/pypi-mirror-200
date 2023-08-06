6a3000fa54da4d9b0228a71ddf51b129a1ca939c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:44 +0000

    Add package pytest-instafail/0.5.0/pytest-instafail-0.5.0.tar.gz

---
+++ packages/pytest-instafail/pytest-instafail-0.5.0.tar.gz/pytest-instafail-0.5.0/README.rst
@@ -0,0 +1,38 @@
+pytest-instafail
+================
+
+|build status|_
+
+.. |build status| image:: https://github.com/pytest-dev/pytest-instafail/actions/workflows/test.yml/badge.svg
+   :alt: Build Status
+.. _build status: https://github.com/pytest-dev/pytest-instafail/actions/workflows/test.yml
+
+pytest-instafail is a plugin for `pytest <https://pytest.org>`_ that shows
+failures and errors instantly instead of waiting until the end of test session.
+
+Requirements
+------------
+
+You will need the following prerequisites in order to use pytest-instafail:
+
+- Python 3.7+ or PyPy3
+- pytest 5 or newer
+
+Installation
+------------
+
+To install pytest-instafail::
+
+    $ pip install pytest-instafail
+
+Then run your tests with::
+
+    $ pytest --instafail
+
+Resources
+---------
+
+- `Issue Tracker <https://github.com/pytest-dev/pytest-instafail/issues>`_
+- `Code <https://github.com/pytest-dev/pytest-instafail/>`_
+- `Development Version
+  <https://github.com/pytest-dev/pytest-instafail/zipball/master#egg=pytest-instafail-dev>`_
---
+++ packages/pytest-instafail/pytest-instafail-0.5.0.tar.gz/pytest-instafail-0.5.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+    name='pytest-instafail',
+    description='pytest plugin to show failures instantly',
+    long_description=open("README.rst").read(),
+    version='0.5.0',
+    url='https://github.com/pytest-dev/pytest-instafail',
+    license='BSD',
+    author='Janne Vanhala',
+    author_email='janne.vanhala@gmail.com',
+    py_modules=['pytest_instafail'],
+    entry_points={'pytest11': ['instafail = pytest_instafail']},
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['pytest>=5'],
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
+)
