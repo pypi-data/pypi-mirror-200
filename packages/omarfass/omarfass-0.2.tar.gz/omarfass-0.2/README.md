b44e2e2a0df181461ee0f4bcd9bea6a07291f6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:21 +0000

    Add package omarfass/0.2/omarfass-0.2.tar.gz

---
+++ packages/omarfass/omarfass-0.2.tar.gz/omarfass-0.2/README.md
@@ -0,0 +1,41 @@
+# omar 
+## om 
+
+**vmv**
+
+*ppppppppppp*
+
+jdvjvd^pp^
+
+djvnjvnsj~ggd~
+
+vvmkmv ~~cdmc~~
+
+* python 
+* c++
+* C#
+  * python c++ php
+* php
+  
+1. jv
+2. ovjfnb
+___
+| Name | code | email |
+|----|----|---|
+|omar|10|omar@gamli.com|
+___
+AAAAAAAAAAAA
+
+[ google ](https://google.com)
+![ gooleff ](https://www.cairo24.com/Upload/libfiles/79/6/50.jpg)
+
+___
+``` bash 
+pip install omarpppl
+``` 
+___
+``` python
+def i ():
+    print("fnfngfnj")
+```
+<h1 style="color:red; text-align: center;">omar</h1>
---
+++ packages/omarfass/omarfass-0.2.tar.gz/omarfass-0.2/setup.py
@@ -0,0 +1,43 @@
+# import setuptools
+# setuptools.setup(
+#     name="omarlibone",
+#     version="1.1",
+#     author="omar Style",
+#     description="omar aAAA",
+#     packages=setuptools.find_packages(),
+#     classifiers=[
+#     "Programming Language :: Python :: 3 ",
+#     "Operating System :: OS Independent",
+#     "Natural Language :: English",
+#     "License :: OSI Approved :: MIT License"
+#     ]
+    
+# )
+
+import setuptools
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+# with open('requirements.txt','r') as fr:
+#     requires = fr.read().split('\n')
+
+setuptools.setup(
+    name='omarfass',
+    version="0.2",
+    author='omar Style',
+    author_email='omarllStyle@gmail.com',
+    description='Omarrrrrv',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Omarail1/omarpoop.git',
+    packages=['omarfass'],
+    install_requires=["requests"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License'],
+    
+   
+)
