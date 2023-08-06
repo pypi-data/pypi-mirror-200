5b6db39e57fd14dbb871cb2f78783df3c1015c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:51 +0000

    Add package panorama-backpack/0.3.1/panorama_backpack-0.3.1-py3-none-any.whl

---
+++ packages/panorama-backpack/panorama_backpack-0.3.1-py3-none-any.whl/backpack/autoidentity.py
@@ -0,0 +1,171 @@
+''' This module contains the :class:`AutoIdentity` class that provides information about the
+application execution environment. '''
+
+import os
+import logging
+import datetime
+from typing import Dict, Optional, Iterator, Any
+import time
+
+import boto3
+from pydantic import BaseModel, Field
+
+class AutoIdentityData(BaseModel):
+    ''' Data class to store auto identity information. '''
+
+    application_instance_id: str = Field(alias='ApplicationInstanceId')
+    ''' Application instance id. '''
+
+    application_name: str = Field(alias='Name')
+    ''' Name of this application. '''
+
+    application_tags: Dict[str, str] = Field(alias='Tags')
+    ''' Tags associated with this application. '''
+
+    device_id: str = Field(alias='DefaultRuntimeContextDevice')
+    ''' Device id of the appliance running this application. '''
+
+    device_name: str = Field(alias='DefaultRuntimeContextDeviceName')
+    '''  Name of this application. '''
+
+    application_created_time: datetime.datetime = Field(alias='CreatedTime')
+    ''' Application deployment time. '''
+
+    application_status: str = Field(alias='HealthStatus')
+    ''' Health status of this application. '''
+
+    application_description: str = Field(alias='Description')
+    ''' The description of this application. '''
+
+    @classmethod
+    def for_test_environment(cls, application_instance_id: str, application_name: str):
+        ''' Initializes a dummy AutoIdentityData to be used in test environment. '''
+        return cls(
+            ApplicationInstanceId=application_instance_id,
+            Name=application_name,
+            Tags={},
+            DefaultRuntimeContextDevice='emulator',
+            DefaultRuntimeContextDeviceName='test_utility_emulator',
+            CreatedTime=datetime.datetime.now(),
+            HealthStatus='TEST_UTILITY',
+            Description=application_name,
+        )
+
+
+class AutoIdentityFetcher:
+    ''' AutoIdentity instance queries metadata of the current application instance.
+
+    The IAM policy associated with the `Panorama Application Role`_
+    of this app should grant the execution of
+    `panorama:ListApplicationInstances`_ operation.
+
+    Args:
+        device_region: The AWS region where this Panorama appliance is registered.
+        application_instance_id: The application instance id. If left to `None`,
+            :class:`AutoIdentity` will try to find the instance id in the environment variable.
+        parent_logger: If you want to connect the logger to a parent, specify it here.
+
+    .. _`Panorama Application Role`:
+        https://docs.aws.amazon.com/panorama/latest/dev/permissions-application.html
+    .. _`panorama:ListApplicationInstances`:
+        https://docs.aws.amazon.com/service-authorization/latest/reference/list_awspanorama.html#awspanorama-actions-as-permissions
+    '''
+
+    # pylint: disable=too-many-instance-attributes,too-few-public-methods
+    # This class functions as a data class that reads its values from the environment
+
+    def __init__(self,
+        device_region: str,
+        application_instance_id: Optional[str] = None,
+        parent_logger: Optional[logging.Logger] = None
+    ):
+        self._logger = (
+            logging.getLogger(self.__class__.__name__) if parent_logger is None else
+            parent_logger.getChild(self.__class__.__name__)
+        )
+        self.application_instance_id = (
+            application_instance_id or os.environ.get('AppGraph_Uid')
+        )
+        if not self.application_instance_id:
+            raise RuntimeError(
+                'Could not find application instance id in environment variable "AppGraph_Uid"'
+            )
+        self.device_region = device_region
+
+    def get_data(self,
+        retry_freq: Optional[float] = 5,
+        max_retry_freq: Optional[float] = 300,
+        max_retry_num: Optional[int] = 50) -> AutoIdentityData:
+        ''' Fetches the auto identity data.
+
+        Args:
+            retry_freq (Optional[float]): If set to a float number, AutoIdentity will keep retrying
+                fetching the auto identity data from remote services if the status of the app was
+                "NOT_AVAILABLE". If set to None, will not retry.
+
+        Raises:
+            RuntimeError: if could not fetch the auto identity information, and retry_freq is set
+                to None.
+        '''
+
+        def fetch() -> Dict[str, Any]:
+            app_instance_data = self._app_instance_data(self.application_instance_id)
+            if not app_instance_data:
+                raise RuntimeError(
+                    'Could not find application instance in service response. '
+                    'Check if application_instance_id=%s '
+                    'and device_region=%s parameters are correct.'
+                    .format(self.application_instance_id, self.device_region)
+                )
+            else:
+                return app_instance_data
+
+        retries = 0
+        while True:
+            app_instance_data = fetch()
+            status = app_instance_data.get('HealthStatus', 'NOT_AVAILABLE')
+            self._logger.info('Application HealthStatus=%s', status)
+            if status in ('NOT_AVAILABLE'):
+                if retry_freq is None:
+                    raise RuntimeError(
+                        f'Application HealthStatus is "{status}" and retry is disabled.'
+                    )
+                else:
+                    self._logger.info('Will retry fetching auto identity data in %s seconds.', retry_freq)
+                    time.sleep(retry_freq)
+                    retry_freq = retry_freq * 2
+                    if max_retry_freq is not None:
+                        retry_freq = min(retry_freq, max_retry_freq)
+                    retries += 1
+                    if max_retry_num is not None and retries > max_retry_num:
+                         raise RuntimeError(
+                            'Maximum number of retries reached.'
+                        )
+                    else:
+                        continue
+            else:
+                return AutoIdentityData(**app_instance_data)
+
+    def _list_app_instances(self, deployed_only=True) -> Iterator[Dict[str, Any]]:
+        session = boto3.Session(region_name=self.device_region)
+        panorama = session.client('panorama')
+        next_token = None
+        while True:
+            kwargs = {'StatusFilter': 'DEPLOYMENT_SUCCEEDED'} if deployed_only else {}
+            if next_token:
+                kwargs['NextToken'] = next_token
+            response = panorama.list_application_instances(**kwargs)
+            inst: Dict[str, Any]
+            for inst in response['ApplicationInstances']:
+                yield inst
+            if 'NextToken' in response:
+                next_token = response['NextToken']
+            else:
+                break
+
+    def _app_instance_data(self, application_instance_id) -> Optional[Dict[str, Any]]:
+        matches = (inst
+            for inst in self._list_app_instances()
+            if inst.get('ApplicationInstanceId') == application_instance_id
+        )
+        return next(matches, None)
---
+++ packages/panorama-backpack/panorama_backpack-0.3.1-py3-none-any.whl/backpack/config/config.py
@@ -0,0 +1,201 @@
+''' This module defines :class:`~backpack.config.ConfigBase`, a base class for Panorama application
+configurations. The class offers two basic functionalities:
+
+- parse parameters from the Panorama application input ports (``panoramasdk.node.inputs``)
+- generate configuration file snippets for ``graph.json`` and ``package.json`` in your Panorama
+  project. For more information about how to use the CLI, refer to :meth:`~backpack.config.tool`.
+'''
+
+import dataclasses
+from typing import Sequence, List, Any, Type, TypeVar, Mapping, Tuple, Optional
+import textwrap
+
+from .serde import ConfigSerDeBase
+
+T = TypeVar('T', bound='ConfigBase')
+
+class ConfigBase:
+    ''' Base class for configuration structures.
+
+    Subclasses must be also dataclasses.
+    '''
+
+    TYPE_MAP = {
+        int: 'int32',
+        float: 'float32',
+        str: 'string',
+        bool: 'boolean'
+    }
+
+    def __init__(self) -> None:
+        assert dataclasses.is_dataclass(self), 'ConfigBase instances must be also dataclasses.'
+
+    @staticmethod
+    def _get_param_name(full_path: Sequence[str]=[]) -> str:
+        return '_'.join(full_path)
+
+    @staticmethod
+    def _get_param_type(field: dataclasses.field) -> str:
+        if 'type' in field.metadata:
+            typename = field.metadata['type']
+        else:
+            typename = ConfigBase.TYPE_MAP.get(field.type)
+        if typename is None:
+            raise ValueError(f'Field has unsupported type: {field}')
+        return typename
+
+    @staticmethod
+    def _get_param_serde(field: dataclasses.field) -> Optional[Type[ConfigSerDeBase]]:
+        return field.metadata.get('serde')
+
+    @staticmethod
+    def _get_param_default(
+        field: dataclasses.field,
+        serde_metadata: Mapping[str, Any]
+    ) -> Optional[str]:
+        if field.default is not dataclasses.MISSING:
+            default = field.default
+        elif field.default_factory  is not dataclasses.MISSING:
+            default = field.default_factory()
+        else:
+            default = None
+        serde = ConfigBase._get_param_serde(field)
+        if serde is not None and default is not None:
+            default = serde.serialize(default, metadata=serde_metadata)
+        return default
+
+    @staticmethod
+    def _get_param_doc(field: dataclasses.field) -> Optional[str]:
+        doc: str = field.metadata.get('__doc__', field.metadata.get('doc'))
+        doc = textwrap.dedent(doc)
+        doc = doc.replace('\n', ' ')
+        doc = doc.replace('  ', ' ')
+        doc = doc.strip()
+        return doc
+
+    def _param_walker(self, _current_path: Sequence[str]=[]) -> Tuple[str, dataclasses.field]:
+        ''' Recursively walks all parameters in the config structure.
+
+        Args:
+            _current_path (Sequence[str]): The current path in the config structure. This is an
+                internal recursion parameter and users should always leave the default empty
+                list value.
+
+        Returns:
+            A generator that yields a tuple for each parameter. The tuple consists of the
+            following values:
+                - full_path (Sequence[str]): The full path of the parameter in the hierarchy
+                - field (dataclasses.field): The original field of the
+        '''
+        fields = dataclasses.fields(self)
+        for fld in fields:
+            current_path = _current_path + [fld.name]
+            if dataclasses.is_dataclass(fld.type):
+                obj = getattr(self, fld.name)
+                for sub_result in obj._param_walker(_current_path=current_path):
+                    yield sub_result
+            else:
+                yield (current_path, fld)
+
+    def get_panorama_definitions(self, serde_metadata: Mapping[str, Any]={}) -> List[Mapping[str, Any]]:
+        ''' Generate the ``nodeGraph.nodes`` snippet in ``graph.json``.
+
+        Returns:
+            A list of dictionaries containing the application parameter node definitions.
+        '''
+        return [
+            {
+                'name': ConfigBase._get_param_name(full_path=full_path),
+                'interface': ConfigBase._get_param_type(field=field),
+                'value': ConfigBase._get_param_default(field=field, serde_metadata=serde_metadata),
+                'overridable': True,
+                'decorator': {
+                    'title': ConfigBase._get_param_name(full_path=full_path),
+                    'description': ConfigBase._get_param_doc(field=field)
+                }
+            }
+            for (full_path, field) in self._param_walker()
+        ]
+
+    def get_panorama_edges(self, code_node_name: str) -> List[Mapping[str, str]]:
+        ''' Generate the ``nodeGraph.edges`` snippet in ``graph.json``
+
+        Returns:
+            A list of dictionaries containing the application edge definitions.
+        '''
+        return [
+            {
+                "producer": ConfigBase._get_param_name(full_path=full_path),
+                "consumer": code_node_name + "." + ConfigBase._get_param_name(full_path=full_path)
+            }
+            for (full_path, _) in self._param_walker()
+        ]
+
+    def get_panorama_app_interface(self) -> List[Mapping[str, str]]:
+        ''' Generate the application interface snippet in app node ``package.json``.
+
+        Returns:
+            A list of dictionaries containing the elements of the application interface definition.
+        '''
+        return [
+            {
+                "name": ConfigBase._get_param_name(full_path=full_path),
+                "type": ConfigBase._get_param_type(field=field)
+            }
+            for (full_path, field) in self._param_walker()
+        ]
+
+    def get_panorama_markdown_doc(self, serde_metadata: Mapping[str, Any]={}) -> str:
+        ''' Generates a markdown table of the parameters that can be used in documentation.
+
+        Returns:
+            Markdown formatted text containing the parameter documentation.
+        '''
+        header = (
+            '| name | type    | default | description |\n'
+            '|------|---------|---------|-------------|\n'
+        )
+        body = '\n'.join([
+            f'| {ConfigBase._get_param_name(full_path=full_path)} '
+            f'| {ConfigBase._get_param_type(field=field)} '
+            f'| {ConfigBase._get_param_default(field=field, serde_metadata=serde_metadata)} '
+            f'| {ConfigBase._get_param_doc(field=field)} |'
+            for (full_path, field) in self._param_walker()
+        ])
+        return header + body
+
+    @classmethod
+    def from_panorama_params(cls: Type[T],
+        inputs: 'panoramasdk.port', # type: ignore
+        serde_metadata: Mapping[str, Any]={}
+    ) -> T:
+        ''' Parses the config values form AWS Panorama input parameters.
+
+        A new Config object is created with the default values. If a particular value is
+        found in the input parameter, its value will override the default value.
+
+        Args:
+            inputs (panoramasdk.port): The input port of the Panorama application node.
+
+        Returns:
+            The config instance filled with the parameter values read from the input port.
+        '''
+        result = cls()
+
+        for (full_path, fld) in result._param_walker():
+            obj = result
+            for name_part in full_path[:-1]:
+                obj = getattr(obj, name_part)
+            key = full_path[-1]
+            name = ConfigBase._get_param_name(full_path=full_path)
+            if not hasattr(inputs, name):
+                continue
+            value = getattr(inputs, name).get()
+            serde = ConfigBase._get_param_serde(field=fld)
+            if serde is not None:
+                value = serde.deserialize(value, metadata=serde_metadata)
+            setattr(obj, key, value)
+        return result
+
+    def asdict(self):
+        return dataclasses.asdict(self)
