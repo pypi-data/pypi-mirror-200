fd4d24a5cd5100c95c71235d6cbeb5414a9be06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:21 +0000

    Add package Geode-ExplicitModeling/2.3.8/Geode_ExplicitModeling-2.3.8-cp37-cp37m-win_amd64.whl

---
+++ packages/Geode-ExplicitModeling/Geode_ExplicitModeling-2.3.8-cp37-cp37m-win_amd64.whl/geode_explicitmodeling/explicit_modeling3d.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_conversion
+import geode_backgroundmesh
+
+from .geode_explicitmodeling_py_explicit_modeling3d import *
+ExplicitModeling3d.initialize()
