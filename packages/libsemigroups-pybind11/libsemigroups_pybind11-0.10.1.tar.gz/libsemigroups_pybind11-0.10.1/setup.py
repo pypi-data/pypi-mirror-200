37412768ee5a1f7846bfd692d17dc7446a3b9d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:42 +0000

    Add package libsemigroups-pybind11/0.10.1/libsemigroups_pybind11-0.10.1.tar.gz

---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-0.10.1.tar.gz/libsemigroups_pybind11-0.10.1/setup.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021, J. D. Mitchell + Maria Tsalakou
+#
+# Distributed under the terms of the GPL license version 3.
+#
+# The full license is in the file LICENSE, distributed with this software.
+
+"""
+This package provides a python interface to the C++ library
+libsemigroups (libsemigroups.rtfd.io).
+"""
+
+import glob
+import os
+import sys
+from pprint import pprint
+
+import pkgconfig
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, find_packages
+
+__dir__ = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, __dir__ + "/libsemigroups_pybind11")
+
+from tools import (  # pylint: disable=import-error, wrong-import-position
+    libsemigroups_version,
+    minimum_libsemigroups_version,
+    compare_version_numbers,
+)
+
+__version__ = "0.10.1"
+
+pprint(os.environ)
+
+if "PKG_CONFIG_PATH" not in os.environ:
+    os.environ["PKG_CONFIG_PATH"] = ""
+
+pkg_config_path = os.environ["PKG_CONFIG_PATH"].split(":")
+
+if "CONDA_PREFIX" in os.environ:
+    conda_env_pkg_config = os.path.join(
+        os.environ["CONDA_PREFIX"], "lib", "pkgconfig"
+    )
+    if (
+        os.path.exists(conda_env_pkg_config)
+        and not conda_env_pkg_config in pkg_config_path
+    ):
+        os.environ["PKG_CONFIG_PATH"] += ":" + conda_env_pkg_config
+
+if "CONDA_DEFAULT_ENV" in os.environ and "CONDA_ENVS_PATH" in os.environ:
+    conda_env_pkg_config = os.path.join(
+        os.environ["CONDA_ENVS_PATH"],
+        os.environ["CONDA_DEFAULT_ENV"],
+        "lib",
+        "pkgconfig",
+    )
+    if (
+        os.path.exists(conda_env_pkg_config)
+        and not conda_env_pkg_config in pkg_config_path
+    ):
+        os.environ["PKG_CONFIG_PATH"] += ":" + conda_env_pkg_config
+
+if "/usr/local/lib/pkgconfig" not in pkg_config_path:
+    if (
+        "PKG_CONFIG_PATH" in os.environ
+        and not len(os.environ["PKG_CONFIG_PATH"]) == 0
+    ):
+        os.environ["PKG_CONFIG_PATH"] += ":/usr/local/lib/pkgconfig"
+    else:
+        os.environ["PKG_CONFIG_PATH"] = "/usr/local/lib/pkgconfig"
+
+if not pkgconfig.exists("libsemigroups"):
+    raise ImportError("cannot locate libsemigroups library")
+
+if not compare_version_numbers(
+    libsemigroups_version(), minimum_libsemigroups_version()
+):
+    raise ImportError(
+        f"libsemigroups version at least {minimum_libsemigroups_version()}"
+        + f" is required, found {libsemigroups_version()}"
+    )
+
+
+class GetPyBind11Include:  # pylint: disable=too-few-public-methods
+    """
+    Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked.
+    """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11  # pylint: disable=import-outside-toplevel
+
+        return pybind11.get_include(self.user)
+
+
+LIBRARY_PATH = pkgconfig.pkgconfig._query(  # pylint: disable=protected-access
+    "libsemigroups", "--libs-only-L"
+)
+
+# The above pkgconfig query can return an empty string (this also happens on
+# the command line). This happens, for example, using pkg-config version 1.8.0
+# on ArchLinux. CN 27/10/2021
+
+assert (
+    len(LIBRARY_PATH) == 0 or LIBRARY_PATH[:2] == "-L"
+), "The first two characters of the library path to the libsemigroups.so etc should be '-L'"
+
+# Try to use pkg-config to add the path to libsemigroups.so etc to
+# LD_LIBRARY_PATH so that python can find it.
+
+if len(LIBRARY_PATH) != 0:
+    LIBRARY_PATH_NO_L = LIBRARY_PATH[2:]
+else:
+    LIBRARY_PATH_NO_L = "/usr/lib"
+    LIBRARY_PATH = "-L/usr/lib"
+
+if os.path.exists(LIBRARY_PATH_NO_L):
+    if (
+        "LD_LIBRARY_PATH" in os.environ
+        and len(os.environ["LD_LIBRARY_PATH"]) != 0
+    ):
+        LD_LIBRARY_PATH = os.environ["LD_LIBRARY_PATH"]
+        if LD_LIBRARY_PATH.find(LIBRARY_PATH_NO_L) == -1:
+            PREFIX = "" if LD_LIBRARY_PATH[-1] == ":" else ":"
+            os.environ["LD_LIBRARY_PATH"] += PREFIX + LIBRARY_PATH_NO_L
+    else:
+        os.environ["LD_LIBRARY_PATH"] = LIBRARY_PATH_NO_L
+    print("LD_LIBRARY_PATH is:")
+    print(os.environ["LD_LIBRARY_PATH"])
+
+LIBSEMIGROUPS_CFLAGS_ONLY_I = (
+    pkgconfig.pkgconfig._query(  # pylint: disable=protected-access
+        "libsemigroups", "--cflags-only-I"
+    )
+)
+
+include_path = [
+    GetPyBind11Include(),
+    GetPyBind11Include(user=True),
+    "/usr/local/include",
+    "/usr/local/include/libsemigroups",
+]
+
+if len(LIBSEMIGROUPS_CFLAGS_ONLY_I) != 0:
+    include_path.extend([x[2:] for x in LIBSEMIGROUPS_CFLAGS_ONLY_I.split(" ")])
+
+if "CONDA_PREFIX" in os.environ:
+    include_path.append(os.path.join(os.environ["CONDA_PREFIX"], "include"))
+    include_path.append(
+        os.path.join(os.environ["CONDA_PREFIX"], "include", "eigen3")
+    )
+
+if "CONDA_DEFAULT_ENV" in os.environ and "CONDA_ENVS_PATH" in os.environ:
+    include_path.append(
+        os.path.join(
+            os.environ["CONDA_ENVS_PATH"],
+            os.environ["CONDA_DEFAULT_ENV"],
+            "include",
+        )
+    )
+    include_path.append(
+        os.path.join(
+            os.environ["CONDA_ENVS_PATH"],
+            os.environ["CONDA_DEFAULT_ENV"],
+            "include",
+            "eigen3",
+        )
+    )
+
+print("Include directories are:")
+pprint(include_path)
+
+ext_modules = [
+    Pybind11Extension(
+        "_libsemigroups_pybind11",
+        glob.glob("src/*.cpp"),
+        include_dirs=include_path,
+        language="c++",
+        libraries=["semigroups"],
+        extra_link_args=[LIBRARY_PATH, "-L/usr/local/lib"],
+    ),
+]
+
+with open(os.path.join(__dir__, "README.md"), "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name="libsemigroups_pybind11",
+    version=__version__,
+    author="James D. Mitchell",
+    author_email="jdm3@st-andrews.ac.uk",
+    url="https://github.com/libsemigroups/libsemigroups_pybind11",
+    description="A python package for the libsemigroups C++ library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    ext_modules=ext_modules,
+    packages=find_packages(),
+    setup_requires=["pkgconfig>=1.5.0"],
+    install_requires=[
+        "pybind11>=2.10.1",
+        "packaging>=20.4",
+        "pkgconfig>=1.5.0",
+    ],
+    tests_require=["pytest==6.2.4"],
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.7.0",
+)
---
+++ packages/libsemigroups-pybind11/libsemigroups_pybind11-0.10.1.tar.gz/libsemigroups_pybind11-0.10.1/src/present.cpp
@@ -0,0 +1,172 @@
+//
+// libsemigroups_pybind11
+// Copyright (C) 2022 James D. Mitchell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// TODO:
+// * iwyu
+
+// C std headers....
+#include <stddef.h>  // for size_t
+#include <stdint.h>  // for int32_t, uint32_t
+
+// C++ stl headers....
+#include <initializer_list>  // for initializer_list
+#include <vector>            // for vector
+
+// libsemigroups....
+#include <libsemigroups/froidure-pin-base.hpp>  // for FroidurePinBase
+#include <libsemigroups/make-present.hpp>       // for make
+#include <libsemigroups/present.hpp>            // for Presentation
+
+// pybind11....
+#include <pybind11/pybind11.h>  // for class_, init, module
+#include <pybind11/stl.h>
+
+// libsemigroups_pybind11....
+#include "main.hpp"  // for init_present
+
+namespace py = pybind11;
+
+namespace libsemigroups {
+
+  namespace {
+
+    template <typename T>
+    std::string presentation_repr(Presentation<T> const& p) {
+      std::ostringstream out;
+
+      out << "<" << (p.contains_empty_word() ? "monoid" : "semigroup")
+          << " presentation with " << p.alphabet().size() << " generators and "
+          << p.rules.size() << " relations of length "
+          << presentation::length(p) + p.alphabet().size() << ">";
+      return out.str();
+    }
+
+    template <typename T>
+    void bind_present(py::module& m, std::string const& name) {
+      using size_type = typename Presentation<T>::size_type;
+
+      py::class_<Presentation<T>>(m, name.c_str())
+          .def(py::init<>())
+          .def(py::init<Presentation<T> const&>())
+          .def("alphabet",
+               py::overload_cast<>(&Presentation<T>::alphabet, py::const_))
+          .def("alphabet",
+               py::overload_cast<size_type>(&Presentation<T>::alphabet))
+          .def("alphabet",
+               py::overload_cast<T const&>(&Presentation<T>::alphabet))
+          .def("alphabet_from_rules", &Presentation<T>::alphabet_from_rules)
+          .def("letter", &Presentation<T>::letter)
+          .def("index", &Presentation<T>::index)
+          .def("contains_empty_word",
+               py::overload_cast<>(&Presentation<T>::contains_empty_word,
+                                   py::const_))
+          .def("contains_empty_word",
+               py::overload_cast<bool>(&Presentation<T>::contains_empty_word))
+          .def_readwrite("rules", &Presentation<T>::rules)
+          .def("validate_alphabet",
+               py::overload_cast<>(&Presentation<T>::validate_alphabet,
+                                   py::const_))
+          .def("validate_letter", &Presentation<T>::validate_letter)
+          .def("validate_rules", &Presentation<T>::validate_rules)
+          .def("validate", &Presentation<T>::validate)
+          .def("__repr__", &presentation_repr<T>);
+
+      m.def("add_rule",
+            py::overload_cast<Presentation<T>&, T const&, T const&>(
+                &presentation::add_rule<T>));
+      m.def("add_rule_and_check",
+            py::overload_cast<Presentation<T>&, T const&, T const&>(
+                &presentation::add_rule_and_check<T>));
+      m.def("add_rules", &presentation::add_rules<T>);
+      m.def("add_identity_rules", &presentation::add_identity_rules<T>);
+      m.def("add_inverse_rules", &presentation::add_inverse_rules<T>);
+      m.def("remove_duplicate_rules", &presentation::remove_duplicate_rules<T>);
+      m.def("remove_trivial_rules", &presentation::remove_trivial_rules<T>);
+      m.def("reduce_complements", &presentation::reduce_complements<T>);
+      m.def("sort_each_rule", &presentation::sort_each_rule<T>);
+      m.def("sort_rules", &presentation::sort_rules<T>);
+      m.def("longest_common_subword", &presentation::longest_common_subword<T>);
+      m.def("replace_subword",
+            py::overload_cast<Presentation<T>&, T const&>(
+                &presentation::replace_subword<T>));
+      m.def(
+          "replace_subword",
+          [](Presentation<T>& p, T const& existing, T const& replace) -> void {
+            presentation::replace_subword(p, existing, replace);
+          });
+      m.def("replace_word", &presentation::replace_word<T>);
+      m.def("length", &presentation::length<T>);
+      m.def("reverse", &presentation::reverse<T>);
+      m.def("normalize_alphabet", &presentation::normalize_alphabet<T>);
+      m.def("remove_redundant_generators",
+            &presentation::remove_redundant_generators<T>);
+      m.def("add_zero_rules", &presentation::add_zero_rules<T>);
+      m.def("are_rules_sorted", &presentation::are_rules_sorted<T>);
+      m.def("change_alphabet", &presentation::change_alphabet<T>);
+      m.def("character", &presentation::character);
+      m.def("letter", &presentation::letter<T>);
+      m.def("first_unused_letter", &presentation::first_unused_letter<T>);
+      m.def("greedy_reduce_length", &presentation::greedy_reduce_length<T>);
+      m.def("longest_rule", [](Presentation<T> const& p) {
+        return std::distance(p.rules.cbegin(), presentation::longest_rule(p));
+      });
+      m.def("longest_rule_length", [](Presentation<T> const& p) {
+        return presentation::longest_rule_length(p);
+      });
+      m.def("shortest_rule", [](Presentation<T> const& p) {
+        return std::distance(p.rules.cbegin(), presentation::shortest_rule(p));
+      });
+      m.def("shortest_rule_length", [](Presentation<T> const& p) {
+        return presentation::shortest_rule_length(p);
+      });
+      m.def("make_semigroup", presentation::make_semigroup<T>);
+
+      m.def(
+          "make",
+          py::overload_cast<FroidurePinBase&>(&make<Presentation<word_type>>));
+
+      m.def("make",
+            [](Presentation<std::string> const& p) -> Presentation<word_type> {
+              return make<Presentation<word_type>>(p);
+            });
+
+      m.def("make",
+            [](Presentation<word_type> const& p) -> Presentation<std::string> {
+              return make<Presentation<std::string>>(p);
+            });
+
+      m.def("make",
+            [](Presentation<word_type> const& p,
+               std::string const&             s) -> Presentation<std::string> {
+              return make<Presentation<std::string>>(p, s);
+            });
+      m.def("is_strongly_compressible",
+            &presentation::is_strongly_compressible<T>);
+      m.def("strongly_compress", &presentation::strongly_compress<T>);
+      m.def("reduce_to_2_generators",
+            &presentation::reduce_to_2_generators<T>,
+            py::arg("p"),
+            py::arg("index") = 0);
+    }
+  }  // namespace
+
+  void init_present(py::module& m) {
+    bind_present<word_type>(m, "PresentationWords");
+    bind_present<std::string>(m, "PresentationStrings");
+  }
+}  // namespace libsemigroups
