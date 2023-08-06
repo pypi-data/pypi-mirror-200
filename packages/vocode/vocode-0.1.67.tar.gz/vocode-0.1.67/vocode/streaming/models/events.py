77a842634ebd359832b2a4f6a045d9329f6e7946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:08 +0000

    Add package vocode/0.1.67/vocode-0.1.67.tar.gz

---
+++ packages/vocode/vocode-0.1.67.tar.gz/vocode-0.1.67/vocode/streaming/models/events.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from vocode.streaming.models.model import TypedModel
+
+
+class Sender(str, Enum):
+    HUMAN = "human"
+    BOT = "bot"
+
+
+class EventType(str, Enum):
+    TRANSCRIPT = "event_transcript"
+    PHONE_CALL_CONNECTED = "event_phone_call_connected"
+
+
+class Event(TypedModel):
+    pass
+
+
+class TranscriptEvent(Event, type=EventType.TRANSCRIPT):
+    text: str
+    sender: Sender
+    timestamp: float
+    conversation_id: str
+
+    def to_string(self, include_timestamp: bool = False) -> str:
+        if include_timestamp:
+            return f"{self.sender.name}: {self.text} ({self.timestamp})"
+        return f"{self.sender.name}: {self.text}"
+
+
+class PhoneCallConnectedEvent(Event, type=EventType.PHONE_CALL_CONNECTED):
+    pass
