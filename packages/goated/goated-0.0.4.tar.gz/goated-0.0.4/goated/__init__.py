b0f8b5d34b1ed27cc63ccbe70983084121a1822b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:18 +0000

    Add package goated/0.0.4/goated-0.0.4.tar.gz

---
+++ packages/goated/goated-0.0.4.tar.gz/goated-0.0.4/goated/__init__.py
@@ -0,0 +1,5 @@
+from .version import __version__
+from .client import *
+from .state import *
+from .instructions import *
+from .utils import *
---
+++ packages/goated/goated-0.0.4.tar.gz/goated-0.0.4/goated/instructions/__init__.py
@@ -0,0 +1,5 @@
+from .enums import *
+from .new_order import NewOrderArgs
+from .cancel_order import CancelOrderArgs
+from .reduce_order import ReduceOrderArgs
+from .update_persistence import UpdateOrderPersistenceArgs
