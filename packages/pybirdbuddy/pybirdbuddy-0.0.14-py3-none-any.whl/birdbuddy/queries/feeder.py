bf2f74ea6615778c2bea4b04dd6d4370fe5c3ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:04 +0000

    Add package pybirdbuddy/0.0.14/pybirdbuddy-0.0.14-py3-none-any.whl

---
+++ packages/pybirdbuddy/pybirdbuddy-0.0.14-py3-none-any.whl/birdbuddy/queries/feeder.py
@@ -0,0 +1,122 @@
+"""Feeder queries"""
+
+TOGGLE_OFF_GRID = """
+mutation feederToggleOffGrid($feederId: ID!, $feederToggleOffGridInput: FeederToggleOffGridInput!) {
+  feederToggleOffGrid(
+    feederId: $feederId
+    feederToggleOffGridInput: $feederToggleOffGridInput
+  ) {
+    ... on FeederToggleOffGridFinishedResult {
+      feeder {
+        offGrid
+      }
+    }
+    ... on FeederToggleOffGridInProgressResult {
+      feeder {
+        offGrid
+      }
+    }
+  }
+}
+""".strip()
+
+SET_OPTIONS = """
+mutation feederUpdate($feederId: ID!, $feederUpdateInput: FeederUpdateInput!) {
+  feederUpdate(feederId: $feederId, feederUpdateInput: $feederUpdateInput) {
+    ... on FeederForOwner {
+      ...ListOwnerFeederFields
+      ...SingleOwnerFeederAdditionalFields
+    }
+  }
+}
+fragment ListOwnerFeederFields on FeederForOwner {
+  ...ListFeederFields
+  availableFirmwareVersion
+  firmwareVersion
+  offGrid
+}
+fragment ListFeederFields on FeederForPrivate {
+  battery {
+    charging
+    percentage
+    state
+    __typename
+  }
+  food {
+    state
+    __typename
+  }
+  id
+  name
+  signal {
+    state
+    value
+    __typename
+  }
+  state
+  temperature {
+    value
+    __typename
+  }
+  __typename
+}
+fragment SingleOwnerFeederAdditionalFields on FeederForOwner {
+  frequency
+  lowBatteryNotification
+  lowFoodNotification
+  offGrid
+  serialNumber
+  temperature {
+    value
+    __typename
+  }
+  __typename
+}
+""".strip()
+
+UPDATE_FIRMWARE = """
+mutation feederFirmwareUpdateStart($feederId: ID!) {
+  feederFirmwareUpdateStart(feederId: $feederId) {
+    ... on FeederFirmwareUpdateFailedResult {
+      failedReason
+      __typename
+    }
+    ... on FeederFirmwareUpdateProgressResult {
+      progress
+      __typename
+    }
+    ... on FeederFirmwareUpdateSucceededResult {
+      __typename
+      feeder {
+        availableFirmwareVersion
+        firmwareVersion
+      }
+    }
+  }
+}
+""".strip()
+
+UPDATE_FIRMWARE_PROGRESS = """
+mutation feederFirmwareUpdateCheckProgress($feederId: ID!) {
+  feederFirmwareUpdateCheckProgress(feederId: $feederId) {
+    ... on FeederFirmwareUpdateFailedResult {
+      failedReason
+      __typename
+    }
+    ... on FeederFirmwareUpdateProgressResult {
+      feeder {
+        state
+      }
+      progress
+      __typename
+    }
+    ... on FeederFirmwareUpdateSucceededResult {
+      __typename
+      feeder {
+        availableFirmwareVersion
+        firmwareVersion
+      }
+    }
+  }
+}
+""".strip()
