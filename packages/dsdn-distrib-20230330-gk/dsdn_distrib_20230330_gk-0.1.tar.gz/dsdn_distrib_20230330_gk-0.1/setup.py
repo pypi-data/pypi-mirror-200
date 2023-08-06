f1b5c7259210cd4d6d4552d8b9e6eb771f041cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:25 +0000

    Add package dsdn-distrib-20230330-gk/0.1/dsdn_distrib_20230330_gk-0.1.tar.gz

---
+++ packages/dsdn-distrib-20230330-gk/dsdn_distrib_20230330_gk-0.1.tar.gz/dsdn_distrib_20230330_gk-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsdn_distrib_20230330_gk',
+      version='0.1',
+      description='Gaussian and binomial distribution',
+      packages=['dsdn_distrib_20230330_gk'],
+      long_description=open('dsdn_distrib_20230330_gk/README.md').read(),
+      author='Georgios Kokkinopoulos',
+      author_email='georgioskokkinopoulos@gmail.com',
+      zip_safe=False)
