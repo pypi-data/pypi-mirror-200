11715695517e138aefaffdecd1270d445453c1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:02 +0000

    Add package python-discord-api/1.1/python-discord-api-1.1.tar.gz

---
+++ packages/python-discord-api/python-discord-api-1.1.tar.gz/python-discord-api-1.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+	long_description = file.read()
+
+link = 'https://github.com/xXxCLOTIxXx/discord/archive/refs/heads/main.zip'
+ver = '1.1'
+
+setup(
+	name = "python-discord-api",
+	version = ver,
+	url = "https://github.com/xXxCLOTIxXx/discord",
+	download_url = link,
+	license = "MIT",
+	author = "Xsarz",
+	author_email = "xsarzy@gmail.com",
+	description = "Library for creating discord bots and scripts.",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	keywords = [
+		"discord.py",
+		"discord",
+		"discord-py",
+		"discord-bot",
+		"api",
+		"python",
+		"python3",
+		"python3.x",
+		"xsarz",
+		"official",
+		"sync",
+		"async"
+	],
+	install_requires = [
+		"requests",
+		"aiohttp",
+		"websocket-client"
+	],
+	packages = find_packages()
+)
