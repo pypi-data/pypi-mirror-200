abef633f3b1ffa769cc162def3f86fef77ac8136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:32 +0000

    Add package celery-pubsub/2.0.0b3/celery-pubsub-2.0.0b3.tar.gz

---
+++ packages/celery-pubsub/celery-pubsub-2.0.0b3.tar.gz/celery-pubsub-2.0.0b3/celery_pubsub/pubsub.py
@@ -0,0 +1,100 @@
+"""Contains the pubsub manager and the pubsub functions."""
+
+from __future__ import annotations
+
+import typing
+
+if typing.TYPE_CHECKING:  # pragma: no cover
+    from typing_extensions import TypeAlias
+else:
+    try:
+        from typing import TypeAlias as TypeAlias
+    except ImportError:
+        try:
+            from typing_extensions import TypeAlias as TypeAlias
+        except ImportError:
+            TypeAlias = None
+
+import celery
+import re
+
+__all__ = [
+    "publish",
+    "publish_now",
+    "subscribe",
+    "unsubscribe",
+]
+
+from celery import Task, group
+from celery.result import AsyncResult, EagerResult
+
+
+PA: TypeAlias = typing.Any  # ParamSpec args
+PK: TypeAlias = typing.Any  # ParamSpec kwargs
+P: TypeAlias = typing.Any  # ParamSpec
+R: TypeAlias = typing.Any  # Return type
+
+
+class PubSubManager:
+    def __init__(self) -> None:
+        super(PubSubManager, self).__init__()
+        self.subscribed: set[tuple[str, re.Pattern[str], Task[P, R]]] = set()
+        self.jobs: dict[str, group] = {}
+
+    def publish(self, topic: str, *args: PA, **kwargs: PK) -> AsyncResult[R]:
+        result = self.get_jobs(topic).delay(*args, **kwargs)
+        return result
+
+    def publish_now(self, topic: str, *args: PA, **kwargs: PK) -> EagerResult[R]:
+        # Ignoring type because of this: https://github.com/sbdchd/celery-types/issues/111
+        result = self.get_jobs(topic).apply(args=args, kwargs=kwargs)  # type: ignore
+        return result
+
+    def subscribe(self, topic: str, task: Task[P, R]) -> None:
+        key = (topic, self._topic_to_re(topic), task)
+        if key not in self.subscribed:
+            self.subscribed.add(key)
+            self.jobs = {}
+
+    def unsubscribe(self, topic: str, task: Task[P, R]) -> None:
+        key = (topic, self._topic_to_re(topic), task)
+        if key in self.subscribed:
+            self.subscribed.discard(key)
+            self.jobs = {}
+
+    def get_jobs(self, topic: str) -> group:
+        if topic not in self.jobs:
+            self._gen_jobs(topic)
+        return self.jobs[topic]
+
+    def _gen_jobs(self, topic: str) -> None:
+        jobs = []
+        for job in self.subscribed:
+            if job[1].match(topic):
+                jobs.append(job[2].s())
+        self.jobs[topic] = celery.group(jobs)
+
+    @staticmethod
+    def _topic_to_re(topic: str) -> re.Pattern[str]:
+        assert isinstance(topic, str)
+        re_topic = topic.replace(".", r"\.").replace("*", r"[^.]+").replace("#", r".+")
+        return re.compile(r"^{}$".format(re_topic))
+
+
+_pubsub_manager: PubSubManager = PubSubManager()
+
+
+def publish(topic: str, *args: PA, **kwargs: PK) -> AsyncResult[R]:
+    return _pubsub_manager.publish(topic, *args, **kwargs)
+
+
+def publish_now(topic: str, *args: PA, **kwargs: PK) -> EagerResult[R]:
+    return _pubsub_manager.publish_now(topic, *args, **kwargs)
+
+
+def subscribe(topic: str, task: Task[P, R]) -> None:
+    return _pubsub_manager.subscribe(topic, task)
+
+
+def unsubscribe(topic: str, task: Task[P, R]) -> None:
+    return _pubsub_manager.unsubscribe(topic, task)
---
+++ packages/celery-pubsub/celery-pubsub-2.0.0b3.tar.gz/celery-pubsub-2.0.0b3/tests/test_pubsub.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+import typing
+
+if typing.TYPE_CHECKING:  # pragma: no cover
+    from typing_extensions import ParamSpec
+else:
+    try:
+        from typing import ParamSpec as ParamSpec
+    except ImportError:
+        try:
+            from typing_extensions import ParamSpec as ParamSpec
+        except ImportError:
+            ParamSpec = None
+
+from celery import Task
+from celery.worker import WorkController
+
+from celery_pubsub import publish, unsubscribe, publish_now
+
+P = ParamSpec("P")
+R = typing.TypeVar("R")
+
+
+def test_subscription(job_c: Task[P, str], celery_worker: WorkController) -> None:
+    from celery_pubsub import publish, subscribe
+
+    res = publish("dummy", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["e"])
+
+    subscribe("dummy", job_c)
+
+    res = publish("dummy", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["e", "c"])
+
+    unsubscribe("dummy", job_c)
+
+    res = publish("dummy", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["e"])
+
+
+def test_1(celery_worker: WorkController) -> None:
+    res = publish("index.low.test", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["d", "e", "f", "g"])
+
+
+def test_2(celery_worker: WorkController) -> None:
+    res = publish("something.else", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["e"])
+
+
+def test_3(celery_worker: WorkController) -> None:
+    res = publish("index.high", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["a", "d", "e"])
+
+
+def test_4(celery_worker: WorkController) -> None:
+    res = publish_now("index", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["c", "e"])
+
+
+def test_5(celery_worker: WorkController) -> None:
+    from celery_pubsub import publish as pub
+
+    res = pub("index.low", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["b", "d", "e"])
+
+
+def test_6(celery_worker: WorkController) -> None:
+    import celery_pubsub as pubsub
+
+    res = pubsub.publish("index.high.some.test", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["d", "e", "g"])
+
+
+def test_7(celery_worker: WorkController) -> None:
+    from celery_pubsub import publish
+
+    res = publish("index.high.test", 4, 8, a15=16, a23=42).get()
+    assert sorted(res) == sorted(["d", "e", "f", "g"])
+
+
+def test_subscription_redundant(
+    job_a: Task[P, str], celery_worker: WorkController
+) -> None:
+    import celery_pubsub
+
+    # Ignoring attr-defined because of this: https://github.com/sbdchd/celery-types/issues/112
+
+    jobs_init = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks  # type: ignore[attr-defined]
+    celery_pubsub.subscribe("redundant.test", job_a)
+    jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks  # type: ignore[attr-defined]
+    celery_pubsub.subscribe("redundant.test", job_a)
+    jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks  # type: ignore[attr-defined]
+    celery_pubsub.unsubscribe("redundant.test", job_a)
+    jobs_end = celery_pubsub.pubsub._pubsub_manager.get_jobs("redundant.test").tasks  # type: ignore[attr-defined]
+
+    assert jobs_before == jobs_after
+    assert jobs_init == jobs_end
+
+
+def test_unsubscribe_nonexistant(
+    job_a: Task[P, str], celery_worker: WorkController
+) -> None:
+    import celery_pubsub
+
+    # Ignoring attr-defined because of this: https://github.com/sbdchd/celery-types/issues/112
+
+    jobs_before = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks  # type: ignore[attr-defined]
+    celery_pubsub.unsubscribe("not.exists", job_a)
+    jobs_after = celery_pubsub.pubsub._pubsub_manager.get_jobs("not.exists").tasks  # type: ignore[attr-defined]
+
+    assert jobs_before == jobs_after
