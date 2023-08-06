f6ef62b3d710bc4ffebdb4048e3d1733b66f5c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:15 +0000

    Add package hydropandas/0.7.3/hydropandas-0.7.3.tar.gz

---
+++ packages/hydropandas/hydropandas-0.7.3.tar.gz/hydropandas-0.7.3/hydropandas/__init__.py
@@ -0,0 +1,32 @@
+# flake8: noqa
+import logging
+
+from .extensions import geo as _geo
+from .extensions import gwobs as _gwobs
+from .extensions import plots as _plots
+from .extensions import stats as _stats
+from .obs_collection import (
+    ObsCollection,
+    read_bro,
+    read_dino,
+    read_fews,
+    read_imod,
+    read_knmi,
+    read_menyanthes,
+    read_modflow,
+    read_waterinfo,
+    read_wiski,
+)
+from .observation import (
+    EvaporationObs,
+    GroundwaterObs,
+    GroundwaterQualityObs,
+    MeteoObs,
+    ModelObs,
+    Obs,
+    PrecipitationObs,
+    WaterlvlObs,
+)
+from .version import __version__
+
+logging.getLogger("hydropandas").addHandler(logging.NullHandler())
---
+++ packages/hydropandas/hydropandas-0.7.3.tar.gz/hydropandas-0.7.3/tests/test_009_fews.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Created on Mon Jun 24 11:43:27 2019.
+
+@author: oebbe
+"""
+import os
+
+import hydropandas as hpd
+from hydropandas.io import fews
+
+dirname, fnames = hpd.util.get_files(
+    "./tests/data/2019-FEWS-test/WaalenBurg_201810-20190215_prod.zip", ".xml"
+)
+
+
+def test_fews_highmemory():
+    fews.read_xml_fname(
+        os.path.join(dirname, fnames[0]), low_memory=False, ObsClass=hpd.WaterlvlObs
+    )
+
+    return
+
+
+def test_fews_lowmemory():
+    fews.read_xml_fname(
+        os.path.join(dirname, fnames[0]), low_memory=True, ObsClass=hpd.WaterlvlObs
+    )
+
+    return
+
+
+def test_obscollection_fews_selection():
+    fews.read_xml_filelist(fnames, hpd.WaterlvlObs, dirname)
+
+    return
