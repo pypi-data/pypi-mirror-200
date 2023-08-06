e0480abb21639c585503e9c522224d4058b1a064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:59 +0000

    Add package container-collection/0.5.2/container_collection-0.5.2.tar.gz

---
+++ packages/container-collection/container_collection-0.5.2.tar.gz/container_collection-0.5.2/src/container_collection/batch/make_batch_job.py
@@ -0,0 +1,25 @@
+def make_batch_job(
+    name: str,
+    image: str,
+    account: str,
+    region: str,
+    user: str,
+    vcpus: int,
+    memory: int,
+    prefix: str,
+) -> dict:
+    return {
+        "jobDefinitionName": f"{user}_{name}",
+        "type": "container",
+        "containerProperties": {
+            "image": f"{account}.dkr.ecr.{region}.amazonaws.com/{user}/{image}",
+            "vcpus": vcpus,
+            "memory": memory,
+            "environment": [
+                {"name": "SIMULATION_TYPE", "value": "AWS"},
+                {"name": "BATCH_WORKING_URL", "value": prefix},
+                {"name": "FILE_SET_NAME", "value": name},
+            ],
+            "jobRoleArn": f"arn:aws:iam::{account}:role/BatchJobRole",
+        },
+    }
