d58e7094a3730b60c8734b3a93de3faa381754a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package netdoc/0.9.49/netdoc-0.9.49.tar.gz

---
+++ packages/netdoc/netdoc-0.9.49.tar.gz/netdoc-0.9.49/netdoc/ingestors/netmiko_cisco_ios_show_lldp_neighbors_detail.py
@@ -0,0 +1,103 @@
+"""Ingestor for netmiko_cisco_ios_show_lldp_neighbors_detail."""
+__author__ = "Andrea Dainese"
+__contact__ = "andrea@adainese.it"
+__copyright__ = "Copyright 2022, Andrea Dainese"
+__license__ = "GPLv3"
+
+from netdoc.schemas import interface, device, discoverable, cable
+from netdoc import utils
+
+
+def ingest(log):
+    """Processing parsed output."""
+    device_o = log.discoverable.device
+    neighbors_per_interface = utils.count_interface_neighbors(
+        log.parsed_output, "local_interface"
+    )
+
+    for item in log.parsed_output:
+        # See https://github.com/networktocode/ntc-templates/tree/master/tests/cisco_ios/show_lldp_neighbors_detail # pylint: disable=line-too-long
+        local_interface_name = item.get("local_interface")
+        local_interface_label = utils.normalize_interface_label(local_interface_name)
+        remote_management_ip = utils.normalize_ip_address_or_none(
+            item.get("management_ip")
+        )
+        remote_name = (
+            item.get("neighbor") if item.get("neighbor") else item.get("chassis_id")
+        )
+        remote_name = utils.normalize_hostname(remote_name)
+        remote_interface_name = (
+            item.get("neighbor_interface")
+            if item.get("neighbor_interface")
+            else item.get("neighbor_port_id")
+        )
+        remote_interface_label = utils.normalize_interface_label(remote_interface_name)
+
+        if neighbors_per_interface.get(local_interface_label) > 1:
+            # Skip interfaces with multiple neighbors
+            continue
+
+        if local_interface_name != utils.parent_interface(
+            local_interface_name
+        ) or remote_interface_name != utils.parent_interface(remote_interface_name):
+            # Skip subinterfaces
+            continue
+
+        # Get or create local Interface
+        local_interface_o = interface.get(
+            device_id=device_o.id, label=local_interface_label
+        )
+        if not local_interface_o:
+            local_interface_data = {
+                "name": local_interface_name,
+                "device_id": device_o.id,
+            }
+            local_interface_o = interface.create(**local_interface_data)
+
+        # Get or create remote Device
+        remote_device_o = device.get(remote_name)
+        if not remote_device_o:
+            remote_device_data = {
+                "name": remote_name,
+                "site_id": device_o.site.id,
+            }
+            remote_device_o = device.create(**remote_device_data)
+
+        # Get or create remote Interface
+        remote_interface_o = interface.get(
+            device_id=remote_device_o.id, label=remote_interface_label
+        )
+        if not remote_interface_o:
+            remote_interface_data = {
+                "name": remote_interface_name,
+                "device_id": remote_device_o.id,
+            }
+            remote_interface_o = interface.create(**remote_interface_data)
+
+        # Create remote Discoverable if not exist
+        if remote_management_ip:
+            # List by device name
+            remote_discoverable_list = discoverable.get_list(device__name=remote_name)
+            # Add list by IP address
+            remote_discoverable_list += discoverable.get_list(
+                address=remote_management_ip
+            )
+            if not remote_discoverable_list:
+                remote_discoverable_data = {
+                    "site_id": log.discoverable.site.id,
+                    "mode": log.discoverable.mode,
+                    "address": remote_management_ip,
+                    "credential_id": log.discoverable.credential.id,
+                    "device_id": remote_device_o.id,
+                }
+                discoverable.create(**remote_discoverable_data)
+
+        # Link
+        cable.link(
+            left_interface_id=local_interface_o.id,
+            right_interface_id=remote_interface_o.id,
+        )
+
+    # Update the log
+    log.ingested = True
+    log.save()
