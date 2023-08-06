61723b1a80409fb5501043acd161380dc4acf76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:09 +0000

    Add package hbsir/0.1.0/hbsir-0.1.0.tar.gz

---
+++ packages/hbsir/hbsir-0.1.0.tar.gz/hbsir-0.1.0/hbsir/metadata.py
@@ -0,0 +1,231 @@
+"""
+Metadata module
+"""
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Literal
+
+import yaml
+
+
+PACKAGE_DIRECTORY = Path(__file__).parent
+ROOT_DIRECTORT = Path().absolute()
+
+
+Attributes = Literal["Urban-Rural", "Province", "Region"]
+
+GeneralTables = Literal[
+    "household_information",
+    "members_properties",
+    "house_specifications",
+]
+
+ExpenditureTables = Literal[
+    "food",
+    "tobacco",
+    "cloth",
+    "home",
+    "furniture",
+    "medical",
+    "transportation",
+    "communication",
+    "entertainment",
+    "education",
+    "hotel",
+    "other",
+    "durable",
+    "investment",
+]
+
+IncomeTables = Literal[
+    "employment_income",
+    "self_employed_income",
+    "other_income",
+    "subsidy",
+    "public_employment_income",
+    "private_employment_income",
+]
+
+OriginalTables = Literal[GeneralTables, ExpenditureTables, IncomeTables]
+
+standard_tables = (
+    "Expenditures",
+)
+StandardTables = Literal[
+    "Expenditures",
+]
+
+Tables = Literal[OriginalTables, StandardTables]
+
+
+def open_yaml(path):
+    """
+    Read the contents of a YAML file relative to the root directory and return it as a dictionary.
+
+    :param path: The path to the YAML file, relative to the root directory.
+    :type path: str
+
+    :return: The contents of the YAML file as a dictionary.
+    :rtype: dict
+
+    :raises yaml.YAMLError: If there is an error parsing the YAML file.
+
+    """
+    path = PACKAGE_DIRECTORY.joinpath(path)
+    with open(path, mode="r", encoding="utf8") as yaml_file:
+        yaml_content = yaml.load(yaml_file, Loader=yaml.CLoader)
+    return yaml_content
+
+
+@dataclass
+class Metadata:
+    """
+    A dataclass for accessing metadata used in other parts of the project.
+
+    """
+
+    tables = open_yaml("metadata/tables.yaml")
+    maps = open_yaml("metadata/maps.yaml")
+    household = open_yaml("metadata/household.yaml")
+    commodities = open_yaml("metadata/commodities.yaml")
+    schema = open_yaml("metadata/schema.yaml")
+    other = open_yaml("metadata/other.yaml")
+
+
+@dataclass
+class Defaults:
+    """
+    This dataclass provides access to default values that are used in other
+    parts of the project. It first attempts to load the settings from a local
+    `hbsir-settings.yaml` file in the root directory. If the file is not found,
+    it loads the settings from the sample file named `settings-sample.yaml`
+    located in config folder in library directory.
+
+    """
+
+    if ROOT_DIRECTORT.joinpath("hbsir-settings.yaml").exists():
+        settings = open_yaml(ROOT_DIRECTORT.joinpath("hbsir-settings.yaml"))
+    elif PACKAGE_DIRECTORY.joinpath("config", "hbsir-settings.yaml").exists():
+        settings = open_yaml(PACKAGE_DIRECTORY.joinpath("config", "hbsir-settings.yaml"))
+    elif PACKAGE_DIRECTORY.joinpath("config", "settings-sample.yaml").exists():
+        settings = open_yaml(PACKAGE_DIRECTORY.joinpath("config", "settings-sample.yaml"))
+    else:
+        raise FileNotFoundError
+
+    # online directory
+    online_dir = settings["online_directory"]
+
+    # local directory
+    pack_dir: Path = PACKAGE_DIRECTORY
+    root_dir: Path = ROOT_DIRECTORT
+    if Path(settings["local_directory"]).is_absolute():
+        local_dir = Path(settings["local_directory"])
+    elif settings["in_root"]:
+        local_dir: Path = root_dir.joinpath(settings["local_directory"])
+    else:
+        local_dir: Path = pack_dir.joinpath(settings["local_directory"])
+    archive_files: Path = local_dir.joinpath(settings["archive_files"])
+    unpacked_data: Path = local_dir.joinpath(settings["unpacked_data"])
+    extracted_data: Path = local_dir.joinpath(settings["extracted_data"])
+    processed_data: Path = local_dir.joinpath(settings["processed_data"])
+
+    first_year: int = settings["first_year"]
+    last_year: int = settings["last_year"]
+
+
+def get_latest_version_year(metadata_dict: dict, year: int) -> int | bool:
+    """
+    Retrieve the most recent available version of metadata that matches or
+    precedes the given year, provided that the metadata is versioned.
+
+    :param metadata_dict: A dictionary representing the metadata.
+    :type metadata_dict: dict
+
+    :param year: The year to which the version of the metadata should match or
+        precede.
+    :type year: int
+
+    :return: The version number of the most recent metadata version that
+        matches or precedes the given year, or None if the metadata is not
+        versioned.
+    :rtype: int or None
+
+    """
+    if not isinstance(metadata_dict, dict):
+        return False
+    if "versions" in metadata_dict:
+        return True
+    version_list = list(metadata_dict.keys())
+    for element in version_list:
+        if not isinstance(element, int):
+            return False
+        if (element < 1300) or (element > 1500):
+            return False
+
+    selected_version = 0
+    for version in version_list:
+        if version <= year:
+            selected_version = max(selected_version, version)
+    return selected_version
+
+
+def get_metadata_version(metadata_dict: dict, year: int) -> dict:
+    """
+    Retrieve the metadata version that matches or precedes the given year,
+    returning the complete metadata for that version.
+
+    :param metadata_dict: A dictionary representing the metadata.
+    :type metadata_dict: dict
+
+    :param year: The year to which the version of the metadata should match or
+        precede.
+    :type year: int
+
+    :return: A dictionary containing the complete metadata for the version that
+        matches or precedes the given year. If the metadata is not versioned, the
+        function returns the original metadata dictionary.
+    :rtype: dict
+
+    """
+    selected_version = get_latest_version_year(metadata_dict, year)
+    if selected_version is True:
+        selected_version = get_latest_version_year(metadata_dict["versions"], year)
+        metadata_version = {key: value for key, value in metadata_dict.items() if key != "versions"}
+        for key, value in metadata_dict["versions"][selected_version].items():
+            metadata_version[key] = value
+
+    elif selected_version is False:
+        metadata_version =  metadata_dict
+
+    else:
+        metadata_version = metadata_dict[selected_version]
+
+    return metadata_version
+
+
+def get_categories(metadata_dict: dict) -> list:
+    """_summary_
+
+    Parameters
+    ----------
+    metadata_dict : dict
+        _description_
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    if "categories" not in metadata_dict:
+        categories_list = [metadata_dict]
+    else:
+        categories_number = list(metadata_dict["categories"].keys())
+        categories_number.sort()
+        categories_list = [metadata_dict["categories"][number] for number in categories_number]
+        shared_infos = [key for key in metadata_dict.keys() if key != "categories"]
+        for category in categories_list:
+            for info in shared_infos:
+                if info not in category.keys():
+                    category[info] = metadata_dict[info]
+    return categories_list
---
+++ packages/hbsir/hbsir-0.1.0.tar.gz/hbsir-0.1.0/hbsir/utils.py
@@ -0,0 +1,295 @@
+"""
+Utility functions
+"""
+
+import subprocess
+from pathlib import Path
+import platform
+from zipfile import ZipFile
+
+from tqdm import tqdm
+import requests
+
+from . import metadata
+
+
+defaults = metadata.Defaults()
+metadata_obj = metadata.Metadata()
+_Tables = metadata.Tables
+
+
+def download_file(
+    url: str, path: str | Path | None = None, show_progress_bar: bool = False
+) -> Path:
+    """
+    Download a file from a URL to a local path.
+
+    :param url: The URL to download the file from.
+    :param path: The local path to save the downloaded file to. If None, a
+        temporary directory is used.
+    :param show_progress_bar: Whether to display a progress bar during the
+        download.
+    :param timeout: The timeout for the download request, in seconds.
+    :returns: The local path of the downloaded file.
+
+    """
+    if isinstance(path, str):
+        path = Path(path)
+        file_name = path.name
+    elif path is None:
+        temp_folder = defaults.pack_dir.joinpath("temp")
+        temp_folder.mkdir(exist_ok=True)
+        file_name = url.split("/")[-1]
+        path = temp_folder.joinpath(file_name)
+    else:
+        file_name = path.name
+
+    response = requests.get(url, timeout=10, stream=True)
+    file_size = response.headers.get("content-length")
+    if file_size is not None:
+        file_size = int(file_size)
+    else:
+        raise FileNotFoundError
+    download_bar = tqdm(
+        desc=f"downloading {file_name}",
+        total=file_size,
+        unit="B",
+        unit_scale=True,
+        disable=not show_progress_bar,
+    )
+    Path(path.parent).mkdir(parents=True, exist_ok=True)
+    with open(path, mode="wb") as file:
+        for chunk in response.iter_content(chunk_size=4096):
+            download_bar.update(len(chunk))
+            file.write(chunk)
+    download_bar.close()
+    return path
+
+
+def download_7zip():
+    """
+    Download the appropriate version of 7-Zip for the current operating system
+    and architecture, and extract it to the root directory.
+
+    """
+    print(
+        f"Downloading 7-Zip for {platform.system()} with {platform.architecture()[0]} architecture"
+    )
+    file_name = f"{platform.system()}-{platform.architecture()[0]}.zip"
+    file_path = Path().joinpath("temp", file_name)
+    file_path = download_file(
+        f"{defaults.online_dir}/7-Zip/{file_name}",
+        show_progress_bar=True,
+    )
+    with ZipFile(file_path) as zip_file:
+        zip_file.extractall(defaults.pack_dir)
+    file_path.unlink()
+
+    if platform.system() == "Linux":
+        defaults.pack_dir.joinpath("7-Zip", "7zz").chmod(0o771)
+
+
+def extract_with_7zip(compressed_file_path: str, output_directory: str) -> None:
+    """
+    Extracts the contents of a compressed file using the 7-Zip tool.
+
+    :param compressed_file_path: The path to the compressed file to be extracted.
+    :type compressed_file_path: str
+    :param output_directory: The path to the directory where the extracted files will be stored.
+    :type output_directory: str
+    :return: None, as this function does not return anything.
+
+    """
+    if platform.system() == "Windows":
+        seven_zip_file_path = Path().joinpath("7-Zip", "7z.exe")
+        if not seven_zip_file_path.exists():
+            download_7zip()
+        subprocess.run(
+            [
+                seven_zip_file_path,
+                "e",
+                compressed_file_path,
+                f"-o{output_directory}",
+                "-y",
+            ],
+            check=False,
+            shell=True,
+        )
+    elif platform.system() == "Linux":
+        seven_zip_file_path = defaults.pack_dir.joinpath("7-Zip", "7zz")
+        if not seven_zip_file_path.exists():
+            download_7zip()
+        subprocess.run(
+            [
+                seven_zip_file_path,
+                "e",
+                compressed_file_path,
+                f"-o{output_directory}",
+                "-y",
+            ],
+            check=False,
+        )
+
+
+def interpret_number_range(
+    number_range_info: int | dict | list,
+    default_end: int | None = None,
+    keyword: str = "code",
+) -> list[int]:
+    """
+    A tool for interpretation of number ranges
+    """
+    number_range: list[int] = []
+    if isinstance(number_range_info, int):
+        number_range.append(number_range_info)
+    elif isinstance(number_range_info, dict):
+        if ("start" in number_range_info) and ("end" in number_range_info):
+            number_range.extend(
+                list(range(number_range_info["start"], number_range_info["end"]))
+            )
+        elif ("start" in number_range_info) and (default_end is not None):
+            number_range.extend(list(range(number_range_info["start"], default_end)))
+        elif keyword in number_range_info:
+            number_range.extend(
+                interpret_number_range(number_range_info[keyword], default_end=default_end)
+            )
+        else:
+            raise KeyError
+    elif isinstance(number_range_info, list):
+        for element in number_range_info:
+            number_range.extend(interpret_number_range(element, default_end=default_end))
+    else:
+        raise KeyError
+
+    return number_range
+
+
+def build_year_interval(
+    from_year: int | None,
+    to_year: int | None,
+    earliest_year: int | None = None,
+    latest_year: int | None = None,
+) -> tuple[int, int]:
+    """
+    Returns a tuple of two integers representing a range of years.
+
+    :param from_year: An integer representing the starting year of the range.
+        If `None`, the default starting year is used.
+    :type from_year: int or None
+
+    :param to_year: An integer representing the ending year of the range. If
+        `None`, the default ending year + 1 is used.
+    :type to_year: int or None
+
+    :return: A tuple representing the range of years.
+    :rtype: tuple of two integers
+
+    :raises: ValueError if `from_year` is greater than `to_year`.
+
+    Example:
+
+    >>> build_year_interval()
+    (1363, 1401)
+
+    >>> build_year_interval(1390)
+    (1390, 1401)
+
+    >>> build_year_interval(1380, 1385)
+    (1380, 1386)
+
+    >>> build_year_interval(1383, 1375)
+    ValueError: `from_year` must be less than `to_year`.
+    """
+    if earliest_year is None:
+        earliest_year = defaults.first_year
+    if latest_year is None:
+        latest_year = defaults.last_year
+
+    if (from_year is not None) and (to_year is not None):
+        if to_year < from_year:
+            raise ValueError("`from_year` is greater than `to_year`")
+        _from_year, _to_year = from_year, to_year + 1
+    elif (from_year is not None) and (to_year is None):
+        _from_year, _to_year = from_year, from_year + 1
+    elif (from_year is None) and (to_year is not None):
+        raise KeyError
+    else:
+        _from_year, _to_year = earliest_year, latest_year + 1
+
+    return _from_year, _to_year
+
+
+def build_year_interval_for_table(
+    table_name: _Tables, from_year: int | None = None, to_year: int | None = None
+) -> list[int]:
+    """_summary_
+
+    Parameters
+    ----------
+    table_name : str
+        _description_
+    from_year : int | None, optional
+        _description_, by default None
+    to_year : int | None, optional
+        _description_, by default None
+
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    availability_info = metadata_obj.tables["yearly_table_availability"][table_name]
+    available_years = interpret_number_range(availability_info, default_end=defaults.last_year+1)
+    from_year, to_year = build_year_interval(from_year, to_year)
+    selected_years = [year for year in range(from_year, to_year) if year in available_years]
+    return selected_years
+
+
+def create_table_year_product(
+    table_name: _Tables | list[_Tables] | tuple[_Tables],
+    from_year: int | None = None,
+    to_year: int | None = None,
+) -> list[tuple[_Tables, int]]:
+    """_summary_
+
+    Parameters
+    ----------
+    table_name : str | List[str]
+        _description_
+    from_year : int | None
+        _description_
+    to_year : int | None, optional
+        _description_, by default None
+
+    Returns
+    -------
+    List[tuple]
+        _description_
+    """
+    if isinstance(table_name, str):
+        table_list: list[_Tables] = [table_name]
+    else:
+        table_list: list[_Tables] = list(table_name)
+
+    product_list = []
+    for _table_name in table_list:
+        years = build_year_interval_for_table(_table_name, from_year, to_year)
+        product_list.extend([(_table_name, year) for year in years])
+    return product_list
+
+
+def is_multi_year(
+    table_name: _Tables | list[_Tables] | tuple[_Tables],
+    from_year: int | None = None,
+    to_year: int | None = None,
+) -> bool:
+    if isinstance(table_name, str):
+        table_list: list[_Tables] = [table_name]
+    else:
+        table_list = [table for table in table_name]
+
+    for _table_name in table_list:
+        years = build_year_interval_for_table(_table_name, from_year, to_year)
+        if len(years) > 1:
+            return True
+    return False
