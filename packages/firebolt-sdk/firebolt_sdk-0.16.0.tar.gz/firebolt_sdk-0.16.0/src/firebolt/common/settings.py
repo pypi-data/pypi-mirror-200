46fc35ab9424a800e0ef4e508bc395d92956d85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:17 +0000

    Add package firebolt-sdk/0.16.0/firebolt_sdk-0.16.0.tar.gz

---
+++ packages/firebolt-sdk/firebolt_sdk-0.16.0.tar.gz/firebolt_sdk-0.16.0/src/firebolt/common/settings.py
@@ -0,0 +1,82 @@
+import logging
+from typing import Optional
+
+from pydantic import BaseSettings, Field, SecretStr, root_validator
+
+from firebolt.client.auth import Auth
+
+logger = logging.getLogger(__name__)
+
+AUTH_CREDENTIALS_DEPRECATION_MESSAGE = """ Passing connection credentials directly in Settings is deprecated.
+ Use Auth object instead.
+ Examples:
+  >>> from firebolt.client.auth import UsernamePassword
+  >>> ...
+  >>> settings = Settings(auth=UsernamePassword(username, password), ...)
+ or
+  >>> from firebolt.client.auth import Token
+  >>> ...
+  >>> settings = Settings(auth=Token(access_token), ...)"""
+
+
+class Settings(BaseSettings):
+    """Settings for Firebolt SDK.
+
+    Attributes:
+        user (Optional[str]): User name
+        password (Optional[str]): User password
+        access_token (Optional[str]): Access token to use for authentication
+            Mutually exclusive with user and password
+        account_name (Optional[str]): Account name
+            Default user account is used if none provided
+        server (Optional[str]): Environment api endpoint (Advanced)
+            Default api endpoint is used if none provided
+        default_region (str): Default region for provisioning
+    """
+
+    auth: Optional[Auth] = Field(None)
+    # Authorization
+    user: Optional[str] = Field(None, env="FIREBOLT_USER")
+    password: Optional[SecretStr] = Field(None, env="FIREBOLT_PASSWORD")
+    # Or
+    access_token: Optional[str] = Field(None, env="FIREBOLT_AUTH_TOKEN")
+
+    account_name: Optional[str] = Field(None, env="FIREBOLT_ACCOUNT")
+    server: str = Field(..., env="FIREBOLT_SERVER")
+    default_region: str = Field(..., env="FIREBOLT_DEFAULT_REGION")
+    use_token_cache: bool = Field(True)
+
+    class Config:
+        """Internal pydantic config."""
+
+        env_file = ".env"
+
+    @root_validator
+    def mutual_exclusive_with_creds(cls, values: dict) -> dict:
+        """Validate that either creds or token is provided.
+
+        Args:
+            values (dict): settings initial values
+
+        Returns:
+            dict: Validated settings values
+
+        Raises:
+            ValueError: Either both or none of credentials and token are provided
+        """
+
+        params_present = (
+            values.get("user") is not None or values.get("password") is not None,
+            values.get("access_token") is not None,
+            values.get("auth") is not None,
+        )
+        if sum(params_present) == 0:
+            raise ValueError(
+                "Provide at least one of auth, user/password or access_token."
+            )
+        if sum(params_present) > 1:
+            raise ValueError("Provide only one of auth, user/password or access_token")
+        if any(values.get(f) for f in ("user", "password", "access_token")):
+            logger.warning(AUTH_CREDENTIALS_DEPRECATION_MESSAGE)
+
+        return values
---
+++ packages/firebolt-sdk/firebolt_sdk-0.16.0.tar.gz/firebolt_sdk-0.16.0/src/firebolt/db/cursor.py
@@ -0,0 +1,112 @@
+from __future__ import annotations
+
+from functools import wraps
+from threading import Lock
+from typing import Any, Generator, List, Optional, Sequence, Tuple, Union
+
+from readerwriterlock.rwlock import RWLockWrite
+
+from firebolt.async_db._types import ColType
+from firebolt.async_db.cursor import BaseCursor as AsyncBaseCursor
+from firebolt.async_db.cursor import (
+    ParameterType,
+    QueryStatus,
+    check_not_closed,
+    check_query_executed,
+)
+from firebolt.utils.util import async_to_sync
+
+
+class Cursor(AsyncBaseCursor):
+    """
+    Class, responsible for executing queries to Firebolt Database.
+    Should not be created directly,
+    use :py:func:`connection.cursor <firebolt.async_db.connection.Connection>`
+
+    Args:
+        description: Information about a single result row
+        rowcount: The number of rows produced by last query
+        closed: True if connection is closed, False otherwise
+        arraysize: Read/Write, specifies the number of rows to fetch at a time
+            with the :py:func:`fetchmany` method
+    """
+
+    __slots__ = AsyncBaseCursor.__slots__ + (
+        "_query_lock",
+        "_idx_lock",
+    )
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self._query_lock = RWLockWrite()
+        self._idx_lock = Lock()
+        super().__init__(*args, **kwargs)
+
+    @wraps(AsyncBaseCursor.execute)
+    def execute(
+        self,
+        query: str,
+        parameters: Optional[Sequence[ParameterType]] = None,
+        skip_parsing: bool = False,
+        async_execution: Optional[bool] = False,
+    ) -> Union[int, str]:
+        with self._query_lock.gen_wlock():
+            return async_to_sync(super().execute)(
+                query, parameters, skip_parsing, async_execution
+            )
+
+    @wraps(AsyncBaseCursor.executemany)
+    def executemany(
+        self,
+        query: str,
+        parameters_seq: Sequence[Sequence[ParameterType]],
+        async_execution: Optional[bool] = False,
+    ) -> Union[int, str]:
+        with self._query_lock.gen_wlock():
+            return async_to_sync(super().executemany)(
+                query, parameters_seq, async_execution
+            )
+
+    @wraps(AsyncBaseCursor._get_next_range)
+    def _get_next_range(self, size: int) -> Tuple[int, int]:
+        with self._idx_lock:
+            return super()._get_next_range(size)
+
+    @wraps(AsyncBaseCursor.fetchone)
+    def fetchone(self) -> Optional[List[ColType]]:
+        with self._query_lock.gen_rlock():
+            return super().fetchone()
+
+    @wraps(AsyncBaseCursor.fetchmany)
+    def fetchmany(self, size: Optional[int] = None) -> List[List[ColType]]:
+        with self._query_lock.gen_rlock():
+            return super().fetchmany(size)
+
+    @wraps(AsyncBaseCursor.fetchall)
+    def fetchall(self) -> List[List[ColType]]:
+        with self._query_lock.gen_rlock():
+            return super().fetchall()
+
+    @wraps(AsyncBaseCursor.nextset)
+    def nextset(self) -> None:
+        with self._query_lock.gen_rlock(), self._idx_lock:
+            return super().nextset()
+
+    # Iteration support
+    @check_not_closed
+    @check_query_executed
+    def __iter__(self) -> Generator[List[ColType], None, None]:
+        while True:
+            row = self.fetchone()
+            if row is None:
+                return
+            yield row
+
+    @wraps(AsyncBaseCursor.get_status)
+    def get_status(self, query_id: str) -> QueryStatus:
+        with self._query_lock.gen_rlock():
+            return async_to_sync(super().get_status)(query_id)
+
+    @wraps(AsyncBaseCursor.cancel)
+    def cancel(self, query_id: str) -> None:
+        with self._query_lock.gen_rlock():
+            return async_to_sync(super().cancel)(query_id)
