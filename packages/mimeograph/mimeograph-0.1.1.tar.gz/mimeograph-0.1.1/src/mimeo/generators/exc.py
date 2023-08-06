e6cf092f03847ba7d7271db7059492e0036c77d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:35 +0000

    Add package mimeograph/0.1.1/mimeograph-0.1.1.tar.gz

---
+++ packages/mimeograph/mimeograph-0.1.1.tar.gz/mimeograph-0.1.1/src/mimeo/generators/exc.py
@@ -0,0 +1,22 @@
+class NotAllowedInstantiation(Exception):
+    pass
+
+
+class InvalidMimeoUtil(Exception):
+    pass
+
+
+class NotASpecialField(Exception):
+    pass
+
+
+class InvalidSpecialFieldValue(Exception):
+    pass
+
+
+class CountryNotFound(Exception):
+    pass
+
+
+class OutOfStock(Exception):
+    pass
