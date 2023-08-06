648495085fb9bfca31e6b85585687907ae64351f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:35 +0000

    Add package universal-greetings/0.2.0/universal_greetings-0.2.0.tar.gz

---
+++ packages/universal-greetings/universal_greetings-0.2.0.tar.gz/universal_greetings-0.2.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['src']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0',
+ 'ruamel-yaml>=0.17.21,<0.18.0',
+ 'urllib3>=1.26.12,<2.0.0']
+
+setup_kwargs = {
+    'name': 'universal-greetings',
+    'version': '0.2.0',
+    'description': 'Universal Greetings Library',
+    'long_description': '# Universal Greetings Library for Python\n\nUniversal Greetings  description\n\n\n## Get started\n\n```bash\npoetry add universal-greetings\n```\n\n```python\nfrom universalgreetings import Hello\n\nhello=Hello()\n\nhello.sayHello()\n\n```\n',
+    'author': 'Alan S. Ferreira',
+    'author_email': 'alansferreira1984@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/alelltech-backstage-demo/python-library-universal-greetings',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
