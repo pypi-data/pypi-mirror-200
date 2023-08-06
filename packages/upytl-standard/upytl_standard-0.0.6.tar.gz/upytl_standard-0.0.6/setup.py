4e0e25646a2b42e0eb528a1814d94f294ecb3984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:43 +0000

    Add package upytl-standard/0.0.6/upytl_standard-0.0.6.tar.gz

---
+++ packages/upytl-standard/upytl_standard-0.0.6.tar.gz/upytl_standard-0.0.6/setup.py
@@ -0,0 +1,50 @@
+import re
+from setuptools import setup
+
+
+def get_module_var(varname):
+    src = 'upytl_standard'
+    regex = re.compile(fr"^{varname}\s*\=\s*['\"](.+?)['\"]", re.M)
+    mobj = next(regex.finditer(open(f"{src}/__init__.py").read()))
+    return mobj.groups()[0]
+
+
+__author__ = get_module_var('__author__')
+__license__ = get_module_var('__license__')
+__version__ = get_module_var('__version__')
+
+
+setup(
+    name="upytl_standard",
+    version=__version__,
+    url="https://github.com/Eudorajab1/upytl-standard",
+    license=__license__,
+    author=__author__,
+    author_email="eudorajab1@gmail.com",
+    maintainer=__author__,
+    maintainer_email="eudorajab1@gmail.com",
+    description="Standard UPYTL - Ultra Pythonic Template Language component library",
+    platforms="any",
+    keywords='python UPYTL webapplication html',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    install_requires=[
+        "upytl>=0.0.7",
+    ],
+    python_requires='>=3.7',
+    packages=['upytl_standard'],
+)
