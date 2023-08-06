18e716d7f69dc382224a7ed93560c53e20766dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:24 +0000

    Add package polymap/0.0.2/polymap-0.0.2.tar.gz

---
+++ packages/polymap/polymap-0.0.2.tar.gz/polymap-0.0.2/tests/test_efs.py
@@ -0,0 +1,20 @@
+from polymap.efs import SecureFile
+from polymap.key import SecureKey
+
+import os
+
+
+def test_encrypt_decrypt():
+    filename = "test_secure_file.txt"
+
+    key = SecureKey.fernet()
+
+    with open(filename, 'w') as f:
+        f.write("This is a test file.")
+
+    sf = SecureFile(filename, key)
+    sf.encrypt()
+    assert os.path.getsize(filename) > 0
+    assert sf.decrypt() == b"This is a test file."
+
+    os.remove(filename)
