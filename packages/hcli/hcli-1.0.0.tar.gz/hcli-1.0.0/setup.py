df56235abbd9980baf0e301fee7a869f6ff6830b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:19 +0000

    Add package hcli/1.0.0/hcli-1.0.0.tar.gz

---
+++ packages/hcli/hcli-1.0.0.tar.gz/hcli-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="hcli",
+    version="1.0.0",
+    packages=["lib"],
+    url="https://github.com/hudduapp/cli",
+    requires=["typer", "rich", "huddu"],
+    license="MIT",
+    author="Joshua3212",
+    author_email="hello@huddu.io",
+    description="Official CLI for huddu.io",
+)
