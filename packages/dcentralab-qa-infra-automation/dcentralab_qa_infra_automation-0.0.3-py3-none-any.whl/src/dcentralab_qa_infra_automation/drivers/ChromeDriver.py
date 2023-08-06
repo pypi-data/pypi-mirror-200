79c2dc2cca85ed9d007f2be251f117a2bb90412a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:38 +0000

    Add package dcentralab-qa-infra-automation/0.0.3/dcentralab_qa_infra_automation-0.0.3-py3-none-any.whl

---
+++ packages/dcentralab-qa-infra-automation/dcentralab_qa_infra_automation-0.0.3-py3-none-any.whl/src/dcentralab_qa_infra_automation/drivers/ChromeDriver.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+"""
+init chrome driver, using ChromeDriverManager for chromeDriver installation
+
+@Author: Efrat Cohen
+@Date: 11.2022
+"""
+
+
+def initChromeDriver():
+    """
+    init chrome driver, using ChromeDriverManager for chromeDriver installation
+    :return: driver - driver instance
+    """
+    pytest.logger.info("chrome driver type injected, initialize chrome browser")
+    # todo -Do something
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    return driver
