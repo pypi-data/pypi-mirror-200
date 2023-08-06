7b84dd8a5b4b736519ce45ccfda7d77791952c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:23 +0000

    Add package odoo14-addon-webcal-exporter/14.0.1.0.1.dev1/odoo14-addon-webcal_exporter-14.0.1.0.1.dev1.tar.gz

---
+++ packages/odoo14-addon-webcal-exporter/odoo14-addon-webcal_exporter-14.0.1.0.1.dev1.tar.gz/odoo14-addon-webcal_exporter-14.0.1.0.1.dev1/odoo/addons/webcal_exporter/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Webcal Exporter',
+    'version': '14.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Export Odoo calendar events to external webcal',
+    'author': 'Coopdevs',
+    'website': 'https://coopdevs.org',
+    'license': 'AGPL-3',
+    'depends': ['base', 'calendar'],
+    'data': [
+        'data/ir_cron_data.xml',
+        'views/res_users_view.xml',
+    ],
+    'external_dependencies': {
+        'python': ['ics', 'requests', 'pytz'],
+    },
+    'installable': True,
+    'application': False,
+}
