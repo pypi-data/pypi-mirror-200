471748739a7dbf7f7e800bc1c43e3873c834a2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:50 +0000

    Add package conifer/1.0.post0/conifer-1.0.post0.tar.gz

---
+++ packages/conifer/conifer-1.0.post0.tar.gz/conifer-1.0.post0/conifer/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("conifer")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+from conifer import converters
+from conifer import backends
+from conifer import utils
