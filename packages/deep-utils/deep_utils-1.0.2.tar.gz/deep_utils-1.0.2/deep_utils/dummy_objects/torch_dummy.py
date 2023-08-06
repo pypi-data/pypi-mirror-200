6a73b1770b06a4e82bae5580920da626f712b012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package deep-utils/1.0.2/deep_utils-1.0.2.tar.gz

---
+++ packages/deep-utils/deep_utils-1.0.2.tar.gz/deep_utils-1.0.2/deep_utils/dummy_objects/torch_dummy.py
@@ -0,0 +1,17 @@
+from deep_utils.dummy_objects.dummy_framework import DummyObject, requires_backends
+
+
+class TensorboardTorch(metaclass=DummyObject):
+    _backend = ["torch"]
+    _module = "deep_utils.callbacks.torch.torch_tensorboard"
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, self._backend, module_name=self._module, cls_name=self.__class__.__name__)
+
+
+class TorchUtils(metaclass=DummyObject):
+    _backend = ["torch"]
+    _module = "deep_utils.utils.torch_utils.torch_utils"
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, self._backend, module_name=self._module, cls_name=self.__class__.__name__)
