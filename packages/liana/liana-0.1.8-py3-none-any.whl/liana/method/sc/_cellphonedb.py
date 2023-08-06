01daa6cd37f35a1ee8411522bc6690f9837b9390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:01 +0000

    Add package liana/0.1.8/liana-0.1.8-py3-none-any.whl

---
+++ packages/liana/liana-0.1.8-py3-none-any.whl/liana/method/sc/_cellphonedb.py
@@ -0,0 +1,55 @@
+from liana.method._Method import Method, MethodMeta
+from .._pipe_utils._get_mean_perms import _get_lr_pvals
+
+
+def _simple_mean(x, y): return (x + y) / 2
+
+
+# Internal Function to calculate CellPhoneDB LR_mean and p-values
+def _cpdb_score(x, perms, ligand_pos, receptor_pos, labels_pos) -> tuple:
+    """
+    Calculate CellPhoneDB-like LR means and p-values
+    
+    Parameters
+    ----------
+    x
+        DataFrame row
+    perms
+        3D tensor with permuted averages per cluster
+    ligand_pos
+        Index of the ligand in the tensor
+    receptor_pos
+        Index of the receptor in the perms tensor
+    labels_pos
+        Index of cell identities in the perms tensor
+
+    Returns
+    -------
+    A tuple with lr_mean and pvalue for x
+
+    """
+    if (x.ligand_means == 0) | (x.receptor_means == 0):
+        return 0, 1
+
+    return _get_lr_pvals(x, perms, ligand_pos, receptor_pos, labels_pos, _simple_mean)
+
+
+# Initialize CPDB Meta
+_cellphonedb = MethodMeta(method_name="CellPhoneDB",
+                          complex_cols=["ligand_means", "receptor_means"],
+                          add_cols=[],
+                          fun=_cpdb_score,
+                          magnitude="lr_means",
+                          magnitude_ascending=False,
+                          specificity="cellphone_pvals",
+                          specificity_ascending=True,
+                          permute=True,
+                          reference="Efremova, M., Vento-Tormo, M., Teichmann, S.A. and "
+                                    "Vento-Tormo, R., 2020. CellPhoneDB: inferring cell–cell "
+                                    "communication from combined expression of multi-subunit "
+                                    "ligand–receptor complexes. Nature protocols, 15(4), "
+                                    "pp.1484-1506. "
+                          )
+
+# Initialize callable Method instance
+cellphonedb = Method(_SCORE=_cellphonedb)
---
+++ packages/liana/liana-0.1.8-py3-none-any.whl/liana/method/sc/_natmi.py
@@ -0,0 +1,47 @@
+from liana.method._Method import Method, MethodMeta
+
+
+def _natmi_score(x) -> tuple:
+    """
+    Calculate NATMI-like expression product and specificity weights
+
+    Parameters
+    ----------
+    x
+        DataFrame row
+
+    Returns
+    -------
+    tuple(expr_prod, spec_weight)
+
+    """
+
+    # magnitude
+    expr_prod = x.ligand_means * x.receptor_means
+
+    # specificity
+    s_lig = (x.ligand_means / x.ligand_means_sums)
+    s_rec = (x.receptor_means / x.receptor_means_sums)
+    spec_weight = s_lig * s_rec
+
+    return expr_prod, spec_weight
+
+
+# Initialize CPDB Meta
+_natmi = MethodMeta(method_name="NATMI",
+                    complex_cols=['ligand_means', 'receptor_means'],
+                    add_cols=['ligand_means_sums', 'receptor_means_sums'],
+                    fun=_natmi_score,
+                    magnitude='expr_prod',
+                    magnitude_ascending=False,
+                    specificity='spec_weight',
+                    specificity_ascending=False,
+                    permute=False,
+                    reference='Hou, R., Denisenko, E., Ong, H.T., Ramilowski, J.A. and Forrest, '
+                              'A.R., 2020. Predicting cell-to-cell communication networks using '
+                              'NATMI. Nature communications, 11(1), pp.1-11. '
+                    )
+
+# Initialize callable Method instance
+natmi = Method(_SCORE=_natmi)
+
