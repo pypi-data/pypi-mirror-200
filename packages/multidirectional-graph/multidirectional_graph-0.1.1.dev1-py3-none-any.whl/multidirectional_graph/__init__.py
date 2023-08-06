ee389005e3e964299b21ac4576cdf829018b1cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:09 +0000

    Add package multidirectional-graph/0.1.1.dev1/multidirectional_graph-0.1.1.dev1-py3-none-any.whl

---
+++ packages/multidirectional-graph/multidirectional_graph-0.1.1.dev1-py3-none-any.whl/multidirectional_graph/__init__.py
@@ -0,0 +1,12 @@
+from multidirectional_graph._graph import MultidirectionalGraph
+# from pkg_resources import resource_filename
+
+# font_path = resource_filename('multidirectional_graph', 'fonts')
+
+# data = resource_filename(Requirement.parse("main_package"), 'mypackage/data')
+
+# print(font_path)
+
+__all__ = [
+    "MultidirectionalGraph"
+]
