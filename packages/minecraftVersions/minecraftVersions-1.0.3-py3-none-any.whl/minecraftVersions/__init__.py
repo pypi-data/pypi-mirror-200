a3b3359210150bb477ddf177a44d25ca95a3b104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:53 +0000

    Add package minecraftVersions/1.0.3/minecraftVersions-1.0.3-py3-none-any.whl

---
+++ packages/minecraftVersions/minecraftVersions-1.0.3-py3-none-any.whl/minecraftVersions/__init__.py
@@ -0,0 +1,15 @@
+"""
+Minecraft Version Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Python wrapper for Minecraft version.
+
+Author: tungdo0602
+"""
+
+__title__ = "minecraftVersions"
+__version__ = "1.0.3"
+__author__ = "tungdo0602"
+__license__ = "MIT"
+
+from .utils import *
