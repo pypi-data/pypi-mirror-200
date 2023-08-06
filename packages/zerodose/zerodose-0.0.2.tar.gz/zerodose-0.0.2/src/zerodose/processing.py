d87665af542bde09d45c89ba4b0025eb7debf209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package zerodose/0.0.2/zerodose-0.0.2.tar.gz

---
+++ packages/zerodose/zerodose-0.0.2.tar.gz/zerodose-0.0.2/src/zerodose/processing.py
@@ -0,0 +1,134 @@
+"""Processing functions for the ZeroDose model."""
+
+import torch
+import torch.nn as nn
+import torchio as tio
+from torchio import SpatialTransform
+from torchio.data.subject import Subject
+
+from zerodose import processing
+from zerodose import utils
+
+
+_X_MIN_MNI = 0
+_X_MAX_MNI = 192
+_Y_MIN_MNI = 21
+_Y_MAX_MNI = 213
+_Z_MIN_MNI = -7
+_Z_MAX_MNI = 185
+
+
+def _crop_mni_to_192(arr: torch.Tensor) -> torch.Tensor:
+    """Crop the MNI image to 192x192x192."""
+    parr = torch.zeros((1, _X_MAX_MNI - _X_MIN_MNI, _Y_MAX_MNI - _Y_MIN_MNI, 192))
+    parr[:, :, :, abs(_Z_MIN_MNI) : 192] = arr[
+        :, _X_MIN_MNI:_X_MAX_MNI, _Y_MIN_MNI:_Y_MAX_MNI, :_Z_MAX_MNI
+    ]
+
+    return parr
+
+
+def _crop_192_to_mni(arr: torch.Tensor) -> torch.Tensor:
+    """Crop the 192x192x192 image to MNI."""
+    parr = torch.zeros((1, 197, 233, 189))
+    parr[:, _X_MIN_MNI:_X_MAX_MNI, _Y_MIN_MNI:_Y_MAX_MNI, :_Z_MAX_MNI] = arr[
+        :, :, :, abs(_Z_MIN_MNI) : 192
+    ]
+
+    return parr
+
+
+def postprocess(arr: torch.Tensor) -> torch.Tensor:
+    """Postprocess the 192x192x192 image to MNI."""
+    return _crop_192_to_mni(arr)
+
+
+class Pad(SpatialTransform):
+    """Pad the MNI image to 192x192x192."""
+
+    def __init__(self, **kwargs) -> None:
+        """Initialize the transform."""
+        super().__init__(**kwargs)
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        """Apply the transform to the subject."""
+        for image in self.get_images(subject):
+            _pad(image)
+
+        return subject
+
+    @staticmethod
+    def is_invertible() -> bool:
+        """Return whether the transform is invertible."""
+        return False
+
+
+def _pad(image: tio.Image) -> None:
+    data = processing._crop_mni_to_192(image.data)
+    image.set_data(data)
+
+
+class ToFloat32(SpatialTransform):
+    """Convert the image to float32."""
+
+    def __init__(self, **kwargs) -> None:
+        """Initialize the transform."""
+        super().__init__(**kwargs)
+
+    def apply_transform(self, subject: Subject) -> Subject:
+        """Apply the transform to the subject."""
+        for image in self.get_images(subject):
+            _to_float(image)
+
+        return subject
+
+    @staticmethod
+    def is_invertible() -> bool:
+        """Return whether the transform is invertible."""
+        return False
+
+
+def _to_float(image: tio.Image) -> None:
+    _data = image.numpy().astype("f")
+    data = torch.as_tensor(_data)
+    image.set_data(data)
+
+
+class QuantileNormalization(nn.Module):
+    """Quantile normalization of the sbPET image."""
+
+    def __init__(
+        self,
+        quantile,
+        sigma_normalization=3,
+    ) -> None:
+        """Initialize the normalization module."""
+        super().__init__()
+        self.quantile = quantile
+        self.smooth_normalization = utils.GaussianSmoothing(
+            channels=1,
+            kernel_size=5 * sigma_normalization,
+            sigma=sigma_normalization,
+            dim=3,
+        )
+
+    def forward(
+        self, pet: torch.Tensor, sbpet: torch.Tensor, mask: torch.Tensor
+    ) -> torch.Tensor:
+        """Normalize the sbPET image."""
+        return self._scale_sbpet(pet, sbpet, mask)
+
+    def _get_normalization_mask(self, pet, mask):
+        qt = torch.quantile(pet[mask], self.quantile)
+        norm_mask = mask & (pet > qt)
+        return norm_mask
+
+    def _scale_sbpet(self, pet, sbpet, mask):
+        for _i in range(2):
+            pet_blurred = self.smooth_normalization(pet)
+            norm_mask = self._get_normalization_mask(pet_blurred, mask)
+            norm_const = torch.mean(pet[norm_mask]) / torch.mean(sbpet[norm_mask])
+            sbpet *= norm_const
+            sbpet[~mask] = pet[~mask]
+
+        return sbpet
---
+++ packages/zerodose/zerodose-0.0.2.tar.gz/zerodose-0.0.2/src/zerodose/run_synthesize.py
@@ -0,0 +1,101 @@
+"""Functions called by the CLI."""
+
+
+from typing import Sequence
+from typing import Tuple
+from typing import Union
+
+import torch
+import torchio as tio
+from torch.utils.data import DataLoader
+from torchio.data import GridAggregator
+from torchio.data import GridSampler
+
+from zerodose import processing
+from zerodose import utils
+from zerodose.dataset import SubjectDataset
+from zerodose.model import ZeroDose
+
+
+def _infer_single_subject(
+    model: ZeroDose,
+    subject: tio.Subject,
+    ps: Tuple[int, int, int],
+    po: Tuple[int, int, int],
+    bs: int,
+    std: Union[float, int],
+    device: Union[torch.device, str],
+) -> torch.Tensor:
+    """Infer a single subject."""
+    grid_sampler = GridSampler(subject, ps, po)
+    patch_loader = DataLoader(
+        grid_sampler, batch_size=bs, num_workers=0  # type: ignore
+    )
+    aggregator = GridAggregator(grid_sampler, overlap_mode="average")
+    aggregator_weight = GridAggregator(grid_sampler, overlap_mode="average")
+    weight = utils.get_gaussian_weight(ps, std).to(device)
+    with torch.no_grad():
+        for patches_batch in patch_loader:
+            patch_x = patches_batch["mr"][tio.DATA].to(device=device)
+            locations = patches_batch[tio.LOCATION]
+            patch_y = model(patch_x)
+            aggregator.add_batch(patch_y * weight, locations)
+            aggregator_weight.add_batch(weight, locations)
+
+    return (
+        aggregator.get_output_tensor().detach()
+        / aggregator_weight.get_output_tensor().detach()
+    )
+
+
+def synthesize_baselines(
+    mri_fnames: Sequence[str],
+    mask_fnames: Sequence[str],
+    out_fnames: Sequence[str],
+    device: Union[torch.device, str] = "cuda:0",
+    sd_weight: Union[float, int] = 5,
+    verbose: bool = False,
+    batch_size: int = 1,
+    stride: int = 2,
+    save_output: bool = True,
+) -> None:
+    """Synthesize baseline PET images from MRI images."""
+    if isinstance(mri_fnames, str):
+        mri_fnames = [mri_fnames]
+    if isinstance(mask_fnames, str):
+        mask_fnames = [mask_fnames]
+    if isinstance(out_fnames, str):
+        out_fnames = [out_fnames]
+
+    if not (len(mri_fnames) == len(mask_fnames) and len(mri_fnames) == len(out_fnames)):
+        raise Exception(
+            """The number of input files {} mask files {}
+            and output files {} must be identical""".format(
+                len(mri_fnames), len(mask_fnames), len(out_fnames)
+            )
+        )
+
+    dataset = SubjectDataset(mri_fnames, mask_fnames, out_fnames)
+    model = utils.get_model()
+
+    model = model.to(device)
+    model.eval()
+    patch_size = (32, 192, 192)
+    patch_overlap = (32 - stride, 192 - stride, 192 - stride)
+
+    for sub in dataset:  # type: ignore
+        if verbose:
+            print(f"Synthesizing sbPET for {sub.mr.path}")
+
+        sbpet = _infer_single_subject(
+            model, sub, patch_size, patch_overlap, batch_size, sd_weight, device
+        )
+
+        sbpet = sbpet.cpu().data * sub.mask.data
+        sbpet = processing.postprocess(sbpet)
+
+        if save_output:
+            if verbose:
+                print(f"Saving to {sub.out_fname}")
+
+            utils.save_nifty(sbpet, sub.out_fname, affine_ref=str(sub.mr.path))
