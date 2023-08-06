76a7c1e093bbe5d621b0e1bb5d9ed872f075320d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package nvidia-dali-tf-plugin-cuda110/1.18.0/nvidia-dali-tf-plugin-cuda110-1.18.0.tar.gz

---
+++ packages/nvidia-dali-tf-plugin-cuda110/nvidia-dali-tf-plugin-cuda110-1.18.0.tar.gz/nvidia-dali-tf-plugin-cuda110-1.18.0/nvidia/dali_tf_plugin/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = '1.18.0'
+__git_sha__ = 'fd86177772d51ee2c5e7c2f5c564488a99a7c48c'
