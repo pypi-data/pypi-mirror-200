a682bfa50f3965e02e656b414636af97ae280b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:41 +0000

    Add package flying-discs/0.3.0/flying_discs-0.3.0-py3-none-any.whl

---
+++ packages/flying-discs/flying_discs-0.3.0-py3-none-any.whl/flying_discs/morrison/coordinates.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Sequence
+
+import numpy as np
+
+
+@dataclass
+class MorrisonPosition2D:
+    x: float
+    z: float
+    vx: float
+    vz: float
+    ax: float
+    az: float
+
+
+class MorrisonTrajectory2D:
+    # pylint: disable=too-many-instance-attributes, invalid-name
+    def __init__(self, positions: Sequence[MorrisonPosition2D]) -> None:
+        self.positions = positions
+        self.X = np.array([p.x for p in self.positions])
+        self.Z = np.array([p.z for p in self.positions])
+        self.VX = np.array([p.vx for p in self.positions])
+        self.VZ = np.array([p.vz for p in self.positions])
+        self.AX = np.array([p.ax for p in self.positions])
+        self.AZ = np.array([p.az for p in self.positions])
+        self.__n = 0  # iterator count
+
+    def __iter__(self) -> "MorrisonTrajectory2D":
+        self.__n = 0
+        return self
+
+    def __next__(self) -> MorrisonPosition2D:
+        if self.__n <= len(self.positions) - 1:
+            result = self.positions[self.__n]
+            self.__n += 1
+            return result
+        raise StopIteration
+
+    def __getitem__(self, i: int) -> MorrisonPosition2D:
+        return self.positions[i]
+
+    def __len__(self) -> int:
+        return len(self.positions)
+
+    def __eq__(self, o: object) -> bool:
+        return self.positions == o
+
+
+@dataclass
+class MorrisonPosition3D:
+    x: float
+    y: float
+    z: float
+    vx: float
+    vy: float
+    vz: float
+    ax: float
+    ay: float
+    az: float
+
+
+class MorrisonTrajectory3D:
+    # pylint: disable=too-many-instance-attributes, invalid-name
+
+    def __init__(self, positions: Sequence[MorrisonPosition3D]) -> None:
+        self.positions = positions
+        self.X = np.array([p.x for p in self.positions])
+        self.Y = np.array([p.y for p in self.positions])
+        self.Z = np.array([p.z for p in self.positions])
+        self.VX = np.array([p.vx for p in self.positions])
+        self.VY = np.array([p.vy for p in self.positions])
+        self.VZ = np.array([p.vz for p in self.positions])
+        self.AX = np.array([p.ax for p in self.positions])
+        self.AY = np.array([p.ay for p in self.positions])
+        self.AZ = np.array([p.az for p in self.positions])
+        self.__n = 0  # iterator count
+
+    def __iter__(self) -> "MorrisonTrajectory3D":
+        self.__n = 0
+        return self
+
+    def __next__(self) -> MorrisonPosition3D:
+        if self.__n <= len(self.positions) - 1:
+            result = self.positions[self.__n]
+            self.__n += 1
+            return result
+        raise StopIteration
+
+    def __getitem__(self, i: int) -> MorrisonPosition3D:
+        return self.positions[i]
+
+    def __len__(self) -> int:
+        return len(self.positions)
+
+    def __eq__(self, o: object) -> bool:
+        return self.positions == o
