26fd1ca59392fa4f3c28ba55efbf5b85e3a3a48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:48 +0000

    Add package silobuster/0.1.0/silobuster-0.1.0.tar.gz

---
+++ packages/silobuster/silobuster-0.1.0.tar.gz/silobuster-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='silobuster',
+    packages=['silobuster'],
+    version='0.1.0',
+    package_dir={'silobuster': 'silobuster'},
+    install_requires=[
+        'XlsxWriter>=3.0.7',
+        'numpy>=1.21.5',
+        'pandas>=1.4.4',
+        'pandas-dedupe>=1.5.0',
+        'Django>=4.1.5',
+        'djangorestframework>=3.14.0',
+        'psycopg2>=2.9.3',
+        'requests>=2.25.1',
+    ],
+)
+
