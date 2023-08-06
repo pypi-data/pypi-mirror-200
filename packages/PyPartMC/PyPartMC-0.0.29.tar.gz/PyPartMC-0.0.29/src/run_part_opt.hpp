1a3024b5d149a33633145b7f04830eb6d714d331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:46 +0000

    Add package PyPartMC/0.0.29/PyPartMC-0.0.29.tar.gz

---
+++ packages/PyPartMC/PyPartMC-0.0.29.tar.gz/PyPartMC-0.0.29/src/run_part_opt.hpp
@@ -0,0 +1,28 @@
+/*##################################################################################################
+# This file is a part of PyPartMC licensed under the GNU General Public License v3 (LICENSE file)  #
+# Copyright (C) 2022 University of Illinois Urbana-Champaign                                       #
+# Authors: https://github.com/open-atmos/PyPartMC/graphs/contributors                              #
+##################################################################################################*/
+
+#pragma once
+
+#include "pmc_resource.hpp"
+#include "gimmicks.hpp"
+#include "pybind11_json/pybind11_json.hpp"
+
+extern "C" void f_run_part_opt_ctor(void *ptr) noexcept;
+extern "C" void f_run_part_opt_dtor(void *ptr) noexcept;
+extern "C" void f_run_part_opt_from_json(const void *ptr) noexcept;
+
+struct RunPartOpt {
+    PMCResource ptr;
+
+    RunPartOpt(const nlohmann::json &json) :
+        ptr(f_run_part_opt_ctor, f_run_part_opt_dtor)
+    {
+        gimmick_ptr() = std::make_unique<InputGimmick>(json); // TODO #117: guard
+        f_run_part_opt_from_json(this->ptr.f_arg());
+        gimmick_ptr().reset();
+    }
+};
+
