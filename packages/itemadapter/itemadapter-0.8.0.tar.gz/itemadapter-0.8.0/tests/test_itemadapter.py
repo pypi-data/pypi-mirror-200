20f8b725a56e69cc425a8ee54e17b49a59389d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:06 +0000

    Add package itemadapter/0.8.0/itemadapter-0.8.0.tar.gz

---
+++ packages/itemadapter/itemadapter-0.8.0.tar.gz/itemadapter-0.8.0/tests/test_itemadapter.py
@@ -0,0 +1,18 @@
+import unittest
+from collections import deque
+
+from itemadapter.adapter import ItemAdapter, DictAdapter
+
+
+class DictOnlyItemAdapter(ItemAdapter):
+    ADAPTER_CLASSES = deque([DictAdapter])
+
+
+class ItemAdapterTestCase(unittest.TestCase):
+    def test_repr(self):
+        adapter = ItemAdapter(dict(foo="bar"))
+        self.assertEqual(repr(adapter), "<ItemAdapter for dict(foo='bar')>")
+
+    def test_repr_subclass(self):
+        adapter = DictOnlyItemAdapter(dict(foo="bar"))
+        self.assertEqual(repr(adapter), "<DictOnlyItemAdapter for dict(foo='bar')>")
