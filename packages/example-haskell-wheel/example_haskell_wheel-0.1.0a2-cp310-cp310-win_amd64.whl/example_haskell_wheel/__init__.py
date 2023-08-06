948cfbf242f9e10de9bff5f9edcc0b9dba364252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:52 +0000

    Add package example-haskell-wheel/0.1.0a2/example_haskell_wheel-0.1.0a2-cp310-cp310-win_amd64.whl

---
+++ packages/example-haskell-wheel/example_haskell_wheel-0.1.0a2-cp310-cp310-win_amd64.whl/example_haskell_wheel/__init__.py
@@ -0,0 +1,13 @@
+from example_haskell_wheel._binding import hs_rts_init, hs_rts_exit, hs_fib
+
+
+def fib(n: int) -> int:
+    hs_rts_init()
+    r = hs_fib(n)
+    hs_rts_exit()
+    return r
+
+
+def main():
+    print("Haskell says the 42nd Fibonacci number is...")
+    print(fib(42))
