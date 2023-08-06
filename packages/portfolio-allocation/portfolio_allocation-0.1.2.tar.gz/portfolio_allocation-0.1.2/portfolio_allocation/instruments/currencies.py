9ccaf2e86d742aa712b9a666398a133d6260b02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:12 +0000

    Add package portfolio-allocation/0.1.2/portfolio_allocation-0.1.2.tar.gz

---
+++ packages/portfolio-allocation/portfolio_allocation-0.1.2.tar.gz/portfolio_allocation-0.1.2/portfolio_allocation/instruments/currencies.py
@@ -0,0 +1,28 @@
+import pycountry
+
+
+def currencies(currency_codes: list[str]) -> dict[str, dict]:
+    result = {}
+    for currency_code in currency_codes:
+        currency = pycountry.currencies.get(alpha_3=currency_code)
+        if currency is None:
+            continue
+        result[currency_code] = {
+            'countries': {
+                _to_country(currency.alpha_3): 1
+            },
+            'industries': {},
+            'fee': 0,
+            'currencies': {
+                currency.alpha_3: 1
+            },
+            'classes': {
+                'Cash': 1
+            }
+        }
+    return result
+
+
+def _to_country(currency: str) -> str:
+    # todo why Europe is missing in the database?
+    return 'Europe' if currency == 'EUR' else pycountry.countries.get(alpha_2=currency[0:2]).name
