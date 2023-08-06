6261bdc6ee9e574cd303db10a70ebf932eb3dbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:34 +0000

    Add package boilerplate-x/0.2.5/boilerplate_x-0.2.5-py3-none-any.whl

---
+++ packages/boilerplate-x/boilerplate_x-0.2.5-py3-none-any.whl/boilerplate_x/cli.py
@@ -0,0 +1,132 @@
+import getpass
+import logging
+import os
+
+import click
+from rich.logging import RichHandler
+
+from .constants import CLI_OPTIONS, CLI_OPTIONS_FULL
+from .generator import ProjectGenerator
+
+logging.basicConfig(level=logging.INFO, handlers=[RichHandler(rich_tracebacks=True)])
+logger = logging.getLogger(__name__)
+
+
+def run(
+    prompt: str,
+    output_path: str,
+    verbose: bool,
+    customisation_kwargs: dict,
+    github_repo_creator_kwargs: dict,
+):
+    """Runs ProjectGenerator."""
+    generator = ProjectGenerator(
+        prompt=prompt,
+        output_path=output_path,
+        verbose=verbose,
+        customisation_kwargs=customisation_kwargs,
+        github_repo_creator_kwargs=github_repo_creator_kwargs,
+    )
+
+    logger.info("Generating project template...")
+    generator.generate_template()
+
+    logger.info("Your project is now ready to use!")
+
+
+@click.command()
+@click.option("-p", "--prompt", required=True, help="Your project idea.")
+@click.option(
+    "-o",
+    "--output-path",
+    "output_path",
+    required=True,
+    help="The output path for the generated project template.",
+)
+@click.option(
+    "-g",
+    "--create-github-repo",
+    "enable_github",
+    is_flag=True,
+    help="Create a GitHub repository for the generated project template.",
+)
+@click.option(
+    "-c",
+    "--customise",
+    "enable_customisation",
+    is_flag=True,
+    help="Enable customisation options for the project.",
+)
+@click.option("-v", "--verbose", is_flag=True, help="Run in verbose mode.")
+def main(
+    prompt: str,
+    output_path: str,
+    enable_customisation: bool,
+    enable_github: bool,
+    verbose: bool,
+):
+    """Boilerplate-X CLI.
+
+    Generates a project boilerplate at the specified output path based on your project idea.
+    """
+    logger.info("Welcome to Boilerplate-X CLI!")
+
+    if not os.environ.get("OPENAI_API_KEY"):
+        os.environ["OPENAI_API_KEY"] = getpass.getpass(
+            prompt="Enter your OpenAI API key: "
+        )
+
+    if enable_customisation:
+        logger.info(
+            "You have enabled customisation options. Please select the options you would like to enable."
+        )
+
+    customisation_kwargs = {
+        opt: CLI_OPTIONS_FULL[
+            click.prompt(
+                f"Enable {opt}?",
+                type=click.Choice(CLI_OPTIONS),
+                default="n",
+                show_choices=True,
+            )
+        ]
+        if enable_customisation
+        else False
+        for opt in [
+            "unit_tests",
+            "dockerization",
+            "github_actions",
+            "pre_commit_hooks",
+        ]
+    }
+
+    github_repo_creator_kwargs = {}
+    if enable_github:
+        logger.info(
+            "You are about to create a GitHub repository for your project. Please provide your GitHub personal access token. "
+            "Visit https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token for more information."
+            "Recommended scopes: 'repo', 'workflow'"
+        )
+        token = getpass.getpass(prompt="Enter your Github personal access token: ")
+        repo_name = click.prompt("Enter the name of the repository")
+        private = (
+            click.prompt(
+                "Make the repository private?",
+                type=click.Choice(CLI_OPTIONS),
+                default="n",
+                show_choices=True,
+            ).lower()
+            == "y"
+        )
+        github_repo_creator_kwargs = {
+            "token": token,
+            "repo_name": repo_name,
+            "private": private,
+            "target_folder": output_path,
+        }
+
+    run(prompt, output_path, verbose, customisation_kwargs, github_repo_creator_kwargs)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/boilerplate-x/boilerplate_x-0.2.5-py3-none-any.whl/boilerplate_x/generator.py
@@ -0,0 +1,105 @@
+import logging
+from pathlib import Path
+
+import yaml
+from langchain.chat_models import ChatOpenAI
+
+from .chain import load_project_file_chain, load_project_structure_chain
+from .github import GithubRepoCreator
+from .schemas import ProjectGeneratorSchema
+
+logger = logging.getLogger(__name__)
+schema = ProjectGeneratorSchema()
+
+
+class ProjectGenerator:
+    """Generates a project boilerplate code based on the project idea."""
+
+    def __init__(
+        self,
+        prompt: str,
+        output_path: str,
+        verbose: bool,
+        customisation_kwargs: dict,
+        github_repo_creator_kwargs: dict,
+    ) -> None:
+        """Constructor.
+
+        Args:
+            prompt: The project idea.
+            output_path: The output path for the generated project template.
+        """
+        self.prompt = prompt
+        self.output_path = output_path
+        self.verbose = verbose
+        self.customisation_kwargs = customisation_kwargs
+        self.github_repo_creator_kwargs = github_repo_creator_kwargs
+
+        self.project_structure_chain = load_project_structure_chain(
+            ChatOpenAI(**schema.project_structure_llm.dict()), self.verbose
+        )
+        self.project_file_chain = load_project_file_chain(
+            ChatOpenAI(**schema.project_file_llm.dict()), self.verbose
+        )
+
+        self.github_repo_url = None
+
+    def generate_template(self) -> None:
+        """Generates the project template."""
+        project_structure = self._generate_project_structure()
+        self._generate_project_files(project_structure)
+        logger.info(f"Your project is now available at {self.output_path} !")
+
+        if self.github_repo_creator_kwargs:
+            logger.info("Pushing your project to GitHub...")
+            github_repo_creator = GithubRepoCreator(**self.github_repo_creator_kwargs)
+            self.github_repo_url = github_repo_creator.setup_github_repo()
+            logger.info(
+                f"Your GitHub repository is now available at {self.github_repo_url} !"
+            )
+
+    def _generate_project_structure(self) -> list[str]:
+        """Generates the project structure."""
+        project_structure_file = (
+            Path(self.output_path) / schema.project_structure_file_name
+        )
+        if project_structure_file.exists():
+            logger.info("Using cached project structure...")
+            project_structure = yaml.safe_load(
+                project_structure_file.read_text().strip()
+            )
+            return project_structure
+
+        logger.info("Generating project structure...")
+        chain_output = self.project_structure_chain.predict(
+            project_idea=self.prompt, **self.customisation_kwargs
+        )
+        project_structure = yaml.safe_load(chain_output.strip())
+
+        # cache the project structure
+        self._write_file(
+            schema.project_structure_file_name, yaml.safe_dump(project_structure)
+        )
+
+        return project_structure
+
+    def _generate_project_files(self, project_structure: list[str]) -> None:
+        """Generates the project files."""
+        project_structure_str = yaml.safe_dump(project_structure)
+        for file_name in project_structure:
+            if (Path(self.output_path) / file_name).exists():
+                logger.info(f"File already exists: {file_name}")
+                continue
+            logger.info(f"Generating file content: {file_name}...")
+            file_content = self.project_file_chain.predict(
+                project_idea=self.prompt,
+                project_structure=project_structure_str,
+                file_name=file_name,
+            )
+            self._write_file(file_name, file_content)
+
+    def _write_file(self, file_name: str, file_content: str):
+        """Writes the file to the output path."""
+        file_path = Path(self.output_path) / file_name
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.write_text(file_content)
