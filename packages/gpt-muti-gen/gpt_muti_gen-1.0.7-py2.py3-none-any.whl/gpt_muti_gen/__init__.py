fa9bb790a42c9a17ee7b4a354854b27d63f3c25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:10 +0000

    Add package gpt-muti-gen/1.0.7/gpt_muti_gen-1.0.7-py2.py3-none-any.whl

---
+++ packages/gpt-muti-gen/gpt_muti_gen-1.0.7-py2.py3-none-any.whl/gpt_muti_gen/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Contact :   liuyuqi.gov@msn.cn
+@Time    :   2022/12/17 00:24:54
+@License :   Copyright © 2017-2022 liuyuqi. All Rights Reserved.
+@Desc    :   
+'''
+from gpt_muti_gen.cgpt_wp import CgptWp
+
+def main():
+    cgptWp = CgptWp()
+    cgptWp.run()
