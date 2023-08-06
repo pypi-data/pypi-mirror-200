315ed4fd4e6a2aaebffbe121f2794076d5e9cae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:06 +0000

    Add package koldstart/0.6.23/koldstart-0.6.23.tar.gz

---
+++ packages/koldstart/koldstart-0.6.23.tar.gz/koldstart-0.6.23/src/koldstart/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from koldstart.api import KoldstartHost, LocalHost, cached, isolated
+from koldstart.sdk import CloudKeyCredentials
+
+local = LocalHost()
+cloud = KoldstartHost()
