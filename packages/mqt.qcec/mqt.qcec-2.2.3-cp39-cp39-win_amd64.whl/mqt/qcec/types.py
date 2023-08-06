79e94d62f1bf46208e8e986c34ca824528bc97df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:07 +0000

    Add package mqt.qcec/2.2.3/mqt.qcec-2.2.3-cp39-cp39-win_amd64.whl

---
+++ packages/mqt.qcec/mqt.qcec-2.2.3-cp39-cp39-win_amd64.whl/mqt/qcec/types.py
@@ -0,0 +1,15 @@
+"""Types for the QCEC module."""
+
+from typing import Literal
+
+ApplicationSchemeName = Literal["sequential", "one_to_one", "proportional", "lookahead", "gate_cost"]
+EquivalenceCriterionName = Literal[
+    "no_information",
+    "not_equivalent",
+    "equivalent",
+    "equivalent_up_to_phase",
+    "equivalent_up_to_global_phase",
+    "probably_equivalent",
+    "probably_not_equivalent",
+]
+StateTypeName = Literal["computational_basis", "random_1Q_basis", "stabilizer"]
