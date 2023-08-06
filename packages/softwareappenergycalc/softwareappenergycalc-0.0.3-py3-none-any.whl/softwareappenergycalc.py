b0232f1d6ea3508ab98dd701d9c25e1c0e79ca4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:43 +0000

    Add package softwareappenergycalc/0.0.3/softwareappenergycalc-0.0.3-py3-none-any.whl

---
+++ packages/softwareappenergycalc/softwareappenergycalc-0.0.3-py3-none-any.whl/softwareappenergycalc.py
@@ -0,0 +1,6 @@
+from pyJoules.energy_meter import measure_energy
+from pyJoules.handler.csv_handler import CSVHandler
+from pyJoules.energy_meter import EnergyContext
+
+def custom_measure_energy():
+	print('Trigger custom measure energy function')
