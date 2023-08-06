4abcb62f77536074ad54cd488c98e178478b0b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:27 +0000

    Add package country-call/0.0.2/country_call-0.0.2.tar.gz

---
+++ packages/country-call/country_call-0.0.2.tar.gz/country_call-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("read_me.md","r") as fh:
+    ld=fh.read()
+
+classifiers=[
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+    
+]
+
+setup(name="country_call",
+version="0.0.2",
+description="Introducing a powerful and easy-to-use Python library for country detection from ambiguous text, designed for developers and data scientists alike.",
+author="Md Shamim Hasan, Kazi Arman Rahat, Mukitul Islam, Anik Roy",
+long_description=ld,
+
+author_email="shihab18015@gmail.com",
+install_requires=['re'],
+keywords=['country','origin','nlp'],
+classifiers=classifiers
+)
