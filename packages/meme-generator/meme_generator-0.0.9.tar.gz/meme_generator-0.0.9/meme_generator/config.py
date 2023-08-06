7dbd49cd38ffc292aa887186da422f2a74cbf8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:27 +0000

    Add package meme-generator/0.0.9/meme_generator-0.0.9.tar.gz

---
+++ packages/meme-generator/meme_generator-0.0.9.tar.gz/meme_generator-0.0.9/meme_generator/config.py
@@ -0,0 +1,59 @@
+import json
+from pathlib import Path
+from typing import List
+
+import toml
+from pydantic import BaseModel, Extra
+
+from .dirs import get_config_file
+
+config_file_path = get_config_file("config.toml")
+
+
+class MemeConfig(BaseModel):
+    load_builtin_memes: bool = True
+    meme_dirs: List[Path] = []
+    meme_disabled_list: List[str] = []
+
+
+class ResourceConfig(BaseModel):
+    resource_url: str = (
+        "https://ghproxy.com/https://raw.githubusercontent.com/MeetWq/meme-generator"
+    )
+
+
+class GifConfig(BaseModel):
+    gif_max_size: float = 10
+    gif_max_frames: int = 100
+
+
+class TranslatorConfig(BaseModel):
+    baidu_trans_appid: str = ""
+    baidu_trans_apikey: str = ""
+
+
+class ServerConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: int = 2233
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    meme: MemeConfig = MemeConfig()
+    resource: ResourceConfig = ResourceConfig()
+    gif: GifConfig = GifConfig()
+    translate: TranslatorConfig = TranslatorConfig()
+    server: ServerConfig = ServerConfig()
+
+    @classmethod
+    def load(cls) -> "Config":
+        return cls.parse_obj(toml.load(config_file_path))
+
+    def dump(self):
+        with open(config_file_path, "w", encoding="utf8") as f:
+            toml.dump(json.loads(self.json()), f)
+
+
+if not config_file_path.exists():
+    meme_config = Config()
+else:
+    meme_config = Config.load()
