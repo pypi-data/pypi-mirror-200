05f714c14961d9a948ca005ad13cbfac126da4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:58 +0000

    Add package budibase-client/0.1/budibase%20client-0.1.tar.gz

---
+++ packages/budibase-client/budibase%20client-0.1.tar.gz/budibase client-0.1/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+# with open('LICENSE') as f:
+#     license = f.read()
+
+setup(
+    name='budibase client',
+    version='0.1',
+    description='A lightweight client for budibase',
+    long_description=readme,
+    author='Nick Schulze',
+    author_email='nick.schulze@uk-koeln.de',
+    #url='https://github.com/kennethreitz/samplemod',
+    license='MIT',
+    packages=['budibase_client'],
+    keywords=['budibase', 'client', 'api'],
+    install_requires=install_requires,
+)
