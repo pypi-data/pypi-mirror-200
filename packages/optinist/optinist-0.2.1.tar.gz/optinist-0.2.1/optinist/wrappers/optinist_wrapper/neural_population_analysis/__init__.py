f83c199cfd2610577d1ec74985cae659730a1f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:17 +0000

    Add package optinist/0.2.1/optinist-0.2.1.tar.gz

---
+++ packages/optinist/optinist-0.2.1.tar.gz/optinist-0.2.1/optinist/wrappers/optinist_wrapper/neural_population_analysis/__init__.py
@@ -0,0 +1,18 @@
+from .correlation import correlation
+from .cross_correlation import cross_correlation
+from .granger import Granger
+
+
+neural_population_analysis_wrapper_dict = {
+    'correlation': {
+        'function': correlation,
+    },
+    'cross_correlation': {
+        'function': cross_correlation,
+        'conda_yaml': 'optinist_env.yaml',
+    },
+    'granger': {
+        'function': Granger,
+        'conda_yaml': 'optinist_env.yaml',
+    },
+}
