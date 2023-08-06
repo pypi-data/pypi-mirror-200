0f16bed7f4782dbd422df777afefa21db17f9ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:16 +0000

    Add package fastiot/0.9/fastiot-0.9.tar.gz

---
+++ packages/fastiot/fastiot-0.9.tar.gz/fastiot-0.9/src/fastiot/cli/model/compose_info.py
@@ -0,0 +1,17 @@
+from typing import List, Dict
+
+from pydantic.main import BaseModel
+
+
+class ServiceComposeInfo(BaseModel):
+    name: str
+    image: str
+    environment: Dict[str, str]
+    ports: List[str]
+    volumes: List[str]
+    devices: List[str] = []
+    tmpfs: List[str] = []
+
+    privileged: bool = False
+    extras: str = ""
+    # Must contain valid YAML including newlines if neccessary.
---
+++ packages/fastiot/fastiot-0.9.tar.gz/fastiot-0.9/src/fastiot/cli/model/service.py
@@ -0,0 +1,38 @@
+""" data models for FastIoT and infrastructure services """
+import os
+from typing import List
+
+from pydantic import BaseModel
+
+from fastiot.cli.constants import MANIFEST_FILENAME
+from fastiot.cli.model import ServiceManifest
+
+
+class Service(BaseModel):
+    """
+    The model class for a service
+    """
+    name: str
+    package: str
+    cache: str = ""
+    """
+    The name to use as the cache on the set docker cache registry. The tag will
+    be appended automatically (in case not empty), please do not specify it.
+    Example: mypackage-cache
+    """
+    extra_caches: List[str] = []
+    """
+    A list of extra caches used to speed up building which will all be read-only. It is intended if you want to read
+    from other caches or different tags. Each extra cache must match a cache name extended by ':' followed by the tag
+    for the cache. It is useful to always include the cache name followed by tag latest to always use latest cache for
+    feature branches.
+    Examples: mypackage-cache:latest, mypackage-cache:my-feature
+    """
+
+    def read_manifest(self, check_service_name: str = "") -> ServiceManifest:
+        """ Reads out the service manifest, if not run before using the yaml-file otherwise just ``self.manifest``"""
+        from fastiot.cli.model.project import ProjectContext  # pylint: disable=import-outside-toplevel
+        default_context = ProjectContext.default
+        manifest_path = os.path.join(default_context.project_root_dir, 'src',
+                                     self.package, self.name, MANIFEST_FILENAME)
+        return ServiceManifest.from_yaml_file(manifest_path, check_service_name=check_service_name)
