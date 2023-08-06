06ea75ebc4b093fd7a55b91f27d0fa64f6cec937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:29 +0000

    Add package Geode-SimplexRemeshGeosciences/1.11.2/Geode_SimplexRemeshGeosciences-1.11.2-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-SimplexRemeshGeosciences/Geode_SimplexRemeshGeosciences-1.11.2-cp310-cp310-win_amd64.whl/geode_simplexremeshgeosciences/cross_section.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_geosciences
+import geode_simplexremesh
+
+from .geode_simplexremeshgeosciences_py_cross_section import *
+SimplexRemeshGeosciencesCrossSection.initialize()
