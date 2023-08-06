31e639800999842f3ce41db59f5a9224f75249a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:00 +0000

    Add package lotronexyz12-sdk/0.1.2/lotronexyz12_sdk-0.1.2.tar.gz

---
+++ packages/lotronexyz12-sdk/lotronexyz12_sdk-0.1.2.tar.gz/lotronexyz12_sdk-0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lotronexyz12_sdk",
+    version="0.1.2",
+    author="Alex",
+    author_email="",
+    description="LOTR coding test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    python_requires='>=3.10.9',
+    install_requires=[
+        'requests'
+    ],
+    keywords=''
+)
