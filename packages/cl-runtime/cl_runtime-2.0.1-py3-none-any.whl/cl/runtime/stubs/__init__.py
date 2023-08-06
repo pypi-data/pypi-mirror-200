73a78c20f357259ca8dc1634f2ab1d93448c26df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:08 +0000

    Add package cl-runtime/2.0.1/cl_runtime-2.0.1-py3-none-any.whl

---
+++ packages/cl-runtime/cl_runtime-2.0.1-py3-none-any.whl/cl/runtime/stubs/__init__.py
@@ -0,0 +1,6 @@
+from cl.runtime.stubs.storage.stub_data import StubData
+from cl.runtime.stubs.storage.stub_record import StubRecord
+from cl.runtime.stubs.storage.stub_class_data import StubClassData
+from cl.runtime.stubs.storage.stub_class_record_key import StubClassRecordKey
+from cl.runtime.stubs.storage.stub_class_record import StubClassRecord
+from cl.runtime.stubs.storage.stub_dict import StubDict
---
+++ packages/cl-runtime/cl_runtime-2.0.1-py3-none-any.whl/cl/runtime/stubs/storage/stub_class_record.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2023-present The CompatibL Runtime Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+import cl.runtime as rt
+from cl.runtime.stubs.storage.stub_class_record_key import StubClassRecordKey
+
+
+@dataclass
+class StubClassRecord(StubClassRecordKey):
+    """Stub dataclass-based record sample used in tests."""
+
+    base_record_field_str: str = rt.class_field()
+    """String attribute of base class."""
+
+    base_record_field_float: float = rt.class_field()
+    """Float attribute of base class."""
+
+    base_record_field_long: int = rt.class_field(typename='long', label="Custom Label", optional=True)
+    """Optional long attribute of base class with custom label."""
+
+    @staticmethod
+    def create(context: rt.Context) -> StubClassRecord:
+        """Return an instance of this class populated with sample data."""
+
+        obj = StubClassRecord()
+        obj.context = context
+        obj.primary_key_field_str = 'abc'
+        obj.primary_key_field_int = 123
+        obj.base_record_field_str = 'def'
+        obj.base_record_field_float = 4.56
+        obj.init()
+        return obj
