66d811ed5643ff2ab87145f6bd4da21e4a376a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:37 +0000

    Add package nlptest/1.0.0rc1/nlptest-1.0.0rc1.tar.gz

---
+++ packages/nlptest/nlptest-1.0.0rc1.tar.gz/nlptest-1.0.0rc1/nlptest/utils/gender_classifier.py
@@ -0,0 +1,30 @@
+import logging
+import os
+
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
+
+
+class GenderClassifier(object):
+    """Helper model to predict the 'gender' of a piece of text."""
+    LABELS = {"LABEL_0": "female", "LABEL_1": "male", "LABEL_2": "unknown"}
+
+    def __init__(self) -> None:
+        logging.getLogger("transformers").setLevel(logging.ERROR)
+
+        tokenizer = AutoTokenizer.from_pretrained("microsoft/xtremedistil-l6-h256-uncased")
+        model = AutoModelForSequenceClassification.from_pretrained(
+            "microsoft/xtremedistil-l6-h256-uncased",
+            num_labels=3
+        )
+
+        curr_dir = os.path.dirname(__file__)
+        ckpt_path = os.path.join(curr_dir, 'checkpoints.ckpt')
+        ckpts = torch.load(ckpt_path)
+        model.load_state_dict(ckpts)
+        self.pipe = pipeline("text-classification", model=model, tokenizer=tokenizer)
+
+    def predict(self, text: str) -> str:
+        """"""
+        prediction = self.pipe(text, truncation=True, max_length=512)[0]["label"]
+        return self.LABELS[prediction]
