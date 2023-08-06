ae47e2c60a798cd5230b96d63afbb7560f2964e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:48 +0000

    Add package v/1.10/v-1.10.tar.gz

---
+++ packages/v/v-1.10.tar.gz/v-1.10/v/cmds.py
@@ -0,0 +1,26 @@
+class Console:
+	def __init__(self):
+		self._bg = "0"
+	def pt(self, *text, end="", join="", flush=True):
+		print(*text, end=end, sep=join, flush=flush)
+
+	def ptln(self, *text, join="", flush=True):
+		pt(*text, end="\n", join=join, flush=flush)
+
+	def write(self, *text, end="", join="", flush=True):
+		pt(*text, end=end, join=join, flush=flush)
+
+	def writeln(self, *text, join="", flush=True):
+		ptln(*text, join=join, flush=flush)
+
+	def read(self, prompt):
+		return input(prompt)
+	
+	def set_bg(self, value):
+		self._bg = value
+		print(f"\u001b[{self._bg}m", end="")
+	
+	def get_bg(self):
+		return self._bg
+
+console = Console()
