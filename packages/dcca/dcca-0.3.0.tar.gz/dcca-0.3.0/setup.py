d9552629a32d50332a49e815684c1acd8a371732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:04 +0000

    Add package dcca/0.3.0/dcca-0.3.0.tar.gz

---
+++ packages/dcca/dcca-0.3.0.tar.gz/dcca-0.3.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+# NOTE: necessary to import __version__
+
+from os import path
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md")) as f:
+    readme = f.read()
+setup(
+    name="dcca",
+    version="0.3.0",
+    description="Python implementation of the Time-Lagged Detrended Cross-Correlation Coefficient Analysis (DCCA)",
+    url="https://github.com/LeonardoAlchieri/dcca",
+    author="Leonardo Alchieri",
+    author_email="leonardo@alchieri.eu",
+    license="GPLv3",
+    packages=["dcca"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=["numpy"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
