d88b661933dc58690e4fa8b1ebe9290ca77a4b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:20 +0000

    Add package pyadcirc/0.0.1/pyadcirc-0.0.1.tar.gz

---
+++ packages/pyadcirc/pyadcirc-0.0.1.tar.gz/pyadcirc-0.0.1/src/pyadcirc/cli.py
@@ -0,0 +1,33 @@
+import six
+from pyfiglet import figlet_format
+
+
+try:
+    from termcolor import colored
+except ImportError:
+    colored = None
+
+import rich_click as click
+from rich_click.cli import patch
+
+patch()
+
+from pyadcirc.viz.figuregen import config
+
+
+@click.group()
+def cli():
+    if colored:
+        six.print_(colored(figlet_format("FIGUREGEN", font="slant"), "blue"))
+    else:
+        six.print_(figlet_format("FIGUREGEN", font="slant"))
+    pass
+
+
+@click.command()
+@click.argument("input_file", type=click.File("r"))
+def run(type: str, input_file: str):
+    pass
+
+
+cli.add_command(config)
