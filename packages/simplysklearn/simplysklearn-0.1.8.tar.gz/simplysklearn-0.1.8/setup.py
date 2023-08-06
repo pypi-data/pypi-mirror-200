adb396c049391e2fbc33597d48240aabb8a149a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:42 +0000

    Add package simplysklearn/0.1.8/simplysklearn-0.1.8.tar.gz

---
+++ packages/simplysklearn/simplysklearn-0.1.8.tar.gz/simplysklearn-0.1.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="simplysklearn", 
+    version="0.1.8",
+    author="Kim Hyun Bin",
+    author_email="KIMH0004@e.ntu.edu.sg",
+    description="Python package to automate machine learning process to showcase metric values for nearly all Scikit-Learn's models.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vanilladucky/simplysklearn",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 
+                      'pandas', 
+                      'scikit-learn',
+                      'seaborn',
+                      'matplotlib',
+                      'tqdm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
