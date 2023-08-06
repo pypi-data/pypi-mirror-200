9625438abb29be068bde920b49f525b19f107167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:19 +0000

    Add package monad-aa/0.0.12/monad_aa-0.0.12.tar.gz

---
+++ packages/monad-aa/monad_aa-0.0.12.tar.gz/monad_aa-0.0.12/src/aa/plugs/initiate_report_generation_plug_point.py
@@ -0,0 +1,13 @@
+# this plug point defines all the possible access patterns and write
+# patterns for the report status object
+# the plugin that implements this plug point can use this information
+# to define the appropriate storage and efficient ways to provide the
+# handle data with that storage
+
+
+class InitiateReportGenerationPlugPoint:
+    def __init__(self):
+        pass
+
+    def produce_msg_report_generate(self, report_id, account_number, date):
+        pass
