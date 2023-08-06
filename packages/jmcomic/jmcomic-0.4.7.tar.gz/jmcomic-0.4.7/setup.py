06f38b54c887453a04bddddc331829d4dc5785b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:03 +0000

    Add package jmcomic/0.4.7/jmcomic-0.4.7.tar.gz

---
+++ packages/jmcomic/jmcomic-0.4.7.tar.gz/jmcomic-0.4.7/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+version = '0.4.7'
+
+setup(
+    name='jmcomic',
+    version=version,
+
+    description='Python API For JMComic (禁漫天堂)',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+
+    url='https://github.com/hect0x7/JMComic-Crawler-Python',
+    author='hect0x7',
+    author_email='93357912+hect0x7@users.noreply.github.com',
+
+    packages=find_packages(),
+    install_requires=[
+        'commonX',
+        'curl_cffi',
+        'PyYAML',
+        'Pillow',
+        'setuptools',
+    ],
+    python_requires=">=3.7",
+
+    keywords=['python', 'jmcomic', '18comic', '禁漫天堂', 'NSFW'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
