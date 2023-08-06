e8bdb8fa2b7498ed71ba3941045b20c802dffac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:14 +0000

    Add package pipster/0.0.5/pipster-0.0.5.tar.gz

---
+++ packages/pipster/pipster-0.0.5.tar.gz/pipster-0.0.5/tests/data/imports.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup as bs  # noqa: F401  # install beautifulsoup4
+import libcst as cst  # noqa: F401
+from sklearn.datasets import load_iris  # install scikit-learn  # noqa: F401
+import requests as r  # noqa: F401
+import numpy, pandas  # noqa: E401,F401
