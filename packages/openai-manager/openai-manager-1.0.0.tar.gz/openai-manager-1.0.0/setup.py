053b3ce9fab586289333a63ab91fa6e80d3fd36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:00 +0000

    Add package openai-manager/1.0.0/openai-manager-1.0.0.tar.gz

---
+++ packages/openai-manager/openai-manager-1.0.0.tar.gz/openai-manager-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    _LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name="openai-manager",
+    license='MIT',
+    author="MrZilinXiao",
+    version="1.0.0",
+    author_email="me@mrxiao.net",
+    description="Speed up your OpenAI requests by balancing prompts to multiple API keys.",
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrZilinXiao/openai-manager",
+    packages=setuptools.find_namespace_packages(),
+    install_requires=["aiohttp", "openai", "tiktoken", "fastapi"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
