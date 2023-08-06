6a8b72d06bbd8f057efdaeb0d4a2752e753a6c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:26 +0000

    Add package einfach/0.0.2/einfach-0.0.2.tar.gz

---
+++ packages/einfach/einfach-0.0.2.tar.gz/einfach-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# here = os.path.abspath(os.path.dirname(__file__))
+
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'A collection of useful code "snippets"'
+LONG_DESCRIPTION = 'A collection of useful code "snippets" or "helpers" that implement little but annoying to add things for you'
+
+# Setting up
+setup(
+    name="einfach",
+    version=VERSION,
+    author="rotgruengelb (Daniel)",
+    author_email="<code@rotgruengelb.net>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'helper', 'helpers', 'help', 'easy code', 'ease of use'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        # "Operating System :: Unix",
+        # "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
