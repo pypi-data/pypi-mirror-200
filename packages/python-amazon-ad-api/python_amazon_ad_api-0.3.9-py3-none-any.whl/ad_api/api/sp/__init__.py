36bc72f9b6ff6757d86b090deb2ba53389e47c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:18 +0000

    Add package python-amazon-ad-api/0.3.9/python_amazon_ad_api-0.3.9-py3-none-any.whl

---
+++ packages/python-amazon-ad-api/python_amazon_ad_api-0.3.9-py3-none-any.whl/ad_api/api/sp/__init__.py
@@ -0,0 +1,39 @@
+from .campaigns import Campaigns
+from .ad_groups import AdGroups
+from .product_ads import ProductAds
+from .bid_recommendations import BidRecommendations
+from .keywords import Keywords
+from .negative_keywords import NegativeKeywords
+from .campaign_negative_keywords import CampaignNegativeKeywords
+from .suggested_keywords import SuggestedKeywords
+from .product_targeting import Targets
+from .negative_product_targeting import NegativeTargets
+from .reports import Reports
+from .snapshots import Snapshots
+from .budget_rules import BudgetRules
+from .campaings_optimization import CampaignOptimization
+from .ranked_keywords_recommendations import RankedKeywordsRecommendations
+from .budget_recommendations import BudgetRecommendations
+from .budget_rules_recommendations import BudgetRulesRecommendations
+from .product_recommendations import ProductRecommendations
+
+__all__ = [
+    "Campaigns",
+    "AdGroups",
+    "ProductAds",
+    "BidRecommendations",
+    "Keywords",
+    "NegativeKeywords",
+    "CampaignNegativeKeywords",
+    "SuggestedKeywords",
+    "Targets",
+    "NegativeTargets",
+    "Reports",
+    "Snapshots",
+    "BudgetRules",
+    "CampaignOptimization",
+    "RankedKeywordsRecommendations",
+    "BudgetRecommendations",
+    "BudgetRulesRecommendations",
+    "ProductRecommendations"
+]
