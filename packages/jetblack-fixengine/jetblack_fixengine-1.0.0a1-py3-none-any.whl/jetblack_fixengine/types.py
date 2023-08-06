9c894b130929b53510fa81fd1f4f93ee4113ac21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:24 +0000

    Add package jetblack-fixengine/1.0.0a1/jetblack_fixengine-1.0.0a1-py3-none-any.whl

---
+++ packages/jetblack-fixengine/jetblack_fixengine-1.0.0a1-py3-none-any.whl/jetblack_fixengine/types.py
@@ -0,0 +1,146 @@
+"""Types"""
+
+from abc import ABCMeta, abstractmethod
+from typing import Any, Mapping, Optional, Tuple
+
+from jetblack_fixparser.fix_message import FixMessageFactory
+
+
+class Session(metaclass=ABCMeta):
+
+    @property
+    @abstractmethod
+    def sender_comp_id(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def target_comp_id(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_seqnums(self) -> Tuple[int, int]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def set_seqnums(self, outgoing_seqnum: int, incoming_seqnum: int) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_outgoing_seqnum(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def set_outgoing_seqnum(self, seqnum: int) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_incoming_seqnum(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def set_incoming_seqnum(self, seqnum: int) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def save_message(self, buf: bytes) -> None:
+        raise NotImplementedError
+
+
+class Store(metaclass=ABCMeta):
+
+    def get_session(self, sender_comp_id: str, target_comp_id: str) -> Session:
+        raise NotImplementedError
+
+
+class InvalidStateTransitionError(Exception):
+    """An invalid state transition"""
+
+
+class LoginError(Exception):
+    """An invalid state transition"""
+
+
+class FIXApplication(metaclass=ABCMeta):
+    """Abstract base class for FIX applications"""
+
+    @property
+    @abstractmethod
+    def session(self) -> Session:
+        """The session
+
+        Returns:
+            Session: The session
+        """
+
+    @property
+    @abstractmethod
+    def fix_message_factory(self) -> FixMessageFactory:
+        """THe FIX message factory.
+
+        Returns:
+            FixMessageFactory: The factory
+        """
+
+    @property
+    @abstractmethod
+    def heartbeat_timeout(self) -> int:
+        """The heartbeat timeout"""
+
+    @property
+    @abstractmethod
+    def heartbeat_threshold(self) -> int:
+        """The heartbeat threshold"""
+
+    async def on_admin_message(self, message: Mapping[str, Any]) -> None:
+        """Called when an admin message is received.
+
+        Args:
+            message (Mapping[str, Any]): The admin message that was sent by the
+                acceptor.
+        """
+
+    async def on_heartbeat(self, message: Mapping[str, Any]) -> None:
+        """Called when a heartbeat is received.
+
+        Args:
+            message (Mapping[str, Any]): The message sent by the acceptor.
+        """
+
+    @abstractmethod
+    async def on_application_message(self, message: Mapping[str, Any]) -> None:
+        """Called when an application message is received.
+
+        Args:
+            message (Mapping[str, Any]): The application message sent by the
+                acceptor.
+        """
+
+    @abstractmethod
+    async def on_logon(self, message: Mapping[str, Any]) -> None:
+        """Called when a logon message is received.
+
+        Args:
+            message (Mapping[str, Any]): The message sent by the acceptor.
+        """
+
+    @abstractmethod
+    async def on_logout(self, message: Mapping[str, Any]) -> None:
+        """Called when a logout message is received.
+
+        Args:
+            message (Mapping[str, Any]): The message sent by the acceptor.
+        """
+
+    @abstractmethod
+    async def send_message(
+            self,
+            msg_type: str,
+            message: Optional[Mapping[str, Any]] = None
+    ) -> None:
+        """Send a FIX message
+
+        Args:
+            msg_type (str): The message type.
+            message (Optional[Mapping[str, Any]], optional): The message.
+                Defaults to None.
+        """
---
+++ packages/jetblack-fixengine/jetblack_fixengine-1.0.0a1-py3-none-any.whl/jetblack_fixengine/utils/date_utils.py
@@ -0,0 +1,82 @@
+"""Date Utils"""
+
+import asyncio
+from calendar import day_name
+from datetime import datetime, date, time, timedelta, tzinfo
+import logging
+from typing import Optional, Tuple
+
+logger = logging.getLogger(__name__)
+
+
+def is_dow_in_range(start_dow: int, end_dow: int, target_dow: int) -> bool:
+    if start_dow <= end_dow:
+        return start_dow <= target_dow and target_dow <= end_dow
+    else:
+        return start_dow <= target_dow or target_dow <= end_dow
+
+
+def is_time_in_range(start_time: time, end_time: time, target_time: time) -> bool:
+    if start_time <= end_time:
+        return start_time <= target_time and target_time <= end_time
+    else:
+        return start_time <= target_time or target_time <= end_time
+
+
+def make_datetime(d: date, t: time, tz: Optional[tzinfo]) -> datetime:
+    return datetime(d.year, d.month, d.day, t.hour, t.minute, t.second, t.microsecond, tzinfo=tz)
+
+
+def delay_for_time_period(now: datetime, start_time: time, end_time: time) -> Tuple[timedelta, datetime]:
+    if start_time < end_time:
+        start_datetime = make_datetime(now.date(), start_time, now.tzinfo)
+        end_datetime = make_datetime(now.date(), end_time, now.tzinfo)
+        if now > end_datetime:
+            start_datetime += timedelta(days=1)
+            end_datetime += timedelta(days=1)
+    else:
+        start_datetime = make_datetime(now.date(), start_time, now.tzinfo)
+        end_datetime = make_datetime(
+            now.date(), end_time, now.tzinfo) + timedelta(days=1)
+
+    time_to_wait = (start_datetime -
+                    now) if now < start_datetime else timedelta(seconds=0)
+    return time_to_wait, end_datetime
+
+
+async def wait_for_day_of_week(now: datetime, start_dow: int, end_dow: int, cancellation_event: asyncio.Event) -> None:
+    while not is_dow_in_range(start_dow, end_dow, now.weekday()):
+        logger.info(
+            f'Today is {now:%A} - waiting for {day_name[start_dow]} to connect.')
+        # Wait a till tomorrow then try again.
+        tomorrow = datetime(now.year, now.month, now.day,
+                            tzinfo=now.tzinfo) + timedelta(days=1)
+        time_to_wait = (tomorrow - now)
+
+        try:
+            await asyncio.wait_for(cancellation_event.wait(), time_to_wait.total_seconds())
+            raise asyncio.CancelledError
+        except asyncio.TimeoutError:
+            now += time_to_wait
+
+
+async def wait_for_time_period(
+        now: datetime,
+        start_time: time,
+        end_time: time,
+        cancellation_event: asyncio.Event
+) -> datetime:
+    # Wait for start time.
+    time_to_wait, end_datetime = delay_for_time_period(
+        now, start_time, end_time)
+    if time_to_wait.total_seconds() == 0:
+        logger.info('No need to wait')
+    else:
+        logger.info(f'Waiting for {time_to_wait}')
+        try:
+            await asyncio.wait_for(cancellation_event.wait(), timeout=time_to_wait.total_seconds())
+            raise asyncio.CancelledError
+        except asyncio.TimeoutError:
+            now = datetime.now()
+
+    return end_datetime
