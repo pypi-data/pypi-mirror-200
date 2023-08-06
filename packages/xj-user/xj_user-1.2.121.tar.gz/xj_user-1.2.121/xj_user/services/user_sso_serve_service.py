3ee4438f6dd089238826c3f52b0eb2ce4ca1d520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:01 +0000

    Add package xj-user/1.2.121/xj_user-1.2.121.tar.gz

---
+++ packages/xj-user/xj_user-1.2.121.tar.gz/xj_user-1.2.121/xj_user/services/user_sso_serve_service.py
@@ -0,0 +1,28 @@
+from django.db.models import F
+from rest_framework.response import Response
+
+from ..models import UserSsoServe, UserSsoToUser
+from xj_user.utils.custom_response import util_response
+
+
+class UserSsoServeService:
+    @staticmethod
+    def list():
+        user_sso_serve_list = UserSsoServe.objects.values("id", "sso_code", "sso_name", "sso_url", "description")
+        return user_sso_serve_list, None
+
+    @staticmethod
+    def add(data):
+        res_set = UserSsoServe.objects.filter(sso_code=data['sso_code']).first()
+        if res_set:
+            return None, "登录代码已存在"
+        user_sso_serve = UserSsoServe.objects.create(**data)
+        if user_sso_serve:
+            return {"contact_book_id": user_sso_serve.id}, None
+
+    @staticmethod
+    def user_sso_to_user(user_id, app_id):
+        res_set = UserSsoToUser.objects.filter(user_id=user_id, app_id=app_id).first()
+        if res_set:
+            return res_set, None
+        return None, "单点登录记录不存在"
