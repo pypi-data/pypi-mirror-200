c07e4f2a40155309e4064de258ee596ee021927e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:04 +0000

    Add package ir-digit/0.0.24/ir_digit-0.0.24-py3-none-any.whl

---
+++ packages/ir-digit/ir_digit-0.0.24-py3-none-any.whl/digit/__init__.py
@@ -0,0 +1,5 @@
+from .download_repo import download_repo
+from .get_dd import get_dd
+from .child_base_data import TableData, LabelData, DocData, ImageData, AudioData, VideoData, GraphData
+import interface
+import private_func
