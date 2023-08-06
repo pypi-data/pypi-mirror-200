1db8f32c99371f5c33d8e53f59fdaaf0eb8f2593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:17 +0000

    Add package LightDB/1.3.3/LightDB-1.3.3.tar.gz

---
+++ packages/LightDB/LightDB-1.3.3.tar.gz/LightDB-1.3.3/lightdb/__init__.py
@@ -0,0 +1,13 @@
+"""See LightDB docstring:
+
+from LightDB import LightDB
+---------------------
+
+:copyright: (c) 2021-2022 by Fl1yd.
+:license: MIT, see LICENSE for more details.
+"""
+
+__all__ = ["LightDB"]
+__version__ = "1.3.3"
+
+from .LightDB import LightDB
