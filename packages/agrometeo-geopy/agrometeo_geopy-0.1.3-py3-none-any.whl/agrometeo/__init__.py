7cc35d5f816f42f715f7fd681cf0522b45457fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:43 +0000

    Add package agrometeo-geopy/0.1.3/agrometeo_geopy-0.1.3-py3-none-any.whl

---
+++ packages/agrometeo-geopy/agrometeo_geopy-0.1.3-py3-none-any.whl/agrometeo/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for agrometeo-geopy."""
+
+__author__ = """Martí Bosch"""
+__email__ = "marti.bosch@protonmail.com"
+__version__ = "0.1.3"
+
+from .core import *
+from .plotting import *
