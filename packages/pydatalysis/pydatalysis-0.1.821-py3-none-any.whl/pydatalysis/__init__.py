096817b909605f4635d6bf5b7b17a669f6ab7246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:05 +0000

    Add package pydatalysis/0.1.821/pydatalysis-0.1.821-py3-none-any.whl

---
+++ packages/pydatalysis/pydatalysis-0.1.821-py3-none-any.whl/pydatalysis/__init__.py
@@ -0,0 +1,9 @@
+from .linear_regression import linear_regression
+from .logistic_regression import logistic_regression
+from .add import add
+
+__all__ = [
+    'linear_regression',
+    'logistic_regression',
+    'add'
+]
