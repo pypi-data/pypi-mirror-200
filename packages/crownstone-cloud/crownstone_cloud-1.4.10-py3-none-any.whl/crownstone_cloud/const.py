892899b75efc6492d1adb5c78db7f0ad9ce496dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:33 +0000

    Add package crownstone-cloud/1.4.10/crownstone_cloud-1.4.10-py3-none-any.whl

---
+++ packages/crownstone-cloud/crownstone_cloud-1.4.10-py3-none-any.whl/crownstone_cloud/const.py
@@ -0,0 +1,10 @@
+"""Constants for the Crownstone cloud library."""
+from typing import Final
+
+BASE_URL: Final = "https://cloud.crownstone.rocks/api/"
+LOGIN_URL: Final = "https://cloud.crownstone.rocks/api/users/login"
+
+# Crownstone abilities
+DIMMING_ABILITY: Final = "dimming"
+SWITCHCRAFT_ABILITY: Final = "switchcraft"
+TAP_TO_TOGGLE_ABILITY: Final = "tapToToggle"
