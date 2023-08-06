70c480efc4ebc939f41319168aa88470b82f7ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:46 +0000

    Add package pysura/0.99.4/pysura-0.99.4.tar.gz

---
+++ packages/pysura/pysura-0.99.4.tar.gz/pysura-0.99.4/pysura/cli/app.py
@@ -0,0 +1,24 @@
+from pysura.pysura_types.root_cmd import RootCmd
+from pysura.cli.google_root import GoogleRoot
+
+
+class App(RootCmd):
+
+    def __init__(self, *arg, **kwargs):
+        super().__init__(*arg, **kwargs)
+        self.intro = "Welcome to Pysura! Type help or ? to list commands."
+        self.prompt = "(pysura_cli) >>> "
+
+    def do_choose_provider(self, _):
+        """
+        Launches the chosen provider's CLI
+        """
+        provider = self.collect("Please choose a provider: [google,]: ", ["google"])
+        if provider == "google":
+            GoogleRoot(logger=self.root).cmdloop()
+        else:
+            print("Invalid provider")
+
+
+def cli():
+    App().cmdloop()
