db13040fc85642cf0127c17a9117f3fd8848da20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:16 +0000

    Add package pydatalysis/0.1.9/pydatalysis-0.1.9-py3-none-any.whl

---
+++ packages/pydatalysis/pydatalysis-0.1.9-py3-none-any.whl/pydatalysis/__init__.py
@@ -0,0 +1,13 @@
+from .linear_regression import linear_regression
+from .logistic_regression import logistic_regression
+from .bi_logistic_regression import bi_logistic_regression
+from .multi_logistic_regression import multi_logistic_regression
+from .add import add
+from .datasets import data
+
+__all__ = [
+    'linear_regression',
+    'logistic_regression',
+    'add',
+    'data'
+]
