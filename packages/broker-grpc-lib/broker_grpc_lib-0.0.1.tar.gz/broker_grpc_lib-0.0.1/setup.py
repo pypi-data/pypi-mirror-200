4870fac0bc7ef3e46bde52003fff04d62a6c7248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:40 +0000

    Add package broker-grpc-lib/0.0.1/broker_grpc_lib-0.0.1.tar.gz

---
+++ packages/broker-grpc-lib/broker_grpc_lib-0.0.1.tar.gz/broker_grpc_lib-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="broker_grpc_lib",
+    version="0.0.1",
+    description="Python package for easier use message brokers with gRPC application",
+    author="multiadmin_optimus_prime",
+    author_email="evgenykond@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic>=1.10.7",
+        "aio-pika>=9.0.5"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Developers",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
