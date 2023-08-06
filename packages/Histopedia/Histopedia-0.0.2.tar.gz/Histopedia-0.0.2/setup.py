056e3889f48eaa82b75e8aee1ef1f61cca373efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:57 +0000

    Add package Histopedia/0.0.2/Histopedia-0.0.2.tar.gz

---
+++ packages/Histopedia/Histopedia-0.0.2.tar.gz/Histopedia-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='Histopedia',
+    version='0.0.2',
+    author='Siddhu Pendyala',
+    author_email='elcientifico.pendyala@hotmail.com',
+    description='python package for scientific historical information',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PyndyalaCoder/Histopedia',
+    project_urls = {
+        "Bug Tracker": "https://github.com/PyndyalaCoder/Histopedia/issues"
+    },
+    license='MIT',
+    packages=['Histopedia'],
+    install_requires=['requests'],
+)
