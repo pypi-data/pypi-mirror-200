add01c09ef204d2933bb00b71af7b875919ef775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:40 +0000

    Add package web3-etl-schemas/0.1.2/web3_etl_schemas-0.1.2.tar.gz

---
+++ packages/web3-etl-schemas/web3_etl_schemas-0.1.2.tar.gz/web3_etl_schemas-0.1.2/web3_etl_schemas/models/ethereum.py
@@ -0,0 +1,27 @@
+from pydantic.fields import Field
+from pydantic.main import BaseModel
+
+from web3_etl_schemas.models.common import HexToInt
+
+
+class Block(BaseModel):
+    """Ethereum Block Model."""
+
+    difficulty: HexToInt
+    extra_data: str = Field(alias="extraData")
+    gas_limit: HexToInt = Field(alias="gasLimit")
+    gas_used: HexToInt = Field(alias="gasUsed")
+    hash: str
+    logs_bloom: str = Field(alias="logsBloom")
+    miner: str
+    mix_hash: str = Field(alias="mixHash")
+    nonce: str
+    number: HexToInt
+    parent_hash: str = Field(alias="parentHash")
+    receipts_root: str = Field(alias="receiptsRoot")
+    sha3_uncles: str = Field(alias="sha3Uncles")
+    size: HexToInt
+    state_root: str = Field(alias="stateRoot")
+    timestamp: HexToInt
+    total_difficulty: HexToInt = Field(alias="totalDifficulty")
+    transactions_root: str = Field(alias="transactionsRoot")
