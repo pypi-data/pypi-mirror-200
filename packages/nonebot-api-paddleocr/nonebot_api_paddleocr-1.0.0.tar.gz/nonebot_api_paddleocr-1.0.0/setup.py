db0d60b6393e4a10a19204da3320939ac749c25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:28 +0000

    Add package nonebot-api-paddleocr/1.0.0/nonebot_api_paddleocr-1.0.0.tar.gz

---
+++ packages/nonebot-api-paddleocr/nonebot_api_paddleocr-1.0.0.tar.gz/nonebot_api_paddleocr-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+setup(name='nonebot_api_paddleocr',  # 包名
+      version='1.0.0',  # 版本号
+      description='nonebot_api_paddleocr',
+      long_description=long_description,
+      author='canxin',
+      author_email='1969730106@qq.com',
+      url='https://github.com/canxin121/nonebot_api_paddleocr',
+      install_requires=[],
+      license='Apache License 2.0',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.10',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
