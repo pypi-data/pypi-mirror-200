3cacf1bc3adb9e88ab77d3bf6ff23335dc3482c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:16 +0000

    Add package web-poet/0.9.0/web-poet-0.9.0.tar.gz

---
+++ packages/web-poet/web-poet-0.9.0.tar.gz/web-poet-0.9.0/web_poet/__init__.py
@@ -0,0 +1,27 @@
+from .fields import field, item_from_fields, item_from_fields_sync
+from .page_inputs import (
+    BrowserHtml,
+    HttpClient,
+    HttpRequest,
+    HttpRequestBody,
+    HttpRequestHeaders,
+    HttpResponse,
+    HttpResponseBody,
+    HttpResponseHeaders,
+    PageParams,
+    RequestUrl,
+    ResponseUrl,
+)
+from .pages import Injectable, ItemPage, ItemWebPage, Returns, WebPage, validates_input
+from .requests import request_downloader_var
+from .rules import (
+    ApplyRule,
+    OverrideRule,
+    PageObjectRegistry,
+    RulesRegistry,
+    consume_modules,
+)
+from .utils import cached_method
+
+default_registry = RulesRegistry()
+handle_urls = default_registry.handle_urls
