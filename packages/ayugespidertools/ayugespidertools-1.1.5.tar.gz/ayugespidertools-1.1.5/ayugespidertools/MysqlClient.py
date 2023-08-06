2de64cd4bced2d081f76f75a448422678b4e81a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:13 +0000

    Add package ayugespidertools/1.1.5/ayugespidertools-1.1.5.tar.gz

---
+++ packages/ayugespidertools/ayugespidertools-1.1.5.tar.gz/ayugespidertools-1.1.5/ayugespidertools/MysqlClient.py
@@ -0,0 +1,42 @@
+from typing import Literal, Optional
+
+import pymysql
+
+__all__ = [
+    "MysqlOrm",
+]
+
+SearchTypeStr = Literal["all", "one"]
+
+
+class MysqlOrm(object):
+    """数据库的简单使用，结合 SqlFormat 方法使用（临时使用）"""
+
+    def __init__(self, pymsql_connect_config: dict):
+        self.connection = pymysql.connect(**pymsql_connect_config)
+        self.cursor = self.connection.cursor()
+
+    def insert_data(self, sql_pre: str, sql_after: tuple):
+        self.cursor.execute(sql_pre, sql_after)
+        self.connection.commit()
+
+    def search_data(
+        self, sql_pre: str, sql_after: tuple, type: SearchTypeStr = "one"
+    ) -> tuple:
+        self.cursor.execute(sql_pre, sql_after)
+        if type == "all":
+            return self.cursor.fetchall()
+        elif type == "one":
+            return self.cursor.fetchone()
+
+    def update_data(self, sql_pre: str, sql_after: tuple):
+        self.cursor.execute(sql_pre, sql_after)
+        self.connection.commit()
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
