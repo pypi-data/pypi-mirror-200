57c35bff270d3afda5bc3c1d673c8309ea4d9de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:00 +0000

    Add package gdshoplib/1.18.12/gdshoplib-1.18.12-py3-none-any.whl

---
+++ packages/gdshoplib/gdshoplib-1.18.12-py3-none-any.whl/gdshoplib/__init__.py
@@ -0,0 +1,5 @@
+from gdshoplib.apps.platforms.base import Platform
+from gdshoplib.apps.product import Product
+from gdshoplib.cli.application import app as application
+
+__all__ = ("Product", "Platform", "application")
