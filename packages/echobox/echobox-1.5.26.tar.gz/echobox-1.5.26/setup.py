d63b33761887a53ed9316937591f49b7b458aad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:06 +0000

    Add package echobox/1.5.26/echobox-1.5.26.tar.gz

---
+++ packages/echobox/echobox-1.5.26.tar.gz/echobox-1.5.26/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='echobox',
+    version='1.5.26',
+    keywords=('echobox'),
+    description='echobox',
+    license='',
+    install_requires=[
+        'six',
+        'ruamel.yaml==0.16.13',
+        'ruamel.yaml.clib==0.2.2',
+        'Jinja2',
+        'netaddr',
+        'requests',
+        'tzlocal==2.1',
+        'redis',
+        'configparser==3.7.4',
+    ],
+
+    scripts=[],
+
+    author='kiuber',
+    author_email='kiuber.zhang@gmail.com',
+    url='',
+
+    packages=find_packages(),
+    platforms='any',
+)
