f9bf198c560b61997f96391783dfb254ca9662bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:51 +0000

    Add package microauth/1.0.0/microauth-1.0.0.tar.gz

---
+++ packages/microauth/microauth-1.0.0.tar.gz/microauth-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = "oauth"
+LONG_DESCRIPTION = "oauth"
+
+# Setting up
+setup(
+    name="microauth",
+    version=VERSION,
+    author="ЕntchenBuilds",
+    author_email="ЕntchenBuilds@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
