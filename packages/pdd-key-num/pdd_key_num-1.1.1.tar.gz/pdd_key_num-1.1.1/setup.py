914bb640a8755e0f27873c38c71047df5c1b599b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:20 +0000

    Add package pdd-key-num/1.1.1/pdd_key_num-1.1.1.tar.gz

---
+++ packages/pdd-key-num/pdd_key_num-1.1.1.tar.gz/pdd_key_num-1.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='pdd_key_num',
+    version='1.1.1',
+    author='sanye',
+    author_email='201419490@qq.com',
+    description='你的第三方库的简介',
+    long_description_content_type="""https://pypi.org/manage/projects/""",
+    url='https://pypi.org/manage/projects/',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
