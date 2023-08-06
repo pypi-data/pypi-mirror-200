c3b333706b94cc8de6f21c858864178e4aabb2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:49 +0000

    Add package amplpy/0.9.1/amplpy-0.9.1-cp36-cp36m-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/amplpy/amplpy-0.9.1-cp36-cp36m-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/amplpy/vendor/ampltools/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.5.1"
+from .notebooks import (
+    ampl_notebook,
+)
+
+from .modules import (
+    activate as activate_license,
+)
+
+from .utils import (
+    cloud_platform_name,
+    register_magics,
+    add_to_path,
+)
