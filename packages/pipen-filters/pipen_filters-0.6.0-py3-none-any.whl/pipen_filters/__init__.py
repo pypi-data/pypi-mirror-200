9e44f8b7d96075a94ec35da5974cd7de13036997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:06 +0000

    Add package pipen-filters/0.6.0/pipen_filters-0.6.0-py3-none-any.whl

---
+++ packages/pipen-filters/pipen_filters-0.6.0-py3-none-any.whl/pipen_filters/__init__.py
@@ -0,0 +1,36 @@
+"""Add a set of useful filters for pipen templates"""
+
+from typing import TYPE_CHECKING
+from pipen import plugin
+
+from .filters import FILTERS
+
+if TYPE_CHECKING:  # pragma: no cover
+    from pipen import Pipen
+
+__version__ = "0.6.0"
+
+
+class PipenFilters:
+    __version__: str = __version__
+
+    @plugin.impl
+    async def on_init(pipen: "Pipen") -> None:  # type: ignore
+        """Add the filters"""
+        config = pipen.config
+        if "template_opts" not in config:  # pragma: no cover
+            config.template_opts = {}
+
+        if "filters" not in config.template_opts:
+            config.template_opts.filters = {}
+        if "globals" not in config.template_opts:
+            config.template_opts.globals = {}
+
+        config.template_opts.filters = {
+            **FILTERS,
+            **config.template_opts.filters,
+        }
+        config.template_opts.globals = {
+            **FILTERS,
+            **config.template_opts.globals,
+        }
