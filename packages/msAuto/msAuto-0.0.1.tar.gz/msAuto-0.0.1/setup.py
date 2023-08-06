cb923ae71abd472a5dacf84f1a170a9b186f569d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:31 +0000

    Add package msAuto/0.0.1/msAuto-0.0.1.tar.gz

---
+++ packages/msAuto/msAuto-0.0.1.tar.gz/msAuto-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="msAuto",
+    version="0.0.1",
+    author="Sellami Mehdi",
+    author_email="sellamimehdi87@gmail.com",
+    url="https://github.com/MehdiSellamiAdministrator",
+    description="A Projet Python package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent", 
+    ]
+)
