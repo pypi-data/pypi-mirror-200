ff25e39fb7dfedee7458db55b550ec38e8df880c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:46 +0000

    Add package usts/0.1.5/usts-0.1.5-py3-none-any.whl

---
+++ packages/usts/usts-0.1.5-py3-none-any.whl/usts/mkmod.py
@@ -0,0 +1,9 @@
+
+from .modules.modstr import ModuleStr
+from .modules.modcpu import ModuleCPU
+from .modules.modram import ModuleRAM
+from .modules.moddisk import ModuleDisk
+from .modules.modtemp import ModuleTemp
+from .modules.modtime import ModuleTime
+from .modules.modweather import ModuleWeather
+
