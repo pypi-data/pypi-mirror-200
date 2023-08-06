34d96c5389c1185ef03d80e4db26c410aeef27c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:59 +0000

    Add package Codetest-laguilhon/0.0.9/Codetest_laguilhon-0.0.9.tar.gz

---
+++ packages/Codetest-laguilhon/Codetest_laguilhon-0.0.9.tar.gz/Codetest_laguilhon-0.0.9/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Codetest_laguilhon",
+    version="0.0.9",
+    author="laguilhon",
+    author_email="lea.aguilhon@gmail.com",
+    description="Test de package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    package_data={
+    "data": ["IMG/*", "f_annexe/*"]
+    },
+    include_package_data=True,
+    install_requires=[
+        "tkinter",
+        "Pillow==8.3.2",
+        "webbrowser",
+        "fpdf==1.7.2",
+        "numpy==1.21.0",
+        "matplotlib==3.4.2",
+        "pandas==1.3.1",
+        "scikit-image==0.18.2",
+        "opencv-python-headless==4.5.3.56",
+        "tensorflow==2.6.0",
+        "keras==2.6.0",
+        "skimage",
+        "cv2",
+    ],
+    python_requires=">=3.7",
+)
