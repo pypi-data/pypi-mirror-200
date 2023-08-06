e2141385fcfcaa52397bdf301621ebf2200ff2b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:37 +0000

    Add package napari-ndtiffs/0.2.1/napari_ndtiffs-0.2.1.tar.gz

---
+++ packages/napari-ndtiffs/napari_ndtiffs-0.2.1.tar.gz/napari_ndtiffs-0.2.1/src/napari_ndtiffs/affine.py
@@ -0,0 +1,71 @@
+"""Deskew utilities"""
+import logging
+from functools import lru_cache
+from time import time
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+@lru_cache(maxsize=None)
+def _get_backend():
+    try:
+        from ._cupy_affine import affine_transform
+
+        backend_name = "CuPy"
+    except ImportError:
+        logger.warning(
+            "Could not import CuPy. " "Falling back to PyOpenCL for affine transforms"
+        )
+        try:
+            from ._ocl_affine import affine_transform
+
+            backend_name = "PyOpenCL"
+        except ImportError:
+            from scipy.ndimage.interpolation import affine_transform
+
+            backend_name = "SciPy"
+            logger.warning(
+                "Could not import CuPy or PyOpenCL. "
+                "Falling back to SciPy for CPU affine transforms"
+            )
+    return affine_transform, backend_name
+
+
+def deskew_block(block, mat=None, out_shape=None, padval=0):
+    affine_transform, backend_name = _get_backend()
+    extradims = block.ndim - 3
+    last3dims = (0,) * extradims + (slice(None),) * 3
+    array = block[last3dims]
+
+    t_start = time()
+    deskewed = affine_transform(array, mat, output_shape=tuple(out_shape[-3:]), order=0)
+    logger.debug(f"\tduration ({backend_name}): {time() - t_start} s")
+    return deskewed[(None,) * extradims + (...,)]
+
+
+deskew_counter = 0
+
+
+def get_deskew_func(shape, dz=0.5, dx=0.1, angle=31.5, padval=0):
+    # calculate affine matrix from globals
+    deskewFactor = np.cos(np.deg2rad(angle)) * dz / dx
+    mat = np.eye(4)
+    mat[2, 0] = -deskewFactor
+
+    # calculate shape of output array
+    (nz, ny, nx) = shape[-3:]
+    out_shape = [1] * (len(shape) - 3) + list(shape[-3:])
+    # new nx
+    out_shape[-1] = int(np.floor((nz - 1) * -mat[2, 0]) + nx)
+    new_dzdx_ratio = np.sin(np.deg2rad(angle)) * dz / dx
+
+    def noisy_deskew(arr):
+        # to see, set:  logging.getLogger("napari_ndtiffs").setLevel(logging.DEBUG)
+        global deskew_counter
+        deskew_counter += 1
+        logger.debug(f"deskew #{deskew_counter}")
+        return deskew_block(arr, mat=mat, out_shape=out_shape, padval=padval)
+
+    return noisy_deskew, out_shape, new_dzdx_ratio
---
+++ packages/napari-ndtiffs/napari_ndtiffs-0.2.1.tar.gz/napari_ndtiffs-0.2.1/tests/test_affine.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pyopencl as cl
+import pytest
+from napari_ndtiffs import _ocl_affine as affine
+from napari_ndtiffs import napari_get_reader
+from scipy.ndimage import affine_transform
+
+
+def test_get_reader_pass():
+    reader = napari_get_reader("fake.file")
+    assert reader is None
+
+
+devices = [d for p in cl.get_platforms() for d in p.get_devices()]
+names = [d.name[:30] for d in devices]
+
+
+@pytest.fixture(params=devices, ids=names)
+def all_gpus(monkeypatch, request):
+    def patched_func():
+        class holder:
+            pass
+
+        GPU = holder()
+
+        GPU.device = request.param
+        GPU.ctx = cl.Context(devices=[GPU.device])
+        GPU.queue = cl.CommandQueue(GPU.ctx)
+        return GPU
+
+    monkeypatch.setattr(affine, "get_gpu", patched_func)
+
+
+def test_affine_nearest(all_gpus):
+    assert affine.cl
+
+    arr = np.ones((64, 256, 256))
+    tmat = np.eye(4)
+    tmat[2, 0] = -2
+    scipy_ = affine_transform(arr, tmat, order=0, output_shape=(64, 256, 396))
+    ours = affine.affine_transform(arr, tmat, order=0, output_shape=(64, 256, 396))
+    assert np.allclose(scipy_, ours)
+
+
+def test_affine_linear(all_gpus):
+    assert affine.cl
+
+    arr = np.ones((64, 256, 256))
+    tmat = np.eye(4)
+    tmat[2, 0] = -2
+    scipy_ = affine_transform(arr, tmat, order=1, output_shape=(81, 256, 396))
+    ours = affine.affine_transform(arr, tmat, order=1, output_shape=(81, 256, 396))
+    assert np.allclose(scipy_, ours)
