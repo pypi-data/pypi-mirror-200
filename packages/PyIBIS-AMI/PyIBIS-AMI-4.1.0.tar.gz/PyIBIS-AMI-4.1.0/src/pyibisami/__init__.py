effe3c14edc64c3e77a581ed6cdfc90dc9533ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:08 +0000

    Add package PyIBIS-AMI/4.1.0/PyIBIS-AMI-4.1.0.tar.gz

---
+++ packages/PyIBIS-AMI/PyIBIS-AMI-4.1.0.tar.gz/PyIBIS-AMI-4.1.0/src/pyibisami/__init__.py
@@ -0,0 +1,14 @@
+"""A package of Python modules, used to configure and test IBIS-AMI models.
+
+.. moduleauthor:: David Banas <capn.freako@gmail.com>
+
+Original Author: David Banas <capn.freako@gmail.com>
+
+Original Date:   3 July 2012
+
+Copyright (c) 2012 by David Banas; All rights reserved World wide.
+"""
+__version__ = "4.1.0"
+__date__ = "March 15, 2023"
+__authors__ = "David Banas & David Patterson"
+__copy__ = "Copyright (c) 2012 David Banas, 2019 David Patterson"
