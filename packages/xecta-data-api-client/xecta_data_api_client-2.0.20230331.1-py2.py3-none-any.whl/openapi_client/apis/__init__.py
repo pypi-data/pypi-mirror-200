b79d73b4a6275f197a505508e0d3cb384db0bc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:18 +0000

    Add package xecta-data-api-client/2.0.20230331.1/xecta_data_api_client-2.0.20230331.1-py2.py3-none-any.whl

---
+++ packages/xecta-data-api-client/xecta_data_api_client-2.0.20230331.1-py2.py3-none-any.whl/openapi_client/apis/__init__.py
@@ -0,0 +1,28 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.casing_api import CasingApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from openapi_client.api.casing_api import CasingApi
+from openapi_client.api.daily_production_api import DailyProductionApi
+from openapi_client.api.deviation_survey_api import DeviationSurveyApi
+from openapi_client.api.esp_api import ESPApi
+from openapi_client.api.formation_property_api import FormationPropertyApi
+from openapi_client.api.gas_lift_valve_api import GasLiftValveApi
+from openapi_client.api.sucker_rod_pump_api import SuckerRodPumpApi
+from openapi_client.api.time_series_configuration_api import TimeSeriesConfigurationApi
+from openapi_client.api.time_series_history_api import TimeSeriesHistoryApi
+from openapi_client.api.tubing_api import TubingApi
+from openapi_client.api.well_api import WellApi
+from openapi_client.api.wellbore_api import WellboreApi
