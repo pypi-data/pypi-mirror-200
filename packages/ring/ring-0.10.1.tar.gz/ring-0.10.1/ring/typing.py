61ddbb74d372c76136d6c366d46f31915777622a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:24 +0000

    Add package ring/0.10.1/ring-0.10.1.tar.gz

---
+++ packages/ring/ring-0.10.1.tar.gz/ring-0.10.1/ring/typing.py
@@ -0,0 +1,12 @@
+try:
+    from typing import Any, Optional, Tuple, List
+except ImportError:
+
+    class _Generic(object):
+        def __getitem__(self, key):
+            return _Generic()
+
+    Optional = _Generic()
+    Tuple = _Generic()
+    List = _Generic()
+    Any = None
