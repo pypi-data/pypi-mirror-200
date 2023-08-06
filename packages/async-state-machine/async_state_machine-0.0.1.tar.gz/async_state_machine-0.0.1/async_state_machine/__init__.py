1abaa961bff96aaf8af8acfb6ef476f3ff8cad08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:04 +0000

    Add package async-state-machine/0.0.1/async_state_machine-0.0.1.tar.gz

---
+++ packages/async-state-machine/async_state_machine-0.0.1.tar.gz/async_state_machine-0.0.1/async_state_machine/__init__.py
@@ -0,0 +1,17 @@
+"""Диаграмма состояний."""
+
+from enum import auto as enum_auto
+
+from .exceptions import NewStateException, StateMachineError
+from .state import State
+from .state_machine import StateMachine
+from .states_enum import StatesEnum
+
+__all__ = [
+    "NewStateException",
+    "State",
+    "StateMachine",
+    "StatesEnum",
+    "StateMachineError",
+    "enum_auto",
+]
