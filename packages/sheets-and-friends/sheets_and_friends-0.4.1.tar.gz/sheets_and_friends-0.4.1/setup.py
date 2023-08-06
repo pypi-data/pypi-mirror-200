49b8d32145188b84ba06c2ca37f1a9999a90a4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:51 +0000

    Add package sheets-and-friends/0.4.1/sheets_and_friends-0.4.1.tar.gz

---
+++ packages/sheets-and-friends/sheets_and_friends-0.4.1.tar.gz/sheets_and_friends-0.4.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sheets_and_friends', 'sheets_and_friends.converters']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click_log',
+ 'glom>=23.1.1,<24.0.0',
+ 'linkml-runtime==1.4.5',
+ 'pandas>=1.3.4,<2.0.0',
+ 'pandasql>=0.7.3,<0.8.0',
+ 'pygsheets>=2.0.5,<3.0.0',
+ 'schemasheets>=0.1.21,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['do_shuttle = sheets_and_friends.shuttle:do_shuttle',
+                     'modifications_and_validation = '
+                     'sheets_and_friends.modifications_and_validation:modifications_and_validation']}
+
+setup_kwargs = {
+    'name': 'sheets-and-friends',
+    'version': '0.4.1',
+    'description': 'Create a LinkML model with as-is imported slots, imported but modified slots (via yq), or newly minted slots (via schemasheets)',
+    'long_description': 'None',
+    'author': 'Mark Andrew Miller',
+    'author_email': 'MAM@lbl.gov',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
