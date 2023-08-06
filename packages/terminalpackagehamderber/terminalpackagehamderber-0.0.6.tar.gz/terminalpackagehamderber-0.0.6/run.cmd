ba994e8568d69596b147c4978c1fd3a72a2a8135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:07 +0000

    Add package terminalpackagehamderber/0.0.6/terminalpackagehamderber-0.0.6.tar.gz

---
+++ packages/terminalpackagehamderber/terminalpackagehamderber-0.0.6.tar.gz/terminalpackagehamderber-0.0.6/run.cmd
@@ -0,0 +1,1 @@
+python %~dp0terminalpackagehamderber\\src\\main.py
---
+++ packages/terminalpackagehamderber/terminalpackagehamderber-0.0.6.tar.gz/terminalpackagehamderber-0.0.6/terminalpackagehamderber/src/terminal_settings.py
@@ -0,0 +1,7 @@
+launch_announcement = "Running KevTerminal!\n"
+settings_dict = {'example_setting': True}
+
+
+def apply_settings():
+    # example_script.settings_veriable = settings_dict['example_setting']
+    print(f"{launch_announcement}")
