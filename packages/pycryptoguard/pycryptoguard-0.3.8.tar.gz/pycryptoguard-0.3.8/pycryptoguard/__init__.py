b7de02cb1a9770030204b54be5d7cfffd9a25b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:40 +0000

    Add package pycryptoguard/0.3.8/pycryptoguard-0.3.8.tar.gz

---
+++ packages/pycryptoguard/pycryptoguard-0.3.8.tar.gz/pycryptoguard-0.3.8/pycryptoguard/__init__.py
@@ -0,0 +1,3 @@
+from .encrypt_decrypt import encrypt_file, decrypt_file, encrypt_folder, decrypt_folder
+
+__all__ = ['encrypt_file', 'decrypt_file', 'encrypt_folder', 'decrypt_folder']
---
+++ packages/pycryptoguard/pycryptoguard-0.3.8.tar.gz/pycryptoguard-0.3.8/pycryptoguard/encrypt_decrypt.py
@@ -0,0 +1,100 @@
+import os
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto import Random
+
+
+def encrypt(key, filename):
+    chunksize = 64 * 1024
+    outputFile = "ec_" + filename
+    filesize = str(os.path.getsize(filename)).zfill(16)
+    IV = Random.new().read(16)
+
+    encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+    with open(filename, 'rb') as infile:
+        with open(outputFile, 'wb') as outfile:
+            outfile.write(filesize.encode('utf-8'))
+            outfile.write(IV)
+
+            while True:
+                chunk = infile.read(chunksize)
+
+                if len(chunk) == 0:
+                    break
+
+                elif len(chunk) % 16 != 0:
+                    chunk += b' ' * (16 - (len(chunk) % 16))
+
+                outfile.write(encryptor.encrypt(chunk))
+
+
+def decrypt(key, filename):
+    chunksize = 64 * 1024
+    outputFile = filename[3:]
+
+    with open(filename, 'rb') as infile:
+        filesize = int(infile.read(16))
+        IV = infile.read(16)
+
+        decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+        with open(outputFile, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+
+                if len(chunk) == 0:
+                    break
+
+                outfile.write(decryptor.decrypt(chunk))
+
+            outfile.truncate(filesize)
+
+
+def get_key(password):
+    hasher = SHA256.new(password.encode('utf-8'))
+    return hasher.digest()
+
+
+def generate_key():
+    return Random.new().read(32)
+
+
+def encrypt_file(key, filepath):
+    from cryptography.fernet import Fernet
+
+    with open(filepath, 'rb') as infile:
+        data = infile.read()
+
+    fernet = Fernet(key)
+    encrypted = fernet.encrypt(data)
+
+    output_folder = "ec_" + os.path.dirname(filepath)
+    os.makedirs(output_folder, exist_ok=True)
+    output_file = os.path.join(output_folder, os.path.basename(filepath))
+
+    with open(output_file, 'wb') as outfile:
+        outfile.write(encrypted)
+
+
+def encrypt_folder(folder_path):
+    from cryptography.fernet import Fernet
+
+    key = Fernet.generate_key()
+
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            encrypt_file(key, filepath)
+
+    return key
+
+
+
+def decrypt_folder(key, foldername):
+    for root, dirs, files in os.walk(foldername):
+        for file in files:
+            filepath = os.path.join(root, file)
+            decrypt(key, filepath)
+            os.remove(filepath)
+            os.rename(os.path.join(root, "dc_" + file), os.path.join(root, file))
