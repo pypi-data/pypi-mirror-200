f6d45035758210322b1cdc0e6163e218fc3c8473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:00 +0000

    Add package paigram-logger/0.0.2/paigram_logger-0.0.2.tar.gz

---
+++ packages/paigram-logger/paigram_logger-0.0.2.tar.gz/paigram_logger-0.0.2/src/pg_logger/__init__.py
@@ -0,0 +1,6 @@
+from pg_logger._config import LoggerConfig
+from pg_logger._handler import *
+from pg_logger._logger import (
+    LogFilter,
+    Logger,
+)
---
+++ packages/paigram-logger/paigram_logger-0.0.2.tar.gz/paigram_logger-0.0.2/src/pg_logger/_typedefs.py
@@ -0,0 +1,5 @@
+from typing import Literal
+
+__all__ = ("COLOR_SYSTEM",)
+
+COLOR_SYSTEM = Literal["auto", "standard", "256", "truecolor", "windows"]
