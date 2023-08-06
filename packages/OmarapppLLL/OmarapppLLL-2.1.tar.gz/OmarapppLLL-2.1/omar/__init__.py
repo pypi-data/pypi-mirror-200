e8af296190760ed50f9b6312f183ea82a7c0516a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:16 +0000

    Add package OmarapppLLL/2.1/OmarapppLLL-2.1.tar.gz

---
+++ packages/OmarapppLLL/OmarapppLLL-2.1.tar.gz/OmarapppLLL-2.1/omar/__init__.py
@@ -0,0 +1,2 @@
+def o ():
+    print("OMar"*100)
---
+++ packages/OmarapppLLL/OmarapppLLL-2.1.tar.gz/OmarapppLLL-2.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="OmarapppLLL",
+    version="2.1",
+    author="omar Style",
+    description="omar aAAA",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3 ",
+    "Operating System :: OS Independent",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License"
+    ]
+    
+)
