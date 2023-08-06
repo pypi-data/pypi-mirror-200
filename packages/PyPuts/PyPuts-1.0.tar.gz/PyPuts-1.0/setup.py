3215315821f38df6fa8f2d6f93c1e2d28276ebbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:59 +0000

    Add package PyPuts/1.0/PyPuts-1.0.tar.gz

---
+++ packages/PyPuts/PyPuts-1.0.tar.gz/PyPuts-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyPuts',
+    version='1.0',
+    description='PyPuts: A Python Library for Colorful and Readable Console Output',
+    author='INeddHelp',
+    author_email='ineddhelpgithub@gmail.com',
+    url='https://github.com/INeddHelp/PyPuts',
+    packages=['pyputs']
+)
