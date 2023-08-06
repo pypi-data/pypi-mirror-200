6511bffb659a8643561900acf671b649281e5049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:05 +0000

    Add package motiondev/0.1.0/motiondev-0.1.0-py3-none-any.whl

---
+++ packages/motiondev/motiondev-0.1.0-py3-none-any.whl/motiondev/__init__.py
@@ -0,0 +1,13 @@
+"""
+Motion Development Bot List API Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Motion Development bot list API.
+:copyright: (c) 2023-present ItsWilliboy
+:license: GNU AGPL 3.0, see LICENSE for more details.
+
+"""
+from .client import *
+from .errors import *
+
+__all__ = ["client", "errors"]
