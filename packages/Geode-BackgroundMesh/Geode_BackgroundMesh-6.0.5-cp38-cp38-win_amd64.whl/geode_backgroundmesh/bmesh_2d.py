a87b1c4de74023f45102a385b08e59b6a41054c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:08 +0000

    Add package Geode-BackgroundMesh/6.0.5/Geode_BackgroundMesh-6.0.5-cp38-cp38-win_amd64.whl

---
+++ packages/Geode-BackgroundMesh/Geode_BackgroundMesh-6.0.5-cp38-cp38-win_amd64.whl/geode_backgroundmesh/bmesh_2d.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .geode_backgroundmesh_py_bmesh_2d import *
+BackgroundMesh2d.initialize()
