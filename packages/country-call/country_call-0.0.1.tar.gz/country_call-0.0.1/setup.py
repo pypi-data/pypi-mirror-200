bfff6ce7c2756bb95450808f20c11b3620d76122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:48 +0000

    Add package country-call/0.0.1/country_call-0.0.1.tar.gz

---
+++ packages/country-call/country_call-0.0.1.tar.gz/country_call-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+
+setup(name="country_call",
+version="0.0.1",
+description="Introducing a powerful and easy-to-use Python library for country detection from ambiguous text, designed for developers and data scientists alike.",
+author="Md Shamim Hasan, Kazi Arman Rahat, Mukitul Islam, Anik Roy",
+author_email="shihab18015@gmail.com",
+install_requires=[]
+)
