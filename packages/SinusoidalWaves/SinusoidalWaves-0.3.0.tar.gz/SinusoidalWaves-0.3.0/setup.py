c05f0525d648c7f0d1668588f212585cbf38e040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:00 +0000

    Add package SinusoidalWaves/0.3.0/SinusoidalWaves-0.3.0.tar.gz

---
+++ packages/SinusoidalWaves/SinusoidalWaves-0.3.0.tar.gz/SinusoidalWaves-0.3.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'SinusoidalWaves',
+  packages = ['SinusoidalWaves'],
+  version = '0.3.0',
+  license='MIT',
+  description = 'Contains Sine and Cosine classes for generating and transforming sinusoidal waves.',
+  author = 'Tyler Burgee',
+  author_email = 'tylerburgee@gmail.com',
+  url = 'https://github.com/TylerBurgee/SinusoidalWaves',
+  download_url = 'https://github.com/TylerBurgee/SinusoidalWaves/archive/refs/tags/v0.3.0.tar.gz',
+  keywords = ['waves', 'sine', 'cosine', 'transformation', 'signal', 'processing'],
+  install_requires=[
+          'numpy',
+          'matplotlib',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11'
+  ],
+)
