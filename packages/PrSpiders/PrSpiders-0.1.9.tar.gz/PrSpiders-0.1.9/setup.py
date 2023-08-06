9c9c90e42d9ca481ec9ed17ad1a22f28b3f69fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:00 +0000

    Add package PrSpiders/0.1.9/PrSpiders-0.1.9.tar.gz

---
+++ packages/PrSpiders/PrSpiders-0.1.9.tar.gz/PrSpiders-0.1.9/setup.py
@@ -0,0 +1,26 @@
+#!python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="PrSpiders",
+    version='0.1.9',
+    author="penr",
+    author_email="1944542244@qq.com",
+    description="Inherit the requests module, add xpath functionality to expand the API, and handle request failures and retries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/peng0928/prequests",
+    packages=find_packages(),
+    install_requires=["requests", "urllib3", "lxml", "xpinyin", "PrSpiders"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
