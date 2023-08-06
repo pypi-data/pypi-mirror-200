24f4afa1af693609b6281572a2165819679a1d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:39 +0000

    Add package meilisearch/0.26.0/meilisearch-0.26.0.tar.gz

---
+++ packages/meilisearch/meilisearch-0.26.0.tar.gz/meilisearch-0.26.0/meilisearch/_httprequests.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+import json
+from typing import Any, Callable, Dict, List, Optional, Union
+
+import requests
+
+from meilisearch.config import Config
+from meilisearch.errors import (
+    MeilisearchApiError,
+    MeilisearchCommunicationError,
+    MeilisearchTimeoutError,
+)
+from meilisearch.version import qualified_version
+
+
+class HttpRequests:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self.headers = {
+            "Authorization": f"Bearer {self.config.api_key}",
+            "User-Agent": qualified_version(),
+        }
+
+    def send_request(
+        self,
+        http_method: Callable,
+        path: str,
+        body: Optional[Union[Dict[str, Any], List[Dict[str, Any]], List[str], str]] = None,
+        content_type: Optional[str] = None,
+    ) -> Any:
+        if content_type:
+            self.headers["Content-Type"] = content_type
+        try:
+            request_path = self.config.url + "/" + path
+            if isinstance(body, bytes):
+                request = http_method(
+                    request_path,
+                    timeout=self.config.timeout,
+                    headers=self.headers,
+                    data=body,
+                )
+            else:
+                request = http_method(
+                    request_path,
+                    timeout=self.config.timeout,
+                    headers=self.headers,
+                    data=json.dumps(body) if body else "" if body == "" else "null",
+                )
+            return self.__validate(request)
+
+        except requests.exceptions.Timeout as err:
+            raise MeilisearchTimeoutError(str(err)) from err
+        except requests.exceptions.ConnectionError as err:
+            raise MeilisearchCommunicationError(str(err)) from err
+
+    def get(self, path: str) -> Any:
+        return self.send_request(requests.get, path)
+
+    def post(
+        self,
+        path: str,
+        body: Optional[Union[Dict[str, Any], List[Dict[str, Any]], List[str], str]] = None,
+        content_type: Optional[str] = "application/json",
+    ) -> Any:
+        return self.send_request(requests.post, path, body, content_type)
+
+    def patch(
+        self,
+        path: str,
+        body: Optional[Union[Dict[str, Any], List[Dict[str, Any]], List[str], str]] = None,
+        content_type: Optional[str] = "application/json",
+    ) -> Any:
+        return self.send_request(requests.patch, path, body, content_type)
+
+    def put(
+        self,
+        path: str,
+        body: Optional[Union[Dict[str, Any], List[Dict[str, Any]], List[str], str]] = None,
+        content_type: Optional[str] = "application/json",
+    ) -> Any:
+        return self.send_request(requests.put, path, body, content_type)
+
+    def delete(
+        self,
+        path: str,
+        body: Optional[Union[Dict[str, Any], List[Dict[str, Any]], List[str]]] = None,
+    ) -> Any:
+        return self.send_request(requests.delete, path, body)
+
+    @staticmethod
+    def __to_json(request: requests.Response) -> Any:
+        if request.content == b"":
+            return request
+        return request.json()
+
+    @staticmethod
+    def __validate(request: requests.Response) -> Any:
+        try:
+            request.raise_for_status()
+            return HttpRequests.__to_json(request)
+        except requests.exceptions.HTTPError as err:
+            raise MeilisearchApiError(str(err), request) from err
