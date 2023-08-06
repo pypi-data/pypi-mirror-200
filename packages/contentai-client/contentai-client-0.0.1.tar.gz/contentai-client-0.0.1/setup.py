80f827e44f968e76baed828f32f89f3cfdd9c0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:29 +0000

    Add package contentai-client/0.0.1/contentai-client-0.0.1.tar.gz

---
+++ packages/contentai-client/contentai-client-0.0.1.tar.gz/contentai-client-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='contentai-client',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='GregoryWiltshire',
+    author_email='Gregory.Wiltshire@warnermedia.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
