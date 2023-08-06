3b0320309e19f6b0a1eda921e7d0ab67dc3d8f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package datayoga-core/1.73.0/datayoga_core-1.73.0-py3-none-any.whl

---
+++ packages/datayoga-core/datayoga_core-1.73.0-py3-none-any.whl/datayoga_core/producer.py
@@ -0,0 +1,26 @@
+from abc import abstractmethod
+from typing import Any, Dict, Generator, List, TypedDict
+
+from .block import Block
+
+Message = TypedDict("Message", {"msg_id": str, "value": Dict[str, Any]})
+
+
+class Producer(Block):
+
+    @abstractmethod
+    def produce(self) -> Generator[Message, None, None]:
+        """Produces data
+
+        Returns:
+            Generator[Message]: Produced data
+        """
+        raise NotImplementedError
+
+    def ack(self, msg_ids: List[str]):
+        """Sends acknowledge for the message IDs of the records that have been processed
+
+        Args:
+            msg_ids (List[str]): Message IDs
+        """
+        pass
