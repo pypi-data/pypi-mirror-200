9fea76308e2c6a90432fb7c0e9936ea28b29bcb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:49 +0000

    Add package twilio-creator/0.0.1/twilio_creator-0.0.1.tar.gz

---
+++ packages/twilio-creator/twilio_creator-0.0.1.tar.gz/twilio_creator-0.0.1/README.md
@@ -0,0 +1,6 @@
+# Easy Twilio
+This is a easy twilio.
+
+# Use
+You need to have an account on twilio.
+(https://www.twilio.com/)
---
+++ packages/twilio-creator/twilio_creator-0.0.1.tar.gz/twilio_creator-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='twilio_creator',
+    version='0.0.1',
+    description='Tis is a easy twilio.',
+    author='PythonProgrammer',
+    author_email='phyton.programmer@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
