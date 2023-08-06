30cfb0976b76c5c23adf26fc3ae29d0f3d788ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:34 +0000

    Add package repid/1.0.1/repid-1.0.1-py3-none-any.whl

---
+++ packages/repid/repid-1.0.1-py3-none-any.whl/repid/serializer.py
@@ -0,0 +1,20 @@
+from typing import Any, Protocol
+
+import orjson
+
+PYDANTIC_IMPORTED = True
+try:
+    import pydantic
+except ImportError:  # pragma: no cover
+    PYDANTIC_IMPORTED = False
+
+
+class SerializerT(Protocol):
+    def __call__(self, data: Any) -> str:
+        ...
+
+
+def default_serializer(data: Any) -> str:
+    if PYDANTIC_IMPORTED and isinstance(data, pydantic.BaseModel):
+        return data.json()
+    return orjson.dumps(data).decode()
---
+++ packages/repid/repid-1.0.1-py3-none-any.whl/repid/utils.py
@@ -0,0 +1,10 @@
+import re
+import sys
+
+VALID_ID = re.compile(r"[a-zA-Z0-9_-]+")
+VALID_NAME = re.compile(r"[a-zA-Z_][a-zA-Z0-9_-]*")  # valid actor and queue names
+
+SLOTS_DATACLASS = {"slots": True} if sys.version_info >= (3, 10) else {}
+FROZEN_DATACLASS = {"frozen": True}  # this is a hack for mypy
+# for some reason directly declaring frozen dataclass makes mypy think that the dataclass isn't
+# implementing the protocol
