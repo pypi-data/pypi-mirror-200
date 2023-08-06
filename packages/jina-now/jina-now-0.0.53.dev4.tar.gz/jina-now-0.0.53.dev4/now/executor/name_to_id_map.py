8410033e90bb402e4f8a026b05779ecabb72c22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:05 +0000

    Add package jina-now/0.0.53.dev4/jina-now-0.0.53.dev4.tar.gz

---
+++ packages/jina-now/jina-now-0.0.53.dev4.tar.gz/jina-now-0.0.53.dev4/now/executor/name_to_id_map.py
@@ -0,0 +1,9 @@
+# TODO this should not be needed in the future
+name_to_id_map = {
+    'NOWPreprocessor': '2hgojz3z',
+    'NOWElasticIndexer': 'mdtuyaaj',
+    'NOWAutoCompleteExecutor2': 'w5w084h7',
+    'NOWGateway': '2m00g87k',
+}
+
+id_to_name_map = {val: key for key, val in name_to_id_map.items()}
