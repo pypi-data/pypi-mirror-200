8c8beed1a3142b692ac06e0133541a85e678b4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:38 +0000

    Add package crudal/0.3.0/crudal-0.3.0-py3-none-any.whl

---
+++ packages/crudal/crudal-0.3.0-py3-none-any.whl/crudal/base_sync.py
@@ -0,0 +1,157 @@
+import typing as t
+
+from sqlalchemy import Result, ScalarResult
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm import DeclarativeBase, Session
+
+from crudal import operations
+from crudal.types import CRUDALType
+from crudal.utils import with_session_sync
+
+_T = t.TypeVar("_T", bound=CRUDALType)
+
+
+def _crud_stmt_scalars(stmt, session: Session) -> ScalarResult:
+    return session.scalars(stmt)
+
+
+def _crud_stmt_execute(stmt, session: Session) -> Result:
+    return session.execute(stmt)
+
+
+class DeclarativeCrudBase(DeclarativeBase):
+    __session__ = None
+
+    @classmethod
+    def _get_primary_key(cls):
+        """Return PK column name"""
+        return inspect(cls).primary_key[0].name
+
+    @classmethod
+    @with_session_sync
+    def find(
+        cls: t.Type[_T],
+        session: Session,
+        /,
+        *,
+        rows: t.Optional[int] = None,
+        offset: int = 0,
+        **filters,
+    ) -> t.Sequence[_T]:
+        """Find items in table.
+
+        Example:
+        ```
+        # find all users with name Andrew
+        User.find(session=session, name="Andrew")
+        ```
+
+        Args:
+            session (Session): SQLAlchemy session
+            rows(int, optional): Number of rows to return. Defaults to None.
+            offset(int, optional): Number of rows to skip. Defaults to 0.
+            **filters: search filters
+
+        Returns:
+            t.Sequence[_T]: Found items
+        """
+        stmt = operations.find(cls, offset=offset, rows=rows, **filters)
+        result = _crud_stmt_scalars(stmt, session=session)
+        return result.all()
+
+    @classmethod
+    @with_session_sync
+    def find_by_pk(
+        cls: t.Type[_T], session: Session, /, *, pk: t.Any
+    ) -> t.Optional[_T]:
+        """Find row by its primary key
+
+        Args:
+            session (Session): SQLAlchemy session
+            pk (t.Any): primary key value
+
+        Raises:
+            ValueError: Multiple rows found by one primary key
+
+        Returns:
+            t.Optional[_T]: found item.
+                If None - no items with such primary keys exists
+        """
+        pk_col = cls._get_primary_key()
+        result = cls.find(session, **{pk_col: pk})
+        if len(result) == 1:
+            return result[0]
+        elif len(result) > 1:
+            raise ValueError(
+                f"Found more than one items ({len(result)}) with {pk_col}={pk}"
+            )
+        else:
+            return None
+
+    @classmethod
+    @with_session_sync
+    def exists(cls, session: Session, /, **filters) -> bool:
+        """Check if items exists in table
+
+        Args:
+            session (AsyncSession): SQLAlchemy session
+            **filters: search filters
+
+        Returns:
+            bool: True if exists, False if not
+        """
+        stmt = operations.find(cls, **filters)
+        result = _crud_stmt_scalars(stmt, session=session)
+        return True if result.first() is not None else False
+
+    @classmethod
+    @with_session_sync
+    def all(cls: t.Type[_T], session: Session, /) -> t.Sequence[_T]:
+        """Get all table items"""
+        stmt = operations.find(cls)
+        result = _crud_stmt_scalars(stmt, session=session)
+        return result.all()
+
+    @classmethod
+    @with_session_sync
+    def delete(cls, session: Session, /, *, commit: bool = False, **filters) -> bool:
+        """Delete items from table"""
+        exists = cls.exists(session, **filters)
+        if not exists:
+            return False
+
+        delete_stmt = operations.delete_(cls=cls, **filters)
+        session.execute(delete_stmt)
+        if commit:
+            session.commit()
+
+        return True
+
+    @classmethod
+    @with_session_sync
+    def add_many(cls: t.Type[_T], session: Session, /, *, items: t.List[_T]) -> None:
+        """Add many new items to table"""
+        session.add_all(items)
+
+    @with_session_sync
+    def add(self: _T, session: Session, /, *, commit: bool = False) -> _T:
+        """Add one item to table.
+
+        Args:
+            session (Session): SQLAlchemy async session
+            commit (bool, optional): Commit or not. Defaults to False.
+        """
+        session.add(self)
+        if commit:
+            session.commit()
+
+        return self
+
+    @classmethod
+    @with_session_sync
+    def update(
+        cls: t.Type[_T], session: Session, /, *, values: dict, **filters
+    ) -> t.Type[_T]:
+        """Update table items values"""
+        stmt = operations.update_(cls, values=values, **filters)
+        return _crud_stmt_execute(stmt=stmt, session=session)
---
+++ packages/crudal/crudal-0.3.0-py3-none-any.whl/crudal/types.py
@@ -0,0 +1,100 @@
+import typing as t
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+_T = t.TypeVar("_T")
+
+
+class CRUDALType(t.Protocol):
+    @classmethod
+    def _get_primary_key(cls) -> str:
+        """Table primary key"""
+        raise NotImplementedError()
+
+    @classmethod
+    def find(
+        cls: t.Type[_T],
+        session: t.Optional[AsyncSession] = None,
+        /,
+        *,
+        rows: t.Optional[int] = None,
+        offset: int = 0,
+        **filters,
+    ) -> t.Sequence[_T]:
+        """Find items"""
+        raise NotImplementedError()
+
+    @classmethod
+    def find_by_pk(cls: t.Type[_T], pk: t.Any, session: Session) -> t.Optional[_T]:
+        raise NotImplementedError()
+
+    @classmethod
+    def exists(cls, session: Session, **filters) -> bool:
+        raise NotImplementedError()
+
+    @classmethod
+    def all(cls: t.Type[_T], session: Session) -> t.Sequence[_T]:
+        raise NotImplementedError()
+
+    @classmethod
+    def delete(cls, session: Session, **filters) -> bool:
+        raise NotImplementedError()
+
+    @classmethod
+    def add_many(cls: t.Type[_T], session: Session, items: t.List[_T]) -> None:
+        raise NotImplementedError()
+
+    def add(self, session: Session, commit: bool = False) -> None:
+        raise NotImplementedError()
+
+
+class CRUDALTypeAsync(t.Protocol):
+    @classmethod
+    def _session(cls):
+        raise NotImplementedError()
+
+    @classmethod
+    def _get_primary_key(cls) -> str:
+        """Table primary key"""
+        raise NotImplementedError()
+
+    @classmethod
+    async def find(
+        cls: t.Type[_T],
+        session: t.Optional[AsyncSession] = None,
+        /,
+        *,
+        rows: t.Optional[int] = None,
+        offset: int = 0,
+        **filters,
+    ) -> t.Sequence[_T]:
+        """Find items"""
+        raise NotImplementedError()
+
+    @classmethod
+    async def find_by_pk(
+        cls: t.Type[_T], pk: t.Any, session: AsyncSession
+    ) -> t.Optional[_T]:
+        raise NotImplementedError()
+
+    @classmethod
+    async def exists(cls, session: AsyncSession, **filters) -> bool:
+        raise NotImplementedError()
+
+    @classmethod
+    async def all(cls: t.Type[_T], session: AsyncSession) -> t.Sequence[_T]:
+        raise NotImplementedError()
+
+    @classmethod
+    async def delete(cls, session: AsyncSession, **filters) -> bool:
+        raise NotImplementedError()
+
+    @classmethod
+    async def add_many(
+        cls: t.Type[_T], session: AsyncSession, items: t.List[_T]
+    ) -> None:
+        raise NotImplementedError()
+
+    async def add(self, session: AsyncSession, commit: bool = False) -> None:
+        raise NotImplementedError()
