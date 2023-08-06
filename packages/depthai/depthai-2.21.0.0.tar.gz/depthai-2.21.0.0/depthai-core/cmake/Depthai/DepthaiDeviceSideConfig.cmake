3fed86c9648604642bec6c880c5d4844c454c259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:05 +0000

    Add package depthai/2.21.0.0/depthai-2.21.0.0.tar.gz

---
+++ packages/depthai/depthai-2.21.0.0.tar.gz/depthai-2.21.0.0/depthai-core/cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -0,0 +1,8 @@
+# Maturity level "snapshot" / "release"
+set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
+
+# "full commit hash of device side binary"
+set(DEPTHAI_DEVICE_SIDE_COMMIT "57ca2c171f6ef53cb83fbbc3aabfe512c67796ce")
+
+# "version if applicable"
+set(DEPTHAI_DEVICE_SIDE_VERSION "")
