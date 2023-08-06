3ab7741ccaf22b927f3c3d36337c98f67ec2af8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:56 +0000

    Add package embed-base-model/0.1.1/embed-base-model-0.1.1.tar.gz

---
+++ packages/embed-base-model/embed-base-model-0.1.1.tar.gz/embed-base-model-0.1.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='embed-base-model',
+    version='0.1.1',
+    license='Apache-2.0',
+    author='Daniel Lee',
+    author_email='rootuser.kr@gmail.com',
+    description='Python abstract class for embedding sentences',
+    long_description=open('README.md').read(),
+    url='https://github.com/asulikeit/embed-base-model',
+    packages=['embedmodel'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ]
+)
