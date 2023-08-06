e269956358f437fb8396e03c982c2de27de2d0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:27 +0000

    Add package pyxk/0.4.5/pyxk-0.4.5-py3-none-any.whl

---
+++ packages/pyxk/pyxk-0.4.5-py3-none-any.whl/pyxk/__init__.py
@@ -0,0 +1,3 @@
+"""
+pip install -U pyxk -i https://pypi.org/simple
+"""
---
+++ packages/pyxk/pyxk-0.4.5-py3-none-any.whl/pyxk/httpx/__init__.py
@@ -0,0 +1,2 @@
+from pyxk.httpx._client import Client
+from pyxk.httpx._api import delete, get, head, options, patch, post, put, request, stream, wget
