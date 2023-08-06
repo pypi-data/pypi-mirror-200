c5d780b1598256c8d9cb3b0d89268475bc01887c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:30 +0000

    Add package ChromaticPrint/0.1.4/ChromaticPrint-0.1.4.tar.gz

---
+++ packages/ChromaticPrint/ChromaticPrint-0.1.4.tar.gz/ChromaticPrint-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ChromaticPrint',
+    version='0.1.04',
+    description='A library for printing colored text in the terminal',
+    author='INeddHelp',
+    author_email='ineddhelpgithub@gmail.com',
+    url='https://github.com/INeddHelp/Colors-python-library',
+    packages=['chromaticprint'],
+    install_requires=['pyyaml'],
+)
