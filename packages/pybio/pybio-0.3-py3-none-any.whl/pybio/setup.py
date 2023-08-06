2f6f2e8db1f00132b458078b52fe3e2f2d82c128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:21 +0000

    Add package pybio/0.3/pybio-0.3-py3-none-any.whl

---
+++ packages/pybio/pybio-0.3-py3-none-any.whl/pybio/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("pybio/README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pybio',
+    version = "0.3",
+    package_dir = {"":"src"},
+    packages=find_packages("src"),
+    description='pybio genomics',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    zip_safe=False,
+    author='Gregor Rot',
+    scripts=["src/pybio/pybio"],
+    author_email='gregor.rot@gmail.com',
+    url='https://github.com/grexor/pybio',
+    keywords=['pybio', 'bioinformatics'],
+    include_package_data=True,
+    package_data={
+        'pybio': ['pybio.config.example'],
+    },
+    install_requires=["pysam", "numpy", "psutil", "bs4", "requests"],
+)
