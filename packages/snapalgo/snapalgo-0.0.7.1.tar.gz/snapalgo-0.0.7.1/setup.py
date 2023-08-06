b5c9b37237426b357d0b4baaae32cd5ca221246a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:39 +0000

    Add package snapalgo/0.0.7.1/snapalgo-0.0.7.1.tar.gz

---
+++ packages/snapalgo/snapalgo-0.0.7.1.tar.gz/snapalgo-0.0.7.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+  name='snapalgo',
+  version='0.0.7.1',
+  description='Coding Helper',
+  long_description=Path("README.md").read_text(encoding="utf-8"),
+  long_description_content_type="text/markdown",
+  url='https://github.com/midnightbot/snapalgo',  
+  author='Anish Adnani',
+  author_email='anishadnani00@gmail.com',
+  license='MIT', 
+  keywords=['python', 'coding', 'template', 'algo', 'algorithms'],
+  packages=find_packages(),
+  install_requires=['pyperclip'] ,
+  classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
