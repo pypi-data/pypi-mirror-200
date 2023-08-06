a73ecf3dec1d7e0c420eceb2ec8b0ad5ab5ea23d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package basicco/10.1.1/basicco-10.1.1.tar.gz

---
+++ packages/basicco/basicco-10.1.1.tar.gz/basicco-10.1.1/tests/test_bases.py
@@ -0,0 +1,27 @@
+# type: ignore
+
+import pytest
+from tippo import GenericMeta
+
+import basicco
+
+
+def test_bases():
+    assert basicco.CompatBaseMeta
+    assert basicco.CompatBase
+    assert basicco.CompatBase()
+
+    assert issubclass(basicco.CompatBaseMeta, GenericMeta)
+
+    assert basicco.BaseMeta
+    assert basicco.Base
+    assert basicco.Base()
+    assert "__weakref__" in basicco.Base.__slots__
+
+    assert basicco.SlottedBaseMeta
+    assert basicco.SlottedBase
+    assert basicco.SlottedBase()
+
+
+if __name__ == "__main__":
+    pytest.main()
