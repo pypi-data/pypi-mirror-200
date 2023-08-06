0126b74e0d986dac5d8e866a35be1e80df700138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:41 +0000

    Add package fastapi-pagination-minus-one/0.0.2/fastapi_pagination_minus_one-0.0.2-py3-none-any.whl

---
+++ packages/fastapi-pagination-minus-one/fastapi_pagination_minus_one-0.0.2-py3-none-any.whl/pagination/paginate_minus_one.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from fastapi_pagination import Page, Params, paginate
+
+from src.pagination.page_query_model import PageQueryModel
+
+
+def paginate_list(item_list: List[any], page: int, size: int) -> PageQueryModel:
+    paginated_result: Page = paginate(
+        item_list,
+        params=Params(page=page + 1, size=size),
+    )
+    output_result: PageQueryModel = PageQueryModel(**paginated_result.__dict__)
+    output_result.page -= 1
+    return output_result
