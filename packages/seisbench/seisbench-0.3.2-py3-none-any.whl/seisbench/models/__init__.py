3107c113ef4bd5c3cb625ef18e8c27ecf8c411a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:24 +0000

    Add package seisbench/0.3.2/seisbench-0.3.2-py3-none-any.whl

---
+++ packages/seisbench/seisbench-0.3.2-py3-none-any.whl/seisbench/models/__init__.py
@@ -0,0 +1,8 @@
+from .aepicker import BasicPhaseAE
+from .base import SeisBenchModel, WaveformModel, WaveformPipeline
+from .cred import CRED
+from .deepdenoiser import DeepDenoiser
+from .dpp import DeepPhasePick, DPPDetector, DPPPicker
+from .eqtransformer import EQTransformer
+from .gpd import GPD
+from .phasenet import PhaseNet, PhaseNetLight
