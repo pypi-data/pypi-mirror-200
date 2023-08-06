5fb3bf6e98f4366b8b687ae808fc6e70f1fb99da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:36 +0000

    Add package pypassfusion/1.5.1.5/pypassfusion-1.5.1.5.tar.gz

---
+++ packages/pypassfusion/pypassfusion-1.5.1.5.tar.gz/pypassfusion-1.5.1.5/pypassfusion/__init__.py
@@ -0,0 +1,5 @@
+name = "PyPassFusion"
+name_desc = "PyPassFusion"
+version = "1.5.1.4"
+description = "Command line tool to generate secure password and save it"
+url = "https://github.com/amirhosseinbonyadi/PyPassFusion"
