0c29ea947836f1ffa08a60e5a494a6c311315c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package pitertools/0.4.0/pitertools-0.4.0.tar.gz

---
+++ packages/pitertools/pitertools-0.4.0.tar.gz/pitertools-0.4.0/pitertools/__init__.py
@@ -0,0 +1,51 @@
+from concurrent.futures import Executor
+from concurrent.futures.thread import ThreadPoolExecutor
+from contextlib import contextmanager
+from typing import Callable, Iterator, Optional, TypeVar
+
+from pitertools.map_theaded import map_parallel_threaded
+from pitertools.map_executor import map_parallel_threadpool
+
+
+T = TypeVar('T')
+G = TypeVar('G')
+
+
+@contextmanager
+def map_parallel(
+    func: Callable[[T], G],
+    it: Iterator[T],
+    concurrency: int,
+    ordered = False,
+    verbose: bool = False,
+    executor: Optional[Executor] = None
+) -> Iterator[G]:
+    """
+    Concurrently pulls items from `it` to run `func` on it, with respect to backpresure - new 
+    items are pulled from `it` only after results are consumed from the result iterator.
+    """
+    res = None
+    if executor:
+        if isinstance(executor, ThreadPoolExecutor):
+            res = map_parallel_threadpool(
+                executor=executor,
+                func=func,
+                it=it,
+                concurrency=concurrency,
+                ordered=ordered,
+                verbose=verbose
+            )
+        else:
+            raise NotImplementedError()
+    else:
+        res = map_parallel_threaded(
+            func=func,
+            it=it,
+            num_workers=concurrency,
+            ordered=ordered,
+            verbose=verbose
+        )
+    try:
+        yield res.start()
+    finally:
+        res.stop()
