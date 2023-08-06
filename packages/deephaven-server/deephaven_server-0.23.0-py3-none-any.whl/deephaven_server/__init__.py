ffc73129009b251867bff2d5a85b021a74005aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:47 +0000

    Add package deephaven-server/0.23.0/deephaven_server-0.23.0-py3-none-any.whl

---
+++ packages/deephaven-server/deephaven_server-0.23.0-py3-none-any.whl/deephaven_server/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2016-2022 Deephaven Data Labs and Patent Pending
+#
+__version__ = "0.23.0"
+
+from .start_jvm import DEFAULT_JVM_PROPERTIES, DEFAULT_JVM_ARGS, start_jvm
+from .server import Server
+
