9901b41946ea94fbaa52b2f8e6371000d4b3d1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:12 +0000

    Add package elastix-napari/0.1.8/elastix_napari-0.1.8-py3-none-any.whl

---
+++ packages/elastix-napari/elastix_napari-0.1.8-py3-none-any.whl/elastix_napari/__init__.py
@@ -0,0 +1,10 @@
+from .elastix_registration import napari_experimental_provide_dock_widget
+
+__author__ = "Viktor van der Valk"
+__email__ = "v.o.van_der_valk@lumc.nl"
+
+__version__ = "0.1.8"
+
+
+def get_module_version():
+    return __version__
