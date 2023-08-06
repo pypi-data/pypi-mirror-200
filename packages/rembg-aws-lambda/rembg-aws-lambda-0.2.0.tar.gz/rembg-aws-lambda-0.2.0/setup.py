64c75ad22396b21aaf993492c4a88eb05e90b256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:56 +0000

    Add package rembg-aws-lambda/0.2.0/rembg-aws-lambda-0.2.0.tar.gz

---
+++ packages/rembg-aws-lambda/rembg-aws-lambda-0.2.0.tar.gz/rembg-aws-lambda-0.2.0/setup.py
@@ -0,0 +1,56 @@
+import os
+import pathlib
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+from setuptools import setup
+
+import versioneer
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+requires = [
+    "numpy~=1.23.5",
+    "onnxruntime~=1.13.1",
+    "opencv-python-headless~=4.6.0.66",
+    "pillow~=9.3.0",
+]
+
+extras_require = {
+    "gpu": ["onnxruntime-gpu~=1.13.1"],
+}
+
+setup(
+    name="rembg-aws-lambda",
+    description="Remove image background",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rnag/rembg-aws-lambda",
+    author="Daniel Gatis",
+    author_email="danielgatis@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    keywords="remove, background, u2net",
+    packages=["rembg"],
+    package_data={'': ['*.onnx']},
+    include_package_data=True,
+    python_requires=">3.7, <3.11",
+    install_requires=requires,
+    extras_require=extras_require,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
