2dec99cc07099e7647f0fc1e43ccb2aea30f4041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:14 +0000

    Add package openai-shell-craft/0.3.0/openai_shell_craft-0.3.0-py3-none-any.whl

---
+++ packages/openai-shell-craft/openai_shell_craft-0.3.0-py3-none-any.whl/shell_craft/prompts/feature_request.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2023 Johnathan P. Irvin and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from .prompt import Prompt
+
+FEATURE_REQUEST_PROMPT = Prompt(
+    content="""
+Fill out this form:
+
+```
+---
+name: <Name Of Feature>
+about: <Short Description>
+labels: < Documentation, Enhancement, Question, Bug, etc. >
+
+---
+
+**Is your feature request related to a problem? Please describe.**
+A clear and concise description of what the problem is. Ex. I'm always frustrated when [...]
+
+**Describe the solution you'd like**
+A clear and concise description of what you want to happen.
+
+**Describe alternatives you've considered**
+A clear and concise description of any alternative solutions or features you've considered.
+```
+"""
+)
