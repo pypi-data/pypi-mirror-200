35baebf473e0724f6bb99cef18b499a113fa184f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:02 +0000

    Add package PyProtolinc/0.1.7/PyProtolinc-0.1.7-cp310-cp310-win_amd64.whl

---
+++ packages/PyProtolinc/PyProtolinc-0.1.7-cp310-cp310-win_amd64.whl/pyprotolinc/assumptions/dav2004r.py
@@ -0,0 +1,315 @@
+import os
+import logging
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from typing import Optional, Union
+
+import pyprotolinc
+import pyprotolinc.riskfactors.risk_factors as risk_factors
+from pyprotolinc.assumptions.providers import StandardRateProvider
+import pyprotolinc._actuarial as act  # type: ignore
+
+# module level logger
+logger = logging.getLogger(__name__)
+
+
+class DAV2004R:
+    """ Represents the DAV2004R table family for annuities. The table is a generation type family
+        with risk factors age, gender and (optionally) the select year.
+
+        Providers can be obtained for:
+
+          - best estimate ("BE", "2. Ordnung")
+          - with safety loadings ("LOADED", "1. Ordnung")
+
+    """
+    def __init__(self,
+                 base_directory: Optional[Union[str, Path]] = None,
+                 trend_t1=10,
+                 trend_t2=25) -> None:
+
+        if base_directory is None:
+            base_directory = os.path.join(pyprotolinc._DEFAULT_TABLES_PATH, "Germany_Annuities_DAV2004R")
+
+        self.base_rates_path = os.path.abspath(os.path.join(base_directory, "Germany_Annuities_DAV2004R.csv"))
+        self.select_rates_path = os.path.abspath(os.path.join(base_directory, "Germany_Annuities_DAV2004R_Select.csv"))
+        self.trend_rates_path = os.path.abspath(os.path.join(base_directory, "Germany_Annuities_DAV2004R_Trends.csv"))
+
+        self.base_year_trend = 1999
+        # trend_t1 and trend_t2 are used for the trend flattening
+        self.trend_t1 = trend_t1
+        self.trend_t2 = trend_t2
+
+        # load and transform base rates
+        tab_DAV2004R_base = pd.read_csv(self.base_rates_path, header=[0, 2, 3], index_col=0)
+        df_tmp = tab_DAV2004R_base.unstack()
+        df_tmp.index.names = ["TABLE_TYPE", "ESTIMATE_TYPE", "GENDER", "AGE"]
+        df_tmp = df_tmp.reset_index().rename({0: "QX"}, axis=1)
+        df_tmp.GENDER = df_tmp.GENDER.map({"Männer": risk_factors.Gender.M, "Frauen": risk_factors.Gender.F})
+        df_tmp.ESTIMATE_TYPE = df_tmp.ESTIMATE_TYPE.map({'2. Ordnung': "BE", 'Bestand': "?", '1. Ordnung': "LOADED"})
+        df_tmp.TABLE_TYPE = df_tmp.TABLE_TYPE.map({'Selektionstafel': "SELECT", 'Aggregattafel': "AGGREGATE"})
+        self.df_base_rates = df_tmp   # .copy()
+
+        # extract the ages
+        self.ages = np.sort(tab_DAV2004R_base.index.values)
+
+        # load and transform the trends
+        tab_DAV2004R_trends = pd.read_csv(self.trend_rates_path, header=[0, 2, 3], index_col=0)
+        # forward fill trend headers - there are some gaps which we fill
+        filled_headers = [[], [], []]
+        for row_count, ind_row in enumerate(tab_DAV2004R_trends.columns):
+            for header_ind in range(len(filled_headers)):
+                if row_count > 0 and ind_row[header_ind].startswith("Unnamed:"):
+                    filled_headers[header_ind].append(filled_headers[header_ind][-1])
+                else:
+                    filled_headers[header_ind].append(ind_row[header_ind])
+        new_headers = pd.MultiIndex.from_arrays(filled_headers, names=tab_DAV2004R_trends.columns.names)
+        tab_DAV2004R_trends.columns = new_headers
+        # re-group the trend frame
+        df_tmp = tab_DAV2004R_trends.unstack()
+        df_tmp.index.names = ["LONG/SHORT", "ESTIMATE_TYPE", "GENDER", "AGE"]
+        df_tmp = df_tmp.reset_index().rename({0: "F"}, axis=1)
+        df_tmp.GENDER = df_tmp.GENDER.map({"Männer": risk_factors.Gender.M, "Frauen": risk_factors.Gender.F})
+        df_tmp.ESTIMATE_TYPE = df_tmp.ESTIMATE_TYPE.str.strip().map({'2. Ordnung': "BE", 'Bestand': "?", '1. Ordnung': "LOADED"})
+        df_tmp["LONG/SHORT"] = df_tmp["LONG/SHORT"].map({
+            'F_1(x)': "F1",
+            'F_1(y)': "F1",
+            'F_2(x)': "F2",
+            'F_2(y)': "F2",
+            'F(x)': "F",
+            'F(y)': "F"
+        })
+        self.df_trend_rates = df_tmp   # .copy()
+
+    def rates_provider(self, table_type: str = 'AGGREGATE', estimate_type: str = "BE", t_begin: int = 1999, t_end: int = 2150) -> Union[StandardRateProvider, act.StandardRateProvider]:
+        """ A rate provider object is returned.
+
+        Arguments:
+
+        :table_table           Can be either 'AGGREGATE' or 'SELECT' depending on the intended purpose (SELECT is not yet implemented)
+        :estimate_type         Can be either 'BE' (best estimate, "2. Ordnung") or 'LOADED' (with loading, "1. Ordnung")
+
+        """
+
+        table_type = table_type.upper()
+        estimate_type = estimate_type.upper()
+
+        df_base = self.df_base_rates[(self.df_base_rates.TABLE_TYPE == table_type) &
+                                     (self.df_base_rates.ESTIMATE_TYPE == estimate_type)]
+        df_base = df_base.set_index(['GENDER', 'AGE'])[["QX"]].unstack("GENDER").droplevel(0, axis=1)
+
+        df_trend = self.df_trend_rates[self.df_trend_rates.ESTIMATE_TYPE == estimate_type]\
+                       .set_index(["LONG/SHORT", "ESTIMATE_TYPE", "GENDER", "AGE"])\
+                       .unstack("GENDER")\
+                       .droplevel(0, axis=1)\
+                       .unstack("LONG/SHORT")\
+                       .droplevel(0, axis=0)
+
+        table = None
+
+        if estimate_type == "BE":
+            table = _calculate_be_tables_with_trend(t_begin, t_end, self.trend_t1, self.trend_t2, df_base, df_trend)
+        elif estimate_type == "LOADED":
+            table = _calculate_loaded_tables_with_trend(t_begin, t_end, self.trend_t1, self.trend_t2, df_base, df_trend)
+        else:
+            raise Exception("Unknow estimate type: {}".format(estimate_type))
+
+        if table_type == 'AGGREGATE':
+            # base rates and trend for 'AGGREGATE'
+            # provider = StandardRateProvider(table,
+            #                                 (risk_factors.CalendarYear, risk_factors.Gender, risk_factors.Age),
+            #                                 offsets=(t_begin, 0, 0))
+            provider = act.StandardRateProvider(rfs=[risk_factors.CalendarYear.get_CRiskFactor(),
+                                                     risk_factors.Gender.get_CRiskFactor(),
+                                                     risk_factors.Age.get_CRiskFactor()
+                                                     ],
+                                                values=table,
+                                                offsets=np.array([t_begin, 0, 0], dtype=np.int32))
+
+        elif table_type == 'SELECT':
+            raise Exception("Not yet implemented.")
+
+        return provider
+
+
+def _G(t, T1, T2):
+    """ Trend interpolation formula, cf.
+        2018-01-24_DAV-Richtlinie_Herleitung_DAV2004R.pdf (online), p. 43
+    """
+    if t < 1999:
+        raise Exception("Table not available for years before 1999")
+    elif t <= 1999 + T1:
+        return 1
+    elif t <= 1999 + T2:
+        return 1.0 - ((t - 1999 - T1) * (t - 1999 - T1 - 1)) / (2 * (T2 - T1) * (t - 1999))
+    else:
+        return (T1 + T2 + 1) / (2 * (t - 1999))
+
+
+def _calculate_be_tables_with_trend(t_begin, t_end, T1, T2, df_base, df_trend):
+    """ Only applicable for BE (not LOADED). Return a three dimensional array containing the mortality
+        rates indexed by:
+
+          - age
+          - sex (index 0=M, 1=F)
+          - calendar year (index 0 corresponds with t_begin)
+    """
+
+    # create container for the result
+    res_table = np.zeros((1 + t_end - t_begin, 2, df_base.shape[0]), dtype=np.float64)
+
+    # base data males
+    qx_male = df_base.values[:, risk_factors.Gender.M]
+    F1x_male = df_trend[(risk_factors.Gender.M, "F1")].values
+    F2x_male = df_trend[(risk_factors.Gender.M, "F2")].values
+
+    # base data females
+    qx_female = df_base.values[:, risk_factors.Gender.F]
+    F1x_female = df_trend[(risk_factors.Gender.F, "F1")].values
+    F2x_female = df_trend[(risk_factors.Gender.F, "F2")].values
+
+    # trend factors
+    for t in range(t_begin, 1 + t_end):
+        g = _G(t, T1, T2)
+        exponential_arg_male = (F2x_male + g * (F1x_male - F2x_male)) * (t - 1999)
+        exponential_arg_female = (F2x_female + g * (F1x_female - F2x_female)) * (t - 1999)
+        res_table[t - t_begin, 0, :] = qx_male * np.exp(-exponential_arg_male)
+        res_table[t - t_begin, 1, :] = qx_female * np.exp(-exponential_arg_female)
+
+    return res_table
+
+
+def _calculate_loaded_tables_with_trend(t_begin, t_end, T1, T2, df_base, df_trend):
+    """ Only applicable for LOADED (not BE)). Return a three dimensional array containing the mortality
+        rates indexed by:
+
+          - age
+          - sex (index 0=M, 1=F)
+          - calendar year (index 0 corresponds with t_begin)
+    """
+
+    # create container for the result
+    res_table = np.zeros((1 + t_end - t_begin, 2, df_base.shape[0]), dtype=np.float64)
+
+    # base data males
+    qx_male = df_base.values[:, risk_factors.Gender.M]
+    Fx_male = df_trend[(risk_factors.Gender.M, "F")].values
+
+    # base data females
+    qx_female = df_base.values[:, risk_factors.Gender.F]
+    Fx_female = df_trend[(risk_factors.Gender.F, "F")].values
+
+    # trend factors
+    for t in range(t_begin, 1 + t_end):
+        exponential_arg_male = Fx_male * (t - 1999)
+        exponential_arg_female = Fx_female * (t - 1999)
+        res_table[t - t_begin, 0, :] = qx_male * np.exp(-exponential_arg_male)
+        res_table[t - t_begin, 1, :] = qx_female * np.exp(-exponential_arg_female)
+
+    return res_table
+
+
+class B20RatesProvider(StandardRateProvider):
+
+    def __init__(self, values, other_risk_factors, offsets=None, shifts_table=None, select_matrix=None):
+        assert shifts_table is not None, "No shifts table provided for the B20RatesProvider."
+        assert select_matrix is not None, "No select factors table provided for the B20RatesProvider."
+
+        super().__init__(values, other_risk_factors, offsets)
+        self.shifts_table = shifts_table
+        self.select_matrix = select_matrix
+        self.is_initialized = False
+
+    def get_risk_factors(self):
+        """ This method returns an iterable of the relevant risk factors. """
+        return [risk_factors.YearsDisabledIfDisabledAtStart] + super().get_risk_factors()
+
+    def initialize(self, **kwargs):
+
+        if self.is_initialized:
+            logger.warn("Reinitialization of B20-Provider. This must not happen in a multithreaded scenario.")
+
+        self.years_of_birth = kwargs["years_of_birth"]
+        self.gender = kwargs["gender"]
+        self.applicable_shifts = self.shifts_table[self.years_of_birth - 1900, self.gender].astype(np.int16)
+
+        self.is_initialized = True
+
+    def get_rates(self, length=None, **kwargs):
+
+        assert self.is_initialized, "B20RatesProvider must be initialized before getting rates."
+        # adjust the ages, make sure they don't become negative
+        kwargs["age"] = np.maximum(kwargs["age"] + self.applicable_shifts, 0)
+
+        gender = kwargs["gender"]
+        yearsdisabledifdisabledatstart = kwargs["yearsdisabledifdisabledatstart"]
+
+        # identify the rows where a select factor applies: should be only those with 0 <= yearsdisabledifdisabledatstart <= 4
+        tmp = np.logical_or(np.isnan(yearsdisabledifdisabledatstart), np.nan_to_num(yearsdisabledifdisabledatstart, 0) > 4)
+        tmp = np.logical_or(tmp, np.nan_to_num(yearsdisabledifdisabledatstart, 0) < 0)
+        relevant_indexes = np.logical_not(tmp)
+
+        ydads_relevant_rows = yearsdisabledifdisabledatstart[relevant_indexes].astype(np.int16)
+
+        # the select factor consists of "1"s except in the "relevant rows"
+        # where it is overwritten
+        select_factor = np.ones(len(yearsdisabledifdisabledatstart))
+        select_factor[relevant_indexes] = self.select_matrix[ydads_relevant_rows, gender[relevant_indexes]]
+
+        # print("yearsdisabledifdisabledatstart ", yearsdisabledifdisabledatstart )
+        # print("select_factor", select_factor)
+
+        # lookup using adjusted ages
+        return select_factor * super().get_rates(length, **kwargs)
+
+
+class DAV2004R_B20:
+    """ Represents the DAV2004R_B20 table family for annuities. The table is a
+        base table with age shifts determined by the year of birth.
+        Providers can be obtained for:
+
+          - best estimate ("BE", "B20")
+          - with safety loadings ("LOADED", "1. Ordnung")
+          - inforce ("IF", "Bestand")
+
+    """
+    def __init__(self, base_directory):
+
+        b20base_rates_path = os.path.join(base_directory, "Germany_Annuities_DAV2004R_AVBase.csv")
+        av_shifts_path = os.path.join(base_directory, "Germany_Annuities_DAV2004R_AV.csv")
+        selects_path = os.path.join(base_directory, "Germany_Annuities_DAV2004R_Select.csv")
+
+        self.df_base_rates = pd.read_csv(b20base_rates_path, header=[0, 1], index_col=0, skiprows=1)
+        self.df_shifts = pd.read_csv(av_shifts_path)
+        self.df_select_factors = pd.read_csv(selects_path, index_col=0, skiprows=1)
+
+    def rates_provider(self, estimate_type="B20"):
+
+        if estimate_type == "B20":
+            av_type = "B20"
+        elif estimate_type == "IF" or estimate_type == "Bestand":
+            av_type = "Bestand"
+        elif estimate_type == "LOADED" or estimate_type == "1. Ordnung":
+            av_type = "1. Ordnung"
+        else:
+            raise Exception("Unknown estimate type for DAV2004R_B20: {}".format(estimate_type))
+
+        base_rates = np.zeros((len(self.df_base_rates), 2))
+        base_rates[:,  risk_factors.Gender.M] = self.df_base_rates[(av_type, "Männer")].values
+        base_rates[:,  risk_factors.Gender.F] = self.df_base_rates[(av_type,  "Frauen")].values
+
+        shifts = np.zeros((len(self.df_shifts), 2), dtype=np.int16)
+        shifts[:, risk_factors.Gender.M] = self.df_shifts["Männer " + av_type].values
+        shifts[:, risk_factors.Gender.F] = self.df_shifts["Frauen " + av_type].values
+
+        select_matrix = np.zeros((len(self.df_select_factors), 2))
+        select_matrix[:, risk_factors.Gender.M] = self.df_select_factors["Männer"]
+        select_matrix[:, risk_factors.Gender.F] = self.df_select_factors["Frauen"]
+
+        provider = B20RatesProvider(base_rates,
+                                    (risk_factors.Age, risk_factors.Gender),   # NOte: This one is applicable but not in the list risk_factors.YearsDisabledIfDisabledAtStart),
+                                    shifts_table=shifts,
+                                    select_matrix=select_matrix)
+        return provider
---
+++ packages/PyProtolinc/PyProtolinc-0.1.7-cp310-cp310-win_amd64.whl/pyprotolinc/assumptions/providers.py
@@ -0,0 +1,282 @@
+""" Assumption provider classes are used to obtain
+    rates for a set of (vectorized) risk factors
+    during a simulation run."""
+
+from abc import ABCMeta, abstractmethod
+from typing import Optional, Any, Union
+import logging
+from enum import IntEnum, unique
+import numpy as np
+import numpy.typing as npt
+
+from pyprotolinc.riskfactors.risk_factors import RiskFactor
+import pyprotolinc._actuarial as actuarial  # type: ignore
+
+
+# module level logger
+logger = logging.getLogger(__name__)
+
+
+@unique
+class AssumptionType(IntEnum):
+    """ Distinguish between best estimate (BE) and reserving assumptions (RES). """
+    BE = 0
+    RES = 1
+
+
+class BaseRatesProvider(metaclass=ABCMeta):
+    """ Base class for `rates providers`."""
+
+    @abstractmethod
+    def get_rates(self, length: int, **kwargs: Any) -> npt.NDArray[np.float64]:
+        raise Exception("Method <get_rate> must be overwritten in subclass")
+
+    @abstractmethod
+    def initialize(self, **kwargs: Any) -> None:
+        """ The initialize hook can be used for setup actions. """
+        pass
+
+    @abstractmethod
+    def get_risk_factors(self) -> list[type[RiskFactor]]:
+        """ This method returns an iterable of the relevant risk factors. """
+        return list()
+
+
+class ZeroRateProvider(BaseRatesProvider):
+
+    def get_rates(self, length: int, **kwargs: Any) -> npt.NDArray[np.float64]:
+        return np.zeros(length)
+
+    def __repr__(self) -> str:
+        return "<ZeroRateProvider>"
+
+    def initialize(self, **kwargs: Any) -> None:
+        """ The initialize hook: nothing needed for this class. """
+        pass
+
+    # def get_risk_factors(self) -> list[type[RiskFactor]]:
+    #     """ This method returns an iterable of the relevant risk factors. """
+    #     return list()
+
+
+class ConstantRateProvider(BaseRatesProvider):
+
+    def __init__(self, const_rate: float) -> None:
+        self.const_rate = const_rate
+
+    def get_rates(self, length: int, **kwargs: Any) -> npt.NDArray[np.float64]:
+        return np.ones(length) * self.const_rate
+
+    def __repr__(self) -> str:
+        return "<ConstantRateProvider with constant {}>".format(self.const_rate)
+
+    def initialize(self, **kwargs: Any) -> None:
+        """ The initialize hook: nothing needed for this class. """
+        pass
+
+
+class StandardRateProvider(BaseRatesProvider):
+    """ Rates provider class is used to return the rates
+        given a selector array for each risk factor. """
+
+    def __init__(self,
+                 values: npt.NDArray[np.float64],
+                 risk_factors: list[type[RiskFactor]],
+                 offsets: Optional[Union[npt.NDArray[np.int32], tuple[int]]] = None) -> None:
+        """ Data to be provided is an n-dimensional numpy array and an iterable of RiskFactors."""
+        self.num_dimensions = len(values.shape)
+
+        # copy offsets into numpy array
+        self.offsets: npt.NDArray[np.int32] = np.zeros(self.num_dimensions, dtype=np.int32)
+        if offsets is not None:
+            assert len(offsets) == self.num_dimensions
+            for k in range(self.num_dimensions):
+                self.offsets[k] = offsets[k]
+
+        assert self.num_dimensions == len(risk_factors), "Number of risk factors and dimension of values must agree!"
+        assert self.num_dimensions >= 0 and self.num_dimensions < 4, "Number of dimensions must be between 0 and 3"
+
+        self.values: npt.NDArray[np.float64] = np.copy(values)
+
+        self.max_indexes = [i - 1 for i in self.values.shape]
+
+        # store the risk factor classes and names
+        self.risk_factor_classes = list(risk_factors)
+        self.risk_factor_names = [rf.__name__.lower() for rf in risk_factors]
+
+    def get_risk_factors(self) -> list[type[RiskFactor]]:
+        """ This method returns an iterable of the relevant risk factors. """
+        return self.risk_factor_classes
+
+    def get_rates(self, length: int = -1, **kwargs: Any) -> npt.NDArray[np.float64]:
+        """ Rates are returned, the arguments must agree with the risk factors and
+            contain numpy arrays that are used to index the values tensor. """
+
+        attr = {k.lower(): v for (k, v) in kwargs.items()}
+
+        # leave this is only in `diagnosis mode`
+        # check for unknown risk factors provided
+        # unknown_rfs = attr.keys() - set(self.risk_factors)
+        # if unknown_rfs:
+        #    logger.debug("Risk factors unknown for this provider: {}, expected only {}"
+        #                 .format(str(unknown_rfs), self.risk_factors))
+
+        # order the selectors
+        selectors = []
+        for rf_name in self.risk_factor_names:
+            selectors.append(attr[rf_name])
+
+        if self.num_dimensions == 0:
+            val: float = self.values[0]
+            return val * np.ones(1)
+        elif self.num_dimensions == 1:
+            if selectors[0].min() - self.offsets[0] < 0:
+                raise Exception("Negative assumption index")
+            # this might not be the correct type but silences the numpy warning
+            # when returning the sliced array directly
+            a1: npt.NDArray[np.float64] = self.values[np.minimum(selectors[0] - self.offsets[0], self.max_indexes[0]), ]  # ignore: type
+            return a1
+        elif self.num_dimensions == 2:
+            if (selectors[0].min() - self.offsets[0] < 0 or
+               selectors[1].min() - self.offsets[1] < 0):
+                raise Exception("Negative assumption index")
+            a2: npt.NDArray[np.float64] = self.values[np.minimum(selectors[0] - self.offsets[0], self.max_indexes[0]),
+                                                      np.minimum(selectors[1] - self.offsets[1], self.max_indexes[1])]
+            return a2
+        elif self.num_dimensions == 3:
+            if (selectors[0].min() - self.offsets[0] < 0 or
+               selectors[1].min() - self.offsets[1] < 0 or
+               selectors[2].min() - self.offsets[2] < 0):
+                raise Exception("Negative assumption index")
+            a3: npt.NDArray[np.float64] = self.values[np.minimum(selectors[0] - self.offsets[0], self.max_indexes[0]),
+                                                      np.minimum(selectors[1] - self.offsets[1], self.max_indexes[1]),
+                                                      np.minimum(selectors[2] - self.offsets[2], self.max_indexes[2])]
+            return a3
+
+        print(self.num_dimensions)
+        raise Exception("Method must be implemented in subclass.")
+
+    def __repr__(self) -> str:
+        return "<StandardRateProvider with factors ({})>".format(str(self.risk_factor_names))
+
+    def initialize(self, **kwargs: Any) -> None:
+        """ The initialize hook: nothing needed for this class. """
+        pass
+
+
+class AssumptionTimestepAdjustment:
+    """ The class converts yearly decrements to decrements applicable for fractions
+        of the year. """
+
+    def __init__(self, timestep: float, num_MultiStateDisabilityStates: int, num_insureds: int) -> None:
+        """ : timestep   - length of the timestep to adjust the assumptions to. """
+        self.timestep = timestep
+        self.num_MultiStateDisabilityStates = num_MultiStateDisabilityStates
+        self.num_insureds = num_insureds
+
+        # Construct some auxiliary indexes.
+        # First an index consisting of 5(=num_MultiStateDisabilityStates) zeros followed by
+        # 5 ones followed by 5 twos etc.
+        # The process continues until num_insureds - 1
+        self.first_ind = np.arange(1, 1 + num_insureds, dtype=np.int32)\
+                           .reshape((num_insureds, 1))\
+                           .dot(np.ones((1, num_MultiStateDisabilityStates), dtype=np.int32))\
+                           .reshape(num_MultiStateDisabilityStates * num_insureds) - 1
+
+        # Secondly an index consisting of the sequence 0, 1, 2, 3, 4 (num_MultiStateDisabilityStates - 1) repeated for each insured
+        self.other_ind = np.ones((num_insureds, 1), dtype=np.int32)\
+                           .dot(np.arange(1, 1 + num_MultiStateDisabilityStates, dtype=np.int32)
+                                  .reshape((1, num_MultiStateDisabilityStates)))\
+                           .reshape(num_MultiStateDisabilityStates * num_insureds) - 1
+
+    def adjust_simple(self, A: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        """ Adjust the matrix A as follows:
+
+            Multiply the decrements by self.timestep to adjust for the fraction of the year. Then fill the diagonals such
+            that the target transition MultiStateDisabilityStates add up to 1 for each insured and starting-state.
+        """
+
+        A_per_step = np.nan_to_num(A) * self.timestep
+        row_sums = A_per_step.sum(axis=2)
+        A_per_step[self.first_ind, self.other_ind, self.other_ind] = 1 - row_sums.reshape(self.num_MultiStateDisabilityStates * self.num_insureds)
+        return A_per_step
+
+
+class AssumptionSetWrapper:
+    """ Wraps an assumption set for both the C and the PY engines."""
+
+    def __init__(self, dim: int) -> None:
+        self._dim = dim
+
+        self.be_transitions: dict[int, dict[int, BaseRatesProvider]] = {}
+        self.res_transitions: dict[int, dict[int, BaseRatesProvider]] = {}
+
+    def add_transition(self, be_or_res: AssumptionType, from_state: int, to_state: int, rates_provider: BaseRatesProvider) -> "AssumptionSetWrapper":
+        """ Add a new transition provider. """
+
+        assert int(from_state) >= 0 and int(from_state) < self._dim, "Unknown state: {}".format(from_state)
+        assert int(to_state) >= 0 and int(to_state) < self._dim, "Unknown state: {}".format(to_state)
+
+        if be_or_res == AssumptionType.BE:
+            transitions = self.be_transitions
+        elif be_or_res == AssumptionType.RES:
+            transitions = self.res_transitions
+        else:
+            raise Exception("Argument `be_or_res` must have value `BE` or `RES`, not {}".format(be_or_res))
+
+        this_trans = transitions.get(from_state)
+        if this_trans is None:
+            this_trans = {}
+            transitions[from_state] = this_trans
+
+        prov_old = this_trans.get(to_state)
+        if prov_old is not None:
+            logger.warn("Overwriting previously set transitions {}->{}".format(from_state, to_state))
+        this_trans[to_state] = rates_provider
+        return self
+
+    def _build_matrix(self,
+                      transitions: dict[int, dict[int, BaseRatesProvider]]) -> list[list[Optional[BaseRatesProvider]]]:
+        # generate a matrix of state transition rates providers
+        transition_provider_matrix = []
+        for i in range(self._dim):
+            new_row: list[Optional[BaseRatesProvider]] = []
+            transition_provider_matrix.append(new_row)
+            from_dict = transitions.get(i)
+            for j in range(self._dim):
+                if from_dict is None:
+                    new_row.append(None)
+                else:
+                    new_row.append(from_dict.get(j))
+
+        return transition_provider_matrix
+        # return Model(self.states_model, transition_provider_matrix)
+
+    def build_rates_provides_matrix(self, at: AssumptionType) -> list[list[Optional[BaseRatesProvider]]]:
+
+        if at == AssumptionType.BE:
+            return self._build_matrix(self.be_transitions)
+        elif at == AssumptionType.RES:
+            return self._build_matrix(self.res_transitions)
+        else:
+            raise Exception(f"Unknown assumption type: {at}")
+
+    def build_assumption_set(self, at: AssumptionType) -> actuarial.AssumptionSet:
+
+        if at == AssumptionType.BE:
+            transitions = self.be_transitions
+        elif at == AssumptionType.RES:
+            transitions = self.res_transitions
+        else:
+            raise Exception(f"Unknown assumption type: {at}")
+
+        acs = actuarial.AssumptionSet(self._dim)
+
+        for from_state, prvdrs_dict in transitions.items():
+            if prvdrs_dict is not None:
+                for to_state, provider in prvdrs_dict.items():
+                    if isinstance(provider, actuarial.ConstantRateProvider):
+                        acs.add_provider_const(from_state, to_state, provider)
+                    elif isinstance(provider, actuarial.StandardRateProvider):
+                        acs.add_provider_std(from_state, to_state, provider)
+        return acs
