f2c68373d944be46b13812a44ceef1b4b54ab744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:06 +0000

    Add package snapmlengine/0.0.2.12/snapmlengine-0.0.2.12.tar.gz

---
+++ packages/snapmlengine/snapmlengine-0.0.2.12.tar.gz/snapmlengine-0.0.2.12/snapmlengine/main.py
@@ -0,0 +1,6 @@
+## like a test file
+## will not be used
+from ml.linear_regression import linear_regression_analysis, create_graphs
+
+ans = linear_regression_analysis('data.xlsx', ['AT', 'V', 'AP', 'RH'], ['PE'])
+g_data = create_graphs(ans)
