996787cb969ae5a79852687b7d9c7b3208a27ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:33 +0000

    Add package pysimulation/1.8/pysimulation-1.8.tar.gz

---
+++ packages/pysimulation/pysimulation-1.8.tar.gz/pysimulation-1.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysimulation',
+    version='1.8',
+    author='rakson',
+    license='MIT',
+    py_modules=['pysimulation'],
+    install_requires=['pandas', 'pytz'],
+)
