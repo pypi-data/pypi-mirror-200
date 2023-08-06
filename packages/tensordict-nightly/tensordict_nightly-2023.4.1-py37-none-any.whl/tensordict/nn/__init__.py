5622bbe76ab54ed633f687b8e47f7958183b9f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:53 +0000

    Add package tensordict-nightly/2023.4.1/tensordict_nightly-2023.4.1-py37-none-any.whl

---
+++ packages/tensordict-nightly/tensordict_nightly-2023.4.1-py37-none-any.whl/tensordict/nn/__init__.py
@@ -0,0 +1,41 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from tensordict.nn.common import (
+    dispatch,
+    make_tensordict,
+    TensorDictModule,
+    TensorDictModuleWrapper,
+)
+from tensordict.nn.functional_modules import (
+    get_functional,
+    is_functional,
+    make_functional,
+    repopulate_module,
+)
+from tensordict.nn.probabilistic import (
+    ProbabilisticTensorDictModule,
+    ProbabilisticTensorDictSequential,
+    set_interaction_mode,
+)
+from tensordict.nn.sequence import TensorDictSequential
+from tensordict.nn.utils import biased_softplus, inv_softplus
+
+__all__ = [
+    "dispatch",
+    "TensorDictModule",
+    "TensorDictModuleWrapper",
+    "get_functional",
+    "make_functional",
+    "repopulate_module",
+    "ProbabilisticTensorDictModule",
+    "ProbabilisticTensorDictSequential",
+    "set_interaction_mode",
+    "TensorDictSequential",
+    "make_tensordict",
+    "biased_softplus",
+    "inv_softplus",
+    "is_functional",
+]
