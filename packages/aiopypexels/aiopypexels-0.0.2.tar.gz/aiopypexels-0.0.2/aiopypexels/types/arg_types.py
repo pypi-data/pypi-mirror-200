a4bd0afb97c9add7eb7051de27a0ccd63276a44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:30 +0000

    Add package aiopypexels/0.0.2/aiopypexels-0.0.2.tar.gz

---
+++ packages/aiopypexels/aiopypexels-0.0.2.tar.gz/aiopypexels-0.0.2/aiopypexels/types/arg_types.py
@@ -0,0 +1,40 @@
+from typing import Literal
+
+ORIENTATION_TYPE = Literal["landscape", "portrait", "square"]
+
+SIZE_TYPE = Literal["large", "medium", "small"]
+
+QUALITY_TYPE = Literal[
+    "medium", "large2x", "large", "small", "portrait", "landscape", "tiny", "original"
+]
+
+LOCALE_TYPE = Literal[
+    "en-US",
+    "pt-BR",
+    "es-ES",
+    "ca-ES",
+    "de-DE",
+    "it-IT",
+    "fr-FR",
+    "sv-SE",
+    "id-ID",
+    "pl-PL",
+    "ja-JP",
+    "zh-TW",
+    "zh-CN",
+    "ko-KR",
+    "th-TH",
+    "nl-NL",
+    "hu-HU",
+    "vi-VN",
+    "cs-CZ",
+    "da-DK",
+    "fi-FI",
+    "uk-UA",
+    "el-GR",
+    "ro-RO",
+    "nb-NO",
+    "sk-SK",
+    "tr-TR",
+    "ru-RU",
+]
---
+++ packages/aiopypexels/aiopypexels-0.0.2.tar.gz/aiopypexels-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aiopypexels",  # This is the name of the package
+    version="0.0.2",  # The initial release version
+    author="ggindinson",  # Full name of the author
+    description="An asynchronous wrapper for Pexels API based on aiohttp, which additionally allows to download photos in any of avaliable resolutions.",
+    # Long description read from the the readme file
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # List of all python modules to be installed
+    packages=setuptools.find_packages(),
+    keywords=["pexels", "api", "photos"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  # Information to filter the project on PyPi website
+    python_requires=">=3.6",  # Minimum version requirement of the package
+    # Directory of the source code of the package
+    install_requires=[
+        "aiohttp",
+        "dacite",
+        "aiocfscrape",
+    ],  # Install other dependencies if any
+)
