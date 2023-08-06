18bbdefb425fad601fded11da2ab0be083ba2902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:20 +0000

    Add package resotocore/3.3.0/resotocore-3.3.0.tar.gz

---
+++ packages/resotocore/resotocore-3.3.0.tar.gz/resotocore-3.3.0/resotocore/ids.py
@@ -0,0 +1,8 @@
+from typing import NewType
+
+TaskDescriptorId = NewType("TaskDescriptorId", str)
+SubscriberId = NewType("SubscriberId", str)
+TaskId = NewType("TaskId", str)
+WorkerId = NewType("WorkerId", str)
+ConfigId = NewType("ConfigId", str)
+NodeId = NewType("NodeId", str)
