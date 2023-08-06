1a513543ce91641e8eaa21b5bd8a9e1e6a38f012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:39 +0000

    Add package malifpy-ffnn/0.0.2/malifpy_ffnn-0.0.2-py3-none-any.whl

---
+++ packages/malifpy-ffnn/malifpy_ffnn-0.0.2-py3-none-any.whl/malifpy_ffnn/layer.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import Callable, List
+import malifpy_ffnn.activation_function as act_fun
+import malifpy_ffnn.neuron as neuron
+
+
+class Layer:
+    def __init__(
+            self, 
+            activation_function: str,
+            totalNeurons: int,
+            weight_dim: List[int],
+            weight: List[float]
+            ) -> None:
+        self.neurons: List[neuron.Neuron] = []
+        self.activation_function = activation_function
+        self.total_neurons = totalNeurons
+        self.weight_dim = weight_dim
+        self.weight = weight
+
+    def get_activation_function(self) -> Callable[..., float]:
+        def act_func(f):
+            return act_fun.softMax(f, self.get_sum_of_weighted_sum())
+        return act_func
+
+    def get_sum_of_weighted_sum(self):
+        sum_ = 0
+        for neuron in self.neurons:
+            sum_ += neuron.get_weighted_sum()
+        return sum_
