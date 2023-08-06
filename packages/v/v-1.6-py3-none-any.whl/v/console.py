f45df44a97db62124749c673badc557c00325252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:43 +0000

    Add package v/1.6/v-1.6-py3-none-any.whl

---
+++ packages/v/v-1.6-py3-none-any.whl/v/console.py
@@ -0,0 +1,12 @@
+def pt(*text, end="", join="", flush=True):
+	print(*text, end=end, sep=join, flush=flush)
+
+def ptln(*text, join="", flush=True):
+	pt(*text, end="\n", join=join, flush=flush)
+
+def write(*text, end="", join="", flush=True):
+	pt(*text, end=end, join=join, flush=flush)
+
+def writeln(*text, join="", flush=True):
+	ptln(*text, join=join, flush=flush)
+
