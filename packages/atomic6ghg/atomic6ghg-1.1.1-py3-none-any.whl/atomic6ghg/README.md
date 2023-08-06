c67db46902fbb176c525bd45e5a8133f3faadde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:55 +0000

    Add package atomic6ghg/1.1.1/atomic6ghg-1.1.1-py3-none-any.whl

---
+++ packages/atomic6ghg/atomic6ghg-1.1.1-py3-none-any.whl/atomic6ghg/README.md
@@ -0,0 +1,18 @@
+# atomic6ghg
+The Atomic6 GHG package is a library to help Python developers speed development of tools
+for calculating estimates of CO2 equivalent greenhouse gas emissions.
+
+Install it from PyPI:
+
+`pip install atomic6ghg`
+
+## Docs
+[Read the Docs on GitHub](https://glasmor.github.io/atomic6ghg/)
+
+The `docs` branch of this repository hosts content for and tools supporting creating the static web-site hosted on GitHub.io Pages. Contributions for the documentation web-site must be PR'd against the `docs` branch.
+
+## Example Application
+Checkout the [online calculator](https://orion.predix.io/calculator). It uses all the features of `atomic6ghg`.
+
+## History
+`Glasmor/atomic6ghg` is a hard fork of [`atomic6-org/ghg`](https://github.com/atomic6-org/ghg) the host of 1.0.1 and 1.0.2 releases. `atomic6-org/ghg` is managed by staff at GE but is not being maintained.
---
+++ packages/atomic6ghg/atomic6ghg-1.1.1-py3-none-any.whl/atomic6ghg/formulas/__init__.py
@@ -0,0 +1,16 @@
+""" init """
+# pylint: disable=no-name-in-module
+from atomic6ghg.formulas.formula import Formula, null_replacer
+from atomic6ghg.formulas.stationary_combustion import StationaryCombustion
+from atomic6ghg.formulas.waste_gases import WasteGases
+from atomic6ghg.formulas.electricity import Electricity
+from atomic6ghg.formulas.purchased_offsets import PurchasedOffsets
+from atomic6ghg.formulas.steam import Steam
+from atomic6ghg.formulas.refrigeration_and_ac import RefrigerationAndAc
+from atomic6ghg.formulas.fire_suppression import FireSuppression
+from atomic6ghg.formulas.commuting import Commuting
+from atomic6ghg.formulas.business_travel import BusinessTravel
+from atomic6ghg.formulas.purchased_gases import PurchasedGases
+from atomic6ghg.formulas.product_transport import ProductTransport
+from atomic6ghg.formulas.waste import Waste
+from atomic6ghg.formulas.mobile_sources import MobileSources
