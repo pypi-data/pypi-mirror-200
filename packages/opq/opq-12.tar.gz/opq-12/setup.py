2d70b55edae8331d8e81db31565f658cbfc65b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:03 +0000

    Add package opq/12/opq-12.tar.gz

---
+++ packages/opq/opq-12.tar.gz/opq-12/setup.py
@@ -0,0 +1,33 @@
+# This file is placed in the Public Domain.
+
+
+"object programming quest"
+
+
+from setuptools import setup
+
+
+def read():
+    return open("README.rst", "r").read()
+
+
+setup(
+    name="opq",
+    version="12",
+    author="Bart Thate",
+    author_email="operbot100@gmail.com",
+    url="http://github.com/operbot/opq",
+    zip_safe=True,
+    description="object programming quest",
+    long_description=read(),
+    long_description_content_type="text/x-rst",
+    license="Public Domain",
+    packages=["opq", "opq.mod"],
+    scripts=["bin/opq"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: Public Domain",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+     ],
+)
---
+++ packages/opq/opq-12.tar.gz/opq-12/test/test_decoder.py
@@ -0,0 +1,17 @@
+# This file is placed in the Public Domain.
+
+
+import unittest
+
+
+from opq.objects import Object, dumps, loads
+
+
+class TestDecoder(unittest.TestCase):
+
+    def test_loads(self):
+        obj = Object()
+        obj.test = "bla"
+        oobj = loads(dumps(obj))
+        self.assertEqual(oobj.test, "bla")
+
