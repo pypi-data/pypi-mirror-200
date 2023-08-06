819684c49ed0bfe58da588b3c4ef999a0c29e97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:14 +0000

    Add package arrow-odbc/0.3.12/arrow_odbc-0.3.12.tar.gz

---
+++ packages/arrow-odbc/arrow_odbc-0.3.12.tar.gz/arrow_odbc-0.3.12/python/arrow_odbc/writer.py
@@ -0,0 +1,122 @@
+from typing import Optional, Any
+
+from pyarrow.cffi import ffi as arrow_ffi
+from arrow_odbc.connect import connect_to_database
+
+from .arrow_odbc import ffi, lib  # type: ignore
+from .error import raise_on_error
+
+
+class BatchWriter:
+    """
+    Writes arrow batches to a database table.
+    """
+
+    def __init__(self, handle):
+        """
+        Low level constructor, users should rather invoke ``insert_into_table``
+        in order to create instances of ``BatchWriter``.
+        """
+
+        # We take ownership of the corresponding writer written in Rust and keep it alive until
+        # `self` is deleted
+        self.handle = handle
+
+    def __del__(self):
+        # Free the resources associated with this handle.
+        lib.arrow_odbc_writer_free(self.handle)
+
+    def write_batch(self, batch):
+        """
+        Fills the internal buffers of the writer with data from the batch. Every
+        time they are full, the data is send to the database. To make sure all
+        the data is is send ``flush`` must be called.
+        """
+        with arrow_ffi.new("struct ArrowArray*") as c_array, arrow_ffi.new(
+            "struct ArrowSchema*"
+        ) as c_schema:
+
+            # Get the references to the C Data structures
+            c_array_ptr = int(arrow_ffi.cast("uintptr_t", c_array))
+            c_schema_ptr = int(arrow_ffi.cast("uintptr_t", c_schema))
+
+            # Export the Array to the C Data structures.
+            batch._export_to_c(c_array_ptr)
+            batch.schema._export_to_c(c_schema_ptr)
+
+            error = lib.arrow_odbc_writer_write_batch(self.handle, c_array, c_schema)
+            raise_on_error(error)
+
+    def flush(self):
+        """
+        Inserts the remaining rows of the last chunk to the database.
+        """
+        error = lib.arrow_odbc_writer_flush(self.handle)
+        raise_on_error(error)
+
+
+def insert_into_table(
+    reader: Any,
+    chunk_size: int,
+    table: str,
+    connection_string: str,
+    user: Optional[str] = None,
+    password: Optional[str] = None,
+    login_timeout_sec: Optional[int] = None,
+):
+    """
+    Consume the batches in the reader and insert them into a table on the database.
+
+    :param reader: Reader is used to iterate over record batches. It must expose a `schema`
+        attribute, referencing an Arrow schema. Each field in the schema must correspond to a
+        column in the table with identical name.
+    :param chunk_size: Number of records to insert in each roundtrip to the database. Independent of
+        batch size (i.e. number of rows in an individual record batch).
+    :param table: Name of a database table to insert into. Used to generate the insert statement for
+        the bulk writer.
+    :param connection_string: ODBC Connection string used to connect to the data source. To find a
+        connection string for your data source try https://www.connectionstrings.com/.
+    :param user: Allows for specifying the user seperatly from the connection string if it is not
+        already part of it. The value will eventually be escaped and attached to the connection
+        string as `UID`.
+    :param password: Allows for specifying the password seperatly from the connection string if it
+        is not already part of it. The value will eventually be escaped and attached to the
+        connection string as `PWD`.
+    :param login_timeout_sec: Number of seconds to wait for a login request to complete before
+        returning to the application. The default is driver-dependent. If ``0``, the timeout is
+        disabled and a connection attempt will wait indefinitely. If the specified timeout exceeds
+        the maximum login timeout in the data source, the driver substitutes that value and uses
+        that instead.
+    """
+    table_bytes = table.encode("utf-8")
+
+    # Allocate structures where we will export the Array data and the Array schema. They will be
+    # released when we exit the with block.
+    with arrow_ffi.new("struct ArrowSchema*") as c_schema:
+        # Get the references to the C Data structures.
+        c_schema_ptr = int(arrow_ffi.cast("uintptr_t", c_schema))
+
+        # Export the schema to the C Data structures.
+        reader.schema._export_to_c(c_schema_ptr)
+
+        connection = connect_to_database(
+            connection_string, user, password, login_timeout_sec
+        )
+
+        # Connecting to the database has been successful. Note that connection does not truly take
+        # ownership of the connection. If it runs out of scope (e.g. due to a raised exception) the
+        # connection would not be closed and its associated resources would not be freed. However
+        # `arrow_odbc_writer_make` will take ownership of connection. Even if it should fail the
+        # connection will be closed.
+
+        writer_out = ffi.new("ArrowOdbcWriter **")
+        error = lib.arrow_odbc_writer_make(
+            connection, table_bytes, len(table_bytes), chunk_size, c_schema, writer_out
+        )
+        raise_on_error(error)
+        writer = BatchWriter(writer_out[0])
+
+    # Write all batches in reader
+    for batch in reader:
+        writer.write_batch(batch)
+    writer.flush()
---
+++ packages/arrow-odbc/arrow_odbc-0.3.12.tar.gz/arrow_odbc-0.3.12/src/reader.rs
@@ -0,0 +1,180 @@
+use std::{
+    ffi::c_void,
+    ptr::swap,
+    os::raw::c_int,
+    ptr::{null_mut, NonNull},
+    slice, str,
+    sync::Arc,
+};
+
+use arrow::ffi::{ArrowArray, FFI_ArrowArray, FFI_ArrowSchema};
+use arrow_odbc::{
+    arrow::{
+        array::{Array, StructArray},
+        record_batch::RecordBatchReader,
+    },
+    odbc_api::{CursorImpl, StatementConnection},
+    BufferAllocationOptions, OdbcReader,
+};
+
+use crate::{parameter::ArrowOdbcParameter, try_, ArrowOdbcError, OdbcConnection};
+
+/// Opaque type holding all the state associated with an ODBC reader implementation in Rust. This
+/// type also has ownership of the ODBC Connection handle.
+pub struct ArrowOdbcReader(OdbcReader<CursorImpl<StatementConnection<'static>>>);
+
+/// Creates an Arrow ODBC reader instance.
+///
+/// Takes ownership of connection even in case of an error. `reader_out` is assigned a NULL pointer
+/// in case the query does not return a result set.
+///
+/// # Safety
+///
+/// * `connection` must point to a valid OdbcConnection. This function takes ownership of the
+///   connection, even in case of an error. So The connection must not be freed explicitly
+///   afterwards.
+/// * `query_buf` must point to a valid utf-8 string
+/// * `query_len` describes the len of `query_buf` in bytes.
+/// * `parameters` must contain only valid pointers. This function takes ownership of all of them
+///   independent if the function succeeds or not. Yet it does not take ownership of the array
+///   itself.
+/// * `parameters_len` number of elements in parameters.
+/// * `max_text_size` optional upper bound for the size of text columns. Use `0` to indicate that no
+///   uppper bound applies.
+/// * `max_binary_size` optional upper bound for the size of binary columns. Use `0` to indicate
+///   that no uppper bound applies.
+/// * `fallibale_allocations`: `TRUE` if allocations should return an error, `FALSE` if it is fine
+///   to abort the process. Enabling might have a performance overhead, so it might be desirable to
+///   disable it, if you know there is enough memory available.
+/// * `reader_out` in case of success this will point to an instance of `ArrowOdbcReader`.
+///   Ownership is transferred to the caller.
+#[no_mangle]
+pub unsafe extern "C" fn arrow_odbc_reader_make(
+    connection: NonNull<OdbcConnection>,
+    query_buf: *const u8,
+    query_len: usize,
+    batch_size: usize,
+    parameters: *const *mut ArrowOdbcParameter,
+    parameters_len: usize,
+    max_text_size: usize,
+    max_binary_size: usize,
+    fallibale_allocations: bool,
+    reader_out: *mut *mut ArrowOdbcReader,
+) -> *mut ArrowOdbcError {
+    let query = slice::from_raw_parts(query_buf, query_len);
+    let query = str::from_utf8(query).unwrap();
+
+    let connection = *Box::from_raw(connection.as_ptr());
+
+    let parameters = if parameters.is_null() {
+        Vec::new()
+    } else {
+        slice::from_raw_parts(parameters, parameters_len)
+            .iter()
+            .map(|&p| Box::from_raw(p).unwrap())
+            .collect()
+    };
+
+    let max_text_size = if max_text_size == 0 {
+        None
+    } else {
+        Some(max_text_size)
+    };
+
+    let max_binary_size = if max_binary_size == 0 {
+        None
+    } else {
+        Some(max_binary_size)
+    };
+
+    let buffer_allocation_options = BufferAllocationOptions {
+        max_text_size,
+        max_binary_size,
+        fallibale_allocations,
+    };
+
+    let maybe_cursor = try_!(connection.0.into_cursor(query, &parameters[..]));
+    if let Some(cursor) = maybe_cursor {
+        let reader = try_!(OdbcReader::with(
+            cursor,
+            batch_size,
+            None,
+            buffer_allocation_options
+        ));
+        *reader_out = Box::into_raw(Box::new(ArrowOdbcReader(reader)))
+    } else {
+        *reader_out = null_mut()
+    }
+    null_mut() // Ok(())
+}
+
+/// Frees the resources associated with an ArrowOdbcReader
+///
+/// # Safety
+///
+/// `reader` must point to a valid ArrowOdbcReader.
+#[no_mangle]
+pub unsafe extern "C" fn arrow_odbc_reader_free(reader: NonNull<ArrowOdbcReader>) {
+    drop(Box::from_raw(reader.as_ptr()));
+}
+
+/// # Safety
+///
+/// * `reader` must be valid non-null reader, allocated by [`arrow_odbc_reader_make`].
+/// * `array_out` and `schema_out` must both point to valid pointers, which themselves may be null.
+#[no_mangle]
+pub unsafe extern "C" fn arrow_odbc_reader_next(
+    mut reader: NonNull<ArrowOdbcReader>,
+    array: *mut c_void,
+    schema: *mut c_void,
+    has_next_out: *mut c_int,
+) -> *mut ArrowOdbcError {
+    let schema = schema as *mut FFI_ArrowSchema;
+    let array = array as *mut FFI_ArrowArray;
+
+    if let Some(result) = reader.as_mut().0.next() {
+        *array = FFI_ArrowArray::empty();
+        *schema = FFI_ArrowSchema::empty();
+
+        let batch = try_!(result);
+        let struct_array: StructArray = batch.into();
+        let arrow_array = try_!(ArrowArray::try_new(struct_array.data().clone()));
+
+        let (ffi_array_ptr, ffi_schema_ptr) = ArrowArray::into_raw(arrow_array);
+
+        // In order to avoid memory leaks we must convert both pointers returned by the `into_raw`
+        // method. So we must back to `Arc` again, so they are freed at the end of this function
+        // call in order to avoid memory leaks. Furthermore it is the callers responsibility to
+        // provide us with the FFI_Arrow* structures to fill, and the caller maintains ownership
+        // over them.
+
+        let mut arc_schema = Arc::from_raw(ffi_schema_ptr);
+        let source_schema = Arc::get_mut(&mut arc_schema).unwrap();
+        swap(schema, source_schema);
+
+        let mut arc_array = Arc::from_raw(ffi_array_ptr);
+        let source_array = Arc::get_mut(&mut arc_array).unwrap();
+        swap(array, source_array);
+
+        *has_next_out = 1;
+    } else {
+        *has_next_out = 0;
+    }
+    null_mut()
+}
+
+/// Retrieve the associated schema from a reader.
+#[no_mangle]
+pub unsafe extern "C" fn arrow_odbc_reader_schema(
+    mut reader: NonNull<ArrowOdbcReader>,
+    out_schema: *mut c_void,
+) -> *mut ArrowOdbcError {
+    let out_schema: *mut FFI_ArrowSchema = out_schema as *mut FFI_ArrowSchema;
+
+    let reader = &mut reader.as_mut().0;
+    let schema_ref = reader.schema();
+    let schema = &*schema_ref;
+    let schema_ffi = try_!(schema.try_into());
+    *out_schema = schema_ffi;
+    null_mut()
+}
