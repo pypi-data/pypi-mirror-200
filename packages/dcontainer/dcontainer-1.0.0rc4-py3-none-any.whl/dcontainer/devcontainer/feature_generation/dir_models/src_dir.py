b952fc3d29daec8b7431165253a7dddb49e936bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:16 +0000

    Add package dcontainer/1.0.0rc4/dcontainer-1.0.0rc4-py3-none-any.whl

---
+++ packages/dcontainer/dcontainer-1.0.0rc4-py3-none-any.whl/dcontainer/devcontainer/feature_generation/dir_models/src_dir.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from easyfs import Directory
+
+from dcontainer.devcontainer.feature_generation.file_models.devcontainer_feature_json import (
+    DevcontainerFeatureJson,
+)
+from dcontainer.devcontainer.feature_generation.file_models.install_sh import InstallSH
+from dcontainer.devcontainer.feature_generation.file_models.library_scripts_sh import (
+    LibraryScriptsSH,
+)
+from dcontainer.devcontainer.models.devcontainer_feature_definition import (
+    FeatureDefinition,
+)
+
+
+class SrcDir(Directory):
+    @classmethod
+    def from_definition_model(
+        cls, definition_model: FeatureDefinition, minilayer_version: Optional[str] = None
+    ) -> "Directory":
+        feature_id = definition_model.id
+
+        virtual_dir = {}
+
+        virtual_dir[f"{feature_id}/library_scripts.sh"] = LibraryScriptsSH(
+            minilayer_version=minilayer_version,
+        )
+
+        virtual_dir[f"{feature_id}/install.sh"] = InstallSH(
+            install_command=definition_model.install_command,
+            options=definition_model.options,
+            dependencies=definition_model.dependencies,
+        )
+        virtual_dir[
+            f"{feature_id}/devcontainer-feature.json"
+        ] = DevcontainerFeatureJson(definition_model)
+
+        return cls(dictionary=virtual_dir)
