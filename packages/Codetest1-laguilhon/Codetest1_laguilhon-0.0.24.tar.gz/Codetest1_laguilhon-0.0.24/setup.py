b3e8ac6e29fd0889f663955b8adb041ffae73368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:21 +0000

    Add package Codetest1-laguilhon/0.0.24/Codetest1_laguilhon-0.0.24.tar.gz

---
+++ packages/Codetest1-laguilhon/Codetest1_laguilhon-0.0.24.tar.gz/Codetest1_laguilhon-0.0.24/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Codetest1_laguilhon",
+    version="0.0.24",
+    author="laguilhon",
+    author_email="lea.aguilhon@gmail.com",
+    description="Test de package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    package={'': './src'},
+
+    include_package_data=True,
+    package_data={"data": ["IMG/*", "f_annexe/*"]
+    },
+
+    install_requires=[
+        "tk",
+        "Pillow",
+        "fpdf",
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "scikit-image",
+        "opencv-python-headless",
+        "tensorflow",
+        "keras",
+    ],
+    python_requires=">=3.7",
+)
