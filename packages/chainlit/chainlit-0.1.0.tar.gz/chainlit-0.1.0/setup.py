32b19a79004eaad325fb1602aca0d66d66acb807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:04 +0000

    Add package chainlit/0.1.0/chainlit-0.1.0.tar.gz

---
+++ packages/chainlit/chainlit-0.1.0.tar.gz/chainlit-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chainlit',
+    version='0.1.0',
+    description='TBD',
+    long_description='TBD',
+    author='',
+    author_email='',
+    url='',
+    packages=find_packages(),
+    classifiers=[],
+    keywords='',
+    install_requires=[],
+)
