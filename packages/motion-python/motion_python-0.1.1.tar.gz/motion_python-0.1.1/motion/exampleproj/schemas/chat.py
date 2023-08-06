8fa24d46631e2d65c456a04a8d00e213e93c101b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:16 +0000

    Add package motion-python/0.1.1/motion_python-0.1.1.tar.gz

---
+++ packages/motion-python/motion_python-0.1.1.tar.gz/motion_python-0.1.1/motion/exampleproj/schemas/chat.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import motion
+
+
+class QuerySource(motion.MEnum):
+    OFFLINE = "Offline"
+    ONLINE = "Online"
+
+
+class Chat(motion.Schema):
+    src: QuerySource
+    session_id: str
+    prompt: str
+    completion: str
+    feedback: bool
