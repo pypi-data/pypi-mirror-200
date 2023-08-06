b66361d82e97fbb7d1ad0b0701db90028951839c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:05 +0000

    Add package masterchicken/0.0.3/masterchicken-0.0.3.tar.gz

---
+++ packages/masterchicken/masterchicken-0.0.3.tar.gz/masterchicken-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path('README.md').parent
+long_description = (this_directory / "README.md").read_text() + '\n\n' + (this_directory / "CHANGELOG.txt").read_text()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: Unix',
+    'Operating System :: MacOS :: MacOS X',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+setup(
+    name='masterchicken',
+    version='0.0.3',
+    description='A basic calculator in "class mathcal:" and csv file reader',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://organicchicken.netlify.app/',
+    author='PythonChicken123',
+    author_email='wave6013@hotmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['chicken', 'laserchicken', 'netlify', 'calculator'],
+    packages=find_packages(),
+    install_requires=[]
+)
