70a1731e41e7bfb0235419ff0c872ab2e5028b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:00 +0000

    Add package ErCore/0.8/ErCore-0.8-py3-none-any.whl

---
+++ packages/ErCore/ErCore-0.8-py3-none-any.whl/ErCore/Core.py
@@ -0,0 +1,47 @@
+import os
+import time
+import platform
+
+
+class ECore:
+    def __init__(self):
+        self.t = 5
+        self.version = None
+        self.IS_WINDOWS = (platform.system() == 'Windows')
+        self.IS_LINUX = (platform.system() == 'Linux')
+        self.IS_MAC = (platform.system() == 'Darwin')
+
+    def info(self):
+        try:
+            with open('VERSION', 'r') as ver:
+                self.version = ver.read()
+            print(f'Autor: Eragod\nProject: ErCore\nVersion: {self.version}')
+        except Exception as e:
+            print(f'Error!\n{e}\nError!')
+
+    def wait(self, t):
+        self.t = t
+        time.sleep(self.t)
+
+    def start(self):
+        file = input('file name: ')
+        if self.IS_WINDOWS:
+            with open(f'run.bat', 'w') as runner:
+                runner.write(f'@echo off\npython {file}.py\npause')
+        if self.IS_LINUX:
+            with open(f'run.sh', 'w') as runner:
+                runner.write(f'#!/bin/bash\n\npython3 {file}.py')
+        if self.IS_MAC:
+            with open(f'run.sh', 'w') as runner:
+                runner.write(f'#!/bin/bash\n\npython3 {file}.py')
+
+
+def EDecor(num):
+    def EraDeCore(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(num):
+                func(*args, **kwargs)
+
+        return wrapper
+
+    return EraDeCore
