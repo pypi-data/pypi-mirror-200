9173594b79798b42d2ddf3bbbce980740e632356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:35 +0000

    Add package Codetest1-laguilhon/0.0.27/Codetest1_laguilhon-0.0.27.tar.gz

---
+++ packages/Codetest1-laguilhon/Codetest1_laguilhon-0.0.27.tar.gz/Codetest1_laguilhon-0.0.27/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Codetest1_laguilhon",
+    version="0.0.27",
+    author="laguilhon",
+    author_email="lea.aguilhon@gmail.com",
+    description="Test de package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    packages=["src/data/IMG","src/data/f_annexes","src/data/Code"],
+
+
+    install_requires=[
+        "tk",
+        "Pillow",
+        "fpdf",
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "scikit-image",
+        "opencv-python-headless",
+        "tensorflow",
+        "keras",
+    ],
+
+    setup_requires=['wheel'],
+
+    python_requires=">=3.7",
+)
---
+++ packages/Codetest1-laguilhon/Codetest1_laguilhon-0.0.27.tar.gz/Codetest1_laguilhon-0.0.27/src/data/Code/Main.py
@@ -0,0 +1,7 @@
+import A1_first_win as f
+
+#### CELEBA DATABASE
+#import image_celeba as imc
+
+
+f.first_win("C'est parti")
