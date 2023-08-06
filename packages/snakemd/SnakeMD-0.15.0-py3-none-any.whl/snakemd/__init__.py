241f6fe23bd42177dd7fa6d3be789be7af2a9b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:32 +0000

    Add package snakemd/0.15.0/SnakeMD-0.15.0-py3-none-any.whl

---
+++ packages/snakemd/SnakeMD-0.15.0-py3-none-any.whl/snakemd/__init__.py
@@ -0,0 +1,31 @@
+from .document import *
+
+def new_doc(name: str = None) -> Document:
+    """
+    Creates a new SnakeMD document. This is a convenience function
+    that allows you to create a new markdown document without having
+    to import the Document class. This is useful for anyone who
+    wants to take advantage of the procedural interface of SnakeMD.
+    For those looking for a bit more control, each element class
+    will need to be imported as needed.
+
+    .. code-block:: Python
+    
+        doc = snakemd.new_doc()
+
+    .. versionadded:: 0.9.0
+
+    :param str name: 
+        the file name of the document without the extension
+        
+        .. deprecated:: 0.13.0
+            parameter is now optional and will be removed in 1.0.0
+            
+    :return: a new Document object
+    """
+    if name:
+        warnings.warn(
+            "name has been deprecated as of 0.13.0", 
+            DeprecationWarning
+        )
+    return Document(name)
