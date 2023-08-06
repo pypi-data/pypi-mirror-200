6980f301acf02f6fa47864be3765e7dd1211500a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:39 +0000

    Add package modal-client/0.48.1727/modal_client-0.48.1727-py3-none-any.whl

---
+++ packages/modal-client/modal_client-0.48.1727-py3-none-any.whl/modal/aio.py
@@ -0,0 +1,31 @@
+# Copyright Modal Labs 2022
+"""**async interfaces for use with `asyncio` event loops**
+
+The async interfaces are mostly mirrors of the blocking ones, with the `Aio` or `aio_` prefixes.
+"""
+
+from .app import AioApp, aio_container_app
+from .dict import AioDict
+from .functions import AioFunction, AioFunctionHandle
+from .image import AioImage
+from .mount import AioMount
+from .object import aio_lookup
+from .queue import AioQueue
+from .secret import AioSecret
+from .shared_volume import AioSharedVolume
+from .stub import AioStub
+
+__all__ = [
+    "AioApp",
+    "AioDict",
+    "AioFunction",
+    "AioImage",
+    "AioMount",
+    "AioQueue",
+    "AioSecret",
+    "AioSharedVolume",
+    "AioStub",
+    "AioFunctionHandle",
+    "aio_container_app",
+    "aio_lookup",
+]
