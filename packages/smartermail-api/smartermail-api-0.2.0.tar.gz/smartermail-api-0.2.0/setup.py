ddd3a26287bc38744a10756fe7b935313434d60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:15 +0000

    Add package smartermail-api/0.2.0/smartermail-api-0.2.0.tar.gz

---
+++ packages/smartermail-api/smartermail-api-0.2.0.tar.gz/smartermail-api-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smartermail-api',
+    version='0.2.0',
+    author='Zachary Sylvester',
+    author_email='zacharysylvester81@gmail.com',
+    description='This is a simple python wrapper for the SmarterMail API.',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    url='https://github.com/zjs81/SmarterToolsPythonWrapper',
+)
