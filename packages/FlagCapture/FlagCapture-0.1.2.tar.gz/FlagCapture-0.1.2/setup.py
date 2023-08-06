bf0848ac0cec86d2ff8bf309ec6c03dd7a28fb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:47 +0000

    Add package FlagCapture/0.1.2/FlagCapture-0.1.2.tar.gz

---
+++ packages/FlagCapture/FlagCapture-0.1.2.tar.gz/FlagCapture-0.1.2/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='FlagCapture', packages=['FlagCapture'], zip_safe=True, version='0.1.2', author='Michael Caggeso', author_email='laurencaggeso@gmail.com', long_description="Better Graphics and shows your turbo", description='Capture The Flag Game', url="https://pypi.org/search/?q=FlagCapture&o=", download_url="https://pypi.org/search/?q=FlagCapture&o=", )
