812be75d1dd420ff1aa37ac4fb1a801c54bd294f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package inductiva/0.0.4/inductiva-0.0.4-py3-none-any.whl

---
+++ packages/inductiva/inductiva-0.0.4-py3-none-any.whl/inductiva/fluids/simulators/dualsphysics.py
@@ -0,0 +1,31 @@
+"""DualSPHysics module of the API."""
+import pathlib
+from typing import Literal, Optional
+
+from inductiva import types
+from inductiva.simulation import Simulator
+
+
+class DualSPHysics(Simulator):
+    """Class to invoke a generic DualSPHysics simulation on the API."""
+
+    @property
+    def api_method_name(self) -> str:
+        return "sph.dualsphysics.run_simulation"
+
+    def run(
+        self,
+        input_dir: types.Path,
+        sim_config_filename: str,
+        output_dir: Optional[types.Path] = None,
+        device: Literal["gpu", "cpu"] = "cpu",
+    ) -> pathlib.Path:
+        """Run the simulation.
+
+        Args:
+            device: Device in which to run the simulation.
+        """
+        return super().run(input_dir,
+                           output_dir,
+                           device=device,
+                           input_filename=sim_config_filename)
---
+++ packages/inductiva/inductiva-0.0.4-py3-none-any.whl/inductiva/fluids/simulators/xbeach.py
@@ -0,0 +1,27 @@
+"""DualSPHysics module of the API."""
+import pathlib
+from typing import Optional
+
+from inductiva import types
+from inductiva.simulation import Simulator
+
+
+class XBeach(Simulator):
+    """Class to invoke a generic XBeach simulation on the API."""
+
+    @property
+    def api_method_name(self) -> str:
+        return "sw.xbeach.run_simulation"
+
+    def run(
+        self,
+        input_dir: types.Path,
+        output_dir: Optional[types.Path] = None,
+        n_cores: int = 1,
+    ) -> pathlib.Path:
+        """Run the simulation.
+
+        Args:
+            n_cores: Number of MPI cores to use for the simulation.
+        """
+        return super().run(input_dir, output_dir=output_dir, n_cores=n_cores)
