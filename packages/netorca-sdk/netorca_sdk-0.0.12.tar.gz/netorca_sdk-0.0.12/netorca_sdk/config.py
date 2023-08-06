d94a1b77137fa4a22f3a1398bfeebae085a21342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:55 +0000

    Add package netorca-sdk/0.0.12/netorca_sdk-0.0.12.tar.gz

---
+++ packages/netorca-sdk/netorca_sdk-0.0.12.tar.gz/netorca_sdk-0.0.12/netorca_sdk/config.py
@@ -0,0 +1,9 @@
+AUTH_ENDPOINT = "/api-token-auth/"
+TEAM_ENDPOINT = "/account/teams/"
+VALIDATE_CONSUMER_SUBMISSION_ENDPOINT = "/orcabase/consumer/submissions/validate/"
+SUBMIT_CONSUMER_SUBMISSION_ENDPOINT = "/orcabase/consumer/submissions/submit/"
+VALIDATE_SERVICEOWNER_SUBMISSION_ENDPOINT = "/orcabase/serviceowner/services/validate/"
+SUBMIT_SERVICEOWNER_SUBMISSION_ENDPOINT = "/orcabase/serviceowner/services/"
+SUBMIT_SERVICEOWNER_SUBMISSION_DOCS_ENDPOINT = "/orcabase/serviceowner/services/$uuid/docs/"
+
+RETRY_TIMES = 1  # number of times request will be retried
