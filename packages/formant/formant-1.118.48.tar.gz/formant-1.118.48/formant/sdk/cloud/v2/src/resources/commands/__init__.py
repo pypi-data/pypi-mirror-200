27f00b0d543ff272e9844a2fd27667a6b59e7517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:39 +0000

    Add package formant/1.118.48/formant-1.118.48.tar.gz

---
+++ packages/formant/formant-1.118.48.tar.gz/formant-1.118.48/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandTemplateParameterMeta, Command, CommandResponseTags, CommandResponseStreamType, CommandRequest, CommandDeliveryEventTags, CommandQuery, CommandDeliveryEventType, CommandParameterMeta, CommandTags, CommandTemplateListResponse, CommandStreamType, CommandResponse, CommandParameter, FileInfo, CommandDeliveryEvent, CommandTemplateTags, CommandRequestListResponse, CommandProgress, CommandTemplate, CommandDeliveryEventStreamType, CommandListResponse
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
