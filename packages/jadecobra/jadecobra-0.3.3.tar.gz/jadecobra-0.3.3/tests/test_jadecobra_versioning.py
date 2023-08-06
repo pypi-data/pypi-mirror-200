116fc42e6a4fcd5ed4b3c2892683ae3d5a227892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:36 +0000

    Add package jadecobra/0.3.3/jadecobra-0.3.3.tar.gz

---
+++ packages/jadecobra/jadecobra-0.3.3.tar.gz/jadecobra-0.3.3/tests/test_jadecobra_versioning.py
@@ -0,0 +1,72 @@
+import src.jadecobra.tester
+import src.jadecobra.versioning
+
+
+class TestJadeCobraVersioning(src.jadecobra.tester.TestCase):
+
+    def test_version_attributes(self):
+        self.assert_attributes_equal(
+            src.jadecobra.versioning,
+            [
+                'Version',
+                '__builtins__',
+                '__cached__',
+                '__doc__',
+                '__file__',
+                '__loader__',
+                '__name__',
+                '__package__',
+                '__spec__',
+                'os',
+                're',
+                'toolkit',
+            ]
+        )
+
+    def test_version_class_attributes(self):
+        self.assert_attributes_equal(
+            src.jadecobra.versioning.Version,
+            [
+                '__class__',
+                '__delattr__',
+                '__dict__',
+                '__dir__',
+                '__doc__',
+                '__eq__',
+                '__format__',
+                '__ge__',
+                '__getattribute__',
+                '__getstate__',
+                '__gt__',
+                '__hash__',
+                '__init__',
+                '__init_subclass__',
+                '__le__',
+                '__lt__',
+                '__module__',
+                '__ne__',
+                '__new__',
+                '__reduce__',
+                '__reduce_ex__',
+                '__repr__',
+                '__setattr__',
+                '__sizeof__',
+                '__str__',
+                '__subclasshook__',
+                '__weakref__',
+                'get_pyproject_version',
+                'git_push',
+                'pyproject',
+                'read_pyproject',
+                'semantic_version_pattern',
+                'update_module_version',
+                'update_pyproject_version'
+            ]
+        )
+
+    def test_update_version(self):
+        version = src.jadecobra.versioning.Version()
+        self.assertEqual(
+            version.get_pyproject_version(),
+            ('0.3.', '2'),
+        )
---
+++ packages/jadecobra/jadecobra-0.3.3.tar.gz/jadecobra-0.3.3/tests/test_z_build_publish.py
@@ -0,0 +1,15 @@
+import src.jadecobra.tester
+import os
+
+class TestZBuildDeploy(src.jadecobra.tester.TestCase):
+
+    def test_z_build_and_publish(self):
+        self.build_and_publish()
+
+    def test_z_published_version_is_test_version(self):
+        os.system('pip install jadecobra')
+        import jadecobra
+        self.assertEqual(
+            jadecobra.__version__,
+            src.jadecobra.__version__
+        )
