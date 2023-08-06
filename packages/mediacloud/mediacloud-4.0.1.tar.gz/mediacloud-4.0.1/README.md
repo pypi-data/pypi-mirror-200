29d9f92e45e36703fd644cdb1dc9b50399e832f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:24 +0000

    Add package mediacloud/4.0.1/mediacloud-4.0.1.tar.gz

---
+++ packages/mediacloud/mediacloud-4.0.1.tar.gz/mediacloud-4.0.1/README.md
@@ -0,0 +1,48 @@
+MediaCloud Python API Client
+============================
+
+🚧 Under construction 🚧  
+
+This is a python client for accessing the MediaCloud API v4. This allows you to perform cross-platform searches and 
+also browse our collection/source/feed directory.
+
+![pylint](https://github.com/mediacloud/api-client/actions/workflows/pylint.yml/badge.svg) ![pytest](https://github.com/mediacloud/api-client/actions/workflows/pytest.yml/badge.svg) [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/mitmedialab/MediaCloud-API-Client/blob/master/LICENSE)
+
+Usage
+-----
+
+First [sign up for an API key](https://search.mediacloud.org/).  Then
+```
+pip install mediacloud
+```
+
+Check `CHANGELOG.md` for a detailed history of changes.
+
+### Examples
+
+Take a look at the test in the `mediacloud/test/` module for more detailed examples.
+
+Development
+-----------
+
+If you are interested in adding code to this module, first clone [the GitHub repository](https://github.com/c4fcm/MediaCloud-API-Client).
+
+### Installing
+
+`make install`
+
+### Testing
+
+`make test`
+
+### Distributing a New Version
+
+If you want to, setup [twin's keyring integration](https://pypi.org/project/twine/) to avoid typing your PyPI
+password over and over. 
+
+1. Run `make test` to make sure all the test pass
+2. Update the version number in `mediacloud/__init__.py`
+3. Make a brief note in the CHANGELOG.md about what changes
+4. Run `make build-release` to create an install package
+5. Run `make release-test` to upload it to PyPI's test platform
+6. Run `make release` to upload it to PyPI
---
+++ packages/mediacloud/mediacloud-4.0.1.tar.gz/mediacloud-4.0.1/mediacloud/api.py
@@ -0,0 +1,105 @@
+import logging
+import datetime as dt
+from typing import Dict, Optional, Union
+import requests
+import mediacloud
+import mediacloud.error
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseApi:
+
+    # Default applied to all queries made to main server. You can alter this on
+    # your instance if you want to bail out more quickly, or know you have longer
+    # running queries
+    TIMEOUT_SECS = 30
+
+    BASE_API_URL = "https://search.mediacloud.org/api/"
+
+    def __init__(self, auth_token=str):
+        if not auth_token:
+            raise mediacloud.error.MCException("No api key set - nothing will work without this")
+        # Specify the auth_token to use for all future requests
+        self._auth_token = auth_token
+        # better performance to put all HTTP through this one object
+        self._session = requests.Session()
+        self._session.headers.update({'Authorization': f'Token {self._auth_token}'})
+
+    def user_profile(self) -> Dict:
+        # :return: basic info about the current user, including their roles
+        return self._query('auth/profile')
+
+    def version(self) -> Dict:
+        """
+        returns dict with (at least):
+        GIT_REV, now (float epoch time), version
+        """
+        return self._query('version')
+
+    def _query(self, endpoint: str, params: Dict = None, method: str = 'GET'):
+        """
+        Centralize making the actual queries here for easy maintenance and testing of HTTP comms
+        """
+        endpoint_url = self.BASE_API_URL + endpoint
+        if method == 'GET':
+            r = self._session.get(endpoint_url, params=params, timeout=self.TIMEOUT_SECS)
+        elif method == 'POST':
+            r = self._session.post(endpoint_url, json=params, timeout=self.TIMEOUT_SECS)
+        else:
+            raise RuntimeError(f"Unsupported method of '{method}'")
+        if r.status_code != 200:
+            raise RuntimeError(f"API Server Error {r.status_code}. Params: {params}")
+        return r.json()
+
+
+class DirectoryApi(BaseApi):
+
+    PLATFORM_ONLINE_NEWS = "online_news"
+    PLATFORM_YOUTUBE = "youtube"
+    PLATFORM_TWITTER = "twitter"
+    PLATFORM_REDDIT = "reddit"
+
+    def collection_list(self, platform: Optional[str] = None, name: Optional[str] = None,
+                        limit: Optional[int] = 0, offset: Optional[int] = 0):
+        params = dict(limit=limit, offset=offset)
+        if name:
+            params['name'] = name
+        if platform:
+            params['platform'] = platform
+        return self._query('sources/collections/', params)
+
+    def source_list(self, platform: Optional[str] = None, name: Optional[str] = None,
+                    collection_id: Optional[int] = None,
+                    limit: Optional[int] = 0, offset: Optional[int] = 0):
+        params = dict(limit=limit, offset=offset)
+        if collection_id:
+            params['collection_id'] = collection_id
+        if name:
+            params['name'] = name
+        if platform:
+            params['platform'] = platform
+        return self._query('sources/sources/', params)
+
+    def feed_list(self, source_id: Optional[int] = None, modified_since: Optional[Union[dt.datetime, int, float]] = None,
+                  modified_before: Optional[Union[dt.datetime, int, float]] = None,
+                  limit: Optional[int] = 0, offset: Optional[int] = 0):
+        params = dict(limit=limit, offset=offset)
+        if source_id:
+            params['source_id'] = source_id
+
+        def epoch_param(t, param):
+            if t is None:
+                return        # parameter not set
+            if isinstance(t, dt.datetime):
+                params[param] = t.timestamp() # get epoch time
+            elif isinstance(t, (int, float)):
+                params[param] = t
+            else:
+                raise ValueError(param)
+
+        epoch_param(modified_since, 'modified_since')
+        epoch_param(modified_before, 'modified_before')
+
+        return self._query('sources/feeds/', params)
