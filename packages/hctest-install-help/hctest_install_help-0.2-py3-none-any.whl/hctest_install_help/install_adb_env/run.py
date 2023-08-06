18e0deda8c4b4e26b6203495714231dc57398e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:21 +0000

    Add package hctest-install-help/0.2/hctest_install_help-0.2-py3-none-any.whl

---
+++ packages/hctest-install-help/hctest_install_help-0.2-py3-none-any.whl/hctest_install_help/install_adb_env/run.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# author: 华测-长风老师
+# file name：run.py
+
+import ctypes
+import sys
+
+
+def run_cmd_as_admin(cmd):
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", "cmd.exe", "/k {}".format(cmd), None, 1)
+    else:
+        import subprocess
+        subprocess.Popen(cmd, shell=True)
+
+
+if sys.platform == "win32":
+    env_file = str(__file__).replace(f"run.py", "adb_env.py")
+    run_cmd_as_admin(f"{sys.executable} {env_file}")
+
+else:
+
+    env_file = str(__file__).replace(f"run.py", "adb_env.py")
+    import os
+
+    sudo_password = input("请输入你的系统管理员的账户密码：")
+    os.system(f'echo "{sudo_password}" | sudo -S {sys.executable} {env_file}')
+
