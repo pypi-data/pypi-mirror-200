1f7013aba1e8e4882093c00dd83a9d38eb1f58f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:19 +0000

    Add package testKuldeep/0.0.4/testKuldeep-0.0.4-py3-none-any.whl

---
+++ packages/testKuldeep/testKuldeep-0.0.4-py3-none-any.whl/testKuldeep.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+def get_dbutils(spark):
+	try:
+		from pyspark.dbutils import DBUtils
+		dbutils = DBUtils(spark)
+	except ImportError:
+		import IPython
+		dbutils = IPython.get_ipython().user_ns["dbutils"]
+	return dbutils
+
+dbutils = get_dbutils(spark)
+		
+def test(path):
+	dbutils.notebook.run(path,1200)
