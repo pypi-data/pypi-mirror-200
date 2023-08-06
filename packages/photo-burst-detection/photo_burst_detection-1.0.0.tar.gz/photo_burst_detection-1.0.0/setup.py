63008fe5e0006afee94ba1bb30c485195c73794b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:32 +0000

    Add package photo-burst-detection/1.0.0/photo_burst_detection-1.0.0.tar.gz

---
+++ packages/photo-burst-detection/photo_burst_detection-1.0.0.tar.gz/photo_burst_detection-1.0.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='photo_burst_detection',
+    version='1.0.0',
+    author='batou9150',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask-login',
+        'flask-ldap3-login',
+    ],
+    description='Flask App for photo burst detection',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/batou9150/photo_burst_detection',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
