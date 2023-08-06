19947bfebe49f228908a2cf6b1d41d23cce0199b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:27 +0000

    Add package plynx/1.10.0/plynx-1.10.0-py3-none-any.whl

---
+++ packages/plynx/plynx-1.10.0-py3-none-any.whl/plynx/constants/parameter_types.py
@@ -0,0 +1,15 @@
+"""Parameter enums"""
+
+
+class ParameterTypes:
+    """Standard parameter types"""
+    STR: str = 'str'
+    INT: str = 'int'
+    FLOAT: str = 'float'
+    BOOL: str = 'bool'
+    TEXT: str = 'text'
+    ENUM: str = 'enum'
+    LIST_STR: str = 'list_str'
+    LIST_INT: str = 'list_int'
+    LIST_NODE: str = 'list_node'
+    CODE: str = 'code'
