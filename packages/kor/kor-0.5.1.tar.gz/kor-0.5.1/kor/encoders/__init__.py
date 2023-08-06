3bc7ca7ead4bc1085e9473dfa61f2766962eaeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:07 +0000

    Add package kor/0.5.1/kor-0.5.1.tar.gz

---
+++ packages/kor/kor-0.5.1.tar.gz/kor-0.5.1/kor/encoders/__init__.py
@@ -0,0 +1,21 @@
+"""Declare public interface for encoders.
+
+An encoder follows the `Encoder` interface.
+
+It can encode, decode and contains instructions about the encoding format for an LLM.
+"""
+from .csv_data import CSVEncoder
+from .encode import InputFormatter, encode_examples, initialize_encoder
+from .json_data import JSONEncoder
+from .typedefs import Encoder
+from .xml import XMLEncoder
+
+__all__ = [
+    "Encoder",
+    "XMLEncoder",
+    "CSVEncoder",
+    "JSONEncoder",
+    "InputFormatter",
+    "encode_examples",
+    "initialize_encoder",
+]
