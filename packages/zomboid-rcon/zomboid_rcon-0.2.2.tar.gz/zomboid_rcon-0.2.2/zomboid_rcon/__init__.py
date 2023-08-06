9730439a0def1758f300435a2e9dbee273b148d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:21 +0000

    Add package zomboid-rcon/0.2.2/zomboid_rcon-0.2.2.tar.gz

---
+++ packages/zomboid-rcon/zomboid_rcon-0.2.2.tar.gz/zomboid_rcon-0.2.2/zomboid_rcon/__init__.py
@@ -0,0 +1,11 @@
+"""
+Zomboid RCON.
+    :copyright: (c) 2023 by JW: https://jackwhitworth.com
+    :license: GPL-3.0, see LICENSE for more details.
+"""
+
+
+from .zomboid_rcon  import ZomboidRCON
+
+__title__ = 'zomboid_rcon'
+__version__ = '0.2.2'
