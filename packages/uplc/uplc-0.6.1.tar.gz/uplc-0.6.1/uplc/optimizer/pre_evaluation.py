bbd1510c34dbf2a175f96ef98bd469ce1f9161bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:39 +0000

    Add package uplc/0.6.1/uplc-0.6.1.tar.gz

---
+++ packages/uplc/uplc-0.6.1.tar.gz/uplc-0.6.1/uplc/optimizer/pre_evaluation.py
@@ -0,0 +1,22 @@
+from copy import copy
+
+from ..util import NodeTransformer
+from ..ast import Program, AST
+from ..machine import Machine
+
+"""
+Optimizes code by pre-evaluating each subterm
+If it throws an error, assume it is not safe to pre-evaluate and don't replace, otherwise replace by result.
+"""
+
+
+class PreEvaluationOptimizer(NodeTransformer):
+    def visit_Program(self, node: Program) -> Program:
+        return Program(version=node.version, term=self.visit(node.term))
+
+    def generic_visit(self, node: AST) -> AST:
+        try:
+            nc = Machine(node).eval()
+        except Exception as e:
+            nc = node
+        return super().generic_visit(nc)
