ee2ec60caf2de66778f94e254176932d6108ee18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:05 +0000

    Add package cappr/0.1.0/cappr-0.1.0-py3-none-any.whl

---
+++ packages/cappr/cappr-0.1.0-py3-none-any.whl/cappr/huggingface/__init__.py
@@ -0,0 +1,7 @@
+"""
+Note that you need to install the HuggingFace requirements to use this module::
+
+    python -m pip install "cappr[hf] @ git+https://github.com/kddubey/cappr.git"
+
+"""
+from . import _utils, classify, _classify_slow
