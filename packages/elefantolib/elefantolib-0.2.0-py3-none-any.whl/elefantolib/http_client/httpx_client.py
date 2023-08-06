aaf000a3038c65d5a06fa40ff5aa57c9b7992dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:23 +0000

    Add package elefantolib/0.2.0/elefantolib-0.2.0-py3-none-any.whl

---
+++ packages/elefantolib/elefantolib-0.2.0-py3-none-any.whl/elefantolib/http_client/httpx_client.py
@@ -0,0 +1,45 @@
+import logging
+from functools import partial
+
+import httpx
+
+from . import BaseClient
+
+
+logger = logging.getLogger(__name__)
+
+
+class HttpxClient(BaseClient):
+
+    def __getattr__(self, item):
+        return partial(self._request, method=item)
+
+    def _request(self, method: str, path: str, raises: bool = False, **kwargs):
+        with httpx.Client(headers=self.headers) as client:
+            kwargs['headers'] = {**self.headers, **kwargs.get('headers', {})}
+
+            try:
+                resp = getattr(client, method)(f'{self.api_url}/{path}', **kwargs)
+
+                return (resp, None) if not raises else resp
+            except Exception as e:
+                logger.error(e)
+                return (None, e) if not raises else None
+
+
+class AsyncHttpxClient(BaseClient):
+
+    def __getattr__(self, item):
+        return partial(self._request, method=item)
+
+    async def _request(self, method: str, path: str, raises: bool = False, **kwargs):
+        async with httpx.AsyncClient(headers=self.headers) as client:
+            kwargs['headers'] = {**self.headers, **kwargs.get('headers', {})}
+
+            try:
+                resp = await getattr(client, method)(f'{self.api_url}/{path}', **kwargs)
+
+                return (resp, None) if not raises else resp
+            except BaseException as e:
+                logger.error(e)
+                return (None, e) if not raises else None
