38ae78d4e20f91bc503481ec95a403966d12a667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:11 +0000

    Add package apache-airflow-providers-sktvane/0.0.34/apache-airflow-providers-sktvane-0.0.34.tar.gz

---
+++ packages/apache-airflow-providers-sktvane/apache-airflow-providers-sktvane-0.0.34.tar.gz/apache-airflow-providers-sktvane-0.0.34/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apache-airflow-providers-sktvane",
+    version="0.0.34",
+    author="aidp",
+    author_email="aidp@sktai.io",
+    description="apache-airflow-providers-sktvane",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["airflow", "sktvane"],
+    license="MIT",
+    url="https://github.com/sktaiflow/sktvane-airflow-providers",
+    packages=setuptools.find_namespace_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
