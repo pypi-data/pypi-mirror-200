e2e8369b6b2c28a32b1c48af0e5890415c40c407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:14 +0000

    Add package HMSFire/0.3.8/HMSFire-0.3.8.tar.gz

---
+++ packages/HMSFire/HMSFire-0.3.8.tar.gz/HMSFire-0.3.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='HMSFire',
+    version='0.3.8',
+    license='MIT',
+    author="Joel",
+    author_email='joel.chacon@cimat.mx',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://ospo-hms-fire.readthedocs.io/en/latest/',
+    keywords='HMSFirepy project',
+    summary='test description',
+    package_data={'HMSFire': ['src/OSPO/HMSFire/*csv']},
+    include_package_data=True,
+    install_requires=[
+          'basemap',
+          'pandas',
+          'matplotlib',
+          'requests',
+          'datetime',
+      ],
+
+)
