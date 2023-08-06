20ea605b4104dc0f637097f9166e5d56248f7c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:27 +0000

    Add package eigenrules/0.1.9/eigenrules-0.1.9.tar.gz

---
+++ packages/eigenrules/eigenrules-0.1.9.tar.gz/eigenrules-0.1.9/eigenrules/__init__.py
@@ -0,0 +1,4 @@
+from .eigendata import RulesEngine
+from .schemas import FeatureImportance, Prediction, Rules
+
+__all__ = ["FeatureImportance", "RulesEngine", "Rules", "Prediction"]
---
+++ packages/eigenrules/eigenrules-0.1.9.tar.gz/eigenrules-0.1.9/eigenrules/exceptions.py
@@ -0,0 +1,6 @@
+def raise_not_implemented():
+    raise NotImplementedError("This function is not implemented")
+
+
+def raise_missing_argument():
+    raise ValueError("Must pass either `data_path` path or `data` arguments.")
