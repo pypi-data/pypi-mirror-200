acb98a8323b456fb278f25059ded67aa5b8879e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:33 +0000

    Add package pycryptoguard/0.3.4/pycryptoguard-0.3.4.tar.gz

---
+++ packages/pycryptoguard/pycryptoguard-0.3.4.tar.gz/pycryptoguard-0.3.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pycryptoguard',
+    version='0.3.4',
+    description='A library for encrypting files with AES',
+    author='INeddHelp',
+    author_email='ineddhelpgithub@gmail.com',
+    packages=['pycryptoguard'],
+    install_requires=['pycryptodome'],
+)
