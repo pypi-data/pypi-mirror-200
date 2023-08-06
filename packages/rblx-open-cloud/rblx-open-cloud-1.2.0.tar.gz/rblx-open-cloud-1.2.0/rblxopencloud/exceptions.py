5a4501f4f66dba2b65dd41ba5654a8c7c1aa8c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:18 +0000

    Add package rblx-open-cloud/1.2.0/rblx-open-cloud-1.2.0.tar.gz

---
+++ packages/rblx-open-cloud/rblx-open-cloud-1.2.0.tar.gz/rblx-open-cloud-1.2.0/rblxopencloud/exceptions.py
@@ -0,0 +1,26 @@
+from typing import Union, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .datastore import EntryInfo
+
+__all__ = (
+    "rblx_opencloudException",
+    "NotFound",
+    "InvalidKey",
+    "RateLimited",
+    "ServiceUnavailable",
+    "PreconditionFailed",
+    "InvalidAsset",
+)
+
+class rblx_opencloudException(Exception): pass
+class NotFound(rblx_opencloudException): pass
+class InvalidKey(rblx_opencloudException): pass
+class RateLimited(rblx_opencloudException): pass
+class ServiceUnavailable(rblx_opencloudException): pass
+class PreconditionFailed(rblx_opencloudException):
+    def __init__(self, value, info, *args: object) -> None:
+        self.value: Optional[Union[str, dict, list, int, float]] = value
+        self.info: Optional[EntryInfo] = info
+        super().__init__(*args)
+class InvalidAsset(rblx_opencloudException): pass
