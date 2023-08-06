3d9ddb98a08563f7f8ecb8ae241d9794918547e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:32 +0000

    Add package pypcs/0.0.1/pypcs-0.0.1.tar.gz

---
+++ packages/pypcs/pypcs-0.0.1.tar.gz/pypcs-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+VERSION = 1
+
+
+setuptools.setup(
+    name="pypcs",
+    version='0.0.' + str(VERSION),
+    author="Jason Lee",
+    author_email="2593292614@qq.com",
+    description="",
+    long_description=open('README.md', 'r', encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com",
+    install_requires=[
+        'numpy==1.24.2',
+        'pygame==2.3.0'
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+)
