ed5ff910ff4b128c0f0ba004b818ea70a2e0d154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:29 +0000

    Add package Pratik-model/0.0.1/Pratik_model.tar.gz

---
+++ packages/Pratik-model/Pratik_model.tar.gz/Pratik_model-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+classifiers = [  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python'
+  ]
+
+
+setup(
+    name='Pratik_model',
+    version='0.0.1',
+    description='This package directly gives you output performance on different models',
+    url='',
+    license='MIT',
+    long_description_content_type =open('README.md').read() + '\n\n' + open('ChangeLog.txt').read(),
+    author='pratik',
+    classifiers=classifiers,
+    author_email='pratikvdatey@gmail.com',
+    keywords='Pratik_model',
+    packages=find_packages(),
+    install_requires=['sklearn','lightgbm','catboost','xgboost'],
+    Source = "https://github.com/pratikdatey/Pratik_model"
+   
+)
