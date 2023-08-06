1f73c1c8d9b59a527cb7ebdbb366b1f3149a74c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:38 +0000

    Add package openapify/0.0.1/openapify-0.0.1.tar.gz

---
+++ packages/openapify/openapify-0.0.1.tar.gz/openapify-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="openapify",
+    version="0.0.1",
+    description="Generate Open API Specification using decorators",
+    platforms="all",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Development Status :: 1 - Planning",
+    ],
+    python_requires=">=3.7",
+    zip_safe=False,
+)
