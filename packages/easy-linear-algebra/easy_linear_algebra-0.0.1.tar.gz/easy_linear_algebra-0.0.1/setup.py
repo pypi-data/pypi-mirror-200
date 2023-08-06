ff1a2303a8704694da98760f64b5d6c1e4becfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package easy-linear-algebra/0.0.1/easy_linear_algebra-0.0.1.tar.gz

---
+++ packages/easy-linear-algebra/easy_linear_algebra-0.0.1.tar.gz/easy_linear_algebra-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="easy_linear_algebra",
+    version="0.0.1",
+    author="Gordon_Shi",
+    author_email="gordon2028261@concordiashanghai.org",
+    description="A module that helps you to calculate easy linear algebra calculations.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
