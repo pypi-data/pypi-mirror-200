281063b17a0aff8fd42ca2b5b317be8be479ad18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:30 +0000

    Add package elefantolib/0.3.0/elefantolib-0.3.0-py3-none-any.whl

---
+++ packages/elefantolib/elefantolib-0.3.0-py3-none-any.whl/elefantolib/context/__init__.py
@@ -0,0 +1,31 @@
+from context import context_service
+import provider
+
+from http_client import httpx_client
+
+
+class BaseContext:
+    HTTP_CLIENT = None
+    CONTEXT_ATTRIBUTES = (
+        'auth_token',
+        'correlation_id',
+        'locale',
+    )
+
+    def __init__(self, pvr: provider.Provider):
+        for attribute in self.CONTEXT_ATTRIBUTES:
+            setattr(self, attribute, getattr(pvr, attribute, None))
+
+        self.services = context_service.ContextService(self.HTTP_CLIENT, self.context)
+
+    @property
+    def context(self):
+        return {a: getattr(self, a) for a in self.CONTEXT_ATTRIBUTES}
+
+
+class PlatformContext(BaseContext):
+    HTTP_CLIENT = httpx_client.HttpxClient
+
+
+class AsyncPlatformContext(BaseContext):
+    HTTP_CLIENT = httpx_client.AsyncHttpxClient
