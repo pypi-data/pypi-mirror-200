31695a6ca20bf1140f63dfca3d6c885111f5b425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:46 +0000

    Add package x2cdict/0.1.46/x2cdict-0.1.46-py3-none-any.whl

---
+++ packages/x2cdict/x2cdict-0.1.46-py3-none-any.whl/x2cdict/posmap.py
@@ -0,0 +1,28 @@
+# Map https://universaldependencies.org/docs/u/pos/ to pos in dictionary
+
+POSMAP = {}
+
+POSMAP['es'] = {
+  "ADV": ['adv.'],
+  "ADJ": ['adj.'],
+  "PRON": ['pron.'],
+  "SCONJ": ['conj.'],
+  "CONJ": ['conj.'],
+  "ADP": ['prep.'],
+  "NOUN": ['m.', 'f.', 'm.f.'],
+  "VERB": ['vi.', 'vt.', 'vr.'],
+  "AUX": ['vi.', 'vt.', 'vr.']
+}
+
+POSMAP['en'] = {
+  "ADV": ['adv.'],
+  "ADJ": ['adj.'],
+  "PRON": ['pron.'],
+  "SCONJ": ['conj.'],
+  "CONJ": ['conj.'],
+  "ADP": ['prep.'],
+  "NOUN": ['n.'],
+  "VERB": ['v.vi.', 'v.vt.', 'vt.vi.', 'vi.vt.', 'v.'],
+  "AUX": ['v.vi.', 'v.vt.', 'vt.vi.', 'vi.vt.', 'v.']
+}
+
