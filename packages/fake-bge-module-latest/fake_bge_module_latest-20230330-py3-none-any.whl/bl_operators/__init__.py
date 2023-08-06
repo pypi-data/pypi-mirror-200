b7e11f3d94460f99045392114914e69f906e20e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:28 +0000

    Add package fake-bge-module-latest/20230330/fake_bge_module_latest-20230330-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20230330-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,47 @@
+import sys
+import typing
+from . import vertexpaint_dirt
+from . import screen_play_rendered_anim
+from . import sequencer
+from . import geometry_nodes
+from . import console
+from . import image
+from . import mesh
+from . import constraint
+from . import bmesh
+from . import view3d
+from . import rigidbody
+from . import node
+from . import add_mesh_torus
+from . import presets
+from . import object_align
+from . import assets
+from . import file
+from . import uvcalc_lightmap
+from . import object_randomize_transform
+from . import spreadsheet
+from . import freestyle
+from . import uvcalc_transform
+from . import anim
+from . import wm
+from . import uvcalc_follow_active
+from . import clip
+from . import userpref
+from . import object_quick_effects
+from . import object
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20230330-py3-none-any.whl/bpy/ops/uilist.py
@@ -0,0 +1,42 @@
+import sys
+import typing
+
+
+def entry_add(list_path: str = "", active_index_path: str = ""):
+    ''' Add an entry to the list after the current active item
+
+    :param list_path: list_path
+    :type list_path: str
+    :param active_index_path: active_index_path
+    :type active_index_path: str
+    '''
+
+    pass
+
+
+def entry_move(list_path: str = "",
+               active_index_path: str = "",
+               direction: typing.Union[str, int] = 'UP'):
+    ''' Move an entry in the list up or down
+
+    :param list_path: list_path
+    :type list_path: str
+    :param active_index_path: active_index_path
+    :type active_index_path: str
+    :param direction: Direction * UP UP -- UP. * DOWN DOWN -- DOWN.
+    :type direction: typing.Union[str, int]
+    '''
+
+    pass
+
+
+def entry_remove(list_path: str = "", active_index_path: str = ""):
+    ''' Remove the selected entry from the list
+
+    :param list_path: list_path
+    :type list_path: str
+    :param active_index_path: active_index_path
+    :type active_index_path: str
+    '''
+
+    pass
