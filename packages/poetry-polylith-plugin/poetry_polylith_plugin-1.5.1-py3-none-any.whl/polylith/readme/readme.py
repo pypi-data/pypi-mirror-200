26439014de44ad329ea59db1b3d627219f49105f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:08 +0000

    Add package poetry-polylith-plugin/1.5.1/poetry_polylith_plugin-1.5.1-py3-none-any.whl

---
+++ packages/poetry-polylith-plugin/poetry_polylith_plugin-1.5.1-py3-none-any.whl/polylith/readme/readme.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from typing import Union
+
+from polylith import log, repo
+
+workspace_template = """\
+# A Python Polylith repo
+
+## Docs
+The official Polylith documentation:
+[high-level documentation](https://polylith.gitbook.io/polylith)
+
+A Python implementation of the Polylith tool:
+[python-polylith](https://github.com/DavidVujic/python-polylith)
+"""
+
+brick_template = """\
+# {name} {brick}
+
+{description}
+"""
+
+
+logger = log.getLogger()
+
+
+def create_readme(path: Path, template: str, **kwargs) -> None:
+    fullpath = path / repo.readme_file
+
+    if fullpath.exists():
+        logger.info(f"A {repo.readme_file} already exists. Skipping this step.")
+        return
+
+    with fullpath.open("w", encoding="utf-8") as f:
+        f.write(template.format(**kwargs))
+
+
+def create_workspace_readme(path: Path, namespace: str) -> None:
+    create_readme(path, workspace_template, namespace=namespace)
+
+
+def create_brick_readme(
+    path: Path, name: str, brick: str, description: Union[str, None]
+) -> None:
+    b = "component" if brick in repo.components_dir else "base"
+
+    create_readme(
+        path, brick_template, name=name, brick=b, description=description or ""
+    )
