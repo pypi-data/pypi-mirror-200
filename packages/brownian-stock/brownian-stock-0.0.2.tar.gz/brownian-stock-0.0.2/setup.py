87448243d11b149f7de1f4867e85efd532b51a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:35 +0000

    Add package brownian-stock/0.0.2/brownian-stock-0.0.2.tar.gz

---
+++ packages/brownian-stock/brownian-stock-0.0.2.tar.gz/brownian-stock-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="brownian-stock",
+    version="0.0.2",
+    long_description="Support tools for assisting analysis and verification of Japanese stocks.",
+    install_requires=[
+        "pandas",
+        "polars",
+        "numpy",
+        "tqdm",
+        "requests",
+        "connectorx",
+        "matplotlib",
+        "pyarrow",
+        "yfinance",
+        "matplotlib",
+        "python-dateutil",
+    ],
+    entry_points={
+        "console_scripts": [
+            "brownian = brownian.main:main",
+        ]
+    },
+    packages=find_packages(),
+    include_dirs=["brownian"]
+)
