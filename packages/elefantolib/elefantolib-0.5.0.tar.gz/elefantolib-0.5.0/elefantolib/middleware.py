ba15bcb35bb99a47d3b2b669d3194389d6b1e773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:09 +0000

    Add package elefantolib/0.5.0/elefantolib-0.5.0.tar.gz

---
+++ packages/elefantolib/elefantolib-0.5.0.tar.gz/elefantolib-0.5.0/elefantolib/middleware.py
@@ -0,0 +1,29 @@
+from elefantolib.provider import django_provider
+
+import context
+
+
+class DjangoPlatformContextMiddleware:
+
+    def __init__(self, get_response):
+        self._get_response = get_response
+
+    def __call__(self, request, *args, **kwargs):
+        pvr = django_provider.DjangoProvider(request=request)
+        request.pfm = context.PlatformContext(pvr=pvr)
+        response = self._get_response(request)
+
+        return response
+
+
+class DjangoAsyncPlatformContextMiddleware:
+
+    def __init__(self, get_response):
+        self._get_response = get_response
+
+    def __call__(self, request, *args, **kwargs):
+        pvr = django_provider.DjangoProvider(request=request)
+        request.async_pfm = context.AsyncPlatformContext(pvr=pvr)
+        response = self._get_response(request)
+
+        return response
