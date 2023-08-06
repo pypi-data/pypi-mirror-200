5437d4db59e1c0904a11c8daa63e3e5da4b15477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:05 +0000

    Add package boilerplate-x/0.2.4/boilerplate_x-0.2.4-py3-none-any.whl

---
+++ packages/boilerplate-x/boilerplate_x-0.2.4-py3-none-any.whl/boilerplate_x/constants.py
@@ -0,0 +1,41 @@
+CLI_OPTIONS = ["y", "n"]
+CLI_OPTIONS_FULL = {"y": "yes", "n": "no"}
+
+DESCRIPTION = "You are an agent to create boilerplate code in any programming language for a given project idea."
+PROJECT_STRUCTURE_TEMPLATE = template = """{description}
+
+The git repository is empty and you need to create a list of files required for the project idea. Include all relevant files usually found in a git repository.
+
+The list should be in YAML format. You MUST format the YAML as follows:
+- foo
+- bar/xzy.abc
+- bar/xyz.def
+
+Customisations:
+Unit tests: {unit_tests}
+Dockerization: {dockerization}
+Github Actions: {github_actions}
+Pre-commit hooks: {pre_commit_hooks}
+
+You must be clear and concise. No explanations required. Make sure you do not include any irrelevant files or empty directories.
+
+Project idea: {project_idea}
+YAML Output:"""
+PROJECT_FILE_TEMPLATE = """{description}
+
+Project idea: {project_idea}
+
+Project structure:
+{project_structure}
+
+You have to create file content based on the following project structure. The file content MUST be related to the project idea and structure.
+
+ONLY create file content for the file name specified below. You must be clear and concise. No explanations required.
+
+File name: {file_name}
+File content:"""
+
+GIT_DEFAULT_COMMIT_MESSAGE = "Initial commit"
+GIT_DEFAULT_REMOTE = "origin"
+GIT_USER = "boilerplate-x"
+GIT_EMAIL = "boilerplate-x@noreply.github.com"
