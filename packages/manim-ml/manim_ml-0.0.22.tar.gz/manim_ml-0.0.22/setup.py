a902e38d778e4be5821b7896bf4901ce1a532d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:29 +0000

    Add package manim-ml/0.0.22/manim_ml-0.0.22.tar.gz

---
+++ packages/manim-ml/manim_ml-0.0.22.tar.gz/manim_ml-0.0.22/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="manim_ml",
+    version="0.0.22",
+    description=("Machine Learning Animations in python using Manim."),
+    packages=find_packages(),
+)
