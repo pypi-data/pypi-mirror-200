5e34a99582879a7d68d9dd1d15fac778336df0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package pushingtest-4980/0.1.3/pushingtest_4980-0.1.3.tar.gz

---
+++ packages/pushingtest-4980/pushingtest_4980-0.1.3.tar.gz/pushingtest_4980-0.1.3/README.md
@@ -0,0 +1,3 @@
+## CI/CD pypi testing
+
+*Some tests on github and ci pipelines for pypi releasing*
---
+++ packages/pushingtest-4980/pushingtest_4980-0.1.3.tar.gz/pushingtest_4980-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pushingtest_4980",
+    version="0.1.3",
+    author="Michael Chupilnick",
+    author_email="your_email@example.com",
+    description="CI/CD test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/your_package_name",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "requests>=2.0.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
