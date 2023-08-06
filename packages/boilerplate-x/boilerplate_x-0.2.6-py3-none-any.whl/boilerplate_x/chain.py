743fcc182493a9c6078621905fc88fac1e99c5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package boilerplate-x/0.2.6/boilerplate_x-0.2.6-py3-none-any.whl

---
+++ packages/boilerplate-x/boilerplate_x-0.2.6-py3-none-any.whl/boilerplate_x/chain.py
@@ -0,0 +1,32 @@
+from langchain import LLMChain, PromptTemplate
+from langchain.schema import BaseLanguageModel
+
+from .constants import DESCRIPTION, PROJECT_FILE_TEMPLATE, PROJECT_STRUCTURE_TEMPLATE
+
+
+def load_project_structure_chain(llm: BaseLanguageModel, verbose: bool = False):
+    """Loads the project structure chain."""
+    prompt = PromptTemplate(
+        template=PROJECT_STRUCTURE_TEMPLATE,
+        input_variables=[
+            "unit_tests",
+            "dockerization",
+            "github_actions",
+            "pre_commit_hooks",
+            "project_idea",
+        ],
+        partial_variables={"description": DESCRIPTION},
+    )
+    return LLMChain(prompt=prompt, llm=llm, verbose=verbose)
+
+
+def load_project_file_chain(llm: BaseLanguageModel, verbose: bool = False):
+    """Loads the project file chain."""
+    prompt = PromptTemplate(
+        template=PROJECT_FILE_TEMPLATE,
+        input_variables=["project_idea", "project_structure", "file_name"],
+        partial_variables={
+            "description": DESCRIPTION,
+        },
+    )
+    return LLMChain(prompt=prompt, llm=llm, verbose=verbose)
