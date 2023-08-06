4c432d88e87d42b0dc7ad4efc92c311f55ddb5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:37 +0000

    Add package simple-embed-pagination/1.0.1/simple-embed-pagination-1.0.1.tar.gz

---
+++ packages/simple-embed-pagination/simple-embed-pagination-1.0.1.tar.gz/simple-embed-pagination-1.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="simple-embed-pagination",
+    version="1.0.1",
+    description="Easily create pagination for discord.py embeds.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    py_modules=["paginator"],
+    package_dir={'': "src"},
+    url="https://github.com/FaddyManatee/embed-pagination",
+    authors=["FaddyManatee", "RajDave69"],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
+)
