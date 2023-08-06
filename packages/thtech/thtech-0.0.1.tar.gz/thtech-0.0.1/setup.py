0a80c13e051be183a26ca0fdb4dfad85b45ac9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:04 +0000

    Add package thtech/0.0.1/thtech-0.0.1.tar.gz

---
+++ packages/thtech/thtech-0.0.1.tar.gz/thtech-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic hello package'
+LONG_DESCRIPTION = 'A basic hello package long desc'
+
+# Setting up
+setup(
+    name="thtech",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
