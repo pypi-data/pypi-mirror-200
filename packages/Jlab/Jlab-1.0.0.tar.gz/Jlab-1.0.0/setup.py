fb9254f33e169e6d13d79d957038afbfc54fc603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:45 +0000

    Add package Jlab/1.0.0/Jlab-1.0.0.tar.gz

---
+++ packages/Jlab/Jlab-1.0.0.tar.gz/Jlab-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().split("\n")
+
+setuptools.setup(
+    name="Jlab",
+    version="1.0.0",
+    author="twfxjjbw",
+    author_email="twfxjjbw@gmail.com",
+    description="Jlab is a package for data analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+    # install_requires = requirements
+)
